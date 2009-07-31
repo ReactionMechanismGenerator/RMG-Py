9a3abecf5408c4bdf2e62e6afbde486056023949
Josh Allen <jwallen@mit.edu>
2009-07-31 21:09:53 +0000

    A first attempt at an algorithm for decoupling kinetic data into values for nodes in each tree.
    git-svn-id: https://rmg.svn.sourceforge.net/svnroot/rmg/trunk@102 37378900-1858-4726-a06b-e54562fdc990

--- source/database.py
+++ source/database.py
@@ -108,36 +108,118 @@ if __name__ == '__main__':
 	#loadThermoDatabases(databasePath)
 	loadKineticsDatabases(databasePath)
 
-	# Prune kinetics dictionaries and trees
-	for key, family in reaction.kineticsDatabase.families.iteritems():
-		forwardTemplate, reverseTemplate = family.getTemplateLists()
-		family.prune(forwardTemplate)
+#	# Prune kinetics dictionaries and trees
+#	for key, family in reaction.kineticsDatabase.families.iteritems():
+#		forwardTemplate, reverseTemplate = family.getTemplateLists()
+#		family.prune(forwardTemplate)
+#
+#		# Print dictionary
+#		print '/////////////////////////////////////////////////////////////////////////////////'
+#		print '//'
+#		print '// Structure dictionary for %s' % family.label
+#		print '//'
+#		print '/////////////////////////////////////////////////////////////////////////////////'
+#		print ''
+#		for label, struct in family.dictionary.iteritems():
+#			if struct.__class__ == str:
+#				union = label + '\nUnion {' + ','.join(family.tree.children[label]) + '}\n'
+#				print union
+#			else:
+#				print struct.toAdjacencyList(label)
+#
+#		# Print tree
+#		print '/////////////////////////////////////////////////////////////////////////////////'
+#		print '//'
+#		print '// Structure tree for %s' % family.label
+#		print '//'
+#		print '/////////////////////////////////////////////////////////////////////////////////'
+#		print ''
+#		print family.tree.write(family.tree.top)
 
-		# Print dictionary
-		print '/////////////////////////////////////////////////////////////////////////////////'
-		print '//'
-		print '// Structure dictionary for %s' % family.label
-		print '//'
-		print '/////////////////////////////////////////////////////////////////////////////////'
-		print ''
-		for label, struct in family.dictionary.iteritems():
-			if struct.__class__ == str:
-				union = label + '\nUnion {' + ','.join(family.tree.children[label]) + '}\n'
-				print union
-			else:
-				print struct.toAdjacencyList(label)
-
-		# Print tree
-		print '/////////////////////////////////////////////////////////////////////////////////'
-		print '//'
-		print '// Structure tree for %s' % family.label
-		print '//'
-		print '/////////////////////////////////////////////////////////////////////////////////'
-		print ''
-		print family.tree.write(family.tree.top)
-
-
-		
 	# Draw kinetics trees
 	#drawKineticsTrees()
 
+	# DECOUPLE LIBRARY DATA
+	family = reaction.kineticsDatabase.families['H abstraction']
+
+	# Get available data
+	nodesList = []; dataList = []
+	for key, value in family.library.iteritems():
+		nodes = key.split(';')
+		if value not in dataList:
+			nodesList.append(nodes)
+			dataList.append(value)
+
+	# Get set of all nodes; remove those with no data
+	nodeSet = family.tree.parent.keys()
+#	nodesToRemove = []
+#	for node in nodeSet:
+#		hasData = False
+#		children = family.tree.descendants(node)
+#		for nodes in nodesList:
+#			if node in nodes: hasData = True
+#			for child in children:
+#				if child in nodes: hasData = True
+#		if not hasData:
+#			nodesToRemove.append(node)
+#	for node in nodesToRemove:
+#		nodeSet.remove(node)
+	
+	# Determine size of matrix and vector
+	ncols = len(nodeSet) + 1
+	nrows = 0
+	for nodes in nodesList:
+		nodeLists = []
+		for node in nodes:
+			nodeList = []
+			temp = node
+			while temp is not None:
+				nodeList.append(temp)
+				temp = family.tree.parent[temp]
+			nodeLists.append(nodeList)
+		nodeLists = data.getAllCombinations(nodeLists)
+		nrows += len(nodeLists)
+
+	import numpy
+	import numpy.linalg
+	import math
+
+	# Initialize matrix and vector
+	A = numpy.zeros((nrows, ncols), float)
+	b = numpy.zeros((nrows, 4), float)
+
+	# Fill in matrix and vector
+	rowcount = 0
+	for nodes, kinetics in zip(nodesList, dataList):
+		nodeLists = []
+		for node in nodes:
+			nodeList = []
+			temp = node
+			while temp is not None:
+				nodeList.append(temp)
+				temp = family.tree.parent[temp]
+			nodeLists.append(nodeList)
+		nodeLists = data.getAllCombinations(nodeLists)
+		for nodeList in nodeLists:
+			for i, node in enumerate(nodeList):
+				temp = node
+				while temp is not None:
+					A[rowcount,nodeSet.index(temp)] += 1
+					temp = family.tree.parent[temp]
+
+			A[rowcount,len(nodeSet)] += 1
+			b[rowcount,0] = math.log(kinetics.A)
+			b[rowcount,1] = kinetics.n
+			b[rowcount,2] = kinetics.alpha
+			b[rowcount,3] = kinetics.E0
+			rowcount += 1
+
+	x, residues, rank, s = numpy.linalg.lstsq(A,b)
+
+	for i in range(len(nodeSet)):
+		print nodeSet[i], x[i,:]
+	print x[len(nodeSet),:]
+#	print x
+#	print residues
+#	print rank, nrows, ncols, 4
+#	print s
