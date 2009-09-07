e4106318d4dacb85a16f2cdc008fd9ad77c89a2a
Josh Allen <jwallen@mit.edu>
2009-09-07 16:37:30 +0000

    Moved ThermoGAData, ThermoNASAPolynomial, and ThermoNASAData from rmg.species to rmg.thermo.
    Also created new base class ThermoData for all of the above. The base class contains the temperature range and the comment, and a list of functions the derived classes should implement.
    
    git-svn-id: https://rmg.svn.sourceforge.net/svnroot/rmg/trunk@127 37378900-1858-4726-a06b-e54562fdc990

--- source/rmg/thermo.py
+++ source/rmg/thermo.py
@@ -34,7 +34,389 @@ seeks to provide functionality for answering the question, "Given a species,
 what are its thermodynamics?"
 """
 
-import species
+import math
+import quantities as pq
+
+import data
+
+################################################################################
+
+class ThermoData:
+	"""
+	A base class for all forms of thermodynamic data used by RMG. The common
+	attributes are:
+
+	========= ============================================================
+	Attribute Meaning
+	========= ============================================================
+	`Trange`  a 2-tuple containin the minimum and maximum temperature in K
+	`comment` a string describing the source of the data
+	========= ============================================================
+	"""
+
+	def __init__(self, Trange=None, comment=''):
+		self.Trange = Trange or (0.0, 0.0)
+		self.comment = comment
+
+	def isTemperatureValid(self, T):
+		"""
+		Return :data:`True` if the temperature `T` in K is within the valid
+		temperature range of the thermodynamic data, or :data:`False` if not.
+		"""
+		if self.Trange is None:
+			return True
+		else:
+			return self.Trange[0] <= T and T <= self.Trange[1]
+
+	def getHeatCapacity(self, T):
+		"""
+		Return the heat capacity in J/mol*K at temperature `T` in K.
+		"""
+		abstract
+
+	def getEnthalpy(self, T):
+		"""
+		Return the enthalpy in J/mol at temperature `T` in K.
+		"""
+		abstract
+
+	def getEntropy(self, T):
+		"""
+		Return the entropy in J/mol*K at temperature `T` in K.
+		"""
+		abstract
+
+	def getFreeEnergy(self, T):
+		"""
+		Return the Gibbs free energy in J/mol at temperature `T` in K.
+		"""
+		abstract
+
+################################################################################
+
+class ThermoGAData(ThermoData):
+	"""
+	A set of thermodynamic parameters as determined from Benson's group
+	additivity data. The attributes are:
+
+	========= ========================================================
+	Attribute Meaning
+	========= ========================================================
+	`H298`    the standard enthalpy of formation at 298 K in J/mol
+	`S298`    the standard entropy of formation at 298 K in J/mol*K
+	`Cp`      the standard heat capacity in J/mol*K at 300, 400, 500,
+		      600, 800, 1000, and 1500 K
+	========= ========================================================
+	"""
+
+	Tlist = list(pq.Quantity([300.0, 400.0, 500.0, 600.0, 800.0, 1000.0, 1500.0], 'K').simplified)
+	Tlist = [float(T) for T in Tlist]
+	
+	def __init__(self, H298=0.0, S298=0.0, Cp=None, comment=''):
+		"""Initialize a set of group additivity thermodynamic data."""
+		ThermoData.__init__(self, Trange=(298.0, 2500.0), comment=comment)
+		self.H298 = H298
+		self.S298 = S298
+		self.Cp = Cp or (0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+
+	def __add__(self, other):
+		"""
+		Add two sets of thermodynamic data together. All parameters are
+		considered additive. Returns a new :class:`ThermoGAData` object that is
+		the sum of the two sets of thermodynamic data.
+		"""
+		new = ThermoGAData()
+		new.H298 = self.H298 + other.H298
+		new.S298 = self.S298 + other.S298
+		new.Cp = [self.Cp[i] + other.Cp[i] for i in range(len(self.Cp))]
+		if self.comment == '': new.comment = other.comment
+		elif other.comment == '': new.comment = self.comment
+		else: new.comment = self.comment + '+ ' + other.comment
+		return new
+	
+	def __str__(self):
+		"""
+		Return a string summarizing the thermodynamic data.
+		"""
+		string = ''
+		string += 'Enthalpy of formation: %s J/mol\n' % (self.H298)
+		string += 'Entropy of formation: %s J/mol*K\n' % (self.S298)
+		for T, Cp in zip(ThermoGAData.Tlist, self.Cp):
+			string += 'Heat capacity at %s K: %s J/mol*K' % (T, Cp)
+		string += 'Comment: %s' % (self.comment)
+		return string
+
+	def getHeatCapacity(self, T):
+		"""
+		Return the heat capacity in J/mol*K at temperature `T` in K.
+		"""
+		if not self.isTemperatureValid(T):
+			raise data.TemperatureOutOfRangeException('Invalid temperature for heat capacity estimation from group additivity.')
+		if T < 300.0:
+			return self.Cp[0]
+		elif T > ThermoGAData.Tlist[-1]:
+			return self.Cp[-1]
+		else:
+			for Tmin, Tmax, Cpmin, Cpmax in zip(ThermoGAData.Tlist[:-1], \
+					ThermoGAData.Tlist[1:], self.Cp[:-1], self.Cp[1:]):
+				if Tmin <= T and T <= Tmax:
+					return (Cpmax - Cpmin) * ((T - Tmin) / (Tmax - Tmin)) + Cpmin
+
+	def getEnthalpy(self, T):
+		"""
+		Return the enthalpy in J/mol at temperature `T` in K.
+		"""
+		H = self.H298
+		if not self.isTemperatureValid(T):
+			raise data.TemperatureOutOfRangeException('Invalid temperature for enthalpy estimation from group additivity.')
+		for Tmin, Tmax, Cpmin, Cpmax in zip(ThermoGAData.Tlist[:-1], \
+				ThermoGAData.Tlist[1:], self.Cp[:-1], self.Cp[1:]):
+			if T > Tmin:
+				slope = (Cpmax - Cpmin) / (Tmax - Tmin)
+				intercept = (Cpmin * Tmax - Cpmax * Tmin) / (Tmax - Tmin)
+				if T < Tmax:	H += 0.5 * slope * (T**2 - Tmin**2) + intercept * (T - Tmin)
+				else:			H += 0.5 * slope * (Tmax**2 - Tmin**2) + intercept * (Tmax - Tmin)
+		if T > ThermoGAData.Tlist[-1]:
+			H += self.Cp[-1] * (T - ThermoGAData.Tlist[-1])
+		return H
+
+	def getEntropy(self, T):
+		"""
+		Return the entropy in J/mol*K at temperature `T` in K.
+		"""
+		S = self.S298
+		if not self.isTemperatureValid(T):
+			raise data.TemperatureOutOfRangeException('Invalid temperature for entropy estimation from group additivity.')
+		for Tmin, Tmax, Cpmin, Cpmax in zip(ThermoGAData.Tlist[:-1], \
+				ThermoGAData.Tlist[1:], self.Cp[:-1], self.Cp[1:]):
+			if T > Tmin:
+				slope = (Cpmax - Cpmin) / (Tmax - Tmin)
+				intercept = (Cpmin * Tmax - Cpmax * Tmin) / (Tmax - Tmin)
+				if T < Tmax:	S += slope * (T - Tmin) + intercept * math.log(T/Tmin)
+				else:			S += slope * (Tmax - Tmin) + intercept * math.log(Tmax/Tmin)
+		if T > ThermoGAData.Tlist[-1]:
+			S += self.Cp[-1] * math.log(T / ThermoGAData.Tlist[-1])
+		return S
+
+	def getFreeEnergy(self, T):
+		"""
+		Return the Gibbs free energy in J/mol at temperature `T` in K.
+		"""
+		if not self.isTemperatureValid(T):
+			raise data.TemperatureOutOfRangeException('Invalid temperature for free energy estimation from group additivity.')
+		return self.getEnthalpy(T) - T * self.getEntropy(T)
+
+	def fromDatabase(self, data, comment):
+		"""
+		Process a list of numbers `data` and associated description `comment`
+		generated while reading from a thermodynamic database.
+		"""
+
+		if len(data) != 12:
+			raise Exception('Invalid list of thermo data; should be a list of numbers of length 12.')
+
+		H298, S298, Cp300, Cp400, Cp500, Cp600, Cp800, Cp1000, Cp1500, \
+			dH, dS, dCp = data
+
+		self.H298 = float(pq.Quantity(H298, 'kcal/mol').simplified)
+		self.S298 = float(pq.Quantity(S298, 'cal/(mol*K)').simplified)
+		self.Cp = list(pq.Quantity([Cp300, Cp400, Cp500, Cp600, Cp800, Cp1000, Cp1500], 'cal/(mol*K)').simplified)
+		for i in range(len(self.Cp)): self.Cp[i] = float(self.Cp[i])
+		self.comment = comment
+
+	def toXML(self, dom, root):
+		"""
+		Generate an XML representation of the thermodynamic data using the
+		:data:`xml.dom.minidom` package. The `dom` and `root` parameters
+		represent the DOM and the element in the DOM used as the parent of
+		the generated XML.
+		"""
+
+		thermo = dom.createElement('thermodynamics')
+		thermo.setAttribute('comment', self.comment)
+		root.appendChild(thermo)
+
+		enthalpy = dom.createElement('enthalpyOfFormation')
+		enthalpy.setAttribute('temperature', '298.0 K')
+		thermo.appendChild(enthalpy)
+		data.createXMLQuantity(dom, enthalpy, self.H298, 'J/mol')
+
+		entropy = dom.createElement('entropyOfFormation')
+		entropy.setAttribute('temperature', '298.0 K')
+		thermo.appendChild(entropy)
+		data.createXMLQuantity(dom, entropy, self.S298, 'J/(mol*K)')
+
+		for i, Cp in enumerate(self.Cp):
+			heatCapacity = dom.createElement('heatCapacity')
+			heatCapacity.setAttribute('temperature', '%s K' % (self.Tlist[i]) )
+			thermo.appendChild(heatCapacity)
+			data.createXMLQuantity(dom, heatCapacity, Cp, 'J/(mol*K)')
+
+
+
+################################################################################
+
+class ThermoNASAPolynomial(ThermoData):
+	"""
+	A single NASA polynomial for thermodynamic data. The `coeffs` attribute 
+	stores the seven polynomial coefficients
+	:math:`\\mathbf{a} = \\left[a_1\\ a_2\\ a_3\\ a_4\\ a_5\\ a_6\\ a_7 \\right]`
+	from which the relevant thermodynamic parameters are evaluated via the
+	expressions
+
+	.. math:: \\frac{C_\\mathrm{p}(T)}{R} = a_1 + a_2 T + a_3 T^2 + a_4 T^3 + a_5 T^4
+
+	.. math:: \\frac{H(T)}{RT} = a_1 + \\frac{1}{2} a_2 T + \\frac{1}{3} a_3 T^2 + \\frac{1}{4} a_4 T^3 + \\frac{1}{5} a_5 T^4 + \\frac{a_6}{T}
+
+	.. math:: \\frac{S(T)}{R} = a_1 \\ln T + a_2 T + \\frac{1}{2} a_3 T^2 + \\frac{1}{3} a_4 T^3 + \\frac{1}{4} a_5 T^4 + a_7
+
+	The above was adapted from `this page <http://www.me.berkeley.edu/gri-mech/data/nasa_plnm.html>`_.
+	"""
+
+	def __init__(self, T_range=None, coeffs=None, comment=''):
+		ThermoData.__init__(self, Trange=T_range, comment=comment)
+		self.coeffs = coeffs or (0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+
+	def getHeatCapacity(self, T):
+		"""
+		Return the heat capacity in J/mol*K at temperature `T` in K.
+		"""
+		if not self.isTemperatureValid(T):
+			raise data.TemperatureOutOfRangeException('Invalid temperature for heat capacity estimation from NASA polynomial.')
+		c = self.coeffs
+		T2 = T*T
+		# Cp/R = a1 + a2 T + a3 T^2 + a4 T^3 + a5 T^4
+#		HeatCapacityOverR = c[0] + c[1]*T + c[2]*T*T + c[3]*T*T*T + c[4]*T*T*T*T
+		HeatCapacityOverR = c[0] + T*(c[1] + T*(c[2] + T*(c[3] + c[4]*T)))
+		Cp = HeatCapacityOverR * constants.R
+		return Cp
+
+	def getEnthalpy(self, T):
+		"""
+		Return the enthalpy in J/mol at temperature `T` in K.
+		"""
+		if not self.isTemperatureValid(T):
+			raise data.TemperatureOutOfRangeException('Invalid temperature for enthalpy estimation from NASA polynomial.')
+		c = self.coeffs
+		T2 = T*T
+		T4 = T2*T2
+		# H/RT = a1 + a2 T /2 + a3 T^2 /3 + a4 T^3 /4 + a5 T^4 /5 + a6/T
+		EnthalpyOverR = ( c[0]*T + c[1]*T2/2 + c[2]*T2*T/3 + c[3]*T4/4 +
+						  c[4]*T4*T/5 + c[5] )
+		H = EnthalpyOverR * constants.R
+		return H
+
+	def getEntropy(self, T):
+		"""
+		Return the entropy in J/mol*K at temperature `T` in K.
+		"""
+		if not self.isTemperatureValid(T):
+			raise data.TemperatureOutOfRangeException('Invalid temperature for entropy estimation from NASA polynomial.')
+		# S/R  = a1 lnT + a2 T + a3 T^2 /2 + a4 T^3 /3 + a5 T^4 /4 + a7
+		c = self.coeffs
+		T2 = T*T
+		T4 = T2*T2
+		EntropyOverR = ( c[0]*math.log(T) + c[1]*T + c[2]*T2/2 +
+					c[3]*T2*T/3 + c[4]*T4/4 + c[6] )
+		S = EntropyOverR * constants.R
+		return S
+
+	def getFreeEnergy(self, T):
+		"""
+		Return the Gibbs free energy in J/mol at temperature `T` in K.
+		"""
+		if not self.isTemperatureValid(T):
+			raise data.TemperatureOutOfRangeException('Invalid temperature for free energy estimation from NASA polynomial.')
+		return self.getEnthalpy(T) - T * self.getEntropy(T)
+
+	def toXML(self, dom, root):
+#   <polynomial>
+#      <validRange>
+#         <bound kind="lower" property="temperature" units="K">300.000</bound>
+#         <bound kind="upper" property="temperature" units="K">1000</bound>
+#      </validRange>
+#      <coefficient id="1" label="a1">4.0733</coefficient>
+#      <coefficient id="2" label="a2">0.011308</coefficient>
+#      <coefficient id="3" label="a3">-1.6565e-005</coefficient>
+#      <coefficient id="4" label="a4">1.1784e-008</coefficient>
+#      <coefficient id="5" label="a5">-3.3006e-012</coefficient>
+#      <coefficient id="6" label="a6">-19054.23</coefficient>
+#      <coefficient id="7" label="a7">4.4083</coefficient>
+#   </polynomial>
+		pass
+
+################################################################################
+
+class ThermoNASAData(ThermoData):
+	"""
+	A set of thermodynamic parameters given by NASA polynomials. This class
+	stores a list of :class:`ThermoNASAPolynomial` objects in the `polynomials`
+	attribute. When evaluating a thermodynamic quantity, a polynomial that
+	contains the desired temperature within its valid range will be used.
+	"""
+
+	def __init__(self, polynomials=None, comment=''):
+		ThermoData.__init__(self, Trange=None, comment=comment)
+		self.polynomials = polynomials or []
+
+	def addPolynomial(self, polynomial):
+		if not isinstance(polynomial,ThermoNASAPolynomial):
+			raise TypeError("Polynomial attribute should be instance of ThermoNASAPolynomial")
+		self.polynomials.append(polynomial)
+
+	def selectPolynomialForTemperature(self, T):
+		for poly in self.polynomials:
+			if poly.isTemperatureValid(T): break
+		else:
+			raise data.TemperatureOutOfRangeException("No polynomial found for T=%s" % T)
+		return poly
+
+	def toXML(self, dom, root):
+# <thermodynamicPolynomials type="nasa7">
+#   <referenceState>
+#      <Tref units="K">298.15</Tref>
+#      <Pref units="Pa">100000</Pref>
+#   </referenceState>
+#   <dfH units="J/mol">-145177.5731</dfH>
+#   <polynomial> FROM NASA_polynomials </polynomial>
+# </thermodynamicPolynomials>
+		pass
+
+	def __str__(self):
+		"""
+		Return a string summarizing the thermodynamic data.
+		"""
+		string = 'NASA polynomials'
+		return string
+
+	def getHeatCapacity(self, T):
+		"""
+		Return the heat capacity in J/mol*K at temperature `T` in K.
+		"""
+		poly = self.selectPolynomialForTemperature(T)
+		return poly.getHeatCapacity(T)
+
+	def getEnthalpy(self, T):
+		"""
+		Return the enthalpy in J/mol at temperature `T` in K.
+		"""
+		poly = self.selectPolynomialForTemperature(T)
+		return poly.getEnthalpy(T)
+
+	def getEntropy(self, T):
+		"""
+		Return the entropy in J/mol*K at temperature `T` in K.
+		"""
+		poly = self.selectPolynomialForTemperature(T)
+		return poly.getEntropy(T)
+
+	def getFreeEnergy(self, T):
+		"""
+		Return the Gibbs free energy in J/mol at temperature `T` in K.
+		"""
+		poly = self.selectPolynomialForTemperature(T)
+		return poly.getFreeEnergy(T)
+
 
 ################################################################################
 
