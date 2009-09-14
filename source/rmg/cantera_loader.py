e2f26c2c0f6f1f3e9c74a701aee14e46f3ee40d8
Richard West <rwest@mit.edu>
2009-09-14 23:10:00 +0000

    Converted spaces to tabs
    (and moved a few comments around)
    
    git-svn-id: https://rmg.svn.sourceforge.net/svnroot/rmg/trunk@142 37378900-1858-4726-a06b-e54562fdc990

--- source/rmg/cantera_loader.py
+++ source/rmg/cantera_loader.py
@@ -48,12 +48,13 @@ sys.path.append('../external')
 import ctml_writer as cti
 from ctml_writer import units, OneAtm
 
-_uConc = None # set these once base units have been set by 
-_uTime = None # importing .cti file inside loadCanteraFile()
+# set these once base units have been set by importing .cti file, inside loadCanteraFile()
+_uConc = None 
+_uTime = None 
 
 if cti._species:
-    logging.info("Resetting cantera memory")
-    reload(cti)
+	logging.info("Resetting cantera memory")
+	reload(cti)
 _species=cti._species
 _reactions=cti._reactions
 _speciesnames=cti._speciesnames
@@ -64,225 +65,225 @@ _speciesByName={}
 # Cantera .cti names, hence lowercase names here:
 
 class ideal_gas(cti.ideal_gas):
-    pass # for now, don't modify this class, but must define it
+	pass # for now, don't modify this class, but must define it
 
 class state(cti.state):
-    pass # for now, don't modify this class, but must define it 
-    
+	pass # for now, don't modify this class, but must define it 
+	
 class species(cti.species):
-    """A species derived from ctml_writer.species.
-    
-    species(name,atoms,note,thermo,transport,charge)
-    """
-    def __init__(self, *args, **kwargs):
-        # first call the parent __init__
-        cti.species.__init__(self, *args, **kwargs)
-        # now put it in the _speciesByName dictionary
-        name=self._name
-        global _speciesByName
-        _speciesByName[name]=self
-        # if overloading this __init__ to fill in the _speciesByName dictionary
-        # does not work well, then replace uses of _speciesByName dictionary
-        # with calls to the function getSpeciesByName()
-        
-    def getRmgSpecies(self):
-        """Fetch the RMG Species object associated with this cantera species"""
-        if not hasattr(self,'_RmgSpecies'):
-            self.makeRmgSpecies()
-        return self._RmgSpecies
-        
-    def makeRmgSpecies(self):
-        """Create an RMG Species object based on this cantera species"""
-        name=self._name
-        id=cti._speciesnames.index(name)
-        self._RmgSpecies = rmg.species.Species(id=id, label=name)
-        # initialise thermo now
-        self._RmgSpecies.thermoData = rmg.species.ThermoNASAData(
-            comment="Imported from Cantera file" )
-        for canterapoly in self._thermo:
-            poly = canterapoly.getRmgPolynomial()
-            self._RmgSpecies.thermoData.addPolynomial(poly)
-            
-    def getCorrectNasaPoly(self,T):
-        """Select the first NASA polynomial that is valid at temperature T"""
-        for nasapoly in self._thermo:
-            if nasapoly.ValidTemperature(T): return nasapoly
-        return None
-                    
+	"""A species derived from ctml_writer.species.
+	
+	species(name,atoms,note,thermo,transport,charge)
+	"""
+	def __init__(self, *args, **kwargs):
+		# first call the parent __init__
+		cti.species.__init__(self, *args, **kwargs)
+		# now put it in the _speciesByName dictionary
+		name=self._name
+		global _speciesByName
+		_speciesByName[name]=self
+		# if overloading this __init__ to fill in the _speciesByName dictionary
+		# does not work well, then replace uses of _speciesByName dictionary
+		# with calls to the function getSpeciesByName()
+		
+	def getRmgSpecies(self):
+		"""Fetch the RMG Species object associated with this cantera species"""
+		if not hasattr(self,'_RmgSpecies'):
+			self.makeRmgSpecies()
+		return self._RmgSpecies
+		
+	def makeRmgSpecies(self):
+		"""Create an RMG Species object based on this cantera species"""
+		name=self._name
+		id=cti._speciesnames.index(name)
+		self._RmgSpecies = rmg.species.Species(id=id, label=name)
+		# initialise thermo now
+		self._RmgSpecies.thermoData = rmg.species.ThermoNASAData(
+			comment="Imported from Cantera file" )
+		for canterapoly in self._thermo:
+			poly = canterapoly.getRmgPolynomial()
+			self._RmgSpecies.thermoData.addPolynomial(poly)
+			
+	def getCorrectNasaPoly(self,T):
+		"""Select the first NASA polynomial that is valid at temperature T"""
+		for nasapoly in self._thermo:
+			if nasapoly.ValidTemperature(T): return nasapoly
+		return None
+					
 class NASA(cti.NASA):
-    """NASA polynomial representation of thermo. For importing Cantera files."""
-    def getRmgPolynomial(self):
-        """Fetch the RMG ThermoNASAPolynomial associated with this Cantera NASA"""
-        if not hasattr(self,'_RmgThermoNASAPolynomial'):
-            self.makeRmgPolynomial()
-        return self._RmgThermoNASAPolynomial
-    def makeRmgPolynomial(self):
-        """Make the RMG ThermoNASAPolynomial associated with this Cantera NASA"""
-        if self._pref > 0.0: raise Exception(
-            "Not sure what to do with customised standard state pressure")
-        poly = rmg.species.ThermoNASAPolynomial( T_range = self._t, 
-                                  coeffs = self._coeffs,
-                                  comment = "Imported from Cantera file")
-        self._RmgThermoNASAPolynomial = poly
-        
+	"""NASA polynomial representation of thermo. For importing Cantera files."""
+	def getRmgPolynomial(self):
+		"""Fetch the RMG ThermoNASAPolynomial associated with this Cantera NASA"""
+		if not hasattr(self,'_RmgThermoNASAPolynomial'):
+			self.makeRmgPolynomial()
+		return self._RmgThermoNASAPolynomial
+	def makeRmgPolynomial(self):
+		"""Make the RMG ThermoNASAPolynomial associated with this Cantera NASA"""
+		if self._pref > 0.0: raise Exception(
+			"Not sure what to do with customised standard state pressure")
+		poly = rmg.species.ThermoNASAPolynomial( T_range = self._t, 
+								  coeffs = self._coeffs,
+								  comment = "Imported from Cantera file")
+		self._RmgThermoNASAPolynomial = poly
+		
 class reaction(cti.reaction):
-    """A chemical reaction. For importing Cantera files"""
-    def getRmgReaction(self):
-        """Fetch the RMG Reaction object associated with this cantera reaction"""
-        if not hasattr(self,'_RmgReaction'):
-            self.makeRmgReaction()
-        return self._RmgReaction
-    def makeRmgReaction(self):
-        """Create an RMG Reaction object based on this cantera reaction"""        
-        reactants=[]
-        for speciesName,order in self._r.items(): #  reactants
-            assert order==int(order), "RMG currently needs elementary reactions"
-            order = int(order)
-            species=_speciesByName[speciesName].getRmgSpecies()
-            reactants.extend([species]*order)
-        products=[]
-        for speciesName,order in self._p.items(): #  reactants
-            assert order==int(order), "RMG currently needs elementary reactions"
-            order = int(order)
-            species=_speciesByName[speciesName].getRmgSpecies()
-            products.extend([species]*order)
-        rxn = rmg.reaction.Reaction(reactants=reactants, products=products)
-        rxn.kinetics = [self.makeRmgKinetics()]
-        self._RmgReaction = rxn
-    def makeRmgKinetics(self):
-        """Create and return an RMG ArrheniusKinetics object based on self._kf"""
-        A = self._kf[0] / _uConc**(self.getReactantNu()-1) / _uTime
-        A = float(A.simplified)
-        n = self._kf[1]
-   #     assert False, 'debug breakpoint'
-        #assert cti._ue=='kcal/mol', 'unit conversion not implemented'
-        Ea=float(pq.Quantity(self._kf[2], cti._ue).simplified)
-        kinetics=rmg.reaction.ArrheniusKinetics(A=A, Ea=Ea, n=n)
-        kinetics.comment="Generated from Cantera expression %s"%(self._kf)
-#        kinetics.Trange = [0.0, 100000.0]
-        return kinetics
-    def getReactantNu(self):
-        """Returns the stoichiometry in the forwards direction.
-        
-        i.e. the number of reactant molecules.
-        uses self._reactantNu to cache the answer"""
-        if hasattr(self,'_reactantNu'): return self._reactantNu
-        reactantNu=0
-        for speciesName,order in self._r.items(): # add reactants
-            reactantNu += order
-        self._reactantNu=reactantNu
-        return reactantNu
-    def getProductNu(self):
-        """Returns the stoichiometry in the reverse direction. 
-        
-        i.e. the number of product molecules.
-        uses self._productNu to cache the answer"""
-        if hasattr(self,'_productNu'): return self._productNu
-        productNu=0
-        for speciesName,order in self._p.items(): # add products
-            productNu += order
-        self._productNu=productNu
-        return productNu
-    def getDeltaNu(self):
-        """Returns the change in stoichiometry of the reaction, delta Nu.
-        
-        deltaNu= productNu - reactantNu
-        uses self._deltaNu to cache the answer"""
-        if hasattr(self,'_deltaNu'): return self._deltaNu
-        self._deltaNu=self.getProductNu()-self.getReactantNu()
-        return self._deltaNu        
+	"""A chemical reaction. For importing Cantera files"""
+	def getRmgReaction(self):
+		"""Fetch the RMG Reaction object associated with this cantera reaction"""
+		if not hasattr(self,'_RmgReaction'):
+			self.makeRmgReaction()
+		return self._RmgReaction
+	def makeRmgReaction(self):
+		"""Create an RMG Reaction object based on this cantera reaction"""		  
+		reactants=[]
+		for speciesName,order in self._r.items(): #	 reactants
+			assert order==int(order), "RMG currently needs elementary reactions"
+			order = int(order)
+			species=_speciesByName[speciesName].getRmgSpecies()
+			reactants.extend([species]*order)
+		products=[]
+		for speciesName,order in self._p.items(): #	 reactants
+			assert order==int(order), "RMG currently needs elementary reactions"
+			order = int(order)
+			species=_speciesByName[speciesName].getRmgSpecies()
+			products.extend([species]*order)
+		rxn = rmg.reaction.Reaction(reactants=reactants, products=products)
+		rxn.kinetics = [self.makeRmgKinetics()]
+		self._RmgReaction = rxn
+	def makeRmgKinetics(self):
+		"""Create and return an RMG ArrheniusKinetics object based on self._kf"""
+		A = self._kf[0] / _uConc**(self.getReactantNu()-1) / _uTime
+		A = float(A.simplified)
+		n = self._kf[1]
+		#assert cti._ue=='kcal/mol', 'unit conversion not implemented'
+		Ea=float(pq.Quantity(self._kf[2], cti._ue).simplified)
+		kinetics=rmg.reaction.ArrheniusKinetics(A=A, Ea=Ea, n=n)
+		kinetics.comment="Generated from Cantera expression %s"%(self._kf)
+		#kinetics.Trange = [0.0, 100000.0]
+		return kinetics
+	def getReactantNu(self):
+		"""Returns the stoichiometry in the forwards direction.
+		
+		i.e. the number of reactant molecules.
+		uses self._reactantNu to cache the answer"""
+		if hasattr(self,'_reactantNu'): return self._reactantNu
+		reactantNu=0
+		for speciesName,order in self._r.items(): # add reactants
+			reactantNu += order
+		self._reactantNu=reactantNu
+		return reactantNu
+	def getProductNu(self):
+		"""Returns the stoichiometry in the reverse direction. 
+		
+		i.e. the number of product molecules.
+		uses self._productNu to cache the answer"""
+		if hasattr(self,'_productNu'): return self._productNu
+		productNu=0
+		for speciesName,order in self._p.items(): # add products
+			productNu += order
+		self._productNu=productNu
+		return productNu
+	def getDeltaNu(self):
+		"""Returns the change in stoichiometry of the reaction, delta Nu.
+		
+		deltaNu= productNu - reactantNu
+		uses self._deltaNu to cache the answer"""
+		if hasattr(self,'_deltaNu'): return self._deltaNu
+		self._deltaNu=self.getProductNu()-self.getReactantNu()
+		return self._deltaNu
 
 def getSpeciesByName(name):
-    """Select a species by its name."""
-    for s in _species:
-        if s._name==name:
-            return s
-    return None
+	"""Select a species by its name."""
+	for s in _species:
+		if s._name==name:
+			return s
+	return None
 
 def loadCanteraFile(filepath):
-    """Load a Cantera input (.cti) file. Returns a CoreEdgeReactionModel"""
-    import os
-    logging.info("Loading Cantera file %s"%filepath)
-    logging.debug("which is located at %s"%os.path.abspath(filepath))
-    base = os.path.basename(filepath)
-    root, ext = os.path.splitext(base)
-    cti.dataset(root)
-    assert not _species, "Oops! There's already cantera species info in memory"
-    try:
-        execfile(filepath)
-    except NameError, error: # python 2.5 syntax
-        logging.exception("RMG's cantera interpreter can't read this cti file."
-            + error.message.lstrip('name '))
-        raise    
-    # the cti file will have called units() which may have changed _umol etc
-    global _uConc
-    global _uTime
-    _uConc=pq.Quantity(1,cti._umol) / pq.Quantity(1,cti._ulen)**3
-    _uTime=pq.Quantity(1,cti._utime)
-
-    model = rmg.model.CoreEdgeReactionModel()
-    for s in _species:
-        model.addSpeciesToCore( s.getRmgSpecies() )
-    for r in _reactions:
-        model.addReactionToCore( r.getRmgReaction() )
-    return model
+	"""Load a Cantera input (.cti) file. Returns a CoreEdgeReactionModel"""
+	import os
+	logging.info("Loading Cantera file %s"%filepath)
+	logging.debug("which is located at %s"%os.path.abspath(filepath))
+	base = os.path.basename(filepath)
+	root, ext = os.path.splitext(base)
+	cti.dataset(root)
+	assert not _species, "Problem: There's already cantera species info in memory."
+	try:
+		execfile(filepath)
+	except NameError, error: # python 2.5 syntax
+		logging.exception("RMG's cantera interpreter can't read this cti file."
+			+ error.message.lstrip('name '))
+		raise	 
+	# the cti file will have called units() which may have changed _umol etc
+	# so we are now in a position to set _uConc and _uTime
+	global _uConc
+	global _uTime
+	_uConc=pq.Quantity(1,cti._umol) / pq.Quantity(1,cti._ulen)**3
+	_uTime=pq.Quantity(1,cti._utime)
+	
+	model = rmg.model.CoreEdgeReactionModel()
+	for s in _species:
+		model.addSpeciesToCore( s.getRmgSpecies() )
+	for r in _reactions:
+		model.addReactionToCore( r.getRmgReaction() )
+	return model
 
 def loadChemkinFile(filepath='chem.inp', thermodb='',trandb=''):
-    """Loads a Chemkin file. (Converts to Cantera first)"""
-    from Cantera import ck2cti
-    import shutil
-    import os
-    oldpath = os.path.abspath(filepath)
-    oldwd   = os.getcwd()
-    filename = os.path.basename(filepath)
-    newpath = os.path.join(oldwd,rmg.constants.scratchDir, filename)
-    
-    if not os.path.exists(newpath) or not os.path.samefile(oldpath,newpath):
-        logging.debug("Copying %s to %s"%(oldpath,newpath))
-        shutil.copy2(oldpath, newpath)
-    ctifile = os.path.splitext(newpath)[0]+'.cti'
-    if os.path.exists(ctifile):
-        #move aside old file
-        os.rename(ctifile,'%s-%d.cti'%(os.path.splitext(ctifile)[0],
-                                         os.lstat(ctifile).st_mtime) )
-    nm='chem'
-    # convert from chemkin to cti
-    try:
-        os.chdir(rmg.constants.scratchDir)
-        ck2cti.ck2cti(infile = newpath, thermodb = thermodb,
-                  trandb = trandb, idtag = nm, debug=0, validate=1)
-    except: 
-        logging.error("Conversion from Chemkin to Cantera failed:")
-        for line in open('ck2cti.log'):
-             logging.error(line.rstrip())
-        os.chdir(oldwd) # change back to where you were
-        raise
-    logging.info('Converting %s from Chemkin to Cantera:'%filepath)
-    for line in open('ck2cti.log'):
-        logging.debug(line.rstrip())
-    os.chdir(oldwd) # change back to where you were
-    model = loadCanteraFile(ctifile)
-    return model
+	"""Loads a Chemkin file. (Converts to Cantera first)"""
+	from Cantera import ck2cti
+	import shutil
+	import os
+	oldpath = os.path.abspath(filepath)
+	oldwd	= os.getcwd()
+	filename = os.path.basename(filepath)
+	newpath = os.path.join(oldwd,rmg.constants.scratchDir, filename)
+	
+	if not os.path.exists(newpath) or not os.path.samefile(oldpath,newpath):
+		logging.debug("Copying %s to %s"%(oldpath,newpath))
+		shutil.copy2(oldpath, newpath)
+	ctifile = os.path.splitext(newpath)[0]+'.cti'
+	if os.path.exists(ctifile):
+		#move aside old file
+		os.rename(ctifile,'%s-%d.cti'%(os.path.splitext(ctifile)[0],
+										 os.lstat(ctifile).st_mtime) )
+	nm='chem'
+	# convert from chemkin to cti
+	try:
+		os.chdir(rmg.constants.scratchDir)
+		ck2cti.ck2cti(infile = newpath, thermodb = thermodb,
+				  trandb = trandb, idtag = nm, debug=0, validate=1)
+	except: 
+		logging.error("Conversion from Chemkin to Cantera failed:")
+		for line in open('ck2cti.log'):
+			 logging.error(line.rstrip())
+		os.chdir(oldwd) # change back to where you were
+		raise
+	logging.info('Converting %s from Chemkin to Cantera:'%filepath)
+	for line in open('ck2cti.log'):
+		logging.debug(line.rstrip())
+	os.chdir(oldwd) # change back to where you were
+	model = loadCanteraFile(ctifile)
+	return model
 
 
 ###### END OF MODULE
 
 
 if __name__ == "__main__":
-    #reload(cti)
-    import sys, os
+	#reload(cti)
+	import sys, os
 
-    rmg.initializeLog(verbose=10)
-    
-    if len(sys.argv)>1:
-        filename = sys.argv[1]
-    else:
-        logging.debug("No file specificed; using default, chem.cti")
-        filename = 'chem.cti' # default input file
-    if os.path.splitext(filename)[1]=='.cti':
-        model = loadCanteraFile(filename)
-    else:
-        model = loadChemkinFile(filename)
-    
+	rmg.initializeLog(verbose=10)
+	
+	if len(sys.argv)>1:
+		filename = sys.argv[1]
+	else:
+		logging.debug("No file specificed; using default, chem.cti")
+		filename = 'chem.cti' # default input file
+	if os.path.splitext(filename)[1]=='.cti':
+		model = loadCanteraFile(filename)
+	else:
+		model = loadChemkinFile(filename)
+	
 
-    
+	
