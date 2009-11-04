e4457d609914887b6ba77ed18aade437e0ec82e3
Richard West <rwest@mit.edu>
2009-11-04 10:06:52 -0500

    Using github-tools sphinx extension to make documentation site compatible with github pages.
    Github pages ignores folders beginning with an underscore

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -28,6 +28,12 @@ sys.path.append(os.path.abspath('../../source'))
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.pngmath']
 
+# github-tools extension from http://github.com/dinoboff/github-tools/
+# removes leading underscore from _static folder, to allow hosting
+# on github pages
+sys.path.append(os.path.abspath('_extensions'))
+extensions.append('github-tools')
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
