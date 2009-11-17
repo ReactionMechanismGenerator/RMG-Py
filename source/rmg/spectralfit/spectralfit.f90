85c7d522a601fbc8284527ae5d6ec9cb28706158
Richard West <rwest@mit.edu>
2009-11-17 16:54:35 -0500

    Made f2py work on MacOS X (now uses gnu95/gfortran)
    Edited the makefile.
    Also turned tabs into spaces to remove gfortran warnings.
    
    Closes #11

--- source/rmg/spectralfit/spectralfit.f90
+++ source/rmg/spectralfit/spectralfit.f90
@@ -1,27 +1,27 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ! 
-!	RMG - Reaction Mechanism Generator
+!   RMG - Reaction Mechanism Generator
 !
-!	Copyright (c) 2002-2009 Prof. William H. Green (whgreen@mit.edu) and the
-!	RMG Team (rmg_dev@mit.edu)
+!   Copyright (c) 2002-2009 Prof. William H. Green (whgreen@mit.edu) and the
+!   RMG Team (rmg_dev@mit.edu)
 ! 
-! 	Permission is hereby granted, free of charge, to any person obtaining a
-! 	copy of this software and associated documentation files (the 'Software'),
-! 	to deal in the Software without restriction, including without limitation
-! 	the rights to use, copy, modify, merge, publish, distribute, sublicense,
-! 	and/or sell copies of the Software, and to permit persons to whom the
-! 	Software is furnished to do so, subject to the following conditions:
+!   Permission is hereby granted, free of charge, to any person obtaining a
+!   copy of this software and associated documentation files (the 'Software'),
+!   to deal in the Software without restriction, including without limitation
+!   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+!   and/or sell copies of the Software, and to permit persons to whom the
+!   Software is furnished to do so, subject to the following conditions:
 ! 
-! 	The above copyright notice and this permission notice shall be included in
-! 	all copies or substantial portions of the Software.
+!   The above copyright notice and this permission notice shall be included in
+!   all copies or substantial portions of the Software.
 ! 
-! 	THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-! 	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-! 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-! 	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-! 	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-! 	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-! 	DEALINGS IN THE SOFTWARE.
+!   THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+!   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+!   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+!   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+!   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+!   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+!   DEALINGS IN THE SOFTWARE.
 ! 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
@@ -32,79 +32,79 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 subroutine fitSpectralData(Cv, Tlist, Ntemp, Nvib, Nhind, vib, hind)
-	! Estimate the spectroscopic degrees of freedom by fitting parameters to
-	! heat capacity data.
-	!
-	! ========== ====== ========================================================
-	! Parameter  Intent Description
-	! ========== ====== ========================================================
-	! `Cv`       in     A list of heat capacities Cv/R at various temperatures
-	!                   for the unknown degrees of freedom (i.e. the known 
-	!                   degrees of freedom should have already been removed)
-	! `Tlist`    in     The temperatures corresponding to the heat capacities
-	! `Ntemp`    in     The number of temperatures and heat capacities provided
-	! `Nvib`     in     The number of 1D quantum harmonic oscillators to fit
-	! `Nhind`    in     The number of 1D Pitzer hindered rotors to fit
-	! `vib`      out    A vector of fitted 1D quantum harmonic oscillator 
-	!                   frequencies in cm^-1
-	! `hind`     out    A matrix of fitted 1D Pitzer hindered rotor frequency-
-	!                   barrier pairs, both in cm^-1
-	! ========== ====== ========================================================
+    ! Estimate the spectroscopic degrees of freedom by fitting parameters to
+    ! heat capacity data.
+    !
+    ! ========== ====== ========================================================
+    ! Parameter  Intent Description
+    ! ========== ====== ========================================================
+    ! `Cv`       in     A list of heat capacities Cv/R at various temperatures
+    !                   for the unknown degrees of freedom (i.e. the known 
+    !                   degrees of freedom should have already been removed)
+    ! `Tlist`    in     The temperatures corresponding to the heat capacities
+    ! `Ntemp`    in     The number of temperatures and heat capacities provided
+    ! `Nvib`     in     The number of 1D quantum harmonic oscillators to fit
+    ! `Nhind`    in     The number of 1D Pitzer hindered rotors to fit
+    ! `vib`      out    A vector of fitted 1D quantum harmonic oscillator 
+    !                   frequencies in cm^-1
+    ! `hind`     out    A matrix of fitted 1D Pitzer hindered rotor frequency-
+    !                   barrier pairs, both in cm^-1
+    ! ========== ====== ========================================================
 
-	integer, intent(in) :: Ntemp
-	integer, intent(in) :: Nvib
-	integer, intent(in) :: Nhind
-	real(8), dimension(1:Ntemp), intent(in) :: Cv
-	real(8), dimension(1:Ntemp), intent(in) :: Tlist
-	real(8), dimension(1:Nvib), intent(out) :: vib
-	real(8), dimension(1:Nhind,1:2), intent(out) :: hind
-	
-	if (Ntemp /= 7) then
-		write (*,'(a)'), 'The number of temperatures must be exactly 7.'
-		stop
-	end if
+    integer, intent(in) :: Ntemp
+    integer, intent(in) :: Nvib
+    integer, intent(in) :: Nhind
+    real(8), dimension(1:Ntemp), intent(in) :: Cv
+    real(8), dimension(1:Ntemp), intent(in) :: Tlist
+    real(8), dimension(1:Nvib), intent(out) :: vib
+    real(8), dimension(1:Nhind,1:2), intent(out) :: hind
+    
+    if (Ntemp /= 7) then
+        write (*,'(a)'), 'The number of temperatures must be exactly 7.'
+        stop
+    end if
 
-	! Fit the harmonic oscillator and hindered rotor modes to the heat capacity
-	! The function fitSpectralDataToHeatCapacity is in calc_freq_code.f90
-	call fitSpectralDataToHeatCapacity(Cv, Tlist, Ntemp, Nvib, Nhind, vib, hind)
+    ! Fit the harmonic oscillator and hindered rotor modes to the heat capacity
+    ! The function fitSpectralDataToHeatCapacity is in calc_freq_code.f90
+    call fitSpectralDataToHeatCapacity(Cv, Tlist, Ntemp, Nvib, Nhind, vib, hind)
 
 end subroutine
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 subroutine fitSpectralDataNoRotors(Cv, Tlist, Ntemp, Nvib, vib)
-	! Estimate the spectroscopic degrees of freedom by fitting parameters to
-	! heat capacity data.
-	!
-	! ========== ====== ========================================================
-	! Parameter  Intent Description
-	! ========== ====== ========================================================
-	! `Cv`       in     A list of heat capacities Cv/R at various temperatures
-	!                   for the unknown degrees of freedom (i.e. the known
-	!                   degrees of freedom should have already been removed)
-	! `Tlist`    in     The temperatures corresponding to the heat capacities
-	! `Ntemp`    in     The number of temperatures and heat capacities provided
-	! `Nvib`     in     The number of 1D quantum harmonic oscillators to fit
-	! `vib`      out    A vector of fitted 1D quantum harmonic oscillator
-	!                   frequencies in cm^-1
-	! ========== ====== ========================================================
+    ! Estimate the spectroscopic degrees of freedom by fitting parameters to
+    ! heat capacity data.
+    !
+    ! ========== ====== ========================================================
+    ! Parameter  Intent Description
+    ! ========== ====== ========================================================
+    ! `Cv`       in     A list of heat capacities Cv/R at various temperatures
+    !                   for the unknown degrees of freedom (i.e. the known
+    !                   degrees of freedom should have already been removed)
+    ! `Tlist`    in     The temperatures corresponding to the heat capacities
+    ! `Ntemp`    in     The number of temperatures and heat capacities provided
+    ! `Nvib`     in     The number of 1D quantum harmonic oscillators to fit
+    ! `vib`      out    A vector of fitted 1D quantum harmonic oscillator
+    !                   frequencies in cm^-1
+    ! ========== ====== ========================================================
 
-	integer, intent(in) :: Ntemp
-	integer, intent(in) :: Nvib
-	real(8), dimension(1:Ntemp), intent(in) :: Cv
-	real(8), dimension(1:Ntemp), intent(in) :: Tlist
-	real(8), dimension(1:Nvib), intent(out) :: vib
+    integer, intent(in) :: Ntemp
+    integer, intent(in) :: Nvib
+    real(8), dimension(1:Ntemp), intent(in) :: Cv
+    real(8), dimension(1:Ntemp), intent(in) :: Tlist
+    real(8), dimension(1:Nvib), intent(out) :: vib
 
-	real(8), dimension(1:0,1:2) :: hind
+    real(8), dimension(1:0,1:2) :: hind
 
-	if (Ntemp /= 7) then
-		write (*,'(a)'), 'The number of temperatures must be exactly 7.'
-		stop
-	end if
+    if (Ntemp /= 7) then
+        write (*,'(a)'), 'The number of temperatures must be exactly 7.'
+        stop
+    end if
 
-	! Fit the harmonic oscillator and hindered rotor modes to the heat capacity
-	! The function fitSpectralDataToHeatCapacity is in calc_freq_code.f90
-	call fitSpectralDataToHeatCapacity(Cv, Tlist, Ntemp, Nvib, 0, vib, hind)
+    ! Fit the harmonic oscillator and hindered rotor modes to the heat capacity
+    ! The function fitSpectralDataToHeatCapacity is in calc_freq_code.f90
+    call fitSpectralDataToHeatCapacity(Cv, Tlist, Ntemp, Nvib, 0, vib, hind)
 
 end subroutine
 
