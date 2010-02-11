fd4ca393509386bc662c713bcb15ac2a817c4a14
Josh Allen <jwallen@mit.edu>
2010-02-11 14:02:20 -0500

    Added Fortran module compiling to setup.py.
    The Fortran and Cython modules can now be compiled by executing
    
    $ python setup.py build_ext --inplace
    
    within the source directory. This means that we don't need the Makefile system
    any longer. The current implementation involves two separate calls to setup()
    -- one for distutils and one for numpy.distutils. Hopefully in the future we
    won't need to do this.

--- source/setup.py
+++ source/setup.py
@@ -32,36 +32,78 @@
 This is the setup file for RMG.
 """
 
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
+def setupCythonModules():
+	
+	from distutils.core import setup
+	from distutils.extension import Extension
+	from Cython.Distutils import build_ext
+	
+	# Stop wasting my time compiling PowerPC-compatible C extensions on my intel Mac
+	import distutils.sysconfig
+	config = distutils.sysconfig.get_config_vars()
+	for key,value in config.iteritems():
+		location = str(value).find('-arch ppc')
+		if location>=0:
+			print "removing '-arch ppc' from %s"%(key)
+			config[key] = value.replace('-arch ppc ','')
+	
+	# The Cython modules to setup
+	ext_modules = [
+		Extension('rmg.chem', ['rmg/chem.py']),
+		Extension('rmg.graph', ['rmg/graph.py']),
+		Extension('rmg.thermo', ['rmg/thermo.py'])
+	]
+
+	setup(name='RMG',
+		version='0.0.1',
+		description='Reaction Mechanism Generator',
+		author='Prof. William H. Green and the RMG Team',
+		author_email='whgreen@mit.edu, rmg_dev@mit.edu',
+		url='http://rmg.sourceforge.net/',
+		packages=['rmg'],
+		cmdclass = {'build_ext': build_ext},
+		ext_modules = ext_modules
+	)
 
 ################################################################################
 
-# Stop wasting my time compiling PowerPC-compatible C extensions on my intel Mac
-import distutils.sysconfig
-config = distutils.sysconfig.get_config_vars()
-for key,value in config.iteritems():
-	location = str(value).find('-arch ppc')
-	if location>=0:
-		print "removing '-arch ppc' from %s"%(key)
-		config[key] = value.replace('-arch ppc ','')
+def configuration(parent_package='',top_path=None):
+	from numpy.distutils.misc_util import Configuration
+	
+	config = Configuration('rmg',parent_package,top_path)
+	config.add_library('rmg_spectral_math', sources=['rmg/spectral/math.f90'])
+	config.add_library('rmg_spectral_cases', sources=['rmg/spectral/cases.f90'])
+	config.add_library('rmg_spectral_dqed', sources=['rmg/spectral/dqed.f90'])
+	config.add_library('rmg_spectral_modes', sources=['rmg/spectral/_modes.f90'])
+	config.add_extension('spectral._modes', sources=['rmg/spectral/_modes.f90'], libraries=['rmg_spectral_math'])
+	config.add_extension('spectral._fit', sources=['rmg/spectral/_fit.f90'], libraries=['rmg_spectral_cases', 'rmg_spectral_dqed', 'rmg_spectral_modes', 'rmg_spectral_math'])
+	
+	config.add_extension('unirxn.states', sources=['rmg/unirxn/states.f90'], libraries=['blas', 'lapack'])
+	config.add_extension('unirxn.mastereqn', sources=['rmg/unirxn/mastereqn.f90'], libraries=['blas', 'lapack'])
+	config.add_extension('unirxn.msc', sources=['rmg/unirxn/msc.f90'], libraries=['blas', 'lapack'])
+	config.add_extension('unirxn.rs', sources=['rmg/unirxn/rs.f90'], libraries=['blas', 'lapack'])
+	
+	return config
+
+def setupFortranModules():
+
+	from numpy.distutils.core import setup
+	
+	setup(name='RMG',
+		version='0.0.1',
+		description='Reaction Mechanism Generator',
+		author='Prof. William H. Green and the RMG Team',
+		author_email='whgreen@mit.edu, rmg_dev@mit.edu',
+		url='http://rmg.sourceforge.net/',
+		packages=['rmg'],
+		configuration = configuration
+	)
 
 ################################################################################
 
-ext_modules = [
-	Extension('rmg.chem', ['rmg/chem.py']),
-	Extension('rmg.graph', ['rmg/graph.py']),
-	Extension('rmg.thermo', ['rmg/thermo.py'])
-]
-
-setup(name='RMG',
-	version='0.0.1',
-	description='Reaction Mechanism Generator',
-	author='Prof. William H. Green and the RMG Team',
-	author_email='whgreen@mit.edu, rmg_dev@mit.edu',
-	url='http://rmg.sourceforge.net/',
-	packages=['rmg'],
-	cmdclass = {'build_ext': build_ext},
-	ext_modules = ext_modules
-)
+if __name__ == '__main__':
+	
+	setupFortranModules()
+	
+	setupCythonModules()
+	
