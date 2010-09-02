d0893ce2ce1bbefa645e60699a594bc29a90f5a8
Josh Allen <jwallen@mit.edu>
2010-09-02 10:36:34 -0400

    Added Makefile and setup.py to facilitate Cython compilation.

---
+++ setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+################################################################################
+#
+#   MEASURE - Master Equation Automatic Solver for Unimolecular REactions
+#
+#   Copyright (c) 2010 by Joshua W. Allen (jwallen@mit.edu)
+#
+#   Permission is hereby granted, free of charge, to any person obtaining a
+#   copy of this software and associated documentation files (the 'Software'),
+#   to deal in the Software without restriction, including without limitation
+#   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#   and/or sell copies of the Software, and to permit persons to whom the
+#   Software is furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included in
+#   all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#   DEALINGS IN THE SOFTWARE.
+#
+################################################################################
+    
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import Cython.Compiler.Options
+
+import numpy
+
+# Create annotated HTML files for each of the Cython modules
+Cython.Compiler.Options.annotate = True
+
+# Turn on profiling capacity for all Cython modules
+#Cython.Compiler.Options.directive_defaults['profile'] = True
+
+# The Cython modules to setup
+# This is a more standard way of doing things, but Cython doesn't like it as much
+packages=['measure']
+ext_modules = [
+    Extension('measure.collision', ['measure/collision.py']),
+    Extension('measure.reaction', ['measure/reaction.py']),
+    Extension('measure.msc', ['measure/msc.py']),
+    Extension('measure.rs', ['measure/rs.py']),
+]
+
+setup(name='MEASURE',
+    version='0.1.0',
+    description='Master Equation Automatic Solver for Unimolecular REactions',
+    author='Joshua W. Allen',
+    author_email='jwallen@mit.edu',
+    url='',
+    packages=packages,
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = ext_modules,
+    include_dirs = [numpy.get_include()],
+)
+
