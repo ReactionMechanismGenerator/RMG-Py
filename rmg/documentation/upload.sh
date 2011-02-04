b0e675e8ece216fd8b49e52a9d6f53653072bf12
Josh Allen <jwallen@mit.edu>
2011-02-04 10:06:45 -0500

    Removed some old database and documentation files from the repository.
    These files were really out of date.

--- rmg/documentation/upload.sh
+++
@@ -1,8 +0,0 @@
-# Run this after building html documentation to upload to github pages so it appears at 
-# http://greengroup.github.com/RMG-Py/
-
-cd build/html
-git add -A
-git add .
-git commit -m "Auto-committed by upload.sh (after sphinx build)"
-git push official gh-pages
