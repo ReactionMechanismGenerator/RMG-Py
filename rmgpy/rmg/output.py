35225e2eff61549af3f0b696273f77a8ee66e628
Josh Allen <jwallen@mit.edu>
2011-02-12 16:52:19 -0500

    Added javascript to output HTML show/hide reactions by family.
    This gives users a means to view only a subset of the reaction model at
    a time, which may make it easier to find individual reactions of
    interest. The jQuery library is utilized for this purpose. Note that I'm
    using check boxes instead of span blocks so that the user has a visual
    cue which families are shown. I've also added check all and uncheck all
    actions.

--- rmgpy/rmg/output.py
+++ rmgpy/rmg/output.py
@@ -74,6 +74,14 @@ def saveOutputHTML(path, reactionModel):
         if not os.path.exists(fstr):
             drawMolecule(spec.molecule[0], fstr)
 
+    # Count the number of reactions from each reaction family
+    # This information will be passed to the jinja template below
+    families = list(set([rxn.family for rxn in reactionModel.core.reactions]))
+    families.sort(key=lambda x: x.label)
+    familyCount = {}
+    for family in families:
+        familyCount[family] = sum([1 for rxn in reactionModel.core.reactions if rxn.family is family])
+
     # Make HTML file
     template = jinja2.Template(
 """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
@@ -84,6 +92,16 @@ def saveOutputHTML(path, reactionModel):
     <style>
         body {
             font-family: sans-serif;
+        }
+        a {
+            color: #993333;
+            text-decoration: none;
+        }
+        a:visited {
+            color: #993333;
+        }
+        a:hover {
+            text-decoration: underline;
         }
         table.reactionList {
             width: 100%;
@@ -103,13 +121,42 @@ def saveOutputHTML(path, reactionModel):
             vertical-align: middle;
         }
     </style>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
+    <script type="text/javascript">
+    function updateFamily(family) {
+        if (family.checked) {
+            $("."+family.value).show();
+        } else {
+            $("."+family.value).hide();
+        }
+    }
+
+    function checkAllFamilies() {
+        $("#familyForm").find("input").each(function() { this.checked = true; updateFamily(this); });
+    }
+
+    function uncheckAllFamilies() {
+        $("#familyForm").find("input").each(function() { this.checked = false; updateFamily(this); });
+    }
+
+    $(document).ready(function() {
+        checkAllFamilies();
+    });
+
+    </script>
 </head>
 
 <body>
 
+<form id='familySelector'>
+    <div>Reaction families:</div>
+{% for family in families %}    <div><input type="checkbox" id="{{ family.label }}" name="family" value="{{ family.label }}" checked="checked" onclick="updateFamily(this);"/><label for="{{ family.label }}">{{ family.label }} ({{ familyCount[family] }})</label></div>
+{% endfor %}    <div><a href="#" onclick="checkAllFamilies();">check all</a> &nbsp; &nbsp; <a href="#" onclick="uncheckAllFamilies();">uncheck all</a></div>
+</form>
+
 <table class="reactionList">
     {% for rxn in reactions %}
-    <tr class="reaction">
+    <tr class="reaction {{ rxn.family.label }}">
         <td class="reactants">{% for reactant in rxn.reactants %}<img src="species/{{ reactant }}.png" alt="{{ reactant }}" title="{{ reactant }}"/>{% if not loop.last %} + {% endif %}{% endfor %}</td>
         <td class="reactionArrow">{% if rxn.reversible %}&hArr;{% else %}&rarr;{% endif %}</td>
         <td class="products">{% for product in rxn.products %}<img src="species/{{ product }}.png" alt="{{ product }}" title="{{ product }}"/>{% if not loop.last %} + {% endif %}{% endfor %}</td>
@@ -122,5 +169,5 @@ def saveOutputHTML(path, reactionModel):
 </html>
 """)
     f = open(path, 'w')
-    f.write(template.render(title='RMG Output', reactions=reactionModel.core.reactions))
+    f.write(template.render(title='RMG Output', reactions=reactionModel.core.reactions, families=families, familyCount=familyCount))
     f.close()
