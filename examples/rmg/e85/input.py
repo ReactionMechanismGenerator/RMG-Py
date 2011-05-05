aa05ef2a2d5bc50fdbdab923c6f6f61a51b72e70
Josh Allen <jwallen@mit.edu>
2011-05-05 14:35:16 -0400

    Updated database section of RMG input files.
    To specify the database, you now specify its root directory, as well as
    the thermo and kinetics libraries to load. The latter is split into seed
    mechanisms and reaction libraries based on where the loading reactions
    are placed. You don't need to specify the thermo and kinetics groups any
    longer; these are always loaded.
    
    The example input files have been updated to reflect the new syntax.

--- examples/rmg/e85/input.py
+++ examples/rmg/e85/input.py
@@ -1,12 +1,9 @@
 # Data sources
 database(
-    thermo_groups='output/RMG_database/thermo_groups',
-    thermo_libraries=[
-        'output/RMG_database/thermo_libraries/primaryThermoLibrary',
-        'output/RMG_database/thermo_libraries/GRI-Mech3.0',
-    ],
-    kinetics_groups='output/RMG_database/kinetics_groups',
-    seed_mechanisms=['output/RMG_database/kinetics_libraries/GRI-Mech3.0'],
+    '../RMG-database',
+    thermoLibraries = ['primaryThermoLibrary', 'GRI-Mech3.0'],
+    reactionLibraries = [],
+    seedMechanisms = ['GRI-Mech3.0'],
 )
 
 # List of species
