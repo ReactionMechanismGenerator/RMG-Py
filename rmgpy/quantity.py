ed12bc558f632fc3de4b00b7a6b7daee5b4e7258
Josh Allen <jwallen@mit.edu>
2011-05-18 12:20:32 -0400

    Updated header section of quantity.py and quantity.pxd.
    The header now says RMG (instead of ChemPy) and updates the year. The
    way the file encoding is specified is also changed.

--- rmgpy/quantity.py
+++ rmgpy/quantity.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# encoding: utf-8
 
 ################################################################################
 #
-#   ChemPy - A chemistry toolkit for Python
+#   RMG - Reaction Mechanism Generator
 #
-#   Copyright (c) 2010 by Joshua W. Allen (jwallen@mit.edu)
+#   Copyright (c) 2009-2011 by the RMG Team (rmg_dev@mit.edu)
 #
 #   Permission is hereby granted, free of charge, to any person obtaining a
 #   copy of this software and associated documentation files (the 'Software'),
