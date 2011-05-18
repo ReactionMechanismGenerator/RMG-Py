ecf1562d1da71b8013dfe866e5308ea4aeceeb7b
Josh Allen <jwallen@mit.edu>
2011-05-18 12:25:52 -0400

    Added unit tests for the new constants object.
    These tests ensure that the values of the constants are correct (or at
    least that they don't change!)

--- unittest/quantityTest.py
+++ unittest/quantityTest.py
@@ -74,5 +74,31 @@ class ConstantsTest(unittest.TestCase):
         
 ################################################################################
 
+class TestConstants(unittest.TestCase):
+    """
+    Contains unit tests of the Constants class. These tests ensure that the
+    values of the constants are correct (or at least that they do not change!)
+    """
+    
+    def testAvogadroConstant(self):
+        self.assertAlmostEqual(constants.Na / pq.constants.N_A.simplified, 1.0, 6)
+
+    def testBoltzmannConstant(self):
+        self.assertAlmostEqual(constants.kB / pq.constants.Boltzmann_constant.simplified, 1.0, 6)
+    
+    def testGasLawConstant(self):
+        self.assertAlmostEqual(constants.R / pq.constants.R.simplified, 1.0, 6)
+    
+    def testPlanckConstant(self):
+        self.assertAlmostEqual(constants.h / pq.constants.Planck_constant.simplified, 1.0, 6)
+    
+    def testSpeedOfLight(self):
+        self.assertAlmostEqual(constants.c / 299792458, 1.0, 6)
+    
+    def testPi(self):
+        self.assertAlmostEqual(constants.pi / math.pi, 1.0, 6)
+
+################################################################################
+
 if __name__ == '__main__':
-    unittest.main( testRunner = unittest.TextTestRunner(verbosity=2) )
+    unittest.main(testRunner=unittest.TextTestRunner(verbosity=2))
