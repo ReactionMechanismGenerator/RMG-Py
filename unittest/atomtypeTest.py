0779c8c3c7b38e6255df1e5a829dc66e96c401c1
Josh Allen <jwallen@mit.edu>
2011-05-19 10:56:10 -0400

    Added unit test for rmgpy.atomtype module.
    This unit test ensures that an AtomType object can be pickled and
    unpickled with no loss of information. Note that an AtomType cannot
    currently be reconstructed from its repr() output, so no unit test
    exists for this purpose. Testing the getAtomType() method requires a
    complete molecule, so we will defer that unit test for the moment.

---
+++ unittest/atomtypeTest.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+This module contains unit tests of the rmgpy.atomtype module.
+"""
+
+import unittest
+
+import rmgpy.atomtype
+from rmgpy.atomtype import AtomType, getAtomType
+
+################################################################################
+
+class TestAtomType(unittest.TestCase):
+    """
+    Contains unit tests of the AtomType class.
+    """
+    
+    def setUp(self):
+        """
+        A function run before each unit test in this class.
+        """
+        self.atomType = rmgpy.atomtype.atomTypes['Cd']
+        
+    def testPickle(self):
+        """
+        Test that an AtomType object can be successfully pickled and
+        unpickled with no loss of information.
+        """
+        import cPickle
+        atomType = cPickle.loads(cPickle.dumps(self.atomType))
+        self.assertEqual(self.atomType.label, atomType.label)
+        self.assertEqual(len(self.atomType.generic), len(atomType.generic))
+        for item1, item2 in zip(self.atomType.generic, atomType.generic):
+            self.assertEqual(item1.label, item2.label)
+        self.assertEqual(len(self.atomType.specific), len(atomType.specific))
+        for item1, item2 in zip(self.atomType.specific, atomType.specific):
+            self.assertEqual(item1.label, item2.label)
+        self.assertEqual(len(self.atomType.incrementBond), len(atomType.incrementBond))
+        for item1, item2 in zip(self.atomType.incrementBond, atomType.incrementBond):
+            self.assertEqual(item1.label, item2.label)
+        self.assertEqual(len(self.atomType.decrementBond), len(atomType.decrementBond))
+        for item1, item2 in zip(self.atomType.decrementBond, atomType.decrementBond):
+            self.assertEqual(item1.label, item2.label)
+        self.assertEqual(len(self.atomType.formBond), len(atomType.formBond))
+        for item1, item2 in zip(self.atomType.formBond, atomType.formBond):
+            self.assertEqual(item1.label, item2.label)
+        self.assertEqual(len(self.atomType.breakBond), len(atomType.breakBond))
+        for item1, item2 in zip(self.atomType.breakBond, atomType.breakBond):
+            self.assertEqual(item1.label, item2.label)
+        self.assertEqual(len(self.atomType.incrementRadical), len(atomType.incrementRadical))
+        for item1, item2 in zip(self.atomType.incrementRadical, atomType.incrementRadical):
+            self.assertEqual(item1.label, item2.label)
+        self.assertEqual(len(self.atomType.decrementRadical), len(atomType.decrementRadical))
+        for item1, item2 in zip(self.atomType.decrementRadical, atomType.decrementRadical):
+            self.assertEqual(item1.label, item2.label)
+        
+################################################################################
+
+if __name__ == '__main__':
+    unittest.main(testRunner=unittest.TextTestRunner(verbosity=2))
