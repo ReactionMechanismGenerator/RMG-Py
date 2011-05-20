fa5e7f8b2b47274624f6e7f84d5cf71503996752
Josh Allen <jwallen@mit.edu>
2011-05-20 09:31:57 -0400

    Refactoring of the rmgpy.species unit tests.
    Since Species acts mostly as a container class for objects that describe
    a single species (thermo, states, etc.), there isn't much new
    functionality to test. For now we are simply testing that we can
    reconstruct a Species object from serialization and from repr() output.

--- unittest/speciesTest.py
+++ unittest/speciesTest.py
@@ -1,28 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+This module contains unit tests of the rmgpy.species module.
+"""
+
 import unittest
 
-from rmgpy.chem.molecule import Molecule
-from rmgpy.chem.species import *
-from rmgpy.chem.thermo import ThermoData
-from rmgpy.chem.states import *
-import rmgpy.chem.constants as constants
+from rmgpy.molecule import Molecule
+from rmgpy.species import *
+from rmgpy.thermo import ThermoData
+from rmgpy.statmech import *
+from rmgpy.quantity import constants
 
 ################################################################################
 
-class SpeciesTest(unittest.TestCase):
+class TestSpecies(unittest.TestCase):
     """
-    Contains unit tests for the rmgpy.chem.species module, used for working
-    with chemical species objects.
+    Contains unit tests for the Species class.
     """
     
-    def testPickle(self):
+    def setUp(self):
         """
-        Test that a Species object can be successfully pickled and
-        unpickled with no loss of information.
+        A method that is run before each unit test in this class.
         """
-        spec0 = Species(
+        self.species = Species(
             index=1,
             label='C2H4',
             thermo=ThermoData(
@@ -44,64 +46,55 @@ class SpeciesTest(unittest.TestCase):
             molecularWeight=0.02803,
             reactive=True
         )
+        
+    def testPickle(self):
+        """
+        Test that a Species object can be successfully pickled and
+        unpickled with no loss of information.
+        """
         import cPickle
-        spec = cPickle.loads(cPickle.dumps(spec0))
-
-        self.assertEqual(spec0.index, spec.index)
-        self.assertEqual(spec0.label, spec.label)
-        self.assertEqual(spec0.thermo.H298, spec.thermo.H298)
-        self.assertEqual(len(spec0.states.modes), len(spec.states.modes))
-        self.assertEqual(len(spec0.molecule), len(spec.molecule))
-        self.assertTrue(spec0.molecule[0].isIsomorphic(spec.molecule[0]))
-        self.assertEqual(spec0.E0, spec.E0)
-        self.assertEqual(spec0.lennardJones.sigma, spec.lennardJones.sigma)
-        self.assertEqual(spec0.lennardJones.epsilon, spec.lennardJones.epsilon)
-        self.assertEqual(spec0.molecularWeight, spec.molecularWeight)
-        self.assertEqual(spec0.reactive, spec.reactive)
+        species = cPickle.loads(cPickle.dumps(self.species))
+        self.assertEqual(self.species.index, species.index)
+        self.assertEqual(self.species.label, species.label)
+        self.assertEqual(self.species.thermo.H298.value, species.thermo.H298.value)
+        self.assertEqual(self.species.thermo.H298.units, species.thermo.H298.units)
+        self.assertEqual(len(self.species.states.modes), len(species.states.modes))
+        self.assertEqual(len(self.species.molecule), len(species.molecule))
+        self.assertTrue(self.species.molecule[0].isIsomorphic(species.molecule[0]))
+        self.assertEqual(self.species.E0.value, species.E0.value)
+        self.assertEqual(self.species.E0.units, species.E0.units)
+        self.assertEqual(self.species.lennardJones.sigma.value, species.lennardJones.sigma.value)
+        self.assertEqual(self.species.lennardJones.sigma.units, species.lennardJones.sigma.units)
+        self.assertAlmostEqual(self.species.lennardJones.epsilon.value / 1.381e-23, species.lennardJones.epsilon.value / 1.381e-23, 4)
+        self.assertEqual(self.species.lennardJones.epsilon.units, species.lennardJones.epsilon.units)
+        self.assertEqual(self.species.molecularWeight.value, species.molecularWeight.value)
+        self.assertEqual(self.species.molecularWeight.units, species.molecularWeight.units)
+        self.assertEqual(self.species.reactive, species.reactive)
 
     def testOutput(self):
         """
         Test that a Species object can be successfully reconstructed
         from its repr() output with no loss of information.
         """
-        spec0 = Species(
-            index=1,
-            label='C2H4',
-            thermo=ThermoData(
-                Tdata=[300.0,400.0,500.0,600.0,800.0,1000.0,1500.0],
-                Cpdata=[3.0,4.0,5.0,6.0,8.0,10.0,15.0],
-                H298=-20.0*4184,
-                S298=50.0*4.184,
-                Tmin=300.0,
-                Tmax=2000.0,
-            ),
-            states=StatesModel(modes=[
-                Translation(mass=0.02803),
-                RigidRotor(linear=False, inertia=[5.6952e-47, 2.7758e-46, 3.3454e-46], symmetry=1),
-                HarmonicOscillator(frequencies=[834.50, 973.31, 975.37, 1067.1, 1238.5, 1379.5, 1472.3, 1691.3, 3121.6, 3136.7, 3192.5, 3221.0]),
-            ]),
-            molecule=[Molecule().fromSMILES('C=C')],
-            E0=0.0,
-            lennardJones=LennardJones(sigma=1e-10, epsilon=constants.kB),
-            molecularWeight=0.02803,
-            reactive=True
-        )
-        exec('spec = %r' % spec0)
-        
-        self.assertEqual(spec0.index, spec.index)
-        self.assertEqual(spec0.label, spec.label)
-        self.assertEqual(spec0.thermo.H298, spec.thermo.H298)
-        self.assertEqual(len(spec0.states.modes), len(spec.states.modes))
-        self.assertEqual(len(spec0.molecule), len(spec.molecule))
-        self.assertTrue(spec0.molecule[0].isIsomorphic(spec.molecule[0]))
-        self.assertEqual(spec0.E0, spec.E0)
-        self.assertAlmostEqual(spec0.lennardJones.sigma, spec.lennardJones.sigma, 6)
-        self.assertAlmostEqual(spec0.lennardJones.epsilon, spec.lennardJones.epsilon, 6)
-        self.assertAlmostEqual(spec0.molecularWeight, spec.molecularWeight, 6)
-        self.assertEqual(spec0.reactive, spec.reactive)
-
+        exec('species = %r' % self.species)
+        self.assertEqual(self.species.index, species.index)
+        self.assertEqual(self.species.label, species.label)
+        self.assertEqual(self.species.thermo.H298.value, species.thermo.H298.value)
+        self.assertEqual(self.species.thermo.H298.units, species.thermo.H298.units)
+        self.assertEqual(len(self.species.states.modes), len(species.states.modes))
+        self.assertEqual(len(self.species.molecule), len(species.molecule))
+        self.assertTrue(self.species.molecule[0].isIsomorphic(species.molecule[0]))
+        self.assertEqual(self.species.E0.value, species.E0.value)
+        self.assertEqual(self.species.E0.units, species.E0.units)
+        self.assertEqual(self.species.lennardJones.sigma.value, species.lennardJones.sigma.value)
+        self.assertEqual(self.species.lennardJones.sigma.units, species.lennardJones.sigma.units)
+        self.assertAlmostEqual(self.species.lennardJones.epsilon.value / 1.381e-23, species.lennardJones.epsilon.value / 1.381e-23, 4)
+        self.assertEqual(self.species.lennardJones.epsilon.units, species.lennardJones.epsilon.units)
+        self.assertEqual(self.species.molecularWeight.value, species.molecularWeight.value)
+        self.assertEqual(self.species.molecularWeight.units, species.molecularWeight.units)
+        self.assertEqual(self.species.reactive, species.reactive)
 
 ################################################################################
 
 if __name__ == '__main__':
-    unittest.main( testRunner = unittest.TextTestRunner(verbosity=2) )
+    unittest.main(testRunner=unittest.TextTestRunner(verbosity=2))
