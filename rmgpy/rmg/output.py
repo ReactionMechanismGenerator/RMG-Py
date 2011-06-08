fbcd20cab49710f5f06ed13291f22e645edd08df
Richard West <rwest@mit.edu>
2011-06-08 11:58:30 +0100

    Local copy of jquery.min.js in external, for offline viewing of results html.
    The results file in HTML format requires jquery for some of the display toggling.
    This commit allows you to view results without an internet connection.

--- rmgpy/rmg/output.py
+++ rmgpy/rmg/output.py
@@ -154,6 +154,7 @@ def saveOutputHTML(path, reactionModel):
         }
     </style>
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
+    <script type="text/javascript" src="../../../external/jquery.min.js"></script>
     <script type="text/javascript">
     function updateFamily(family) {
         if (family.checked) {
