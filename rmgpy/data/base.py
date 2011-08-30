508da4d6f6e41d5ed47f9884540e1e654fc63306
Josh Allen <jwallen@mit.edu>
2011-08-30 17:43:20 -0400

    Don't consider other labeled atoms when descending tree.
    When descending the tree, we first match the labeled atoms in the group
    with the corresponding atoms in the structure. However, previously we
    were allowing non-labeled atoms in the group to match other labeled atoms
    in the structure. This led to multiple siblings matching the structure,
    which RMG-Py would handle by stopping its tree descent at that point.
    By making this change, most of the multiply-matched sibling warnings go
    away.
    
    I'm not sure this is the best implementation (it's a bit of a hack), but
    it seems to work.

--- rmgpy/data/base.py
+++ rmgpy/data/base.py
@@ -813,8 +813,20 @@ class Database:
                             " has bond but group {0} doesn't".format(node))
                 # Passed semantic checks, so add to maps of already-matched atoms
                 initialMap[atom] = center
+            # Labeled atoms in the structure that are not in the group should
+            # not be considered in the isomorphism check, so remove them temporarily
+            # Without this we would hit a lot of nodes that are ambiguous
+            removedAtoms = []
+            for label, atom in structure.getLabeledAtoms().iteritems():
+                if label not in centers:
+                    removedAtoms.append(atom)
+                    structure.atoms.remove(atom)
             # use mapped (labeled) atoms to try to match subgraph
-            return structure.isSubgraphIsomorphic(group, initialMap)
+            result = structure.isSubgraphIsomorphic(group, initialMap)
+            # Restore atoms removed in previous step
+            for atom in removedAtoms:
+                structure.atoms.append(atom)
+            return result
 
     def descendTree(self, structure, atoms, root=None):
         """
