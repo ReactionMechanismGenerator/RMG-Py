447d3507cd480ba32a2752ec01eb54dddd59e4e8
Nick Vandewiele <nmvdewie@nick-lct-laptop.(none)>
2012-05-24 22:41:27 +0200

    CCLib parser utility added.
    Used as a package under the working directory of RMG-Py.

---
+++ cclib/method/__init__.py
@@ -0,0 +1,15 @@
+"""
+cclib (http://cclib.sf.net) is (c) 2006, the cclib development team
+and licensed under the LGPL (http://www.gnu.org/copyleft/lgpl.html).
+"""
+
+__revision__ = "$Revision: 631 $"
+
+from density import Density
+from cspa import CSPA
+from mpa import MPA
+from lpa import LPA
+from opa import OPA
+from mbo import MBO
+from fragments import FragmentAnalysis
+from cda import CDA
