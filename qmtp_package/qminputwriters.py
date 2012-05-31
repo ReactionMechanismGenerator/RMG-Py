4776eedf7e7feb7e6fdcbcd7f7fcab7a944c6b6a
Nick Vandewiele <nmvdewie@nick-lct-laptop.(none)>
2012-05-31 11:20:27 -0400

    qm input writer keywords for mopac added as field
    two dict's are creating storing the keywords that are added
    to the QM input file based on the attempt number.
    
    the if elif else clauses are removed. I removed Greg's comments
    at each keyword attempt although they are very usefull in knowing
    why he exactly chose those keywords. They should converted in unit tests
    eventually.
    
    This design should be better to be externalized completely
    from the code so that the user has the power to decide which
    keywords will be tried out at which attempt number.

--- qmtp_package/qminputwriters.py
+++ qmtp_package/qminputwriters.py
@@ -73,99 +73,31 @@ class MOPACPM3InputWriter(QMInputWriter):
         self.fillMultiplicityKeywords()
         self.keywords = {}
         
-        self.inputExtension = '.mop'
-   
-    def getMopacRadicalString(self, multipl):
-        '''
+        self.keywordsTop = {}#keywords that will be added at the top of the qm input file
+        self.keywordsTop[1] = " precise nosym"
+        self.keywordsTop[2] = " precise nosym gnorm=0.0 nonr"
+        self.keywordsTop[3] = " precise nosym gnorm=0.0"
+        self.keywordsTop[4] = " precise nosym gnorm=0.0 bfgs"
+        self.keywordsTop[5] = " precise nosym recalc=10 dmax=0.10 nonr cycles=2000 t=2000"
         
-        returns the extra Mopac keywords to use for radical species, given the spin multiplicity 
-        (radical number + 1)
-     
-        '''
-        try:
-            keyword = self.multiplicityKeywords.get(multipl)
-        except Exception as e:
-            logging.critical('Invalid multiplicity encountered: "+multipl'+str(multipl))
-        return keyword  
-    
+        self.keywordsBottom = {}#keywords that will be added at the bottom of the qm input file
+        self.keywordsBottom[1] = "oldgeo thermo nosym precise "
+        self.keywordsBottom[2] = "oldgeo thermo nosym precise "
+        self.keywordsBottom[3] = "oldgeo thermo nosym precise "
+        self.keywordsBottom[4] = "oldgeo thermo nosym precise "
+        self.keywordsBottom[5] = "oldgeo thermo nosym precise "
+        
+        self.inputExtension = '.mop'
     
      
     def createKeywords(self):
-        radicalString = self.getMopacRadicalString(self.multiplicity)
-        try:
-            if self.attemptNumber % MOPACPM3InputWriter.scriptAttempts == 1:
-                inpKeyStrBoth="pm3 "+radicalString
-                inpKeyStrTop=" precise nosym"
-                '''
-                 * 7/10/09: based on a quick review of recent results, 
-                 * keyword combo #1 rarely works, and when it did (CJAINEUZFLXGFA-UHFFFAOYAUmult3 
-                 * (InChI=1/C8H16O5Si/c1-4-11-14(9,12-5-2)13-8-6-10-7(8)3/h7-8H,3-6H2,1-2H3/mult3)), 
-                 * the grad. norm on the force step was about 1.7 (too large) 
-                 * I manually removed this result and re-ran...the entropy was increased 
-                 * by nearly 20 cal/mol-K...perhaps we should add a check for the "WARNING" 
-                 * that MOPAC prints out when the gradient is high 7/22/09: for the case of 
-                 * FUGDBSHZYPTWLG-UHFFFAOYADmult3 (InChI=1/C5H8/c1-4-3-5(4)2/h4-5H,1-3H2/mult3), 
-                 * adding nosym seemed to resolve 1. large discrepancies from Gaussian and 2. 
-                 * negative frequencies in mass-weighted coordinates and possibly related issue 
-                 * in discrepancies between regular and mass-weighted coordinate frequencies
-                 '''
-                inpKeyStrBottom="oldgeo thermo nosym precise "
-            elif self.attemptNumber%self.scriptAttempts == 2:
-                '''
-                 * #7/9/09: used for VCSJVABXVCFDRA-UHFFFAOYAI 
-                 * (InChI=1/C8H19O5Si/c1-5-10-8(4)13-14(9,11-6-2)12-7-3/h8H,5-7H2,1-4H3) 
-                 * all existing Gaussian keywords also failed the Gaussian result 
-                 * was also rectified, but the resulting molecule was over 70 kcal/mol less stable,
-                 *  probably due to a large amount of spin contamination (~1.75 in fixed Gaussian 
-                 *  result vs. 0.754 for MOPAC)
-                '''
-                inpKeyStrBoth="pm3 "+radicalString
-                inpKeyStrTop=" precise nosym gnorm=0.0 nonr"
-                inpKeyStrBottom="oldgeo thermo nosym precise "
-            elif self.attemptNumber%self.scriptAttempts == 3:
-                '''
-                 * #7/8/09: used for ADMPQLGIEMRGAT-UHFFFAOYAUmult3 
-                 * (InChI=1/C6H14O5Si/c1-4-9-12(8,10-5-2)11-6(3)7/h6-7H,3-5H2,1-2H3/mult3) 
-                 * all existing Gaussian keywords also failed however, the Gaussian result
-                 *  was also rectified, and the resulting conformation was about 1.0 kcal/mol 
-                 *  more stable than the one resulting from this, so fixed Gaussian result was 
-                 *  manually copied to QMFiles folder
-                '''
-                inpKeyStrBoth="pm3 "+radicalString
-                inpKeyStrTop=" precise nosym gnorm=0.0"
-                '''
-                 * #precise appeared to be necessary for the problematic case (to avoid negative frequencies)
-                '''
-                inpKeyStrBottom="oldgeo thermo nosym precise " 
-            elif self.attemptNumber%self.scriptAttempts == 4:
-                '''
-                 * #7/8/09: used for GYFVJYRUZAKGFA-UHFFFAOYALmult3 
-                 * (InChI=1/C6H14O6Si/c1-3-10-13(8,11-4-2)12-6-5-9-7/h6-7H,3-5H2,1-2H3/mult3) 
-                 * case (negative frequency issues in MOPAC) (also, none of the existing Gaussian
-                 *  combinations worked with it) note that the Gaussian result appears to be a 
-                 *  different conformation as it is about 0.85 kcal/mol more stable, so the Gaussian 
-                 *  result was manually copied to QMFiles directory note that the MOPAC output included 
-                 *  a very low frequency (4-5 cm^-1)
-                '''
-                inpKeyStrBoth="pm3 "+radicalString
-                inpKeyStrTop=" precise nosym gnorm=0.0 bfgs"
-                '''
-                 * precise appeared to be necessary for the problematic case (to avoid negative frequencies)
-                '''
-                inpKeyStrBottom="oldgeo thermo nosym precise " 
-            elif self.attemptNumber%self.scriptAttempts == 0:
-                '''
-                 * used for troublesome HGRZRPHFLAXXBT-UHFFFAOYAVmult3 
-                 * (InChI=1/C3H2O4/c4-2(5)1-3(6)7/h1H2/mult3) case 
-                 * (negative frequency and large gradient issues)                
-                '''
-                inpKeyStrBoth="pm3 "+radicalString
-                inpKeyStrTop=" precise nosym recalc=10 dmax=0.10 nonr cycles=2000 t=2000"
-                inpKeyStrBottom="oldgeo thermo nosym precise "
-        except Exception as e:
-            logging.error('Error in writing inputkeywords.txt \n'+str(e))
-            
-            
+        '''
+        Based on the attempt number keywords will be added to an QM input file.
+        '''
+        inpKeyStrBoth = "pm3 "+self.multiplicityKeywords[self.multiplicity]
+        inpKeyStrTop = self.keywordsTop[self.attemptNumber]
+        inpKeyStrBottom = self.keywordsBottom[self.attemptNumber]
+           
         if qmtp.QMTP.usePolar:
             if self.multiplicity == 1:
                polarString = "\n" + "\n" + "\n"+ "oldgeo polar nosym precise " + inpKeyStrBoth
@@ -179,7 +111,6 @@ class MOPACPM3InputWriter(QMInputWriter):
         self.keywords[MOPACKEYWORDS.TOP] = inpKeyStrTop
         self.keywords[MOPACKEYWORDS.BOTTOM] = inpKeyStrBottom
 
-        
         return self.keywords
     
     
