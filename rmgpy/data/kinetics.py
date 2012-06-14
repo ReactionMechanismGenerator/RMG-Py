e99c97fa4e09438b01266c1fe2d4eb79556a776a
Pierre Bhoorasingh <pierreyb@gmail.com>
2012-06-14 14:16:36 -0400

    Commented erroneous code in data/kinetics.
    The segment of code causes a fatal error in simulations where
    RMG cannot generate the proper reverse reaction. I will fix this
    bug once I integrate with the qmtp as it may change how I approach
    the bounds matrix build.

--- rmgpy/data/kinetics.py
+++ rmgpy/data/kinetics.py
@@ -2142,82 +2142,83 @@ class KineticsFamily(Database):
             # 2 reactants
             else:
                 pass
-                actionList = reaction.family.forwardRecipe.actions
-                for action in actionList:
-                    if action[0].lower() == 'form_bond':
-                        lbl1 = action[1]
-                        lbl2 = action[3]
-                    elif action[0].lower() == 'break_bond':
-                        lbl3 = action[1]
-                        lbl4 = action[3]
-                
-                # Find the atom being transferred in the reaction
-                if lbl1 == lbl3 or lbl1 == lbl4:
-                    lblAt = lbl1
-                else:
-                    lblAt = lbl2
-                
-                # Derive the bounds matrix from the reactants and products
-                try:
-                    reaction.reactants[0].getLabeledAtom(lblAt)
-                    reactant = reaction.reactants[0]
-                    reactant2 = reaction.reactants[1]
-                except ValueError:
-                    reactant = reaction.reactants[1]
-                    reactant2 = reaction.reactants[0]
-                    
-                try:
-                    reaction.products[0].getLabeledAtom(lblAt)
-                    product = reaction.products[0]
-                except ValueError:
-                    product = reaction.products[1]
-                
-                # Merge the reactants to generate the TS template
-                buildTS = reactant.merge(reactant2)
-                
-                # Check for rdkit molecules
-                if reactant.rdMol == None:
-                    Molecule.generate3dGeometry(reactant)
-                if product.rdMol == None:
-                    Molecule.generate3dGeometry(product)
-                if buildTS.rdMol == None:
-                    Molecule.generate3dGeometry(buildTS)
-                    
-                # Check for sorting labels
-                if reactant.atoms[0].sortingLabel != 0:
-                    reactant = fixSortLabel(reactant)
-                if product.atoms[0].sortingLabel != 0:
-                    product = fixSortLabel(product)
-                
-                # Generate the bounds matrices for the reactant and product with the transfered atom
-                boundsMat1 = rdkit.Chem.rdDistGeom.GetMoleculeBoundsMatrix(reactant.rdMol)
-                boundsMat2 = rdkit.Chem.rdDistGeom.GetMoleculeBoundsMatrix(product.rdMol)
-                
-                # Get the total size of the TS bounds matrix and initialize it
-                totSize = len(boundsMat1) + len(boundsMat2) - 1
-                boundsMat = numpy.ones((totSize, totSize)) * 1000
-                
-                # Add bounds matrix 1 to corresponding place of the TS bounds matrix
-                boundsMat[:len(boundsMat1),:len(boundsMat1)] = boundsMat1
-                
-                # Fill the bottom left of the bounds matrix with minima
-                boundsMat[len(boundsMat1):, :len(boundsMat1)] = numpy.ones((len(boundsMat)-len(boundsMat1), len(boundsMat1))) * 1.07
-                
-                # Add bounds matrix 2, but it has to shift to the end of bounds matrix 1, and shift 
-                # numbers for the reacting atom which has already been included from above
-                rAtLbl = reactant.getLabeledAtom(lblAt).sortingLabel
-                pAtLbl = product.getLabeledAtom(lblAt).sortingLabel
-                boundsMat[len(boundsMat1):len(boundsMat1)+pAtLbl, rAtLbl] = boundsMat2[pAtLbl, :pAtLbl]
-                boundsMat[rAtLbl, len(boundsMat1):len(boundsMat1)+pAtLbl] = boundsMat2[:pAtLbl, pAtLbl]
-                boundsMat[rAtLbl, len(boundsMat1)+pAtLbl+1:] = boundsMat2[pAtLbl, pAtLbl+1:]
-                boundsMat[len(boundsMat1)+pAtLbl+1:, rAtLbl] = boundsMat2[pAtLbl+1:, pAtLbl]
-                
-                # Remove all the parts of the transfered atom from the second bounds matrix
-                # Incorporate the rest into the TS bounds matrix
-                boundsMat2 = numpy.delete(numpy.delete(boundsMat2, pAtLbl, 1), pAtLbl, 0)
-                boundsMat[-len(boundsMat2):, -len(boundsMat2):] = boundsMat2
-                
-                #********what now!!!??!?
+                # actionList = reaction.family.forwardRecipe.actions
+                # for action in actionList:
+                #     if action[0].lower() == 'form_bond':
+                #         lbl1 = action[1]
+                #         lbl2 = action[3]
+                #     elif action[0].lower() == 'break_bond':
+                #         lbl3 = action[1]
+                #         lbl4 = action[3]
+                # 
+                # # Find the atom being transferred in the reaction
+                # if lbl1 == lbl3 or lbl1 == lbl4:
+                #     lblAt = lbl1
+                # else:
+                #     lblAt = lbl2
+                # 
+                # # Derive the bounds matrix from the reactants and products
+                # try:
+                #     reaction.reactants[0].getLabeledAtom(lblAt)
+                #     reactant = reaction.reactants[0]
+                #     reactant2 = reaction.reactants[1]
+                # except ValueError:
+                #     reactant = reaction.reactants[1]
+                #     reactant2 = reaction.reactants[0]
+                #     
+                # try:
+                #     reaction.products[0].getLabeledAtom(lblAt)
+                #     product = reaction.products[0]
+                # except ValueError:
+                #     product = reaction.products[1]
+                # 
+                # # Merge the reactants to generate the TS template
+                # import ipdb; ipdb.set_trace()
+                # buildTS = reactant.merge(reactant2)
+                # 
+                # # Check for rdkit molecules
+                # if reactant.rdMol == None:
+                #     Molecule.generate3dGeometry(reactant)
+                # if product.rdMol == None:
+                #     Molecule.generate3dGeometry(product)
+                # if buildTS.rdMol == None:
+                #     Molecule.generate3dGeometry(buildTS)
+                #     
+                # # Check for sorting labels
+                # if reactant.atoms[0].sortingLabel != 0:
+                #     reactant = fixSortLabel(reactant)
+                # if product.atoms[0].sortingLabel != 0:
+                #     product = fixSortLabel(product)
+                # 
+                # # Generate the bounds matrices for the reactant and product with the transfered atom
+                # boundsMat1 = rdkit.Chem.rdDistGeom.GetMoleculeBoundsMatrix(reactant.rdMol)
+                # boundsMat2 = rdkit.Chem.rdDistGeom.GetMoleculeBoundsMatrix(product.rdMol)
+                # 
+                # # Get the total size of the TS bounds matrix and initialize it
+                # totSize = len(boundsMat1) + len(boundsMat2) - 1
+                # boundsMat = numpy.ones((totSize, totSize)) * 1000
+                # 
+                # # Add bounds matrix 1 to corresponding place of the TS bounds matrix
+                # boundsMat[:len(boundsMat1),:len(boundsMat1)] = boundsMat1
+                # 
+                # # Fill the bottom left of the bounds matrix with minima
+                # boundsMat[len(boundsMat1):, :len(boundsMat1)] = numpy.ones((len(boundsMat)-len(boundsMat1), len(boundsMat1))) * 1.07
+                # 
+                # # Add bounds matrix 2, but it has to shift to the end of bounds matrix 1, and shift 
+                # # numbers for the reacting atom which has already been included from above
+                # rAtLbl = reactant.getLabeledAtom(lblAt).sortingLabel
+                # pAtLbl = product.getLabeledAtom(lblAt).sortingLabel
+                # boundsMat[len(boundsMat1):len(boundsMat1)+pAtLbl, rAtLbl] = boundsMat2[pAtLbl, :pAtLbl]
+                # boundsMat[rAtLbl, len(boundsMat1):len(boundsMat1)+pAtLbl] = boundsMat2[:pAtLbl, pAtLbl]
+                # boundsMat[rAtLbl, len(boundsMat1)+pAtLbl+1:] = boundsMat2[pAtLbl, pAtLbl+1:]
+                # boundsMat[len(boundsMat1)+pAtLbl+1:, rAtLbl] = boundsMat2[pAtLbl+1:, pAtLbl]
+                # 
+                # # Remove all the parts of the transfered atom from the second bounds matrix
+                # # Incorporate the rest into the TS bounds matrix
+                # boundsMat2 = numpy.delete(numpy.delete(boundsMat2, pAtLbl, 1), pAtLbl, 0)
+                # boundsMat[-len(boundsMat2):, -len(boundsMat2):] = boundsMat2
+                # 
+                # #********what now!!!??!?
                 
         # A --> B + C or A + B --> C
         else:
