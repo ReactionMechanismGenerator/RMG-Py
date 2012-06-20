ab0e5da8c56817ed80552d94e02b7cec7b9d1a97
Richard West <r.west@neu.edu>
2012-06-20 13:25:08 -0400

    Change suggested RMG_workingDirectory path due to qm package move

--- rmgpy/qm/__init__.py
+++ rmgpy/qm/__init__.py
@@ -3,5 +3,5 @@ if not os.environ.get("RMG_workingDirectory"):
     import os.path
     message = "Please set your RMG_workingDirectory environment variable.\n" +\
         "(eg. export RMG_workingDirectory=%s )" % \
-        os.path.abspath(os.path.join(os.path.dirname(__file__),'..'))
+        os.path.abspath(os.path.join(os.path.dirname(__file__),'..','..'))
     raise Exception(message)
