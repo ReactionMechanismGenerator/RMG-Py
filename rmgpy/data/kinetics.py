d76c42ae7230a88c08f16c5a03046b9510ab8bbe
Sean Troiano <stroiano7@gmail.com>
2012-08-27 13:19:41 -0400

    Minor tweak to KineticsLibrary.saveOld.writeArrhenius
    Prevented possible lack of whitespace between Arrhenius parameters,
    per recommendation by @rwest in edf92cff90d4a0e9a82e9ecbb11113dc53dab8f4.

--- rmgpy/data/kinetics.py
+++ rmgpy/data/kinetics.py
@@ -1369,7 +1369,7 @@ class KineticsLibrary(Database):
             pass
         
         def writeArrhenius(f, arrhenius):
-            f.write(' {0:<12.3E}{1:>8.3f}{2:>12.2f}    {3}{4:g} {5:g} {6:g}\n'.format(
+            f.write(' {0:<12.3E} {1:>7.3f} {2:>11.2f}    {3}{4:g} {5:g} {6:g}\n'.format(
                 arrhenius.A.value,
                 arrhenius.n.value,
                 arrhenius.Ea.value / 4.184,
