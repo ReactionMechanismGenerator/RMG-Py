c32d4adc4fa939a2ce397240aa1096e9b53c5ae3
Josh Allen <jwallen@mit.edu>
2012-09-01 12:44:34 -0400

    Added SphericalTopRotor class to rmgpy.statmech.rotation module.
    The SphericalTopRotor class represents a three-dimensional rigid rotor
    with a single rotational constant. This occurs in molecules such as
    methane with a high degree of symmetry. The NonlinearRotor class is an
    approximate generalization for nonspherical rotors.

--- rmgpy/statmech/rotation.pyx
+++ rmgpy/statmech/rotation.pyx
@@ -611,3 +611,177 @@ cdef class KRotor(Rotation):
             if densStates0 is not None:
                 densStates = schrodinger.convolve(densStates0, densStates)
         return densStates
+
+################################################################################
+
+cdef class SphericalTopRotor(Rotation):
+    """
+    A statistical mechanical model of a three-dimensional rigid rotor with a
+    single rotational constant: a spherical top. The attributes are:
+    
+    ======================== ===================================================
+    Attribute                Description
+    ======================== ===================================================
+    `inertia`                The moment of inertia of the rotor
+    `rotationalConstant`     The rotational constant of the rotor
+    `symmetry`               The symmetry number of the rotor
+    `quantum`                ``True`` to use the quantum mechanical model, ``False`` to use the classical model
+    ======================== ===================================================
+    
+    Note that the moment of inertia and the rotational constant are simply two
+    ways of representing the same quantity; only one of these can be specified
+    independently.
+
+    In the majority of chemical applications, the energies involved in the
+    rigid rotor place it very nearly in the classical limit at all relevant
+    temperatures; therefore, the classical model is used by default. 
+    """
+    
+    def __init__(self, inertia=None, symmetry=1, quantum=False, rotationalConstant=None):
+        Rotation.__init__(self, symmetry, quantum)
+        if inertia is not None and rotationalConstant is not None:
+            raise ValueError('Only one of moment of inertia and rotational constant can be specified.')
+        elif rotationalConstant is not None:
+            self.rotationalConstant = rotationalConstant
+        else:
+            self.inertia = inertia
+
+    def __repr__(self):
+        """
+        Return a string representation that can be used to reconstruct the
+        SphericalTopRotor object.
+        """
+        result = 'SphericalTopRotor(inertia={0!r}, symmetry={1:d}'.format(self.inertia, self.symmetry)
+        if self.quantum:
+            result += ', quantum=True'
+        result += ')'
+        return result
+
+    def __reduce__(self):
+        """
+        A helper function used when pickling a SphericalTopRotor object.
+        """
+        return (SphericalTopRotor, (self.inertia, self.symmetry, self.quantum, None))
+            
+    property inertia:
+        """The moment of inertia of the rotor."""
+        def __get__(self):
+            return self._inertia
+        def __set__(self, value):
+            self._inertia = quantity.Inertia(value)
+    
+    property rotationalConstant:
+        """The rotational constant of the rotor."""
+        def __get__(self):
+            cdef double I = self._inertia.value_si
+            cdef double B = constants.h / (8 * constants.pi * constants.pi * I) / (constants.c * 100.)
+            return quantity.Quantity(B,"cm^-1")
+        def __set__(self, B):
+            cdef double I
+            B = quantity.Frequency(B)
+            I = constants.h / (8 * constants.pi * constants.pi * (B.value_si * constants.c * 100.))
+            self._inertia = quantity.ScalarQuantity(I / (constants.amu * 1e-20), "amu*angstrom^2")
+
+    cpdef double getLevelEnergy(self, int J) except -1:
+        """
+        Return the energy of level `J` in kJ/mol.
+        """
+        return getRotationalConstantEnergy(self.inertia.value_si) * J * (J + 1)
+    
+    cpdef int getLevelDegeneracy(self, int J) except -1:
+        """
+        Return the degeneracy of level `J`.
+        """
+        return (2 * J + 1) * (2 * J + 1)
+    
+    cpdef double getPartitionFunction(self, double T) except -1:
+        """
+        Return the value of the partition function :math:`Q(T)` at the
+        specified temperature `T` in K.
+        """
+        cdef double B, Q, theta
+        if self.quantum:
+            Q = schrodinger.getPartitionFunction(T, self.getLevelEnergy, self.getLevelDegeneracy, 0) / self.symmetry
+        else:
+            B = getRotationalConstantEnergy(self._inertia.value_si)
+            theta = constants.R * T / B
+            Q = sqrt(theta * theta * theta * constants.pi) / self.symmetry
+        return Q
+    
+    cpdef double getHeatCapacity(self, double T) except -100000000:
+        """
+        Return the heat capacity in J/mol*K for the degree of freedom at the
+        specified temperature `T` in K.
+        """
+        cdef double Cv
+        if self.quantum:
+            Cv = schrodinger.getHeatCapacity(T, self.getLevelEnergy, self.getLevelDegeneracy, 0)
+        else:
+            Cv = 1.5
+        return Cv * constants.R
+
+    cpdef double getEnthalpy(self, double T) except 100000000:
+        """
+        Return the enthalpy in J/mol for the degree of freedom at the
+        specified temperature `T` in K.
+        """
+        cdef double H
+        if self.quantum:
+            H = schrodinger.getEnthalpy(T, self.getLevelEnergy, self.getLevelDegeneracy, 0)
+        else:
+            H = 1.5
+        return H * constants.R * T
+    
+    cpdef double getEntropy(self, double T) except -100000000:
+        """
+        Return the entropy in J/mol*K for the degree of freedom at the
+        specified temperature `T` in K.
+        """
+        cdef double S
+        if self.quantum:
+            S = schrodinger.getEntropy(T, self.getLevelEnergy, self.getLevelDegeneracy, 0) - log(self.symmetry)
+        else:
+            S = log(self.getPartitionFunction(T)) + 1.5
+        return S * constants.R
+
+    cpdef numpy.ndarray getSumOfStates(self, numpy.ndarray Elist, numpy.ndarray sumStates0=None):
+        """
+        Return the sum of states :math:`N(E)` at the specified energies `Elist`
+        in J/mol above the ground state. If an initial sum of states 
+        `sumStates0` is given, the rotor sum of states will be convoluted into
+        these states.
+        """
+        cdef double B, theta
+        cdef numpy.ndarray sumStates
+        if self.quantum:
+            sumStates = schrodinger.getSumOfStates(Elist, self.getLevelEnergy, self.getLevelDegeneracy, 0, sumStates0) / self.symmetry
+        elif sumStates0 is not None:
+            sumStates = schrodinger.convolve(sumStates0, self.getDensityOfStates(Elist, densStates0=None))
+        else:
+            if sumStates0 is not None:
+                sumStates = schrodinger.convolve(sumStates0, self.getDensityOfStates(Elist, densStates0=None))
+            else:
+                B = getRotationalConstantEnergy(self._inertia.value_si)
+                theta = B * B * B
+                sumStates = 4.0/3.0 * Elist * numpy.sqrt(Elist / theta) / self.symmetry
+        return sumStates
+    
+    cpdef numpy.ndarray getDensityOfStates(self, numpy.ndarray Elist, numpy.ndarray densStates0=None):
+        """
+        Return the density of states :math:`\\rho(E) \\ dE` at the specified
+        energies `Elist` in J/mol above the ground state. If an initial density
+        of states `densStates0` is given, the rotor density of states will be
+        convoluted into these states.
+        """
+        cdef double theta, inertia, dE
+        cdef numpy.ndarray densStates
+        if self.quantum:
+            densStates = schrodinger.getDensityOfStates(Elist, self.getLevelEnergy, self.getLevelDegeneracy, 0, densStates0) / self.symmetry
+        else:
+            dE = Elist[1] - Elist[0]
+            B = getRotationalConstantEnergy(self._inertia.value_si)
+            theta = B * B * B
+            densStates = 2.0 * numpy.sqrt(Elist / theta) / self.symmetry * dE
+            if densStates0 is not None:
+                densStates = schrodinger.convolve(densStates0, densStates)
+        return densStates
--- rmgpy/statmech/rotationTest.py
+++ rmgpy/statmech/rotationTest.py
@@ -622,3 +622,227 @@ class TestKRotor(unittest.TestCase):
         self.assertEqual(self.mode.inertia.units, mode.inertia.units)
         self.assertEqual(self.mode.symmetry, mode.symmetry)
         self.assertEqual(self.mode.quantum, mode.quantum)
+
+################################################################################
+
+class TestSphericalTopRotor(unittest.TestCase):
+    """
+    Contains unit tests of the SphericalTopRotor class.
+    """
+    
+    def setUp(self):
+        """
+        A function run before each unit test in this class.
+        """
+        self.inertia = 11.75
+        self.symmetry = 2
+        self.quantum = False
+        self.mode = SphericalTopRotor(
+            inertia = (self.inertia,"amu*angstrom^2"), 
+            symmetry = self.symmetry, 
+            quantum = self.quantum,
+        )
+        
+    def test_getRotationalConstant(self):
+        """
+        Test getting the SphericalTopRotor.rotationalConstant property.
+        """
+        Bexp = 1.434692
+        Bact = self.mode.rotationalConstant.value_si
+        self.assertAlmostEqual(Bexp, Bact, 4)
+        
+    def test_setRotationalConstant(self):
+        """
+        Test setting the SphericalTopRotor.rotationalConstant property.
+        """
+        B = self.mode.rotationalConstant
+        B.value_si *= 2
+        self.mode.rotationalConstant = B
+        Iexp = 0.5 * self.inertia
+        Iact = self.mode.inertia.value_si * constants.Na * 1e23
+        self.assertAlmostEqual(Iexp, Iact, 4)
+        
+    def test_getLevelEnergy(self):
+        """
+        Test the SphericalTopRotor.getLevelEnergy() method.
+        """
+        B = self.mode.rotationalConstant.value_si * constants.h * constants.c * 100.
+        B *= constants.Na
+        for J in range(0, 100):
+            Eexp = B * J * (J + 1)
+            Eact = self.mode.getLevelEnergy(J)
+            if J == 0:
+                self.assertEqual(Eact, 0)
+            else:
+                self.assertAlmostEqual(Eexp, Eact, delta=1e-4*Eexp)
+    
+    def test_getLevelDegeneracy(self):
+        """
+        Test the SphericalTopRotor.getLevelDegeneracy() method.
+        """
+        for J in range(0, 100):
+            gexp = (2 * J + 1)**2
+            gact = self.mode.getLevelDegeneracy(J)
+            self.assertEqual(gexp, gact, '{0} != {1}'.format(gact, gexp))
+    
+    def test_getPartitionFunction_classical(self):
+        """
+        Test the SphericalTopRotor.getPartitionFunction() method for a classical
+        rotor.
+        """
+        self.mode.quantum = False
+        Tlist = numpy.array([300,500,1000,1500,2000])
+        Qexplist = numpy.array([1552.74, 3340.97, 9449.69, 17360.2, 26727.8])
+        for T, Qexp in zip(Tlist, Qexplist):
+            Qact = self.mode.getPartitionFunction(T)
+            self.assertAlmostEqual(Qexp, Qact, delta=1e-4*Qexp)
+            
+    def test_getPartitionFunction_quantum(self):
+        """
+        Test the SphericalTopRotor.getPartitionFunction() method for a quantum
+        rotor.
+        """
+        self.mode.quantum = True
+        Tlist = numpy.array([300,500,1000,1500,2000])
+        Qexplist = numpy.array([1555.42, 3344.42, 9454.57, 17366.2, 26734.7])
+        for T, Qexp in zip(Tlist, Qexplist):
+            Qact = self.mode.getPartitionFunction(T)
+            self.assertAlmostEqual(Qexp, Qact, delta=1e-4*Qexp)
+            
+    def test_getHeatCapacity_classical(self):
+        """
+        Test the SphericalTopRotor.getHeatCapacity() method using a classical rotor.
+        """
+        self.mode.quantum = False
+        Tlist = numpy.array([300,500,1000,1500,2000])
+        Cvexplist = numpy.array([1.5, 1.5, 1.5, 1.5, 1.5]) * constants.R
+        for T, Cvexp in zip(Tlist, Cvexplist):
+            Cvact = self.mode.getHeatCapacity(T)
+            self.assertAlmostEqual(Cvexp, Cvact, delta=1e-4*Cvexp)
+    
+    def test_getHeatCapacity_quantum(self):
+        """
+        Test the SphericalTopRotor.getHeatCapacity() method using a quantum rotor.
+        """
+        self.mode.quantum = True
+        Tlist = numpy.array([300,500,1000,1500,2000])
+        Cvexplist = numpy.array([1.5, 1.5, 1.5, 1.5, 1.5]) * constants.R
+        for T, Cvexp in zip(Tlist, Cvexplist):
+            Cvact = self.mode.getHeatCapacity(T)
+            self.assertAlmostEqual(Cvexp, Cvact, delta=1e-4*Cvexp)
+       
+    def test_getEnthalpy_classical(self):
+        """
+        Test the SphericalTopRotor.getEnthalpy() method using a classical rotor.
+        """
+        self.mode.quantum = False
+        Tlist = numpy.array([300,500,1000,1500,2000])
+        Hexplist = numpy.array([1.5, 1.5, 1.5, 1.5, 1.5]) * constants.R * Tlist
+        for T, Hexp in zip(Tlist, Hexplist):
+            Hact = self.mode.getEnthalpy(T)
+            self.assertAlmostEqual(Hexp, Hact, delta=1e-4*Hexp)
+    
+    def test_getEnthalpy_quantum(self):
+        """
+        Test the SphericalTopRotor.getEnthalpy() method using a quantum rotor.
+        """
+        self.mode.quantum = True
+        Tlist = numpy.array([300,500,1000,1500,2000])
+        Hexplist = numpy.array([1.49828, 1.49897, 1.49948, 1.49966, 1.49974]) * constants.R * Tlist
+        for T, Hexp in zip(Tlist, Hexplist):
+            Hact = self.mode.getEnthalpy(T)
+            self.assertAlmostEqual(Hexp, Hact, delta=1e-4*Hexp)
+
+    def test_getEntropy_classical(self):
+        """
+        Test the SphericalTopRotor.getEntropy() method using a classical rotor.
+        """
+        self.mode.quantum = False
+        Tlist = numpy.array([300,500,1000,1500,2000])
+        Sexplist = numpy.array([8.84778, 9.61402, 10.6537, 11.2619, 11.6935]) * constants.R
+        for T, Sexp in zip(Tlist, Sexplist):
+            Sact = self.mode.getEntropy(T)
+            self.assertAlmostEqual(Sexp, Sact, delta=1e-4*Sexp)
+    
+    def test_getEntropy_quantum(self):
+        """
+        Test the SphericalTopRotor.getEntropy() method using a quantum rotor.
+        """
+        self.mode.quantum = True
+        Tlist = numpy.array([300,500,1000,1500,2000])
+        Sexplist = numpy.array([8.84778, 9.61402, 10.6537, 11.2619, 11.6935]) * constants.R
+        for T, Sexp in zip(Tlist, Sexplist):
+            Sact = self.mode.getEntropy(T)
+            self.assertAlmostEqual(Sexp, Sact, delta=1e-4*Sexp)
+
+    def test_getSumOfStates_classical(self):
+        """
+        Test the SphericalTopRotor.getSumOfStates() method using a classical rotor.
+        """
+        self.mode.quantum = False
+        Elist = numpy.arange(0, 2000*11.96, 1.0*11.96)
+        densStates = self.mode.getDensityOfStates(Elist)
+        sumStates = self.mode.getSumOfStates(Elist)
+        for n in range(20, len(Elist)):
+            self.assertAlmostEqual(numpy.sum(densStates[0:n+1]) / sumStates[n], 1.0, 1)
+
+    def test_getSumOfStates_quantum(self):
+        """
+        Test the SphericalTopRotor.getSumOfStates() method using a quantum rotor.
+        """
+        self.mode.quantum = True
+        Elist = numpy.arange(0, 2000*11.96, 1.0*11.96)
+        densStates = self.mode.getDensityOfStates(Elist)
+        sumStates = self.mode.getSumOfStates(Elist)
+        for n in range(1, len(Elist)):
+            self.assertAlmostEqual(numpy.sum(densStates[0:n+1]) / sumStates[n], 1.0, 3)
+
+    def test_getDensityOfStates_classical(self):
+        """
+        Test the SphericalTopRotor.getDensityOfStates() method using a classical
+        rotor.
+        """
+        self.mode.quantum = False
+        Tlist = numpy.array([300,400,500])
+        Elist = numpy.arange(0, 2000*11.96, 1.0*11.96)
+        for T in Tlist:
+            densStates = self.mode.getDensityOfStates(Elist)
+            Qact = numpy.sum(densStates * numpy.exp(-Elist / constants.R / T))
+            Qexp = self.mode.getPartitionFunction(T)
+            self.assertAlmostEqual(Qexp, Qact, delta=1e-2*Qexp)
+
+    def test_getDensityOfStates_quantum(self):
+        """
+        Test the SphericalTopRotor.getDensityOfStates() method using a quantum rotor.
+        """
+        self.mode.quantum = True
+        Tlist = numpy.array([300,400,500])
+        Elist = numpy.arange(0, 4000*11.96, 2.0*11.96)
+        for T in Tlist:
+            densStates = self.mode.getDensityOfStates(Elist)
+            Qact = numpy.sum(densStates * numpy.exp(-Elist / constants.R / T))
+            Qexp = self.mode.getPartitionFunction(T)
+            self.assertAlmostEqual(Qexp, Qact, delta=1e-2*Qexp)
+
+    def test_repr(self):
+        """
+        Test that a SphericalTopRotor object can be reconstructed from its
+        repr() output with no loss of information.
+        """
+        exec('mode = {0!r}'.format(self.mode))
+        self.assertAlmostEqual(self.mode.inertia.value, mode.inertia.value, 6)
+        self.assertEqual(self.mode.inertia.units, mode.inertia.units)
+        self.assertEqual(self.mode.symmetry, mode.symmetry)
+        self.assertEqual(self.mode.quantum, mode.quantum)
+        
+    def test_pickle(self):
+        """
+        Test that a SphericalTopRotor object can be pickled and unpickled
+        with no loss of information.
+        """
+        import cPickle
+        mode = cPickle.loads(cPickle.dumps(self.mode))
+        self.assertAlmostEqual(self.mode.inertia.value, mode.inertia.value, 6)
+        self.assertEqual(self.mode.inertia.units, mode.inertia.units)
+        self.assertEqual(self.mode.symmetry, mode.symmetry)
+        self.assertEqual(self.mode.quantum, mode.quantum)
