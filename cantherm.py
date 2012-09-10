a4fc8236d0285f7f272ef0f293bd3d24cda72ee0
Josh Allen <jwallen@mit.edu>
2012-09-10 09:55:37 -0400

    Updated cantherm.py script to use CanTherm class.
    This remains the route to running individual CanTherm calculations using
    standalone input files. Most of the functionality has been moved out of
    this execution script and into the rmgpy.cantherm submodule.

--- cantherm.py
+++ cantherm.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# encoding: utf-8
 
 ################################################################################
 #
-#   CanTherm
-#    
-#   Copyright (c) 2010 by Joshua W. Allen (jwallen@mit.edu)
+#   RMG - Reaction Mechanism Generator
+#
+#   Copyright (c) 2002-2009 Prof. William H. Green (whgreen@mit.edu) and the
+#   RMG Team (rmg_dev@mit.edu)
 #
 #   Permission is hereby granted, free of charge, to any person obtaining a
-#   copy of this software and associated documentation files (the 'Software'),
+#   copy of this software and associated documentation files (the "Software"),
 #   to deal in the Software without restriction, including without limitation
 #   the rights to use, copy, modify, merge, publish, distribute, sublicense,
 #   and/or sell copies of the Software, and to permit persons to whom the
@@ -17,165 +18,44 @@
 #   The above copyright notice and this permission notice shall be included in
 #   all copies or substantial portions of the Software.
 #
-#   THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 #   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 #   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 #   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #   DEALINGS IN THE SOFTWARE.
 #
 ################################################################################
 
-import argparse
-import logging
-import time
+"""
+This is the main executable script for CanTherm, a tool for computing chemical
+reaction rates and other properties used in detailed kinetics models using
+various methodologies and theories. To run CanTherm, use the command ::
+
+    $ python cantherm.py FILE
+
+where ``FILE`` is the path to a CanTherm input file describing the job to
+execute. CanTherm will run the specified job, writing the output to 
+``output.py`` and a log to both the console and to ``cantherm.log``, with both
+files appearing in the same directory as the input file. Some additional
+command-line arguments are available; run the command ::
+
+    $ python cantherm.py -h
+
+for more information.
+"""
+
+import os
 import os.path
-import numpy
+import logging
 
-################################################################################
+from rmgpy.cantherm.main import *
 
-def parseCommandLineArguments():
-    """
-    Parse the command-line arguments being passed to CanTherm.
-    """
+cantherm = CanTherm()
 
-    parser = argparse.ArgumentParser(description="""
-        CanTherm is a tool for computing the thermodynamics properties of
-        chemical species and the high-pressure-limit rate coefficients for
-        chemical reactions using the results of quantum chemistry calculations.
-    """)
-    parser.add_argument('file', metavar='FILE', type=str, nargs=1,
-        help='a job to run')
-    
-    # Option for controlling the output file location
-    parser.add_argument('-o', '--output', metavar='OUTFILE', type=str, nargs=1,
-        help='specify location of output file')
+# Parse and validate the command-line arguments
+cantherm.parseCommandLineArguments()   
 
-    # Options for controlling the amount of information printed to the console
-    # By default a moderate level of information is printed; you can either
-    # ask for less (quiet), more (verbose), or much more (debug)
-    group2 = parser.add_mutually_exclusive_group()
-    group2.add_argument('-q', '--quiet', action='store_true', help='only print warnings and errors')
-    group2.add_argument('-v', '--verbose', action='store_true', help='print more verbose output')
-
-    return parser.parse_args()
-
-################################################################################
-
-def initializeLogging(args):
-    """
-    Initialize the logging system. The level of information printed is 
-    determined by looking at the ``args.quiet`` and ``args.verbose`` attributes
-    to see if either of the corresponding flags were set. The parameter `args`
-    is an object returned by the ``argparse`` module.
-    """
-
-    # Set up logging system
-    level = logging.INFO
-    if args.quiet: 
-        level = logging.WARNING
-    elif args.verbose: 
-        level = logging.DEBUG
-
-    # Reassign the level names so that they look better on printing
-    logging.addLevelName(logging.CRITICAL, 'CRITICAL: ')
-    logging.addLevelName(logging.ERROR, 'ERROR: ')
-    logging.addLevelName(logging.WARNING, 'Warning: ')
-    logging.addLevelName(logging.INFO, '')
-    logging.addLevelName(logging.DEBUG, '')
-
-    # Create logger
-    logger = logging.getLogger()
-    logger.setLevel(level)
-
-    # Create console handler and set level to debug
-    # Also send everything to stdout rather than stderr
-    import sys
-    ch = logging.StreamHandler(sys.stdout)
-    ch.setLevel(level)
-
-    # Create formatter and add to console handler
-    formatter = logging.Formatter('%(levelname)s%(message)s')
-    ch.setFormatter(formatter)
-
-    # Remove any old handlers that might exist
-    while logger.handlers:
-        logger.removeHandler(logger.handlers[0])
-
-    # Add ch to logger
-    logger.addHandler(ch)
-
-################################################################################
-
-def execute(path, output):
-    """
-    Execute the CanTherm job located at `path` on disk, saving the output to
-    `output` on disk.
-    """
-    
-    try:
-        f = open(path)
-    except IOError, e:
-        logging.error('The input file "%s" could not be opened.' % (path))
-        logging.error('Check that the file exists and that you have read access.')
-        return
-
-    logging.info('Executing job "%s"...' % os.path.abspath(path))
-
-    # Change directory to that of the job file
-    # This way all relative paths given in the job file will be valid
-    root, ext = os.path.split(path)
-    os.chdir(root)
-
-    from rmgpy.cantherm.input import setOutputFile, setModelChemistry, hinderedRotor, loadSpecies, loadTransitionState, loadReaction, generateStates, generateThermo, generateKinetics
-    setOutputFile(output)    
-    
-    global_context = { '__builtins__': None }
-    local_context = {
-        '__builtins__': None,
-        'True': True,
-        'False': False,
-        'range': range,
-        'modelChemistry': setModelChemistry,
-        'HinderedRotor': hinderedRotor,
-        'species': loadSpecies,
-        'transitionState': loadTransitionState,
-        'reaction': loadReaction,
-        'states': generateStates,
-        'thermo': generateThermo,
-        'kinetics': generateKinetics,
-    }
-
-    try:
-        exec f in global_context, local_context
-    except (NameError, TypeError, SyntaxError), e:
-        logging.error('The input file "%s" was invalid:' % (path))
-        raise
-    finally:
-        f.close()
-
-################################################################################
-
-if __name__ == '__main__':
-    
-    # Parse the command-line arguments
-    args = parseCommandLineArguments()
-    outputDirectory = os.path.dirname(os.path.abspath(args.file[0]))
-    out = os.path.join(outputDirectory, 'output.py')
-    if args.output:
-        out = os.path.abspath(args.output[0])
-            
-    # Initialize the logging system
-    initializeLogging(args)
-    
-    # Log start timestamp
-    logging.info('CanTherm execution initiated at ' + time.asctime() + '\n')
-    
-    # Execute job
-    execute(args.file[0], out)
-
-    # Log end timestamp
-    logging.info('')
-    logging.info('CanTherm execution terminated at ' + time.asctime())
-    
+# Execute the job
+cantherm.execute()
