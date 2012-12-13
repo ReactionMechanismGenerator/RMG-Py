ef9eae7f546846adb7a9b6209c01fe83cdc6bdf8
Josh Allen <jwallen@mit.edu>
2012-12-13 11:43:40 -0500

    Restored Gaussian unit tests to working order.
    These tests now pass.

--- rmgpy/cantherm/gaussianTest.py
+++ rmgpy/cantherm/gaussianTest.py
@@ -5,8 +5,9 @@ import numpy
 import unittest
 import os
 
-from rmgpy.cantherm.gaussian import *
-from rmgpy.statmech import *
+from rmgpy.cantherm.gaussian import GaussianLog
+from rmgpy.statmech import Conformer, IdealGasTranslation, LinearRotor, NonlinearRotor, HarmonicOscillator, HinderedRotor
+import rmgpy.constants as constants
 
 ################################################################################
 
@@ -22,25 +23,26 @@ class GaussianTest(unittest.TestCase):
         molecular degrees of freedom can be properly read.
         """
 
-        log = GaussianLog(os.path.join(os.path.dirname(__file__),'ethylene.log'))
-        s = log.loadStates()
+        log = GaussianLog(os.path.join(os.path.dirname(__file__),'test','ethylene.log'))
+        conformer = log.loadConformer()
         E0 = log.loadEnergy()
         
-        self.assertTrue(len([mode for mode in s.modes if isinstance(mode,Translation)]) == 1)
-        self.assertTrue(len([mode for mode in s.modes if isinstance(mode,RigidRotor)]) == 1)
-        self.assertTrue(len([mode for mode in s.modes if isinstance(mode,HarmonicOscillator)]) == 1)
-        self.assertTrue(len([mode for mode in s.modes if isinstance(mode,HinderedRotor)]) == 0)
+        self.assertTrue(len([mode for mode in conformer.modes if isinstance(mode,IdealGasTranslation)]) == 1)
+        self.assertTrue(len([mode for mode in conformer.modes if isinstance(mode,NonlinearRotor)]) == 1)
+        self.assertTrue(len([mode for mode in conformer.modes if isinstance(mode,HarmonicOscillator)]) == 1)
+        self.assertTrue(len([mode for mode in conformer.modes if isinstance(mode,HinderedRotor)]) == 0)
 
-        trans = [mode for mode in s.modes if isinstance(mode,Translation)][0]
-        rot = [mode for mode in s.modes if isinstance(mode,RigidRotor)][0]
-        vib = [mode for mode in s.modes if isinstance(mode,HarmonicOscillator)][0]
+        trans = [mode for mode in conformer.modes if isinstance(mode,IdealGasTranslation)][0]
+        rot = [mode for mode in conformer.modes if isinstance(mode,NonlinearRotor)][0]
+        vib = [mode for mode in conformer.modes if isinstance(mode,HarmonicOscillator)][0]
         Tlist = numpy.array([298.15], numpy.float64)
-        self.assertAlmostEqual(trans.getPartitionFunction(Tlist) / 5.83338e6, 1.0, 3)
-        self.assertAlmostEqual(rot.getPartitionFunction(Tlist) / 2.59622e3, 1.0, 3)
-        self.assertAlmostEqual(vib.getPartitionFunction(Tlist) / 1.0481e0, 1.0, 3)
+        self.assertAlmostEqual(trans.getPartitionFunction(Tlist), 5.83338e6, delta=1e1)
+        self.assertAlmostEqual(rot.getPartitionFunction(Tlist), 2.59622e3, delta=1e-2)
+        self.assertAlmostEqual(vib.getPartitionFunction(Tlist), 1.0481e0, delta=1e-4)
 
-        self.assertAlmostEqual(E0 / 6.02214179e23 / 4.35974394e-18 / -78.563169, 1.0, 2)
-        self.assertEqual(s.spinMultiplicity, 1)
+        self.assertAlmostEqual(E0 / constants.Na / constants.E_h, -78.416641, 4)
+        self.assertEqual(conformer.spinMultiplicity, 1)
+        self.assertEqual(conformer.opticalIsomers, 1)
 
     def testLoadOxygenFromGaussianLog(self):
         """
@@ -48,25 +50,26 @@ class GaussianTest(unittest.TestCase):
         molecular degrees of freedom can be properly read.
         """
 
-        log = GaussianLog(os.path.join(os.path.dirname(__file__),'oxygen.log'))
-        s = log.loadStates()
+        log = GaussianLog(os.path.join(os.path.dirname(__file__),'test','oxygen.log'))
+        conformer = log.loadConformer()
         E0 = log.loadEnergy()
         
-        self.assertTrue(len([mode for mode in s.modes if isinstance(mode,Translation)]) == 1)
-        self.assertTrue(len([mode for mode in s.modes if isinstance(mode,RigidRotor)]) == 1)
-        self.assertTrue(len([mode for mode in s.modes if isinstance(mode,HarmonicOscillator)]) == 1)
-        self.assertTrue(len([mode for mode in s.modes if isinstance(mode,HinderedRotor)]) == 0)
+        self.assertTrue(len([mode for mode in conformer.modes if isinstance(mode,IdealGasTranslation)]) == 1)
+        self.assertTrue(len([mode for mode in conformer.modes if isinstance(mode,LinearRotor)]) == 1)
+        self.assertTrue(len([mode for mode in conformer.modes if isinstance(mode,HarmonicOscillator)]) == 1)
+        self.assertTrue(len([mode for mode in conformer.modes if isinstance(mode,HinderedRotor)]) == 0)
 
-        trans = [mode for mode in s.modes if isinstance(mode,Translation)][0]
-        rot = [mode for mode in s.modes if isinstance(mode,RigidRotor)][0]
-        vib = [mode for mode in s.modes if isinstance(mode,HarmonicOscillator)][0]
+        trans = [mode for mode in conformer.modes if isinstance(mode,IdealGasTranslation)][0]
+        rot = [mode for mode in conformer.modes if isinstance(mode,LinearRotor)][0]
+        vib = [mode for mode in conformer.modes if isinstance(mode,HarmonicOscillator)][0]
         Tlist = numpy.array([298.15], numpy.float64)
-        self.assertAlmostEqual(trans.getPartitionFunction(Tlist) / 7.11169e6, 1.0, 3)
-        self.assertAlmostEqual(rot.getPartitionFunction(Tlist) / 7.13316e1, 1.0, 3)
-        self.assertAlmostEqual(vib.getPartitionFunction(Tlist) / 1.000037e0, 1.0, 3)
-
-        self.assertAlmostEqual(E0 / 6.02214179e23 / 4.35974394e-18 / -150.374756, 1.0, 4)
-        self.assertEqual(s.spinMultiplicity, 3)
+        self.assertAlmostEqual(trans.getPartitionFunction(Tlist), 7.11169e6, delta=1e1)
+        self.assertAlmostEqual(rot.getPartitionFunction(Tlist), 7.13316e1, delta=1e-4)
+        self.assertAlmostEqual(vib.getPartitionFunction(Tlist), 1.00037e0, delta=1e-4)
+        
+        self.assertAlmostEqual(E0 / constants.Na / constants.E_h, -150.374756, 4)
+        self.assertEqual(conformer.spinMultiplicity, 3)
+        self.assertEqual(conformer.opticalIsomers, 1)
 
 if __name__ == '__main__':
     unittest.main( testRunner = unittest.TextTestRunner(verbosity=2) )
