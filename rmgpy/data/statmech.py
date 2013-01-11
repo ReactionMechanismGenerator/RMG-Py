f39e2898eaa1229b3739a50aaaf3d36f45594ba3
Josh Allen <jwallen@mit.edu>
2013-01-11 11:10:38 -0500

    Added (hardcoded) functionality for handling rings when generating frequencies.
    In the new implementation, if a heavy atom in the molecule is in a cycle,
    then a new functional group ringCH is incremented for each C-H bond in the
    cycle. This is done instead of descending the functional group tree to
    find the most specific group. This must be hardcoded as a special case
    because our adjacency list representation is not well equipped to handle
    "C-H bond with the carbon atom in a cycle" as a functional group.
    
    This was tested with ethylene oxide (C1OC1). A QC calculation showed that
    the lowest frequency in this molecule is ~800 cm^-1. With the old
    algorithm, the system was overspecified and contained several frequencies
    in the 300-800 cm^-1 range. Worse, the frequencies did not actually
    reflect the heat capacity they were fitted to, leading to invalid Keq
    values when running the pdep code. The new algorithm performs much better.

--- rmgpy/data/statmech.py
+++ rmgpy/data/statmech.py
@@ -319,10 +319,32 @@ class StatmechGroups(Database):
         frequencies = []
         groupCount = {}
 
+        # This is an additional hardcoded functional group for C-H with C in a ring
+        # It is hardcoded because the adjacency list format isn't well-equipped
+        # to handle this sort of functional group
+        ringCH = Entry(
+            label = 'ringCH',
+            item = None,
+            data = GroupFrequencies([(2750., 3150., 1), (900., 1100., 1)]),
+        )
+        
         # Generate estimate of thermodynamics
         for atom in molecule.atoms:
             # Iterate over heavy (non-hydrogen) atoms
-            if atom.isNonHydrogen():
+            if atom.isHydrogen(): continue
+            if molecule.isAtomInCycle(atom):
+                # Atom is in cycle
+                # Add each C-H bond to the ringCH group
+                # This is hardcoding of functional groups!
+                if atom.isCarbon():
+                    for atom2 in atom.edges:
+                        if atom2.isHydrogen():
+                            try:
+                                groupCount[ringCH] += 1
+                            except KeyError:
+                                groupCount[ringCH] = 1
+            else:
+                # Atom is not in cycle, so find a group for it
                 node = self.__getNode(molecule, {'*':atom})
                 if node is not None:
                     try:
