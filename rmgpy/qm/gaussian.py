b80c5b6dc3c1e870aef01f0ef624a066b76441bb
Pierre Bhoorasingh <pierreyb@gmail.com>
2013-01-27 23:21:21 -0500

    Added pm6 and pm7 calculations

--- rmgpy/qm/gaussian.py
+++ rmgpy/qm/gaussian.py
@@ -228,3 +228,95 @@ class GaussianMolPM3(GaussianMol):
         if attempt > self.scriptAttempts:
             attempt -= self.scriptAttempts
         return self.keywords[attempt-1]
+
+
+
+class GaussianMolPM6(GaussianMol):
+
+    #: Keywords that will be added at the top of the qm input file
+    keywords = [
+               "# pm6 opt=(verytight,gdiis) freq IOP(2/16=3)",
+               "# pm6 opt=(verytight,gdiis) freq IOP(2/16=3) IOP(4/21=2)",
+               "# pm6 opt=(verytight,calcfc,maxcyc=200) freq IOP(2/16=3) nosymm" ,
+               "# pm6 opt=(verytight,calcfc,maxcyc=200) freq=numerical IOP(2/16=3) nosymm",
+               "# pm6 opt=(verytight,gdiis,small) freq IOP(2/16=3)",
+               "# pm6 opt=(verytight,nolinear,calcfc,small) freq IOP(2/16=3)",
+               "# pm6 opt=(verytight,gdiis,maxcyc=200) freq=numerical IOP(2/16=3)",
+               "# pm6 opt=tight freq IOP(2/16=3)",
+               "# pm6 opt=tight freq=numerical IOP(2/16=3)",
+               "# pm6 opt=(tight,nolinear,calcfc,small,maxcyc=200) freq IOP(2/16=3)",
+               "# pm6 opt freq IOP(2/16=3)",
+               "# pm6 opt=(verytight,gdiis) freq=numerical IOP(2/16=3) IOP(4/21=200)",
+               "# pm6 opt=(calcfc,verytight,newton,notrustupdate,small,maxcyc=100,maxstep=100) freq=(numerical,step=10) IOP(2/16=3) nosymm",
+               "# pm6 opt=(tight,gdiis,small,maxcyc=200,maxstep=100) freq=numerical IOP(2/16=3) nosymm",
+               "# pm6 opt=(tight,gdiis,small,maxcyc=200,maxstep=100) freq=numerical IOP(2/16=3) nosymm",
+               "# pm6 opt=(verytight,gdiis,calcall,small,maxcyc=200) IOP(2/16=3) IOP(4/21=2) nosymm",
+               "# pm6 opt=(verytight,gdiis,calcall,small) IOP(2/16=3) nosymm",
+               "# pm6 opt=(calcall,small,maxcyc=100) IOP(2/16=3)",
+               ]
+
+    @property
+    def scriptAttempts(self):
+        "The number of attempts with different script keywords"
+        return len(self.keywords)
+
+    @property
+    def maxAttempts(self):
+        "The total number of attempts to try"
+        return 2 * len(self.keywords)
+
+    def inputFileKeywords(self, attempt):
+        """
+        Return the top keywords for attempt number `attempt`.
+
+        NB. `attempt`s begin at 1, not 0.
+        """
+        assert attempt <= self.maxAttempts
+        if attempt > self.scriptAttempts:
+            attempt -= self.scriptAttempts
+        return self.keywords[attempt-1]
+        
+class GaussianMolMP2(GaussianMol):
+
+    #: Keywords that will be added at the top of the qm input file
+    keywords = [
+               "# mp2 opt=(verytight,gdiis) freq IOP(2/16=3)",
+               "# mp2 opt=(verytight,gdiis) freq IOP(2/16=3) IOP(4/21=2)",
+               "# mp2 opt=(verytight,calcfc,maxcyc=200) freq IOP(2/16=3) nosymm" ,
+               "# mp2 opt=(verytight,calcfc,maxcyc=200) freq=numerical IOP(2/16=3) nosymm",
+               "# mp2 opt=(verytight,gdiis,small) freq IOP(2/16=3)",
+               "# mp2 opt=(verytight,nolinear,calcfc,small) freq IOP(2/16=3)",
+               "# mp2 opt=(verytight,gdiis,maxcyc=200) freq=numerical IOP(2/16=3)",
+               "# mp2 opt=tight freq IOP(2/16=3)",
+               "# mp2 opt=tight freq=numerical IOP(2/16=3)",
+               "# mp2 opt=(tight,nolinear,calcfc,small,maxcyc=200) freq IOP(2/16=3)",
+               "# mp2 opt freq IOP(2/16=3)",
+               "# mp2 opt=(verytight,gdiis) freq=numerical IOP(2/16=3) IOP(4/21=200)",
+               "# mp2 opt=(calcfc,verytight,newton,notrustupdate,small,maxcyc=100,maxstep=100) freq=(numerical,step=10) IOP(2/16=3) nosymm",
+               "# mp2 opt=(tight,gdiis,small,maxcyc=200,maxstep=100) freq=numerical IOP(2/16=3) nosymm",
+               "# mp2 opt=(tight,gdiis,small,maxcyc=200,maxstep=100) freq=numerical IOP(2/16=3) nosymm",
+               "# mp2 opt=(verytight,gdiis,calcall,small,maxcyc=200) IOP(2/16=3) IOP(4/21=2) nosymm",
+               "# mp2 opt=(verytight,gdiis,calcall,small) IOP(2/16=3) nosymm",
+               "# mp2 opt=(calcall,small,maxcyc=100) IOP(2/16=3)",
+               ]
+
+    @property
+    def scriptAttempts(self):
+        "The number of attempts with different script keywords"
+        return len(self.keywords)
+
+    @property
+    def maxAttempts(self):
+        "The total number of attempts to try"
+        return 2 * len(self.keywords)
+
+    def inputFileKeywords(self, attempt):
+        """
+        Return the top keywords for attempt number `attempt`.
+
+        NB. `attempt`s begin at 1, not 0.
+        """
+        assert attempt <= self.maxAttempts
+        if attempt > self.scriptAttempts:
+            attempt -= self.scriptAttempts
+        return self.keywords[attempt-1]
