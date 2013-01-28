ef6c37ede325a0dc4733079b241649f616c6aceb
Belinda Slakman <slakman.b@husky.neu.edu>
2013-01-28 18:21:00 -0500

    Corrected some errors to solvation module and its test class.

--- rmgpy/data/solvationTest.py
+++ rmgpy/data/solvationTest.py
@@ -22,16 +22,17 @@ class TestSoluteDatabase(TestCase):
 		self.database.load(os.path.join(settings['database.directory'], 'thermo'))
 		
 		self.testCases = [
-		
-		['cyclopentane',		'C1CCCC1',		0.10,	0,	0.263,	2.477,	0],
-		['methylcyclopentane',	'C1(CCCC1)C',	0.10,	0,	0.225,	2.816,	0],
-		['cyclohexane',			'C1CCCCC1',		0.10,	0,	0.305,	2.964,	0]
-		
+		['octane', 'CCCCCCCC', 0.127,	0.085, 0.04,	3.766,	0.0030	, 1.2358], # from RMG-Java
+		#['cyclopentane',		'C1CCCC1',		0.10,	0,	0.263,	2.477,	0],
+		#['methylcyclopentane',	'C1(CCCC1)C',	0.10,	0,	0.225,	2.816,	0],
+		#['cyclohexane',			'C1CCCCC1',		0.10,	0,	0.305,	2.964,	0]
+		#
 		]
 		
-		for name, smiles, S, B, E, L, A in self.testCases:
+		for name, smiles, S, B, E, L, A, V in self.testCases:
 			species = Species(molecule=[Molecule(SMILES=smiles)])
 			soluteData = self.database.getSoluteData(Species(molecule=[species.molecule[0]]))
+			print name, soluteData
 			print self.assertEqual(soluteData.S, S)
 			print self.assertEqual(soluteData.B, B)
 			print self.assertEqual(soluteData.E, E)
