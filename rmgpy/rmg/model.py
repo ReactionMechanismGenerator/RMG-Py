1ef8c6e668cc58f5e48b049fc9cdfa819408e79a
Beat Buesser <bbuesser@mit.edu>
2013-05-06 11:17:15 -0400

    Added the function saveChemkinFileEdge.
    This function writes for debugging the molecules in the edge into a
    ChemkinFile similar to the function saveChemkinFile for the core
    molecules but with additional information.

--- rmgpy/rmg/model.py
+++ rmgpy/rmg/model.py
@@ -1488,3 +1488,16 @@ class CoreEdgeReactionModel:
         saveChemkinFile(verbose_path, speciesList, rxnList, verbose = True)
         if dictionaryPath:
             saveSpeciesDictionary(dictionaryPath, speciesList)
+            
+    def saveChemkinFileEdge(self, path, verbose_path, dictionaryPath=None):
+        """
+        Save a Chemkin file for the current model edge as well as any desired output
+        species and reactions to `path`.
+        """
+        from rmgpy.chemkin import saveChemkinFile, saveSpeciesDictionaryEdge
+        speciesList = self.edge.species + self.outputSpeciesList
+        rxnList = self.edge.reactions + self.outputReactionList
+        saveChemkinFile(path, speciesList, rxnList, verbose = False)
+        saveChemkinFile(verbose_path, speciesList, rxnList, verbose = True)
+        if dictionaryPath:
+            saveSpeciesDictionaryEdge(dictionaryPath, speciesList)
