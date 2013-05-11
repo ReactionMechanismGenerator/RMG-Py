5509575dba49df391df06d5ef6cb779588ae2f17
Richard West <r.west@neu.edu>
2013-05-11 15:09:25 -0400

    Tweak error message to help debugging.
    Also tweak a docstring.

--- rmgpy/rmg/model.py
+++ rmgpy/rmg/model.py
@@ -514,7 +514,8 @@ class CoreEdgeReactionModel:
 
     def enlarge(self, newObject):
         """
-        Enlarge a reaction model by processing `newObject`. If `newObject` is a
+        Enlarge a reaction model by processing the objects in the list `newObject`. 
+        If `newObject` is a
         :class:`rmg.species.Species` object, then the species is moved from
         the edge to the core and reactions generated for that species, reacting
         with itself and with all other species in the model core. If `newObject`
@@ -576,7 +577,7 @@ class CoreEdgeReactionModel:
                 self.processNewReactions(newReactions, newSpecies, pdepNetwork)
     
             else:
-                raise TypeError('Unable to use object {0} to enlarge reaction model; expecting an object of class rmg.model.Species or rmg.model.PDepNetwork.'.format(obj))
+                raise TypeError('Unable to use object {0} to enlarge reaction model; expecting an object of class rmg.model.Species or rmg.model.PDepNetwork, not {1}'.format(obj, obj.__class__))
 
             # If there are any core species among the unimolecular product channels
             # of any existing network, they need to be made included
