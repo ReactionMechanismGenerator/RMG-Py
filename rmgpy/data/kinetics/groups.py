723d73caf00261442075eea925e8540dfd04e9b9
Richard West <r.west@neu.edu>
2013-06-19 14:00:17 -0400

    Add missing imports necessary for kinetics database group fitting.
    I guess these were only used in bits of the code that are called from
    RMG-database/kineticsGroups.py, but they're definitely needed for that.

--- rmgpy/data/kinetics/groups.py
+++ rmgpy/data/kinetics/groups.py
@@ -39,6 +39,8 @@ import os.path
 import re
 import logging
 import codecs
+import math
+import numpy
 from copy import copy, deepcopy
 
 from rmgpy.data.base import *
@@ -50,7 +52,7 @@ from rmgpy.kinetics import Arrhenius, ArrheniusEP, ThirdBody, Lindemann, Troe, \
                            Chebyshev, KineticsData, PDepKineticsModel
 from rmgpy.molecule import Bond, GroupBond, Group
 from rmgpy.species import Species
-
+from rmgpy.quantity import constants
 from .common import KineticsError, UndeterminableKineticsError, saveEntry
 
 ################################################################################
