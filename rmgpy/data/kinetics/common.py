0f1f39be294e99a38a67cb39e16b715dd19a573e
Richard West <r.west@neu.edu>
2013-06-28 21:41:18 -0400

    Remove old (and broken) code for saving distances in a dict() format.
    The whole entry.data[entry.data.keys()[0]][0] thing is broken anyway.
    This shouldn't be needed now the TS_groups.py is in the new format.

--- rmgpy/data/kinetics/common.py
+++ rmgpy/data/kinetics/common.py
@@ -182,13 +182,6 @@ def saveEntry(f, entry):
     elif isinstance(entry.data, DistanceData):
         distances = prettify(repr(entry.data))
         f.write('    distances = {0},\n'.format(distances.replace('\n', '\n    ')))
-    elif isinstance(entry.data, dict):
-        distances = entry.data[entry.data.keys()[0]][0]
-        f.write('    distances = {\n')
-        f.write('        "d12": {0},\n'.format(distances[0]))
-        f.write('        "d23": {0},\n'.format(distances[2]))
-        f.write('        "d13": {0},\n'.format(distances[1]))
-        f.write('    },\n')
     elif entry.data is not None:
         efficiencies = None
         if hasattr(entry.data, 'efficiencies'):
