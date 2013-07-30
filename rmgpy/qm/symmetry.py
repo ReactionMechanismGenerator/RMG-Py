1d52f0e27522fcbf276cef07894f1d84e15c249c
Richard West <r.west@neu.edu>
2013-07-30 15:39:35 -0400

    Tweak to symmetry logging. See issue #130

--- rmgpy/qm/symmetry.py
+++ rmgpy/qm/symmetry.py
@@ -189,7 +189,6 @@ class SymmetryJob:
         else:
             logging.exception("Couldn't find point group from symmetry output:\n{0}".format(output))
             return "Not found"
-            raise RuntimeError("Couldn't find point group in symmetry output.")
 
         logging.info("Point group: "+ result)
         return result;
@@ -251,6 +250,8 @@ class SymmetryJob:
                 return pointGroupDictionary[pointGroupName]
             else:
                 logging.info("Attempt number {0} did not identify a recognized point group ({1}).".format(attempt,pointGroupName))
+                if attempt+2 == len(self.argumentsList):
+                    logging.warning("Using last-resort symmetry estimation options; symmetry may be underestimated.")
         logging.critical("Final attempt did not identify a recognized point group. Exiting.")
         return None
 
