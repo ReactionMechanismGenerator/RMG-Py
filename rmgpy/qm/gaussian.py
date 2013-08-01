acaa0325ab2ca5fa3bcd4122fde6f7de19412a57
Pierre Bhoorasingh <pierreyb@gmail.com>
2013-08-01 16:52:38 -0400

    Created Gaussian transition state geometry estimator in qm.
    It worked as a script and it has been cleaned up somewhat,
    but it needs testing. This generates a geometry from distance
    geometry and group estimates (see tsGroups branch in RMG-database).
    
    It has NOT been linked to RMG as yet to work on generated
    reactions as it is not quite ready for that as yet.

--- rmgpy/qm/gaussian.py
+++ rmgpy/qm/gaussian.py
@@ -7,6 +7,7 @@ from subprocess import Popen, PIPE
 
 from qmdata import CCLibData
 from molecule import QMMolecule
+from reaction import QMReaction
 
 class Gaussian:
     """
@@ -28,13 +29,6 @@ class Gaussian:
 
     usePolar = False
     
-    #: List of phrases that indicate failure
-    #: NONE of these must be present in a succesful job.
-    failureKeys = [
-                   'ERROR TERMINATION',
-                   'IMAGINARY FREQUENCIES'
-                   ]
-    
     #: List of phrases to indicate success.
     #: ALL of these must be present in a successful job.
     successKeys = [
@@ -54,6 +48,83 @@ class Gaussian:
         
         return self.verifyOutputFile()
         
+    def parse(self):
+        """
+        Parses the results of the Gaussian calculation, and returns a CCLibData object.
+        """
+        parser = cclib.parser.Gaussian(self.outputFilePath)
+        parser.logger.setLevel(logging.ERROR) #cf. http://cclib.sourceforge.net/wiki/index.php/Using_cclib#Additional_information
+        cclibData = parser.parse()
+        radicalNumber = sum([i.radicalElectrons for i in self.molecule.atoms])
+        qmData = CCLibData(cclibData, radicalNumber+1)
+        return qmData
+    
+    
+    
+class GaussianMol(QMMolecule, Gaussian):
+    """
+    A base Class for calculations of molecules using Gaussian. 
+    
+    Inherits from both :class:`QMMolecule` and :class:`Gaussian`.
+    """
+    
+    #: List of phrases that indicate failure
+    #: NONE of these must be present in a succesful job.
+    failureKeys = [
+                   'ERROR TERMINATION',
+                   'IMAGINARY FREQUENCIES'
+                   ]
+    
+    def writeInputFile(self, attempt):
+        """
+        Using the :class:`Geometry` object, write the input file
+        for the `attmept`th attempt.
+        """
+    
+        obConversion = openbabel.OBConversion()
+        obConversion.SetInAndOutFormats("mol", "gjf")
+        mol = openbabel.OBMol()
+    
+        obConversion.ReadFile(mol, self.getMolFilePathForCalculation(attempt) )
+    
+        mol.SetTitle(self.geometry.uniqueIDlong)
+        obConversion.SetOptions('k', openbabel.OBConversion.OUTOPTIONS)
+        input_string = obConversion.WriteString(mol)
+        top_keys = self.inputFileKeywords(attempt)
+        with open(self.inputFilePath, 'w') as gaussianFile:
+            gaussianFile.write(top_keys)
+            gaussianFile.write(input_string)
+            gaussianFile.write('\n')
+            if self.usePolar:
+                gaussianFile.write('\n\n\n')
+                gaussianFile.write(polar_keys)
+    
+    def inputFileKeywords(self, attempt):
+        """
+        Return the top keywords.
+        """
+        raise NotImplementedError("Should be defined by subclass, eg. GaussianMolPM3")
+    
+    def generateQMData(self):
+        """
+        Calculate the QM data and return a QMData object.
+        """
+        self.createGeometry()
+        if self.verifyOutputFile():
+            logging.info("Found a successful output file already; using that.")
+        else:
+            success = False
+            for attempt in range(1, self.maxAttempts+1):
+                self.writeInputFile(attempt)
+                success = self.run()
+                if success:
+                    logging.info('Attempt {0} of {1} on species {2} succeeded.'.format(attempt, self.maxAttempts, self.molecule.toAugmentedInChI()))
+                    break
+            else:
+                raise Exception('QM thermo calculation failed for {0}.'.format(self.molecule.toAugmentedInChI()))
+        result = self.parse() # parsed in cclib
+        return result
+    
     def verifyOutputFile(self):
         """
         Check's that an output file exists and was successful.
@@ -124,82 +195,10 @@ class Gaussian:
         
         #InChIs do not match (most likely due to limited name length mirrored in log file (240 characters), but possibly due to a collision)
         return self.checkForInChiKeyCollision(logFileInChI) # Not yet implemented!
-        
-    def parse(self):
-        """
-        Parses the results of the Gaussian calculation, and returns a CCLibData object.
-        """
-        parser = cclib.parser.Gaussian(self.outputFilePath)
-        parser.logger.setLevel(logging.ERROR) #cf. http://cclib.sourceforge.net/wiki/index.php/Using_cclib#Additional_information
-        cclibData = parser.parse()
-        radicalNumber = sum([i.radicalElectrons for i in self.molecule.atoms])
-        qmData = CCLibData(cclibData, radicalNumber+1)
-        return qmData
-    
-    
-    
-class GaussianMol(QMMolecule, Gaussian):
-    """
-    A base Class for calculations of molecules using Gaussian. 
-    
-    Inherits from both :class:`QMMolecule` and :class:`Gaussian`.
-    """
-    
-    def writeInputFile(self, attempt):
-        """
-        Using the :class:`Geometry` object, write the input file
-        for the `attmept`th attempt.
-        """
-    
-        obConversion = openbabel.OBConversion()
-        obConversion.SetInAndOutFormats("mol", "gjf")
-        mol = openbabel.OBMol()
-    
-        obConversion.ReadFile(mol, self.getMolFilePathForCalculation(attempt) )
-    
-        mol.SetTitle(self.geometry.uniqueIDlong)
-        obConversion.SetOptions('k', openbabel.OBConversion.OUTOPTIONS)
-        input_string = obConversion.WriteString(mol)
-        top_keys = self.inputFileKeywords(attempt)
-        with open(self.inputFilePath, 'w') as gaussianFile:
-            gaussianFile.write(top_keys)
-            gaussianFile.write(input_string)
-            gaussianFile.write('\n')
-            if self.usePolar:
-                gaussianFile.write('\n\n\n')
-                gaussianFile.write(polar_keys)
-    
-    def inputFileKeywords(self, attempt):
-        """
-        Return the top keywords.
-        """
-        raise NotImplementedError("Should be defined by subclass, eg. GaussianMolPM3")
-    
-    def generateQMData(self):
-        """
-        Calculate the QM data and return a QMData object.
-        """
-        self.createGeometry()
-        if self.verifyOutputFile():
-            logging.info("Found a successful output file already; using that.")
-        else:
-            success = False
-            for attempt in range(1, self.maxAttempts+1):
-                self.writeInputFile(attempt)
-                success = self.run()
-                if success:
-                    logging.info('Attempt {0} of {1} on species {2} succeeded.'.format(attempt, self.maxAttempts, self.molecule.toAugmentedInChI()))
-                    break
-            else:
-                raise Exception('QM thermo calculation failed for {0}.'.format(self.molecule.toAugmentedInChI()))
-        result = self.parse() # parsed in cclib
-        return result
-    
-
 
 class GaussianMolPM3(GaussianMol):
 
-    #: Keywords that will be added at the top of the qm input file
+    #: Keywords that will be added at the top of the qm input files
     keywords = [
                "# pm3 opt=(verytight,gdiis) freq IOP(2/16=3)",
                "# pm3 opt=(verytight,gdiis) freq IOP(2/16=3) IOP(4/21=2)",
@@ -333,3 +332,228 @@ class GaussianMolMP2(GaussianMol):
         if attempt > self.scriptAttempts:
             attempt -= self.scriptAttempts
         return self.keywords[attempt-1]
+
+##########################################################################################
+
+class GaussianTS(QMReaction, Gaussian):
+    """
+    A base Class for calculations of transition states using Gaussian. 
+
+    Inherits from both :class:`QMReaction` and :class:`Gaussian`.
+    """
+    
+    
+    #: List of phrases that indicate failure
+    #: NONE of these must be present in a succesful job.
+    failureKeys = [
+                   'ERROR TERMINATION',
+                   ]
+    
+    def writeInputFile(self, attempt):
+        """
+        Using the :class:`Geometry` object, write the input file
+        for the `attmept`th attempt.
+        """
+        chk_file = '%chk=' + self.uniqueID
+        obConversion = openbabel.OBConversion()
+        obConversion.SetInAndOutFormats("mol", "gjf")
+        mol = openbabel.OBMol()
+        
+        obConversion.ReadFile(mol, self.geometry.getRefinedMolFilePath() )
+        
+        mol.SetTitle(self.uniqueID)
+        obConversion.SetOptions('k', openbabel.OBConversion.OUTOPTIONS)
+        input_string = obConversion.WriteString(mol)
+        numProc = '%nprocshared=' + '4' # could be something that could be set in the qmSettings
+        top_keys = self.keywords[0]
+        title = ' ' + self.uniqueID
+        with open(self.inputFilePath, 'w') as gaussianFile:
+            gaussianFile.write(numProc)
+            gaussianFile.write('\n')
+            gaussianFile.write(chk_file)
+            gaussianFile.write('\n')
+            gaussianFile.write(top_keys)
+            gaussianFile.write(input_string)
+    
+    def writeIRCFile(self):
+        """
+        Using the :class:`Geometry` object, write the input file for the 
+        IRC calculation on the transition state. The geometry is taken 
+        from the checkpoint file created during the geometry search.
+        """
+        chk_file = '%chk=' + self.uniqueID
+        numProc = '%nprocshared=' + '4' # could be something that could be set in the qmSettings
+        top_keys = self.keywords[2]
+        chrgMult = '1 2'
+        with open(self.inputFilePath, 'w') as gaussianFile:
+            gaussianFile.write(numProc)
+            gaussianFile.write('\n')
+            gaussianFile.write(chk_file)
+            gaussianFile.write('\n')
+            gaussianFile.write(top_keys)
+            gaussianFile.write('\n\n')
+            gaussianFile.write(chrgMult)
+            gaussianFile.write('\n\n')
+            
+    def inputFileKeywords(self, attempt):
+        """
+        Return the top keywords.
+        """
+        raise NotImplementedError("Should be defined by subclass, eg. GaussianTSM062X")
+
+    def generateQMKinetics(self):
+        """
+        Calculate the QM data and return a QMData object.
+        """
+        self.createGeometry()
+        if self.verifyOutputFile():
+            logging.info("Found a successful output file already; using that.")
+        else:
+            success = False
+            for attempt in range(1, self.maxAttempts+1):
+                self.writeInputFile(attempt)
+                self.writeIRCFile()
+                success = self.run()
+                if success:
+                    logging.info('Attempt {0} of {1} on species {2} succeeded.'.format(attempt, self.maxAttempts, self.molecule.toAugmentedInChI()))
+                    break
+            else:
+                raise Exception('QM thermo calculation failed for {0}.'.format(self.molecule.toAugmentedInChI()))
+        result = self.parse() # parsed in cclib
+        return result
+    
+    def verifyOutputFile(self):
+        """
+        Check's that an output file exists and was successful.
+        
+        Returns a boolean flag that states whether a successful GAUSSIAN simulation already exists for the molecule with the 
+        given (augmented) InChI Key.
+        
+        The definition of finding a successful simulation is based on these criteria:
+        1) finding an output file with the file name equal to the the reaction unique ID
+        2) NOT finding any of the keywords that are denote a calculation failure
+        3) finding all the keywords that denote a calculation success.
+        
+        If any of the above criteria is not matched, False will be returned and the procedures to start a new calculation 
+        will be initiated.
+        """
+        if not os.path.exists(self.outputFilePath):
+            logging.info("Output file {0} does not exist.".format(self.outputFilePath))
+            return False
+        
+        # Initialize dictionary with "False"s 
+        successKeysFound = dict([(key, False) for key in self.successKeys])
+        
+        with open(self.outputFilePath) as outputFile:
+            for line in outputFile:
+                line = line.strip()
+                
+                for element in self.failureKeys: #search for failure keywords
+                    if element in line:
+                        logging.error("Gaussian output file contains the following error: {0}".format(element) )
+                        return False
+                    
+                for element in self.successKeys: #search for success keywords
+                    if element in line:
+                        successKeysFound[element] = True
+        
+        # Check that ALL 'success' keywords were found in the file.
+        if not all( successKeysFound.values() ):
+            logging.error('Not all of the required keywords for sucess were found in the output file!')
+            return False
+        else:
+            return True
+    
+    def verifyTSGeometry(self):
+        """
+        Check's that the resulting geometries of the path analysis match the reaction.
+        """
+        
+        """
+        Compares IRC geometries to input geometries.
+        """
+        # Search IRC output for steps on each side of the path
+        readFile = file(ircOutput)
+        pth1 = list()
+        steps = list()
+        for line in readFile.readlines():
+            if line.startswith(' Point Number:'):
+                if int(line.split()[2]) > 0:
+                    if int(line.split()[-1]) == 1:
+                        ptNum = int(line.split()[2])
+                        pth1.append(ptNum)
+                    else:
+                        pass
+            elif line.startswith('  # OF STEPS ='):
+                numStp = int(line.split()[-1])
+                steps.append(numStp)
+        
+        # This indexes the coordinate to be used from the parsing
+        if steps == []:
+            notes = ' IRC failed '
+            return 0, notes
+        else:
+            pth1End = sum(steps[:pth1[-1]])		
+            # Compare the reactants and products
+            ircParse = external.cclib.parser.Gaussian(ircOutput)
+            ircParse = ircParse.parse()
+        
+            atomnos = ircParse.atomnos
+            atomcoords = ircParse.atomcoords
+        
+            # Convert the IRC geometries into RMG molecules
+            # We don't know which is reactant or product, so take the two at the end of the
+            # paths and compare to the reactants and products
+        
+            mol1 = fromXYZ(atomcoords[pth1End], atomnos)
+            mol2 = fromXYZ(atomcoords[-1], atomnos)
+            
+            # Had trouble with isIsomorphic, but resetting the connectivity seems to fix it (WHY??).
+            reactant.resetConnectivityValues()
+            product.resetConnectivityValues()
+            mol1.resetConnectivityValues()
+            mol2.resetConnectivityValues()
+            
+            if reactant.isIsomorphic(mol1) and product.isIsomorphic(mol2):
+                    notes = 'Verified TS'
+                    return 1, notes
+            elif reactant.isIsomorphic(mol2) and product.isIsomorphic(mol1):
+                    notes = 'Verified TS'
+                    return 1, notes
+            else:
+                notes = 'Saddle found, but wrong one'
+                return 0, notes
+        
+class GaussianTSM062X(GaussianTS):
+
+    #: Keywords that will be added at the top of the qm input file
+    keywords = [
+               "# m062x/6-31+g(d,p) opt=(ts,calcall,tight,noeigentest)  int=ultrafine nosymm",
+               "# m062x/6-31+g(d,p) opt=(ts,calcall,noeigentest) nosymm",
+               "# m062x/6-31+g(d,p) irc=(calcall,report=read) geom=allcheck guess=check nosymm",
+               ]
+    """
+    This needs some work, to determine options that are best used. Commented out the
+    methods for now.
+    """
+    
+    # @property
+    # def scriptAttempts(self):
+    #     "The number of attempts with different script keywords"
+    #     return len(self.keywords)
+    # 
+    # @property
+    # def maxAttempts(self):
+    #     "The total number of attempts to try"
+    #     return 2 * len(self.keywords)
+    # 
+    # def inputFileKeywords(self, attempt):
+    #     """
+    #     Return the top keywords for attempt number `attempt`.
+    # 
+    #     NB. `attempt`s begin at 1, not 0.
+    #     """
+    #     assert attempt <= self.maxAttempts
+    #     if attempt > self.scriptAttempts:
+    #         attempt -= self.scriptAttempts
+    #     return self.keywords[attempt-1]
--- rmgpy/qm/reaction.py
+++ rmgpy/qm/reaction.py
@@ -1,11 +1,19 @@
 import os
 import logging
+import external.cclib.parser
+import openbabel
+import time
+from subprocess import Popen
+from copy import deepcopy
 import numpy
 
 from rmgpy.molecule import Molecule
 from rmgpy.species import TransitionState
 from molecule import QMMolecule, Geometry
 from collections import defaultdict
+from rmgpy.data.base import Entry
+from rmgpy.data.kinetics import KineticsFamily, ReactionRecipe, saveEntry
+from rmgpy.data.kinetics.transitionstates import TransitionStates, DistanceData
 
 try:
     import rdkit
@@ -13,6 +21,9 @@ try:
     from rdkit.Chem.Pharm3D import EmbedLib
 except ImportError:
     logging.info("To use transition state searches, you must correctly install rdkit")
+
+transitionStates = TransitionStates()
+transitionStates.load(os.path.join(os.getenv('HOME'), 'Code/RMG-database/input/kinetics/families/H_Abstraction'), None, None)
 
 class QMReaction:
     
@@ -25,9 +36,22 @@ class QMReaction:
         self.reaction = reaction
         self.settings = settings
         
+        species = []
+        for molecule in self.reaction.reactants:
+            species.append(molecule.toSMILES())
+        for molecule in self.reaction.products:
+            species.append(molecule.toSMILES())
+        
+        species.sort()
+        stringID = ''
+        for marker in species:
+            stringID = stringID + marker
+        
+        self.uniqueID = stringID
+        
         self.geometry = None
         self.transitionState = None
-        
+    
     def getFilePath(self, extension):
         """
         Should return the path to the file with the given extension.
@@ -37,8 +61,17 @@ class QMReaction:
         Need to define some reaction line notation.
         Possibly '<Reaction_Family>/<reactant1SMILES>+<reactant2SMILES>--<product1SMILES>+<product2SMILES>' ???
         """
-        raise NotImplementedError("This should be a unique string representing the reaction")
         return os.path.join(self.settings.fileStore, self.uniqueID  + extension)
+    
+    @property
+    def outputFilePath(self):
+        """Get the output file name."""
+        return self.getFilePath(self.outputFileExtension)
+    
+    @property
+    def inputFilePath(self):
+        """Get the input file name."""
+        return self.getFilePath(self.inputFileExtension)
         
     def fixSortLabel(self, molecule):
         """
@@ -69,37 +102,31 @@ class QMReaction:
         
         return rdKitMol
         
-    def generateBoundsMatrix(self, molecule, settings):
+    def generateBoundsMatrix(self, molecule):
         """
         Uses rdkit to generate the bounds matrix of a rdkit molecule.
         """
-        self.geometry, multiplicity = getGeometry(molecule, settings)
-        rdKitMol = getRDKitMol(self.geometry)
+        self.geometry, multiplicity = self.getGeometry(molecule, self.settings)
+        rdKitMol = self.getRDKitMol(self.geometry)
         boundsMatrix = rdkit.Chem.rdDistGeom.GetMoleculeBoundsMatrix(rdKitMol)
         
         return rdKitMol, boundsMatrix, multiplicity
-        
-    def generateGeometry(self):
-        quantumMechanics = QMCalculator()
-        quantumMechanics.settings.software = 'gaussian'
-        quantumMechanics.settings.fileStore = 'QMfiles'
-        quantumMechanics.settings.scratchDirectory = 'scratch'
-        quantumMechanics.settings.onlyCyclics = False
-        quantumMechanics.settings.maxRadicalNumber = 0
-        
-        
-        notes = ''
-        
-        reactant = fixSortLabel(TS[0])
-        product = fixSortLabel(TS[1])
-        
-        rRDMol, tsBM, tsMult = generateBoundsMatrix(reactant, quantumMechanics.settings)
-        
+    
+    def setLimits(self, bm, lbl1, lbl2, num, diff):
+        if lbl1 > lbl2:
+            bm[lbl2][lbl1] = num + diff
+            bm[lbl1][lbl2] = num
+        else:
+            bm[lbl2][lbl1] = num
+            bm[lbl1][lbl2] = num + diff
+    
+        return bm
+    
+    def editMatrix(self, reactant, bm):
         # edit bounds distances to align reacting atoms
-        if family.lower() == 'h_abstraction':
-            sect = len(reactant.split()[1].atoms)
-        
-            tsBM[sect:,:sect] = 1.8
+        if self.reaction.label.lower() == 'h_abstraction':
+            sect = len(reactant.split()[1].atoms)        
+            bm[sect:,:sect] = 1.8
         
             lbl1 = reactant.getLabeledAtom('*1').sortingLabel
             lbl2 = reactant.getLabeledAtom('*2').sortingLabel
@@ -108,60 +135,35 @@ class QMReaction:
             labels = [lbl1, lbl2, lbl3]
             atomMatch = ((lbl1,),(lbl2,),(lbl3,))
             
-            reaction = Reaction(reactants=reactant.split(), products=product.split())
-            distanceData = transitionStates.estimateDistances(reaction)
+            distanceData = transitionStates.estimateDistances(self.reaction)
             
-            tsBM = editMatrix(tsBM, lbl1, lbl2, distanceData.distances['d12'], 0.1)
-            tsBM = editMatrix(tsBM, lbl2, lbl3, distanceData.distances['d23'], 0.001)
-            tsBM = editMatrix(tsBM, lbl1, lbl3, distanceData.distances['d13'], 0.001)
+            bm = self.setLimits(bm, lbl1, lbl2, distanceData.distances['d12'], 0.1)
+            bm = self.setLimits(bm, lbl2, lbl3, distanceData.distances['d23'], 0.001)
+            bm = self.setLimits(bm, lbl1, lbl3, distanceData.distances['d13'], 0.001)
+        
+        return bm, labels, atomMatch
+        
+    def generateGeometry(self):
+        if len(self.reaction.reactants)==2:
+            reactant = self.reaction.reactants[0].merge(self.reaction.reactants[1])
+        if len(self.reaction.products)==2:
+            product = self.reaction.products[0].merge(self.reaction.products[1])
+        
+        reactant = self.fixSortLabel(reactant)
+        product = self.fixSortLabel(product)
+        
+        tsRDMol, tsBM, tsMult = self.generateBoundsMatrix(reactant)
+        tsBM, labels, atomMatch = self.editMatrix(reactant, tsBM)
+        atoms = len(reactant.atoms)
+        distGeomAttempts = 5*(atoms-3) # number of conformers embedded from the bounds matrix
         
         setBM = rdkit.DistanceGeometry.DoTriangleSmoothing(tsBM)
-        
         if setBM:
-            tssorted_atom_list = reactant.vertices[:]
-            qmcalcTS = rmgpy.qm.gaussian.GaussianMolPM3(reactant, quantumMechanics.settings)
-            reactant.vertices = tssorted_atom_list
-        
-            qmcalcTS.createGeometry(tsBM,atomMatch)
-        
-            geometryTS = qmcalcTS.geometry
-            tsmolFilePathForCalc = qmcalcTS.getMolFilePathForCalculation(attempt)
-        
-            # Create filenames
-            tsFilePath = os.path.join(quantumMechanics.settings.fileStore, 'transitionState' + str(count) + '.gjf')
-            tsOutPath = os.path.join(quantumMechanics.settings.fileStore, 'transitionState' + str(count) + '.log')
-            ircInPath = os.path.join(quantumMechanics.settings.fileStore, 'transitionStateIRC' + str(count) + '.gjf')
-            ircOutPath = os.path.join(quantumMechanics.settings.fileStore, 'transitionStateIRC' + str(count) + '.log')
-            tsOutputDataFile = os.path.join(quantumMechanics.settings.fileStore, 'data' + str(count) + outputFileExtension)
-        
-            # QM saddle search
-            # Write and run the TS optimization
-            tsConverge = 0
-            if os.path.exists(tsOutPath):
-                tsConverge = checkOutput(tsOutPath)
-            else:
-                writeTSInputFile(tsFilePath, tsmolFilePathForCalc, geometryTS, family)
-                run(executablePath, tsFilePath, tsOutPath)
-                tsConverge = checkOutput(tsOutPath)
-                # writeSubFile(tsFilePath.split('.')[0])
-                # os.system('bsub < ' + tsFilePath.split('.')[0] + '.sh')
-        
-            # Validation
-            if tsConverge == 2:
-                # Error in internal coodinate system, continue calculation in cartesian
-                writeTSCartInput(tsFilePath, count)
-                run(executablePath, tsFilePath, tsOutPath)
-                tsConverge = checkOutput(tsOutPath)
-                # os.system('bsub < ' + tsFilePath.split('.')[0] + '.sh')
+            self.geometry.rd_embed(tsRDMol, distGeomAttempts, bm=tsBM, match=atomMatch)
             
-            # If saddle found, write and run the IRC calculation to check the TS geometry
-            if tsConverge == 1:
-                writeIRCInput(ircInPath, count)
-                run(executablePath, ircInPath, ircOutPath)
-                # writeSubFile(ircInPath.split('.')[0])
-                # os.system('bsub < ' + ircInPath.split('.')[0] + '.sh')
-                ircCheck, notes = testGeometries(reactant, product, ircOutPath, notes)
-                if ircCheck == 1:
-                    vibFreq, activeAts, atomDist = parse(tsOutPath, tsOutputDataFile, labels)
-                    writeRxnOutputFile(tsOutputDataFile, reactant, product, vibFreq, activeAts, atomDist, notes)
-                    
+            self.writeInputFile(1)
+            self.run()
+            self.writeIRCFile()
+            self.run()
+            self.verifyTSGeometry()
+            
