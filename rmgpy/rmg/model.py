c92c9ef3429e9eb92e06a6909f32f29d5abe8c63
Jacob Barlow <barlow.ja@husky.neu.edu>
2013-09-30 11:23:10 -0400

    fixed typo

--- rmgpy/rmg/model.py
+++ rmgpy/rmg/model.py
@@ -76,7 +76,7 @@ class Species(rmgpy.species.Species):
         """
         A helper function used when pickling an object.
         """
-        return (Species, (self.index, self.label, self.thermo, self.conformer, self.molecule, self.transporData, self.molecularWeight, self.dipoleMoment, self.polarizability, self.Zrot, self.energyTransferModel, self.reactive, self.coreSizeAtCreation),)
+        return (Species, (self.index, self.label, self.thermo, self.conformer, self.molecule, self.transportData, self.molecularWeight, self.dipoleMoment, self.polarizability, self.Zrot, self.energyTransferModel, self.reactive, self.coreSizeAtCreation),)
 
     def generateThermoData(self, database, thermoClass=NASA, quantumMechanics=None):
         """
