888f1c450ebb3fabf31d63fb9ddf738c52a28558
Connie Gao <connieg@mit.edu>
2013-10-11 11:26:12 -0400

    Add example files and script for running stand alone thermo estimation module.

---
+++ examples/thermoEstimator/input.py
@@ -0,0 +1,32 @@
+database(
+    thermoLibraries = ['primaryThermoLibrary', 'GRI-Mech3.0']   
+)
+
+species(
+    label='DIPK',
+    structure=SMILES("CC(C)C(=O)C(C)C"),
+)
+species(
+    label='O2',
+    structure=SMILES("[O][O]"),
+)
+species(
+    label='R_tert',
+    structure=SMILES("CC(C)C(=O)[C](C)C"),
+)
+species(
+    label='R_pri',
+    structure=SMILES("CC(C)C(=O)C(C)[CH2]"),
+)
+species(
+    label='Cineole',
+    structure=SMILES('CC12CCC(CC1)C(C)(C)O2'),
+)
+
+quantumMechanics(
+    software='mopac',
+    fileStore='QMfiles', # relative to where you run it? defaults to inside the output folder.
+    scratchDirectory = None, # not currently used
+    onlyCyclics = True,
+    maxRadicalNumber = 0,
+)
---
+++ examples/thermoEstimator/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run the thermo estimator on the given thermo input file
+python ../../thermoEstimator.py input.py
