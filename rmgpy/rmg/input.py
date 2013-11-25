f9dea822823a319a43c72644564a886df875bb0f
keceli <keceli@gmail.com>
2013-11-25 12:48:38 -0500

    Allows RMG to continue when there is a duplicate species
    Originally, RMG stops if any two species are identical in the initial species list. Since this can happen frequently when using thermoEstimator with a large list of spcies, I changed it, so that RMG
    ignores the duplicate, and continue execution.

--- rmgpy/rmg/input.py
+++ rmgpy/rmg/input.py
@@ -92,9 +92,12 @@ def database(
 def species(label, structure, reactive=True):
     logging.debug('Found {0} species "{1}" ({2})'.format('reactive' if reactive else 'nonreactive', label, structure.toSMILES()))
     spec, isNew = rmg.reactionModel.makeNewSpecies(structure, label=label, reactive=reactive)
-    assert isNew, "Species {0} is a duplicate of {1}. Species in input file must be unique".format(label,spec.label)
-    rmg.initialSpecies.append(spec)
-    speciesDict[label] = spec
+    #assert isNew, "Species {0} is a duplicate of {1}. Species in input file must be unique".format(label,spec.label)
+    if isNew:
+    	rmg.initialSpecies.append(spec)
+    	speciesDict[label] = spec
+    else:
+	logging.info("Species {0} is a duplicate of {1}. Avoid it and continue calculation ...".format(label,spec.label))
     
 def SMARTS(string):
     return Molecule().fromSMARTS(string)
