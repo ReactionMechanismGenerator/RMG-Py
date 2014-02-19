a55a3a929ff0b455ec75957bbad6432ba46d6e46
Connie Gao <connieg@mit.edu>
2014-02-19 15:38:07 -0500

    Add generateReactions example to the examples folder.

---
+++ examples/generateReactions/input.py
@@ -0,0 +1,62 @@
+# Data sources
+database(
+    thermoLibraries = ['primaryThermoLibrary'],
+    reactionLibraries = [],
+    seedMechanisms = [],
+    kineticsDepositories = ['training'],
+    kineticsFamilies = ['!Intra_Disproportionation','!Substitution_O'],
+    kineticsEstimator = 'rate rules',
+)
+
+# List of species
+species(
+    label='ethane',
+    reactive=True,
+    structure=SMILES("CC"),
+)
+
+species(
+    label='H',
+    reactive=True,
+    structure=SMILES("[H]"),
+)
+
+species(
+	label='butane',
+	reactive=True,
+	structure=SMILES("CCCC"),
+)
+
+# Reaction systems
+simpleReactor(
+    temperature=(1350,'K'),
+    pressure=(1.0,'bar'),
+    initialMoleFractions={
+        "ethane": 1.0,
+    },
+    terminationConversion={
+        'ethane': 0.9,
+    },
+    terminationTime=(1e6,'s'),
+)
+
+simulator(
+    atol=1e-16,
+    rtol=1e-8,
+)
+
+model(
+    toleranceKeepInEdge=0.0,
+    toleranceMoveToCore=0.1,
+    toleranceInterruptSimulation=0.1,
+    maximumEdgeSpecies=100000
+)
+
+options(
+    units='si',
+    saveRestartPeriod=None,
+    drawMolecules=False,
+    generatePlots=False,
+    saveEdgeSpecies=False,
+    saveConcentrationProfiles=True,
+)
