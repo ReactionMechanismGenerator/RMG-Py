bc07fa697b7434bbac3230ecd7662de193214db5
Beat Buesser <bbuesser@mit.edu>
2014-02-28 07:30:18 -0500

    Don't saturate groups read from adjacency lists
    - groups do not have to be complete and can contain only part of the
    neighboring, bonded atoms

--- rmgpy/data/base.py
+++ rmgpy/data/base.py
@@ -371,7 +371,7 @@ class Database:
                     self.entries[label].item = makeLogicNode(' '.join(lines[1:]) )
                 # Otherwise convert adjacency list to molecule or pattern
                 elif pattern:
-                    self.entries[label].item = Group().fromAdjacencyList(record,saturateH=True)
+                    self.entries[label].item = Group().fromAdjacencyList(record,saturateH=False)
                 else:
                     self.entries[label].item = Molecule().fromAdjacencyList(record,saturateH=True)
         except InvalidAdjacencyListError, e:
