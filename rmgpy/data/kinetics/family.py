13db891d098eda6bbe9b99bc60e12651a63475f0
Nathan Yee <nyee@mit.edu>
2014-03-31 14:35:24 -0400

    Add function checkWellFormed to family.py
    This function is used to check for malformed kinetic families. I have also
    removed the function isWellFormed from base.py because it is deprecated by
    this new function (and also didn't work due to the many changes we've been
    making).
    
    The new function checks for:
    -rule labels that have group names which do not exist in groups.py
    -rule adjLists that do not match with the corresponding adj list in
    groups.py
    -Each group must exist in the tree
    -Each group adjList must actually be a more specific case of it's parent's adjList

--- rmgpy/data/kinetics/family.py
+++ rmgpy/data/kinetics/family.py
@@ -38,7 +38,7 @@ import codecs
 from copy import copy, deepcopy
 
 from rmgpy.data.base import Database, Entry, LogicNode, LogicOr, ForbiddenStructures,\
-                            ForbiddenStructureException, getAllCombinations
+                            ForbiddenStructureException, getAllCombinations, DatabaseError
 from rmgpy.reaction import Reaction
 from rmgpy.kinetics import Arrhenius, ArrheniusEP, ThirdBody, Lindemann, Troe, \
                            PDepArrhenius, MultiArrhenius, MultiPDepArrhenius, \
@@ -1941,3 +1941,133 @@ class KineticsFamily(Database):
             return 's^-1'
         else:
             raise ValueError('Unable to determine units of rate coefficient for reaction family "{0}".'.format(self.label))
+    
+    def checkWellFormed(self):
+        """
+        Returns a tuple of malformed database entries:
+        
+        noGroup is a list of nodes in the rules that has no corresponding group in
+        groups.py
+        
+        noMatchingGroup is a dictionary with entry labels from the rules as a key
+        and entry labels from groups as values. These are groups where rule.py's
+        adj list does not match group.py's.
+        
+        notInTree is a list of groups that do not appear in the tree
+        
+        notSubgroup is a dictionary with group labels as keys and atom indexes 
+        as values. Each key is a group where the child's adj list is not a
+        true child of it's parent. The list of indexes corresponds to the
+        child's adj list index, where the atom is not a true child. 
+        
+        probablyProducts is a list of groups which do not apepar in the
+        tree, but are probably products (as opposed to reactants) which
+        are created in the database loading. These are not necessarily
+        malformations, but because I'm not certain where they came from,
+        I decided to list them.
+        """
+        
+        
+        #A function to add to the not in Subgroup dictionary
+        def appendToDict(dictionary, key, value):
+            if key not in dictionary:
+                dictionary[key]=[value]
+            else:
+                dictionary[key].append(value) 
+            return dictionary
+
+        #list of nodes that are not wellFormed
+        noGroup=[]
+        noMatchingGroup={}
+        tempNoMatchingGroup={}
+        notInTree=[]
+        notSubgroup={}
+        probablyProduct=[]
+        
+        # Give correct arguments for each type of database
+#         if isinstance(self, KineticsFamily):
+        library=self.rules.entries
+        groups=self.groups.entries
+        topNodes=self.getRootTemplate()
+
+        # Make list of all node names in library
+        libraryNodes=[]
+        for nodes in library:
+            libraryNodes.append(nodes)
+
+        try:
+            #Each label in rules.py should be be in the form group1;group2;group3 etc
+            #and each group must appear in groups.py
+            for libraryNode in libraryNodes:
+                nodes=libraryNode.split(';')
+                for libraryEntry in library[libraryNode]:
+                    for nodeName in nodes:
+                        if nodeName not in groups:
+                            noGroup.append(nodeName)
+                            #If the node is not in the dictionary, we can't do the rest of the check
+                            continue
+                        #Each adj list in rules.py should match the adj list in group's.py
+                        for libraryGroup in libraryEntry.item.reactants:
+                            #break if we find a match between two groups
+                            if isinstance(groups[nodeName].item, Group) and isinstance(libraryGroup, Group):
+                                if groups[nodeName].item.isIsomorphic(libraryGroup):
+                                    break
+                            #break if we find a match between two logic nodes
+                            elif isinstance(groups[nodeName].item, LogicOr) and isinstance(libraryGroup, LogicOr):
+                                if groups[nodeName].item==libraryGroup:
+                                    break
+                        #Otherwise no match is found, so we add it to the tempNoMatchingGroup
+                        else:
+                            tempNoMatchingGroup=appendToDict(tempNoMatchingGroup, libraryNode, nodeName)
+            #eliminate duplicates
+            for key, nodeList in tempNoMatchingGroup.iteritems():
+                noMatchingGroup[key]=list(set(nodeList))
+                
+            # Each group in groups.py should appear in the tree
+            # This is true when ascending through parents leads to a top node   
+            for nodeName in groups:
+                nodeGroup=self.groups.entries[nodeName]
+                ascendParent=nodeGroup
+                while ascendParent not in topNodes:
+                    child=ascendParent
+                    ascendParent=ascendParent.parent
+                    if ascendParent is None or child not in ascendParent.children:
+                        if child.index==-1:
+                            probablyProduct.append(child.label)
+                            break
+                        else:
+                        # If a group is not in a tree, we want to save the uppermost parent, not necessarily the original node
+                            notInTree.append(child.label)
+                            break
+                #For a correct child-parent relationship, each atom in the parent should have a corresponding child atom in the child.
+                nodeParent=nodeGroup.parent
+                #Atoms may be in a different order initially. Need to sort both child and parent first
+                #Don't need to do check for topNodes
+                if nodeParent is not None:                         
+                    if isinstance(nodeParent.item, LogicOr):
+                        if not nodeGroup.label in nodeParent.item.components:
+                            #-1 index means the child is not in the LogicOr
+                            notSubgroup[nodeName]=nodeParent.label
+                            continue
+                        else:
+                            #if the parent is a LogicOr, we want to keep ascending until we get to a group or hit a discontinuity (could be
+                            #malformed tree or just ascending past the top node)
+                            while isinstance(nodeParent.item, LogicOr):
+                                nodeParent=nodeParent.parent
+                                if nodeParent == None: break
+                        if nodeParent == None: continue
+#                         nodeParent.item.sortAtoms()
+                    elif isinstance(nodeGroup.item, LogicOr):
+                        print nodeGroup, ' is an intermediate LogicOr. See if it can be replaced with a adj list.'
+                        continue
+                    #If both the parent and child are graphs, we can use the function isSubgroupIsomorphic if it is actually a child
+                    if not nodeGroup.item.isSubgraphIsomorphic(nodeParent.item):
+                        notSubgroup[nodeName]=nodeParent.label
+        except DatabaseError, e:
+            logging.error(str(e))
+        
+        #eliminate duplicates
+        noGroup=list(set(noGroup))
+        notInTree=list(set(notInTree))
+        
+        return (noGroup, noMatchingGroup, notInTree, notSubgroup, probablyProduct)
