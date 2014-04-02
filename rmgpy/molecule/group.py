1628bfccba856033996d66a3a257f62ee2eba07d
Richard West <r.west@neu.edu>
2014-04-01 23:31:47 -0400

    Fix bugs in GroupAtom charge tests.
    Equivalent check needs to ensure the matches work both ways.
    isSpecificCaseOf only needs to check self is contained within other.
    (and both need to not just check self against self)

--- rmgpy/molecule/group.py
+++ rmgpy/molecule/group.py
@@ -320,8 +320,13 @@ class GroupAtom(Vertex):
                 if radical1 == radical2 and spin1 == spin2: break
             else:
                 return False
-        #Each charge in self must have an equivalent in other
+        #Each charge in self must have an equivalent in other (and vice versa)
         for charge1 in self.charge:
+            for charge2 in other.charge:
+                if charge1 == charge2: break
+            else:
+                return False
+        for charge1 in other.charge:
             for charge2 in self.charge:
                 if charge1 == charge2: break
             else:
@@ -357,7 +362,7 @@ class GroupAtom(Vertex):
                 return False
         #Each charge in self must have an equivalent in other
         for charge1 in self.charge:
-            for charge2 in self.charge:
+            for charge2 in other.charge:
                 if charge1 == charge2: break
             else:
                 return False
@@ -787,7 +792,6 @@ class Group(Graph):
         The function `isIsomorphic` respects wildcards, while this function
         does not, make it more useful for checking groups to groups (as
         opposed to molecules to groups)
-        
         """
         # It only makes sense to compare a Group to a Group for full
         # isomorphism, so raise an exception if this is not what was requested
