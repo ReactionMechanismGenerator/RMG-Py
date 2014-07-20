8dea87d8dcf6aadcb2920474a123cecddf8bb764
Enoch Dames <enoch.dames@gmail.com>
2014-07-20 14:19:34 -0400

    fixup for reading in atomic number in cantherm
    didn't realize the qchem mod was not properly reading in atomic numbers.
    Caused cantherm to crash when trying to write cartesian coordinates of
    species to the output file

--- examples/cantherm/species/C2H5/ethyl.py
+++ examples/cantherm/species/C2H5/ethyl.py
@@ -26,12 +26,12 @@ energy = {
 
 geometry = GaussianLog('ethyl_cbsqb3.log')
 
-frequencies = GaussianLog('ethyl_b3lyp.log')
+frequencies = GaussianLog('ethyl_cbsqb3.log')
 
 """pivot are the two atoms that are attached to the rotor
 top contains the atoms that are being rotated including one of the atoms from pivots
 symmetry is the symmetry number of the scan
 fit is fit of the scan data. It defaults to 'best', but can also be assigned as 'cosine' or 'fourier'"""
 rotors = [
-    HinderedRotor(scanLog=GaussianLog('ethyl_scan_72.log'), pivots=[1,2], top=[1,3,4], symmetry=6, fit='best')
+#    HinderedRotor(scanLog=GaussianLog('ethyl_scan_72.log'), pivots=[1,2], top=[1,3,4], symmetry=6, fit='best')
 ]
