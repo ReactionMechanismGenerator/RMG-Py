097050d1a8d4f6e33f2bf98a11d7cd158398210f
Pierre Bhoorasingh <pierreyb@gmail.com>
2014-08-11 18:04:43 -0400

    Modify qm/main to store files in method subfolders
    Also includes some default values for other options.
    The tests have been updated to match the modified
    QMCalculator settings.

--- rmgpy/qm/gaussian.py
+++ rmgpy/qm/gaussian.py
@@ -132,7 +132,7 @@ class Gaussian:
                 if line.startswith("InChI="):
                     logFileInChI = line #output files should take up to 240 characters of the name in the input file
                     InChIFound = True
-                    if logFileInChI == self.geometry.uniqueIDlong:
+                    if self.geometry.uniqueIDlong in logFileInChI:
                         InChIMatch = True
                     elif self.geometry.uniqueIDlong.startswith(logFileInChI):
                         logging.info("InChI too long to check, but beginning matches so assuming OK.")
