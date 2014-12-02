e485aac2364c626ac0df2564e7cea1ec28571d94
Connie Gao <connieg@mit.edu>
2014-12-02 10:55:09 -0500

    Define a wildcard multiplicity in the group adjlist to be "multiplicity x"
    Now multiplicity is defined in exactly the same syntax as unapaired electrons,
    lone pairs, and charge.  This line can be omitted altogether in the group adjlist
    and still represent a wildcard.
    
    e.g. these following adjlists for Group1 and Group2 represent identical groups:
    
    Group1
    multiplicity x
    1 R u0
    
    Group2
    1 R u0

--- rmgpy/molecule/adjlist.py
+++ rmgpy/molecule/adjlist.py
@@ -475,13 +475,17 @@ def fromAdjacencyList(adjlist, group=False, saturateH=False):
         line = lines.pop(0)
         if group:
             match = re.match('\s*multiplicity\s+\[\s*(\d(?:,\s*\d)*)\s*\]\s*$', line)
+            if not match:
+                rematch = re.match('\s*multiplicity\s+x\s*$', line)
+                assert rematch, "Invalid multiplicity line '{0}'. Should be a list like 'multiplicity [1,2,3]' or a wildcard 'multiplicity x'".format(line)
+            else:
             # should match "multiplicity [1]" or " multiplicity   [ 1, 2, 3 ]" or " multiplicity [1,2,3]"
             # and whatever's inside the [] (excluding leading and trailing spaces) should be captured as group 1.
+            # If a wildcard is desired, this line can be omitted or replaced with 'multiplicity x'
             # Multiplicities must be only one digit (i.e. less than 10)
-            # The (?:,\s*\d)* matches patters like ", 2" 0 or more times, but doesn't capture them (because of the leading ?:)
-            assert match, "Invalid multiplicity line '{0}'. Should be a list like 'multiplicity [1,2,3]'".format(line)
-            multiplicities = match.group(1).split(',')
-            multiplicity = [int(i) for i in multiplicities]
+            # The (?:,\s*\d)* matches patters like ", 2" 0 or more times, but doesn't capture them (because of the leading ?:)            
+                multiplicities = match.group(1).split(',')
+                multiplicity = [int(i) for i in multiplicities]
         else:
             match = re.match('\s*multiplicity\s+\d+\s*$', line)
             assert match, "Invalid multiplicity line '{0}'. Should be an integer like 'multiplicity 2'".format(line)
