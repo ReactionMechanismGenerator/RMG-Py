ff2e47014af30c0c0ea97c67958ba31d6dd598d0
Connie Gao <connieg@mit.edu>
2015-03-26 14:56:34 -0400

    Fix to make drawing of molecules with CO and implicit hydrogens draw the hydrogens.

--- rmgpy/molecule/draw.py
+++ rmgpy/molecule/draw.py
@@ -165,7 +165,7 @@ class MoleculeDrawer:
         # they will need special attention
         self.__findRingGroups()
         # Handle carbon monoxide special case
-        if self.molecule.getFormula() == 'CO':
+        if self.molecule.getFormula() == 'CO' and len(atomsToRemove) == 0:
             # RDKit does not accept atom type Ot
             self.molecule.removeAtom(self.molecule.atoms[-1])
             self.symbols = ['CO']
