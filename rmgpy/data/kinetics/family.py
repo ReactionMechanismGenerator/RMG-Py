65b9e25cbf4a73cb6870997b0a9df94f7ed7f67d
Connie Gao <connieg@mit.edu>
2015-04-07 17:30:03 -0400

    Use swapped template kinetics for R_Recombination if the rank is better.
    Previously, we only checked for exactness.

--- rmgpy/data/kinetics/family.py
+++ rmgpy/data/kinetics/family.py
@@ -1696,9 +1696,19 @@ class KineticsFamily(Database):
             # The kinetics could be stored exactly with the template labels swapped
             # If this gives an exact match and the other gives an estimate, then keep the exact match
             # Not sure how to decide which to keep if both are exact or both are estimates
-            kinetics0 = self.rules.estimateKinetics(template[::-1], degeneracy)
-            if 'exact' in kinetics0.comment.lower() and 'exact' not in kinetics.comment.lower():
-                kinetics = kinetics0
+            kinetics0, entry0 = self.rules.estimateKinetics(template[::-1], degeneracy)
+            if entry0:
+                # Matched an exact entry in the other direction
+                if entry:
+                    if entry0.rank < entry.rank:
+                        # other direction has a higher (smaller valued) rank than original direction
+                        kinetics = kinetics0
+                        entry = entry0
+                else:
+                    # No exact match in original direction, so use the match from the other direction
+                    kinetics = kinetics0
+                    entry = entry0
+                
         return kinetics, entry
 
     def getRateCoefficientUnits(self):
