5669060c254d0b0f8fba87d9303c736b1df5c5aa
Richard West <r.west@neu.edu>
2015-05-31 00:34:00 -0400

    Fix and tidy up the copy of external in build.sh
    I'm surprised there are different syntaxes for "cp"
    but apparently so!
    
    Also added 'make QM' to build symmetry, though I'm not sure
    it's moved into the right place yet.

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@ export CC=${PREFIX}/bin/gcc
 export CXX=${PREFIX}/bin/g++
 export F77=${PREFIX}/bin/gfortran
 export F90=${PREFIX}/bin/gfortran
-make -j ${CPU_COUNT}
+make -j${CPU_COUNT}
+make QM
 $PYTHON setup.py install
 
-cd ${PREFIX}/lib/python${PY_VER}/site-packages/
-mkdir external
-cp ${SRC_DIR}/external/* external/ -r 
+# lazy "install" of everything in our 'external' folder.
+# most of which should probably be elsewhere
+cp -R ${SRC_DIR}/external ${SP_DIR}
