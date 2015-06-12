5cbf0801a2dd10139a29e47a5d9c987f674263a1
Richard West <r.west@neu.edu>
2015-06-12 10:24:18 -0400

    docs: (temporarily) keep warnings in the documentation

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -23,6 +23,8 @@ sys.path.insert(0, os.path.abspath('../../'))
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
 
+keep_warnings = True
+
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax']
