dd1f7b974ac89fcfa4dcc8d6fa6b9d09957afd20
Richard West <r.west@neu.edu>
2015-06-12 16:27:25 -0400

    Use the renamed sphinx theme (newer sphinx) and stylesheet

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -96,12 +96,15 @@ autodoc_default_flags = ['members', 'inherited-members']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
+
+# stylesheet to use
+html_style = 'rmg.css'
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
