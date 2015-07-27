c9a7ec941ded5b344d3283e729e766093c777fff
Richard West <r.west@neu.edu>
2015-07-27 14:14:43 -0400

    Copy liquid solver to surface solver.
    We start with Liquid because we want constant volume.
    
    Added surface solver to be cythonized in setup.py

---
+++ rmgpy/solver/surface.pyx
@@ -0,0 +1,304 @@
+################################################################################
+#
+#   RMG - Reaction Mechanism Generator
+#
+#   Copyright (c) 2002-2010 Prof. William H. Green (whgreen@mit.edu) and the
+#   RMG Team (rmg_dev@mit.edu)
+#
+#   Permission is hereby granted, free of charge, to any person obtaining a
+#   copy of this software and associated documentation files (the 'Software'),
+#   to deal in the Software without restriction, including without limitation
+#   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#   and/or sell copies of the Software, and to permit persons to whom the
+#   Software is furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included in
+#   all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#   DEALINGS IN THE SOFTWARE.
+#
+################################################################################
+
+"""
+Contains the :class:`SimpleReactor` class, providing a reaction system
+consisting of a homogeneous, isothermal, isobaric batch reactor.
+"""
+
+import numpy
+cimport numpy
+
+import itertools
+
+from base cimport ReactionSystem
+cimport cython
+
+import rmgpy.constants as constants
+cimport rmgpy.constants as constants
+from rmgpy.quantity import Quantity
+from rmgpy.quantity cimport ScalarQuantity, ArrayQuantity
+
+cdef class LiquidReactor(ReactionSystem):
+    """
+    A reaction system consisting of a homogeneous, isothermal, constant volume batch
+    reactor. These assumptions allow for a number of optimizations that enable
+    this solver to complete very rapidly, even for large kinetic models.
+    """
+
+    cdef public ScalarQuantity T
+    cdef public ScalarQuantity P    
+    cdef public double V
+    cdef public bint constantVolume
+    cdef public dict initialConcentrations
+
+    def __init__(self, T, initialConcentrations, termination, sensitiveSpecies=None, sensitivityThreshold=1e-3):
+        ReactionSystem.__init__(self, termination, sensitiveSpecies, sensitivityThreshold)
+        self.T = Quantity(T)
+        self.P = Quantity(100000.,'kPa') # Arbitrary high pressure (1000 Bar) to get reactions in the high-pressure limit!
+        self.initialConcentrations = initialConcentrations # should be passed in SI
+        self.V = 0 # will be set from initialConcentrations in initializeModel
+        self.constantVolume = True
+        
+    def convertInitialKeysToSpeciesObjects(self, speciesDict):
+        """
+        Convert the initialConcentrations dictionary from species names into species objects,
+        using the given dictionary of species.
+        """
+        initialConcentrations = {}
+        for label, moleFrac in self.initialConcentrations.iteritems():
+            initialConcentrations[speciesDict[label]] = moleFrac
+        self.initialConcentrations = initialConcentrations
+
+    cpdef initializeModel(self, list coreSpecies, list coreReactions, list edgeSpecies, list edgeReactions, list pdepNetworks=None, atol=1e-16, rtol=1e-8, sensitivity=False, sens_atol=1e-6, sens_rtol=1e-4, filterReactions=False):
+        """
+        Initialize a simulation of the simple reactor using the provided kinetic
+        model.
+        """
+
+        # First call the base class version of the method
+        # This initializes the attributes declared in the base class
+        ReactionSystem.initializeModel(self, coreSpecies, coreReactions, edgeSpecies, edgeReactions, pdepNetworks, atol, rtol, sensitivity, sens_atol, sens_rtol)
+
+        # Set initial conditions
+        self.set_initial_conditions()
+
+        # Compute reaction thresholds if reaction filtering is turned on
+        if filterReactions:
+            ReactionSystem.set_initial_reaction_thresholds(self)
+
+        # Generate forward and reverse rate coefficients k(T,P)
+        self.generate_rate_coefficients(coreReactions, edgeReactions)
+
+        ReactionSystem.compute_network_variables(self, pdepNetworks)
+        
+        ReactionSystem.set_initial_derivative(self)
+
+        # Initialize the model
+        ReactionSystem.initialize_solver(self)
+
+    def generate_rate_coefficients(self, coreReactions, edgeReactions):
+        """
+        Populates the forwardRateCoefficients, reverseRateCoefficients and equilibriumConstants
+        arrays with the values computed at the temperature and (effective) pressure of the 
+        reacion system.
+        """
+        
+        for rxn in itertools.chain(coreReactions, edgeReactions):
+            j = self.reactionIndex[rxn]
+            self.kf[j] = rxn.getRateCoefficient(self.T.value_si, self.P.value_si)
+            if rxn.reversible:
+                self.Keq[j] = rxn.getEquilibriumConstant(self.T.value_si)
+                self.kb[j] = self.kf[j] / self.Keq[j]
+
+    def set_initial_conditions(self):
+        """
+        Sets the initial conditions of the rate equations that represent the 
+        current reactor model.
+
+        The volume is set to the value in m3 required to contain 
+        one mole total of core species at start.
+
+        The coreSpeciesConcentrations array is set to the values stored in the
+        initial concentrations dictionary.
+
+        The initial number of moles of a species j is computed and stored in the
+        y0 instance attribute.
+
+        """
+        ReactionSystem.set_initial_conditions(self)
+
+        for spec, conc in self.initialConcentrations.iteritems():
+            i = self.get_species_index(spec)
+            self.coreSpeciesConcentrations[i] = conc
+        
+        V = 1.0 / numpy.sum(self.coreSpeciesConcentrations)
+        self.V = V 
+        
+        for j in xrange(self.numCoreSpecies):
+            self.y0[j] = self.coreSpeciesConcentrations[j] * V
+            
+
+    @cython.boundscheck(False)
+    def residual(self, double t, numpy.ndarray[numpy.float64_t, ndim=1] y, numpy.ndarray[numpy.float64_t, ndim=1] dydt, numpy.ndarray[numpy.float64_t, ndim=1] senpar = numpy.zeros(1, numpy.float64)):
+
+        """
+        Return the residual function for the governing DAE system for the
+        simple reaction system.
+        """
+        cdef numpy.ndarray[numpy.int_t, ndim=2] ir, ip, inet
+        cdef numpy.ndarray[numpy.float64_t, ndim=1] res, kf, kr, knet, delta, equilibriumConstants
+        cdef int numCoreSpecies, numCoreReactions, numEdgeSpecies, numEdgeReactions, numPdepNetworks
+        cdef int i, j, z, first, second, third
+        cdef double k, V, reactionRate
+        cdef numpy.ndarray[numpy.float64_t, ndim=1] coreSpeciesConcentrations, coreSpeciesRates, coreReactionRates, edgeSpeciesRates, edgeReactionRates, networkLeakRates
+        cdef numpy.ndarray[numpy.float64_t, ndim=1] C
+        cdef numpy.ndarray[numpy.float64_t, ndim=2] jacobian, dgdk
+
+        ir = self.reactantIndices
+        ip = self.productIndices
+        equilibriumConstants = self.Keq
+
+        kf = self.kf
+        kr = self.kb
+        
+        inet = self.networkIndices
+        knet = self.networkLeakCoefficients
+
+        numCoreSpecies = len(self.coreSpeciesRates)
+        numCoreReactions = len(self.coreReactionRates)
+        numEdgeSpecies = len(self.edgeSpeciesRates)
+        numEdgeReactions = len(self.edgeReactionRates)
+        numPdepNetworks = len(self.networkLeakRates)
+        
+        
+        res = numpy.zeros(numCoreSpecies, numpy.float64)
+
+        coreSpeciesConcentrations = numpy.zeros_like(self.coreSpeciesConcentrations)
+        coreSpeciesRates = numpy.zeros_like(self.coreSpeciesRates)
+        coreReactionRates = numpy.zeros_like(self.coreReactionRates)
+        edgeSpeciesRates = numpy.zeros_like(self.edgeSpeciesRates)
+        edgeReactionRates = numpy.zeros_like(self.edgeReactionRates)
+        networkLeakRates = numpy.zeros_like(self.networkLeakRates)
+
+        C = numpy.zeros_like(self.coreSpeciesConcentrations)
+        V =  self.V # constant volume reactor
+
+        for j in xrange(numCoreSpecies):
+            C[j] = y[j] / V
+            coreSpeciesConcentrations[j] = C[j]
+        
+        for j in xrange(ir.shape[0]):
+            k = kf[j]
+            if ir[j,0] >= numCoreSpecies or ir[j,1] >= numCoreSpecies or ir[j,2] >= numCoreSpecies:
+                reactionRate = 0.0
+            elif ir[j,1] == -1: # only one reactant
+                reactionRate = k * C[ir[j,0]]
+            elif ir[j,2] == -1: # only two reactants
+                reactionRate = k * C[ir[j,0]] * C[ir[j,1]]
+            else: # three reactants!! (really?)
+                reactionRate = k * C[ir[j,0]] * C[ir[j,1]] * C[ir[j,2]]
+            k = kr[j]
+            if ip[j,0] >= numCoreSpecies or ip[j,1] >= numCoreSpecies or ip[j,2] >= numCoreSpecies:
+                pass
+            elif ip[j,1] == -1: # only one reactant
+                reactionRate -= k * C[ip[j,0]]
+            elif ip[j,2] == -1: # only two reactants
+                reactionRate -= k * C[ip[j,0]] * C[ip[j,1]]
+            else: # three reactants!! (really?)
+                reactionRate -= k * C[ip[j,0]] * C[ip[j,1]] * C[ip[j,2]]
+
+            # Set the reaction and species rates
+            if j < numCoreReactions:
+                # The reaction is a core reaction
+                coreReactionRates[j] = reactionRate
+
+                # Add/substract the total reaction rate from each species rate
+                # Since it's a core reaction we know that all of its reactants
+                # and products are core species
+                first = ir[j,0]
+                coreSpeciesRates[first] -= reactionRate
+                second = ir[j,1]
+                if second != -1:
+                    coreSpeciesRates[second] -= reactionRate
+                    third = ir[j,2]
+                    if third != -1:
+                        coreSpeciesRates[third] -= reactionRate
+                first = ip[j,0]
+                coreSpeciesRates[first] += reactionRate
+                second = ip[j,1]
+                if second != -1:
+                    coreSpeciesRates[second] += reactionRate
+                    third = ip[j,2]
+                    if third != -1:
+                        coreSpeciesRates[third] += reactionRate
+
+            else:
+                # The reaction is an edge reaction
+                edgeReactionRates[j-numCoreReactions] = reactionRate
+
+                # Add/substract the total reaction rate from each species rate
+                # Since it's an edge reaction its reactants and products could
+                # be either core or edge species
+                # We're only interested in the edge species
+                first = ir[j,0]
+                if first >= numCoreSpecies: edgeSpeciesRates[first-numCoreSpecies] -= reactionRate
+                second = ir[j,1]
+                if second != -1:
+                    if second >= numCoreSpecies: edgeSpeciesRates[second-numCoreSpecies] -= reactionRate
+                    third = ir[j,2]
+                    if third != -1:
+                        if third >= numCoreSpecies: edgeSpeciesRates[third-numCoreSpecies] -= reactionRate
+                first = ip[j,0]
+                if first >= numCoreSpecies: edgeSpeciesRates[first-numCoreSpecies] += reactionRate
+                second = ip[j,1]
+                if second != -1:
+                    if second >= numCoreSpecies: edgeSpeciesRates[second-numCoreSpecies] += reactionRate
+                    third = ip[j,2]
+                    if third != -1:
+                        if third >= numCoreSpecies: edgeSpeciesRates[third-numCoreSpecies] += reactionRate
+
+        for j in xrange(inet.shape[0]):
+            k = knet[j]
+            if inet[j,1] == -1: # only one reactant
+                reactionRate = k * C[inet[j,0]]
+            elif inet[j,2] == -1: # only two reactants
+                reactionRate = k * C[inet[j,0]] * C[inet[j,1]]
+            else: # three reactants!! (really?)
+                reactionRate = k * C[inet[j,0]] * C[inet[j,1]] * C[inet[j,2]]
+            networkLeakRates[j] = reactionRate
+
+        self.coreSpeciesConcentrations = coreSpeciesConcentrations
+        self.coreSpeciesRates = coreSpeciesRates
+        self.coreReactionRates = coreReactionRates
+        self.edgeSpeciesRates = edgeSpeciesRates
+        self.edgeReactionRates = edgeReactionRates
+        self.networkLeakRates = networkLeakRates
+
+        res = coreSpeciesRates * V 
+        
+        
+        if self.sensitivity:
+            delta = numpy.zeros(len(y), numpy.float64)
+            delta[:numCoreSpecies] = res
+            if self.jacobianMatrix is None:
+                jacobian = self.jacobian(t,y,dydt,0,senpar)
+            else:
+                jacobian = self.jacobianMatrix
+            dgdk = ReactionSystem.computeRateDerivative(self)
+            for j in xrange(numCoreReactions+numCoreSpecies):
+                for i in xrange(numCoreSpecies):
+                    for z in xrange(numCoreSpecies):
+                        delta[(j+1)*numCoreSpecies + i] += jacobian[i,z]*y[(j+1)*numCoreSpecies + z] 
+                    delta[(j+1)*numCoreSpecies + i] += dgdk[i,j]
+
+        else:
+            delta = res
+        delta = delta - dydt
+        
+        # Return DELTA, IRES.  IRES is set to 1 in order to tell DASPK to evaluate the sensitivity residuals
+        return delta, 1
