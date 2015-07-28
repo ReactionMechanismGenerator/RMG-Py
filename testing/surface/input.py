4e7c308e3619a040deeb445f53be4d498e0f1e87
Richard West <r.west@neu.edu>
2015-07-28 13:56:43 -0400

    Made an input file for testing surface jobs.

---
+++ testing/surface/input.py
@@ -0,0 +1,58 @@
+# Data sources
+database(
+    thermoLibraries = ['primaryThermoLibrary'],
+    reactionLibraries = [],
+    seedMechanisms = [],
+    kineticsDepositories = ['training'],
+    kineticsFamilies = 'default',
+    kineticsEstimator = 'rate rules',
+)
+
+# List of species
+species(
+    label='methyl',
+    reactive=True,
+    structure=SMILES("[CH3]"),
+)
+
+species(
+    label='site',
+    reactive=True,
+    structure=adjacencyList("1 X u0"),
+)
+
+# Reaction systems
+surfaceReactor(
+    temperature=(1350,'K'),
+    initialPressure=(1.0, 'bar'),
+    initialGasMoleFractions={
+        "methyl": 1.0,
+    },
+    initialSurfaceCoverages={
+        "site": 1.0,
+    },
+    surfaceVolumeRatio = (10., 'm^-1'),
+    surfaceSiteDensity=(2.9e-9, 'mol/cm^2'),
+    terminationTime=(1e-3,'s'),
+)
+
+simulator(
+    atol=1e-16,
+    rtol=1e-8,
+)
+
+model(
+    toleranceKeepInEdge=0.0,
+    toleranceMoveToCore=0.1,
+    toleranceInterruptSimulation=0.1,
+    maximumEdgeSpecies=100000
+)
+
+options(
+    units='si',
+    saveRestartPeriod=None,
+    generateOutputHTML=True,
+    generatePlots=False,
+    saveEdgeSpecies=True,
+    saveSimulationProfiles=True,
+)
