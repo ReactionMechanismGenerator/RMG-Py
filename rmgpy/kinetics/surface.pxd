dcd92a6a5322e8909dcd0331a27f09af220bbf95
Richard West <r.west@neu.edu>
2015-07-28 11:36:31 -0400

    Made a 'surface' module in rmgpy.kinetics
    it has the rmgpy.kinetics.StickingCoefficient class
    
    Fixed some errors in StickingCoefficient
    
    Cythonize the rmgpy/kinetics/surface file
    
    Fix SurfaceArrhenius class pickle and copy methods.
    
    Important to have __reduce__ return the correct Class,
    otherwise when you copy or pickle a SurfaceArrhenius
    it becomes an Arrhenius
    
    fix cython import in rmgpy/kinetics/surface.pxd

---
+++ rmgpy/kinetics/surface.pxd
@@ -0,0 +1,53 @@
+################################################################################
+#
+#   RMG - Reaction Mechanism Generator
+#
+#   Copyright (c) 2002-2009 Prof. William H. Green (whgreen@mit.edu) and the
+#   RMG Team (rmg_dev@mit.edu)
+#
+#   Permission is hereby granted, free of charge, to any person obtaining a
+#   copy of this software and associated documentation files (the "Software"),
+#   to deal in the Software without restriction, including without limitation
+#   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#   and/or sell copies of the Software, and to permit persons to whom the
+#   Software is furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included in
+#   all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#   DEALINGS IN THE SOFTWARE.
+#
+################################################################################
+
+cimport numpy
+
+from rmgpy.kinetics.model cimport KineticsModel
+from rmgpy.kinetics.arrhenius cimport Arrhenius, ArrheniusEP
+from rmgpy.quantity cimport ScalarQuantity, ArrayQuantity
+
+################################################################################
+
+cdef class StickingCoefficient(KineticsModel):
+    
+    cdef public ScalarQuantity _A
+    cdef public ScalarQuantity _n
+    cdef public ScalarQuantity _Ea
+    cdef public ScalarQuantity _T0
+    
+    cpdef double getStickingCoefficient(self, double T) except -1
+
+    cpdef changeT0(self, double T0)
+
+    cpdef bint isIdenticalTo(self, KineticsModel otherKinetics) except -2
+    
+    cpdef changeRate(self, double factor)
+
+################################################################################
+cdef class SurfaceArrhenius(Arrhenius):
+    pass
---
+++ rmgpy/kinetics/surface.pyx
@@ -0,0 +1,195 @@
+# cython: embedsignature=True, cdivision=True
+
+################################################################################
+#
+#   RMG - Reaction Mechanism Generator
+#
+#   Copyright (c) 2002-2009 Prof. William H. Green (whgreen@mit.edu) and the
+#   RMG Team (rmg_dev@mit.edu)
+#
+#   Permission is hereby granted, free of charge, to any person obtaining a
+#   copy of this software and associated documentation files (the "Software"),
+#   to deal in the Software without restriction, including without limitation
+#   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#   and/or sell copies of the Software, and to permit persons to whom the
+#   Software is furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included in
+#   all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#   DEALINGS IN THE SOFTWARE.
+#
+################################################################################
+
+import numpy
+from libc.math cimport exp, log, sqrt, log10
+
+cimport rmgpy.constants as constants
+import rmgpy.quantity as quantity
+
+################################################################################
+
+cdef class StickingCoefficient(KineticsModel):
+    """
+    A kinetics model to give Sticking Coefficients for surface adsorption. The attributes
+    are:
+
+    =============== =============================================================
+    Attribute       Description
+    =============== =============================================================
+    `A`             The preexponential factor
+    `T0`            The reference temperature
+    `n`             The temperature exponent
+    `Ea`            The activation energy
+    `Tmin`          The minimum temperature at which the model is valid, or zero if unknown or undefined
+    `Tmax`          The maximum temperature at which the model is valid, or zero if unknown or undefined
+    `Pmin`          The minimum pressure at which the model is valid, or zero if unknown or undefined
+    `Pmax`          The maximum pressure at which the model is valid, or zero if unknown or undefined
+    `comment`       Information about the model (e.g. its source)
+    =============== =============================================================
+    
+    """
+    
+    def __init__(self, A=None, n=0.0, Ea=None, T0=(1.0,"K"), Tmin=None, Tmax=None, Pmin=None, Pmax=None, comment=''):
+        KineticsModel.__init__(self, Tmin=Tmin, Tmax=Tmax, Pmin=Pmin, Pmax=Pmax, comment=comment)
+        self.A = A
+        self.n = n
+        self.Ea = Ea
+        self.T0 = T0
+        
+    def __repr__(self):
+        """
+        Return a string representation that can be used to reconstruct the
+        StickingCoefficient object.
+        """
+        string = 'StickingCoefficient(A={0!r}, n={1!r}, Ea={2!r}, T0={3!r}'.format(self.A, self.n, self.Ea, self.T0)
+        if self.Tmin is not None: string += ', Tmin={0!r}'.format(self.Tmin)
+        if self.Tmax is not None: string += ', Tmax={0!r}'.format(self.Tmax)
+        if self.Pmin is not None: string += ', Pmin={0!r}'.format(self.Pmin)
+        if self.Pmax is not None: string += ', Pmax={0!r}'.format(self.Pmax)
+        if self.comment != '': string += ', comment="""{0}"""'.format(self.comment)
+        string += ')'
+        return string
+
+    def __reduce__(self):
+        """
+        A helper function used when pickling a StickingCoefficient object.
+        """
+        return (StickingCoefficient, (self.A, self.n, self.Ea, self.T0, self.Tmin, self.Tmax, self.Pmin, self.Pmax, self.comment))
+
+    property A:
+        """The preexponential factor."""
+        def __get__(self):
+            return self._A
+        def __set__(self, value):
+            self._A = quantity.Dimensionless(value)
+
+    property n:
+        """The temperature exponent."""
+        def __get__(self):
+            return self._n
+        def __set__(self, value):
+            self._n = quantity.Dimensionless(value)
+
+    property Ea:
+        """The activation energy."""
+        def __get__(self):
+            return self._Ea
+        def __set__(self, value):
+            self._Ea = quantity.Energy(value)
+
+    property T0:
+        """The reference temperature."""
+        def __get__(self):
+            return self._T0
+        def __set__(self, value):
+            self._T0 = quantity.Temperature(value)
+
+    cpdef double getStickingCoefficient(self, double T) except -1:
+        """
+        Return the sticking coefficient (dimensionless) at temperature `T` in K. 
+        """
+        cdef double A, n, Ea, T0, stickingCoefficient
+        A = self._A.value_si
+        n = self._n.value_si
+        Ea = self._Ea.value_si
+        T0 = self._T0.value_si
+        stickingCoefficient = A * (T / T0)**n * exp(-Ea / (constants.R * T))
+        assert 0 <= stickingCoefficient
+        return min(stickingCoefficient, 1.0)
+
+    cpdef changeT0(self, double T0):
+        """
+        Changes the reference temperature used in the exponent to `T0` in K, 
+        and adjusts the preexponential factor accordingly.
+        """
+        self._A.value_si /= (self._T0.value_si / T0)**self._n.value_si
+        self._T0.value_si = T0
+
+    cpdef bint isIdenticalTo(self, KineticsModel otherKinetics) except -2:
+        """
+        Returns ``True`` if kinetics matches that of another kinetics model.  Must match temperature
+        and pressure range of kinetics model, as well as parameters: A, n, Ea, T0. (Shouldn't have pressure
+        range if it's Arrhenius.) Otherwise returns ``False``.
+        """
+        if not isinstance(otherKinetics,StickingCoefficient):
+            return False
+        if not KineticsModel.isIdenticalTo(self, otherKinetics):
+            return False
+        if (not self.A.equals(otherKinetics.A) or not self.n.equals(otherKinetics.n)
+            or not self.Ea.equals(otherKinetics.Ea) or not self.T0.equals(otherKinetics.T0)):
+            return False
+                
+        return True
+    
+    cpdef changeRate(self, double factor):
+        """
+        Changes A factor in Arrhenius expression by multiplying it by a ``factor``.
+        """
+        self._A.value_si *= factor
+
+################################################################################
+
+
+cdef class SurfaceArrhenius(Arrhenius):
+    """
+    A kinetics model based on (modified) Arrhenius for surface reactions
+    """
+    property A:
+        """The preexponential factor. 
+    
+        This is the only thing different from a normal Arrhenius class."""
+        def __get__(self):
+            return self._A
+        def __set__(self, value):
+            self._A = quantity.SurfaceRateCoefficient(value)
+    
+    def __repr__(self):
+        """
+        Return a string representation that can be used to reconstruct the
+        SurfaceArrhenius object.
+        """
+        string = 'SurfaceArrhenius(A={0!r}, n={1!r}, Ea={2!r}, T0={3!r}'.format(self.A, self.n, self.Ea, self.T0)
+        if self.Tmin is not None: string += ', Tmin={0!r}'.format(self.Tmin)
+        if self.Tmax is not None: string += ', Tmax={0!r}'.format(self.Tmax)
+        if self.Pmin is not None: string += ', Pmin={0!r}'.format(self.Pmin)
+        if self.Pmax is not None: string += ', Pmax={0!r}'.format(self.Pmax)
+        if self.comment != '': string += ', comment="""{0}"""'.format(self.comment)
+        string += ')'
+        return string
+    
+    def __reduce__(self):
+        """
+        A helper function used when pickling a SurfaceArrhenius object.
+        """
+        return (SurfaceArrhenius, (self.A, self.n, self.Ea, self.T0, self.Tmin, self.Tmax, self.Pmin, self.Pmax, self.comment))
+
+
+################################################################################
+
