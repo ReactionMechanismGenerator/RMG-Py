c4107820f07e3c46b5098c6690b74bb4295c78e1
Richard West <r.west@neu.edu>
2015-08-14 16:52:38 -0400

    When saving kinetics libraries, check that "labels" are correct.
    Since these are no longer just labels, but the actual
    definition of the reaction, it's important that they
    are correct. This should check that they are.
    Perhaps we should create them here instead,
    or use something other than "label",
    but this will do for now.

--- rmgpy/data/kinetics/common.py
+++ rmgpy/data/kinetics/common.py
@@ -135,6 +135,9 @@ def saveEntry(f, entry):
         f.write('    label = "{0}",\n'.format(entry.label))
 
     if isinstance(entry.item, Reaction):
+        assert entry.label == str(entry.item), ("Reactions are now defined solely their labels, "
+                                                "but reaction {0!s} has label {1!r}".format(
+                                                 entry.item, entry.label))
 #        for i, reactant in enumerate(entry.item.reactants):
 #            if isinstance(reactant, Molecule):
 #                f.write('    reactant{0:d} = \n'.format(i+1))
