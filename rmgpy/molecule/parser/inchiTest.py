9699965e45bddc0a0f249568ffee454399cd05ce
Nick Vandewiele <nickvandewiele@gmail.com>
2015-08-18 18:32:51 -0400

    define InChI and AugmentedInChI classes in inchi module

---
+++ rmgpy/molecule/parser/inchiTest.py
@@ -0,0 +1,82 @@
+import unittest
+
+from rmgpy.molecule.parser.inchi import AugmentedInChI, InChI, ignore_prefix
+
+
+class InChITest(unittest.TestCase):
+
+    def test_constructor(self):
+        inchi1 = InChI('InChI=1S/foo')
+        assert inchi1 is not None
+
+        with self.assertRaises(AssertionError):
+            inchi1 = InChI('foo')
+
+    def test_compare(self):
+        inchi1 = InChI('InChI=1S/foo')
+        inchi2 = InChI('InChI=1/foo')
+        inchi3 = InChI('InChI=1/bar')
+        
+        assert inchi1 == inchi2
+        assert not inchi1 != inchi2
+
+        assert (inchi1 < inchi2) is (inchi1 > inchi2)
+        assert (inchi1 < inchi3) is not (inchi1 > inchi3)
+
+class AugmentedInChITest(unittest.TestCase):
+
+    def test_constructor(self):
+        aug_inchi1 = AugmentedInChI('InChI=1S/foo')
+
+        assert aug_inchi1 == 'foo', aug_inchi1
+        assert aug_inchi1.mult == 1
+        assert aug_inchi1.u_indices is None
+
+        aug_inchi2 = AugmentedInChI('InChI=1S/foo/mult100')
+
+        assert aug_inchi2 == 'foo/mult100', aug_inchi2
+        assert aug_inchi2.mult == 100
+        assert aug_inchi2.u_indices is None
+
+        aug_inchi3 = AugmentedInChI('InChI=1S/foo/mult200/u1,3')
+
+        assert aug_inchi3 == 'foo/mult200/u1,3', aug_inchi3
+        assert aug_inchi3.mult == 200
+        assert aug_inchi3.u_indices == [1,3]
+
+    def test_compare(self):
+        aug_inchi1 = AugmentedInChI('InChI=1S/foo')
+        aug_inchi2 = AugmentedInChI('InChI=1S/foo/mult100')
+        aug_inchi3 = AugmentedInChI('InChI=1S/foo/mult200/u1,3')
+        aug_inchi4 = AugmentedInChI('InChI=1/foo')
+        
+        assert aug_inchi1 == aug_inchi4
+        assert not aug_inchi1 != aug_inchi4
+
+        assert aug_inchi1 != aug_inchi2
+        assert not aug_inchi1 == aug_inchi2        
+
+        assert aug_inchi3 != aug_inchi2
+        assert not aug_inchi3 == aug_inchi2        
+
+    def testReduce(self):
+        import pickle
+
+        aug_inchi = AugmentedInChI('InChI=1S/foo/mult200/u1,3')
+        aug_inchi2 = pickle.loads(pickle.dumps(aug_inchi))
+
+        assert aug_inchi == aug_inchi2
+        assert aug_inchi.mult == aug_inchi2.mult
+        assert aug_inchi.u_indices == aug_inchi2.u_indices
+        
+class IgnorePrefixTest(unittest.TestCase):
+
+    def test_ignore(self):
+        string = 'InChI=1S/foo'
+        assert ignore_prefix(string) == 'foo'
+
+        with self.assertRaises(AssertionError):
+            ignore_prefix('foo')
+
+if __name__ == '__main__':
+    unittest.main()
