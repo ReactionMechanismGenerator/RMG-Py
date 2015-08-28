c4090244614e099f5e187ce0b43c161830b6b08f
nickvandewiele <nickvandewiele@gmail.com>
2015-08-28 13:39:13 -0400

    add ethane oxidation example with P-dep (eg6)

--- Makefile
+++ Makefile
@@ -122,6 +122,12 @@ eg5: all
 	@ echo "Running heptane-eg5 example."
 	python rmg.py testing/heptane-eg5/input.py
 
+eg6: all
+	mkdir -p testing/ethane-oxidation
+	rm -rf testing/ethane-oxidation/*
+	cp examples/rmg/ethane-oxidation/input.py testing/ethane-oxidation/input.py
+	@ echo "Running ethane-oxidation example."
+	python rmg.py testing/ethane-oxidation/input.py
 
 ######### 
 # Section for setting up MOPAC calculations on the Travis-CI.org server
---
+++ examples/rmg/ethane-oxidation/input.py
@@ -0,0 +1,71 @@
+# Data sources
+database(
+    thermoLibraries = ['primaryThermoLibrary'],
+    reactionLibraries = [],
+    seedMechanisms = [],
+    kineticsDepositories = ['training'], 
+    kineticsFamilies = 'default',
+    kineticsEstimator = 'rate rules',
+)
+
+# List of species
+species(
+    label='ethane',
+    reactive=True,
+    structure=SMILES("CC"),
+)
+
+species(
+    label='O2',
+    reactive=True,
+    structure=SMILES("[O][O]"),
+)
+
+
+species(
+    label='Ar',
+    reactive=False,
+    structure=SMILES("[Ar]"),
+)
+
+# Reaction systems
+simpleReactor(
+    temperature=(1500,'K'),
+    pressure=(2.0,'bar'),
+    initialMoleFractions={
+        "ethane": 1,
+        "O2": 3.5,
+        "Ar": 1,
+    },
+    terminationConversion={
+        'O2': 0.001,
+    },
+    terminationTime=(1,'s'),
+)
+
+simulator(
+    atol=1e-16,
+    rtol=1e-8,
+)
+
+model(
+    toleranceKeepInEdge=0.0001,
+    toleranceMoveToCore=0.5,
+    maximumEdgeSpecies=1000
+)
+
+pressureDependence(
+    method='modified strong collision',
+    maximumGrainSize=(0.5,'kcal/mol'),
+    minimumNumberOfGrains=250,
+    temperatures=(300,3000,'K',8),
+    pressures=(0.001,100,'bar',5),
+    interpolation=('Chebyshev', 6, 4),
+)
+
+options(
+    units='si',
+    saveRestartPeriod=None,
+    drawMolecules=False,
+    generatePlots=False,
+)
