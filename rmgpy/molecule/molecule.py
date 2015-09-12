88404efe79fa9792e67fce8c6db63b6233654490
Nick Vandewiele <nickvandewiele@gmail.com>
2015-09-12 12:55:35 -0400

    do not remove H's when generating an rdmol in generateAroResonanceIsomers

--- rmgpy/molecule/molecule.py
+++ rmgpy/molecule/molecule.py
@@ -1692,7 +1692,7 @@ class Molecule(Graph):
         if self.isCyclic():
             molecule = self.copy(deep=True)
             try:
-                rdkitmol, rdAtomIndices = parser.toRDKitMol(molecule, removeHs=True, returnMapping=True)
+                rdkitmol, rdAtomIndices = parser.toRDKitMol(molecule, removeHs=False, returnMapping=True)
             except:
                 return []
             aromatic = False
