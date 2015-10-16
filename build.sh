931c1150101fffac9d4a1c2d531bf89f692abd91
Richard West <r.west@neu.edu>
2015-10-16 01:09:54 -0400

    Make anaconda build use official __version__ number

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ make -j${CPU_COUNT}
 make QM
 $PYTHON setup.py install
 
+# Save version number stored in rmgpy/__init__.py file
+$PYTHON -c 'from rmgpy import __version__; print __version__' > ${SRC_DIR}/__conda_version__.txt
+
 # lazy "install" of everything in our 'external' folder.
 # most of which should probably be elsewhere
 cp -R ${SRC_DIR}/external ${SP_DIR}
