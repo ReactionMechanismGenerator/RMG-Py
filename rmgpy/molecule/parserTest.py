3de4a5f2ddb1bd69828dbc9406b0ceb45562e9fe
Connie Gao <connieg@mit.edu>
2015-10-17 15:54:19 -0400

    Add a unit test for parser.toRDKitMol() to test atoms and bondMappings

--- rmgpy/molecule/parserTest.py
+++ rmgpy/molecule/parserTest.py
@@ -47,3 +47,33 @@ class parserTest(unittest.TestCase):
         aug_inchi = 'InChI=1S/CH2/h1H2/mult1/u0'
         mol = parser.fromAugmentedInChI(Molecule(), aug_inchi)
         self.assertTrue(mol.multiplicity == 1)
+
+    def test_toRDKitMol(self):
+        """
+        Test that toRDKitMol returns correct indices and atom mappings.
+        """
+        bondOrderDict = {'SINGLE':'S','DOUBLE':'D','TRIPLE':'T','AROMATIC':'B'}
+        mol = parser.fromSMILES(Molecule(), 'C1CCC=C1C=O')
+        rdkitmol, rdAtomIndices = mol.toRDKitMol(removeHs=False, returnMapping=True, sanitize=True)
+        for atom in mol.atoms:
+            # Check that all atoms are found in mapping
+            self.assertTrue(atom in rdAtomIndices)
+            # Check that all bonds are in rdkitmol with correct mapping and order
+            for connectedAtom, bond in atom.bonds.iteritems():
+                bondType = str(rdkitmol.GetBondBetweenAtoms(rdAtomIndices[atom],rdAtomIndices[connectedAtom]).GetBondType())
+                rdkitBondOrder = bondOrderDict[bondType]
+                self.assertEqual(bond.order, rdkitBondOrder)
+        
+        # Test for removeHs = True        
+        rdkitmol2, rdAtomIndices2 = mol.toRDKitMol(removeHs=True, returnMapping=True, sanitize=True)
+        
+        for atom in mol.atoms:
+            # Check that all non-hydrogen atoms are found in mapping
+            if atom.symbol != 'H':
+                self.assertTrue(atom in rdAtomIndices)
+                # Check that all bonds connected to non-hydrogen have the correct mapping and order
+                for connectedAtom, bond in atom.bonds.iteritems():
+                    if connectedAtom.symbol != 'H':
+                        bondType = str(rdkitmol.GetBondBetweenAtoms(rdAtomIndices[atom],rdAtomIndices[connectedAtom]).GetBondType())
+                        rdkitBondOrder = bondOrderDict[bondType]
+                        self.assertEqual(bond.order, rdkitBondOrder)   
