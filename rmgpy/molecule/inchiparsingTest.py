5a1aba903c3a452261a459394176bd7c15e2a4c5
Nick Vandewiele <nickvandewiele@gmail.com>
2015-10-23 11:45:14 -0400

    Create InChIGenerationTest class within inchiparsingTest
    a first step to create separate tests for parsing and generation.

--- rmgpy/molecule/inchiparsingTest.py
+++ rmgpy/molecule/inchiparsingTest.py
@@ -1,15 +1,14 @@
+import re
 import unittest
 
 from rmgpy.molecule import Molecule
 from rmgpy.molecule.parser import fromAugmentedInChI
 from rmgpy.species import Species
 
-class InChIParsingGenerationTest(unittest.TestCase):
+class InChIParsingTest(unittest.TestCase):
 
     def compare(self, inchi, mult, u_indices=[]):
         from rmgpy.molecule.util import retrieveElementCount, VALENCES, ORDERS
-        from rmgpy.species import Species
-        import re
         
         aug_inchi = 'InChI=1/' + inchi  + '/mult' + str(mult)
         u_layer = ','.join([str(i) for i in u_indices]) if u_indices else None
@@ -36,9 +35,6 @@ class InChIParsingGenerationTest(unittest.TestCase):
     def test_Ethane_parsing(self):
         inchi = 'C2H6/c1-2/h1-2H3'
         mult = 1
-
-        aug_inchi = 'InChI=1/' + inchi  + '/mult' + str(mult)
-
         self.compare(inchi, mult)
         
     def test_Ethyl_parsing(self):
@@ -323,30 +319,6 @@ class InChIParsingGenerationTest(unittest.TestCase):
         u_indices = [1, 1]
         mol = self.compare(inchi, mult, u_indices)
 
-    def test_Buta13diyl_triplet(self):
-        """
-        C=CC.C.
-        """
-        adjlist = """
-        multiplicity 3
-1  C u1 p0 c0 {2,S} {5,S} {6,S}
-2  C u1 p0 c0 {1,S} {3,S} {7,S}
-3  C u0 p0 c0 {2,S} {4,D} {8,S}
-4  C u0 p0 c0 {3,D} {9,S} {10,S}
-5  H u0 p0 c0 {1,S}
-6  H u0 p0 c0 {1,S}
-7  H u0 p0 c0 {2,S}
-8  H u0 p0 c0 {3,S}
-9  H u0 p0 c0 {4,S}
-10 H u0 p0 c0 {4,S}
-"""
-        mol = Molecule().fromAdjacencyList(adjlist)
-        spc = Species(molecule=[mol])
-
-        aug_inchi = spc.getAugmentedInChI()
-        self.assertEqual(Species(molecule=[Molecule().fromAugmentedInChI(aug_inchi)]).isIsomorphic(spc), True)
-
-
     def test_C6H8O2(self):
         inchi = 'C6H8O2/c1-3-5(7)6(8)4-2/h3-6H,1-2H2'
         mult = 3
@@ -621,32 +593,6 @@ multiplicity 3
         mult = 3
         u_indices = [1,3]
         self.compare(inchi, mult, u_indices)
-
-
-    def test_C6H10_tetrarad(self):
-        adjlist = """
-1  C u1 p0 c0 {3,S} {7,S} {8,S}
-2  C u1 p0 c0 {4,S} {9,S} {10,S}
-3  C u1 p0 c0 {1,S} {5,S} {11,S}
-4  C u1 p0 c0 {2,S} {6,S} {12,S}
-5  C u0 p0 c0 {3,S} {6,S} {13,S} {14,S}
-6  C u0 p0 c0 {4,S} {5,S} {15,S} {16,S}
-7  H u0 p0 c0 {1,S}
-8  H u0 p0 c0 {1,S}
-9  H u0 p0 c0 {2,S}
-10 H u0 p0 c0 {2,S}
-11 H u0 p0 c0 {3,S}
-12 H u0 p0 c0 {4,S}
-13 H u0 p0 c0 {5,S}
-14 H u0 p0 c0 {5,S}
-15 H u0 p0 c0 {6,S}
-16 H u0 p0 c0 {6,S}
-        """
-
-        mol = Molecule().fromAdjacencyList(adjlist)
-
-        aug_inchi = mol.toAugmentedInChI()
-        self.assertTrue(aug_inchi.endswith('/u1,2,3,4'))
     
     def test_C8H22(self):
         from rmgpy.molecule.parser import toRDKitMol, parse_E_layer
@@ -725,112 +671,6 @@ multiplicity 3
         expected = [[1, 2, 3], [4, 5, 6]]
         self.assertTrue(len(e_layer) == len(expected) and sorted(e_layer) == sorted(expected))
 
-    def test_C7H8(self):
-        """Looks a lot like toluene but with 1 double bond replaced by a biradical."""
-
-        """unpaired electrons on tertiary carbon, and on carbon in para position."""
-        adjlist = """
-1  C u1 p0 c0 {2,S} {3,S} {4,S}
-2  C u0 p0 c0 {1,S} {7,D} {10,S}
-3  C u0 p0 c0 {1,S} {6,D} {11,S}
-4  C u0 p0 c0 {1,S} {12,S} {13,S} {14,S}
-5  C u1 p0 c0 {6,S} {7,S} {15,S}
-6  C u0 p0 c0 {3,D} {5,S} {8,S}
-7  C u0 p0 c0 {2,D} {5,S} {9,S}
-8  H u0 p0 c0 {6,S}
-9  H u0 p0 c0 {7,S}
-10 H u0 p0 c0 {2,S}
-11 H u0 p0 c0 {3,S}
-12 H u0 p0 c0 {4,S}
-13 H u0 p0 c0 {4,S}
-14 H u0 p0 c0 {4,S}
-15 H u0 p0 c0 {5,S}
-        """
-
-        mol = Molecule().fromAdjacencyList(adjlist)
-        spc = Species(molecule=[mol])
-        spc.generateResonanceIsomers()
-        from rmgpy.molecule.parser import create_U_layer
-        for mol in spc.molecule:
-            print create_U_layer(mol)
-        print spc.getAugmentedInChI()
-
-        inchi = 'InChI=1S/C7H8/c1-7-5-3-2-4-6-7/h2-6H,1H3'
-        mult = 3
-        u_indices = [2,3]
-        self.compare(inchi, mult, u_indices) 
-    
-    def test_C8H8(self):
-        """Looks a lot like cycloctene but with 1 double bond replaced by a biradical."""
-
-        adjlist = """
-1  C u0 p0 c0 {2,S} {5,D} {9,S}
-2  C u0 p0 c0 {1,S} {3,D} {10,S}
-3  C u0 p0 c0 {2,D} {4,S} {11,S}
-4  C u1 p0 c0 {3,S} {6,S} {12,S}
-5  C u0 p0 c0 {1,D} {8,S} {14,S}
-6  C u1 p0 c0 {4,S} {7,S} {15,S}
-7  C u0 p0 c0 {6,S} {8,D} {13,S}
-8  C u0 p0 c0 {5,S} {7,D} {16,S}
-9  H u0 p0 c0 {1,S}
-10 H u0 p0 c0 {2,S}
-11 H u0 p0 c0 {3,S}
-12 H u0 p0 c0 {4,S}
-13 H u0 p0 c0 {7,S}
-14 H u0 p0 c0 {5,S}
-15 H u0 p0 c0 {6,S}
-16 H u0 p0 c0 {8,S}
-        """
-
-        mol = Molecule().fromAdjacencyList(adjlist)
-        spc = Species(molecule=[mol])
-        spc.generateResonanceIsomers()
-        from rmgpy.molecule.parser import create_U_layer
-        for mol in spc.molecule:
-            print create_U_layer(mol)
-        print spc.getAugmentedInChI()
-
-        # inchi = 'InChI=1S/C7H8/c1-7-5-3-2-4-6-7/h2-6H,1H3'
-        # mult = 3
-        # u_indices = [2,3]
-        # self.compare(inchi, mult, u_indices) 
-
-    def test_benzyne(self):
-        benzyne = 'InChI=1S/C6H4/c1-2-4-6-5-3-1/h1-4H'
-
-        adjlist = """
-1  C u0 p0 c0 {2,T} {6,S}
-2  C u0 p0 c0 {1,T} {3,S}
-3  C u0 p0 c0 {2,S} {4,D} {7,S}
-4  C u0 p0 c0 {3,D} {5,S} {8,S}
-5  C u0 p0 c0 {4,S} {6,D} {9,S}
-6  C u0 p0 c0 {1,S} {5,D} {10,S}
-7  H u0 p0 c0 {3,S}
-8  H u0 p0 c0 {4,S}
-9  H u0 p0 c0 {5,S}
-10 H u0 p0 c0 {6,S}
-        """
-        mol = Molecule().fromAdjacencyList(adjlist)
-        spc = Species(molecule=[mol])
-        spc.generateResonanceIsomers()
-        from rmgpy.molecule.parser import create_U_layer
-        for mol in spc.molecule:
-            print create_U_layer(mol)
-        print spc.getAugmentedInChI()
-
-        benzatetraene = 'InChI=1S/C6H4/c1-2-4-6-5-3-1/h1-4H'
-
-        mult = 1
-        u_indices = []
-        self.compare(benzyne, mult, u_indices)
-
-    def test_H(self):
-        adjlist = """
-multiplicity 2
-1 H u1 p0 c0
-"""
-        mol = Molecule().fromAdjacencyList(adjlist)
-        print mol.toAugmentedInChI()
 
 class ParseELayerTest(unittest.TestCase):
     def test_no_equivalence_layer(self):
@@ -912,6 +752,174 @@ class CreateULayerTest(unittest.TestCase):
             u_layers.append(u_layer)
 
         self.assertEquals(u_layers[0], u_layers[1])
+
+class InChIGenerationTest(unittest.TestCase):
+    def compare(self, adjlist, aug_inchi):
+        spc = Species(molecule=[Molecule().fromAdjacencyList(adjlist)])
+        spc.generateResonanceIsomers()
+
+        ignore_prefix = r"(InChI=1+)(S*)/"
+
+        exp = re.split(ignore_prefix, aug_inchi)[-1]
+        comp = re.split(ignore_prefix, spc.getAugmentedInChI())[-1]
+        self.assertEquals(exp, comp)
+
+    def test_C5H5(self):
+        """
+        Test that the unpaired electron of 1,3-cyclopentadienyl radical always
+        ends up on the 1-carbon atom.
+        """
+
+        adjlist = """
+1 C 0 {2,D} {5,S}
+2 C 0 {1,D} {3,S} 
+3 C 0 {2,S} {4,D} 
+4 C 0 {3,D} {5,S} 
+5 C 1 {4,S} {1,S}
+        """
+
+        aug_inchi = 'InChI=1S/C5H5/c1-2-4-5-3-1/h1-5H/mult2/u1'
+        self.compare(adjlist, aug_inchi)
+
+
+    def test_C7H8(self):
+        """Looks a lot like toluene but with 1 double bond replaced by a biradical."""
+
+        """unpaired electrons on tertiary carbon, and on carbon in para position."""
+        adjlist = """
+1  C u1 p0 c0 {2,S} {3,S} {4,S}
+2  C u0 p0 c0 {1,S} {7,D} {10,S}
+3  C u0 p0 c0 {1,S} {6,D} {11,S}
+4  C u0 p0 c0 {1,S} {12,S} {13,S} {14,S}
+5  C u1 p0 c0 {6,S} {7,S} {15,S}
+6  C u0 p0 c0 {3,D} {5,S} {8,S}
+7  C u0 p0 c0 {2,D} {5,S} {9,S}
+8  H u0 p0 c0 {6,S}
+9  H u0 p0 c0 {7,S}
+10 H u0 p0 c0 {2,S}
+11 H u0 p0 c0 {3,S}
+12 H u0 p0 c0 {4,S}
+13 H u0 p0 c0 {4,S}
+14 H u0 p0 c0 {4,S}
+15 H u0 p0 c0 {5,S}
+        """
+
+        aug_inchi = 'InChI=1S/C7H8/c1-7-5-3-2-4-6-7/h2-6H,1H3/mult3/u2,3'
+        self.compare(adjlist, aug_inchi)
+    
+    def test_C8H8(self):
+        """Looks a lot like cycloctene but with 1 double bond replaced by a biradical."""
+
+        adjlist = """
+1  C u0 p0 c0 {2,S} {5,D} {9,S}
+2  C u0 p0 c0 {1,S} {3,D} {10,S}
+3  C u0 p0 c0 {2,D} {4,S} {11,S}
+4  C u1 p0 c0 {3,S} {6,S} {12,S}
+5  C u0 p0 c0 {1,D} {8,S} {14,S}
+6  C u1 p0 c0 {4,S} {7,S} {15,S}
+7  C u0 p0 c0 {6,S} {8,D} {13,S}
+8  C u0 p0 c0 {5,S} {7,D} {16,S}
+9  H u0 p0 c0 {1,S}
+10 H u0 p0 c0 {2,S}
+11 H u0 p0 c0 {3,S}
+12 H u0 p0 c0 {4,S}
+13 H u0 p0 c0 {7,S}
+14 H u0 p0 c0 {5,S}
+15 H u0 p0 c0 {6,S}
+16 H u0 p0 c0 {8,S}
+        """
+
+        aug_inchi = 'InChI=1S/C8H8/c1-2-4-6-8-7-5-3-1/h1-8H/mult3/u1,2'
+        self.compare(adjlist, aug_inchi)
+
+    def test_benzyne(self):
+
+        adjlist = """
+1  C u0 p0 c0 {2,T} {6,S}
+2  C u0 p0 c0 {1,T} {3,S}
+3  C u0 p0 c0 {2,S} {4,D} {7,S}
+4  C u0 p0 c0 {3,D} {5,S} {8,S}
+5  C u0 p0 c0 {4,S} {6,D} {9,S}
+6  C u0 p0 c0 {1,S} {5,D} {10,S}
+7  H u0 p0 c0 {3,S}
+8  H u0 p0 c0 {4,S}
+9  H u0 p0 c0 {5,S}
+10 H u0 p0 c0 {6,S}
+        """
+        benzatetraene = 'InChI=1S/C6H4/c1-2-4-6-5-3-1/h1-4H'
+        aug_inchi = 'InChI=1S/C6H4/c1-2-4-6-5-3-1/h1-4H/mult1'
+        self.compare(adjlist, aug_inchi)
+
+    def test_H(self):
+        adjlist = """
+multiplicity 2
+1 H u1 p0 c0
+"""
+        aug_inchi = 'InChI=1S/H/mult2/u1'
+        self.compare(adjlist, aug_inchi)
+
+
+    def test_C6H8(self):
+        """
+        Test that the 2 unpaired electrons of .CC(=C)C(C.)=C
+        do not end up at the same side of the central C-C bond.
+        """
+        adjlist = """
+1 C 0 {2,D}
+2 C 0 {1,D} {3,S} {4,S}
+3 C 1 {2,S}
+4 C 0 {2,S} {5,S} {6,D}
+5 C 1 {4,S}
+6 C 0 {4,D}
+        """
+
+        aug_inchi = 'InChI=1S/C6H8/c1-5(2)6(3)4/h1-4H2/mult3/u1,3'
+        self.compare(adjlist, aug_inchi)
+
+
+    def test_C6H10_tetrarad(self):
+        adjlist = """
+1  C u1 p0 c0 {3,S} {7,S} {8,S}
+2  C u1 p0 c0 {4,S} {9,S} {10,S}
+3  C u1 p0 c0 {1,S} {5,S} {11,S}
+4  C u1 p0 c0 {2,S} {6,S} {12,S}
+5  C u0 p0 c0 {3,S} {6,S} {13,S} {14,S}
+6  C u0 p0 c0 {4,S} {5,S} {15,S} {16,S}
+7  H u0 p0 c0 {1,S}
+8  H u0 p0 c0 {1,S}
+9  H u0 p0 c0 {2,S}
+10 H u0 p0 c0 {2,S}
+11 H u0 p0 c0 {3,S}
+12 H u0 p0 c0 {4,S}
+13 H u0 p0 c0 {5,S}
+14 H u0 p0 c0 {5,S}
+15 H u0 p0 c0 {6,S}
+16 H u0 p0 c0 {6,S}
+        """
+
+        aug_inchi = 'InChI=1S/C6H10/c1-3-5-6-4-2/h3-4H,1-2,5-6H2/mult5/u1,2,3,4'
+        self.compare(adjlist, aug_inchi)
+
+    def test_Buta13diyl_triplet(self):
+        """
+        C=CC.C.
+        """
+        adjlist = """
+        multiplicity 3
+1  C u1 p0 c0 {2,S} {5,S} {6,S}
+2  C u1 p0 c0 {1,S} {3,S} {7,S}
+3  C u0 p0 c0 {2,S} {4,D} {8,S}
+4  C u0 p0 c0 {3,D} {9,S} {10,S}
+5  H u0 p0 c0 {1,S}
+6  H u0 p0 c0 {1,S}
+7  H u0 p0 c0 {2,S}
+8  H u0 p0 c0 {3,S}
+9  H u0 p0 c0 {4,S}
+10 H u0 p0 c0 {4,S}
+"""
+
+        aug_inchi = 'InChI=1S/C4H6/c1-3-4-2/h3-4H,1-2H2/mult3/u1,2'
+        self.compare(adjlist, aug_inchi)
 
 if __name__ == '__main__':
     unittest.main()
