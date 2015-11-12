faa7e38e777945ef22d00fe4aca3040c5dd76785
Nick Vandewiele <nickvandewiele@gmail.com>
2015-11-12 17:12:11 -0500

    remove updateConnectivityValues from Molecule.update(...)
    this function should only be called when the vertices need to be
    resorted. The sortVertices method will figure out whether that is true
    or not.

--- rmgpy/molecule/molecule.py
+++ rmgpy/molecule/molecule.py
@@ -699,7 +699,6 @@ class Molecule(Graph):
         Update multiplicity, and sort atoms using the new
         connectivity values.
         """
-        self.updateConnectivityValues()
         self.updateAtomTypes()
         self.updateMultiplicity()
         self.sortVertices()
