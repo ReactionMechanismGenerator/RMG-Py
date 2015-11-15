54cd6e5fa36d3083aeb4839755ac2eeb2ee34946
Nick Vandewiele <nickvandewiele@gmail.com>
2015-11-15 11:52:18 -0500

    add GenerateReactionsTest unit test

---
+++ rmgpy/tools/testGenerateReactions.py
@@ -0,0 +1,19 @@
+import unittest
+import os.path
+
+from rmgpy.rmg.main import RMG
+from rmgpy.tools.generate_reactions import *
+
+class GenerateReactionsTest(unittest.TestCase):
+
+    def test(self):
+        folder = os.path.join(os.getcwd(),'rmgpy/tools/data/generate')
+        
+        inputFile = os.path.join(folder,'input.py')
+        
+        rmg = RMG()
+        rmg = execute(rmg, inputFile, folder)
+
+        self.assertIsNotNone(rmg)
+        self.assertIsNotNone(rmg.reactionModel.outputSpeciesList)
+        self.assertIsNotNone(rmg.reactionModel.outputReactionList)
