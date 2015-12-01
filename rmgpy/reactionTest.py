cf9bb1ab52eddecd7e722298e4fc2c022e99c7e9
Nick Vandewiele <nickvandewiele@gmail.com>
2015-11-30 20:50:08 -0500

    added reverse coefficent test for Troe format

--- rmgpy/reactionTest.py
+++ rmgpy/reactionTest.py
@@ -786,6 +786,70 @@ class TestReaction(unittest.TestCase):
             krevrev = reversereverseKinetics.getRateCoefficient(T, P)
             self.assertAlmostEqual(korig / krevrev, 1.0, 0)
 
+
+    def testGenerateReverseRateCoefficientTroe(self):
+        """
+        Test the Reaction.generateReverseRateCoefficient() method works for the Troe format.
+        """
+
+        from rmgpy.kinetics import Troe
+
+        arrheniusHigh = Arrhenius(
+            A = (1.39e+16,"cm^3/(mol*s)"), 
+            n = -0.534, 
+            Ea = (2.243,"kJ/mol"), 
+            T0 = (1,"K"),
+        )
+        arrheniusLow = Arrhenius(
+            A = (2.62e+33,"cm^6/(mol^2*s)"), 
+            n = -4.76, 
+            Ea = (10.21,"kJ/mol"), 
+            T0 = (1,"K"),
+        )
+        alpha = 0.783
+        T3 = 74
+        T1 = 2941
+        T2 = 6964
+        efficiencies = {"C": 3, "C(=O)=O": 2, "CC": 3, "O": 6, "[Ar]": 0.7, "[C]=O": 1.5, "[H][H]": 2}
+        Tmin = 300.
+        Tmax = 2000.
+        Pmin = 0.01
+        Pmax = 100.
+        comment = """H + CH3 -> CH4"""
+        troe = Troe(
+            arrheniusHigh = arrheniusHigh,
+            arrheniusLow = arrheniusLow,
+            alpha = alpha,
+            T3 = (T3,"K"),
+            T1 = (T1,"K"),
+            T2 = (T2,"K"),
+            Tmin = (Tmin,"K"),
+            Tmax = (Tmax,"K"),
+            Pmin = (Pmin,"bar"),
+            Pmax = (Pmax,"bar"),
+            efficiencies = efficiencies,
+            comment = comment,
+        )
+         
+        original_kinetics = troe
+        
+        self.reaction2.kinetics = original_kinetics
+
+        reverseKinetics = self.reaction2.generateReverseRateCoefficient()
+
+        self.reaction2.kinetics = reverseKinetics
+        # reverse reactants, products to ensure Keq is correctly computed
+        self.reaction2.reactants, self.reaction2.products = self.reaction2.products, self.reaction2.reactants
+        reversereverseKinetics = self.reaction2.generateReverseRateCoefficient()
+
+        # check that reverting the reverse yields the original
+        Tlist = numpy.arange(Tmin, Tmax, 200.0, numpy.float64)
+        P = 1e5
+        for T in Tlist:
+            korig = original_kinetics.getRateCoefficient(T, P)
+            krevrev = reversereverseKinetics.getRateCoefficient(T, P)
+            self.assertAlmostEqual(korig / krevrev, 1.0, 0)
+
     def testTSTCalculation(self):
         """
         A test of the transition state theory k(T) calculation function,
