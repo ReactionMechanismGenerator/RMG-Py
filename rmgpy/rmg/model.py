fa309bf0c76768d5451242f1cf5249da0795ddb8
Connie Gao <connieg@mit.edu>
2015-12-04 18:13:54 -0500

    Add comments to ReactionModel.react function indicate uni or bi-molecular

--- rmgpy/rmg/model.py
+++ rmgpy/rmg/model.py
@@ -634,10 +634,12 @@ class CoreEdgeReactionModel:
         """
         reactionList = []
         if speciesB is None:
+            # React in a unimolecular reaction
             for moleculeA in speciesA.molecule:
                 reactionList.extend(database.kinetics.generateReactionsFromFamilies([moleculeA], products=None))
                 moleculeA.clearLabeledAtoms()
         else:
+            # React in a bimolecular reaction
             for moleculeA in speciesA.molecule:
                 for moleculeB in speciesB.molecule:
                     reactionList.extend(database.kinetics.generateReactionsFromFamilies([moleculeA, moleculeB], products=None))
