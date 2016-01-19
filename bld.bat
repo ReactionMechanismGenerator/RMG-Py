52a675e74b77bf66abc9a831d24b589d12424ecb
Connie Gao <connieg@mit.edu>
2016-01-19 00:44:08 -0500

    Beginnings of a windows build for RMG-Py

---
+++ bld.bat
@@ -0,0 +1,17 @@
+set CC=gcc
+set CXX=g++
+set F77=gfortran
+set F90=gfortran
+
+
+mingw32-make -j${CPU_COUNT}
+mingw32-make QM
+
+%PYTHON% setup.py install
+
+:: Save version number stored in rmgpy/__init__.py file
+%PYTHON% -c 'from rmgpy import __version__; print __version__' > %SRC_DIR%\__conda_version__.txt
+
+:: lazy "install" of everything in our 'external' folder.
+:: most of which should probably be elsewhere
+copy -R %SRC_DIR%\external %SP_DIR%
