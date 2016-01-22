c1881be77a8ad5bcde4819180166b0dbce545331
Connie Gao <connieg@mit.edu>
2016-01-22 12:17:14 -0500

    Have generateReactions duplicate test be more specific
    Instead of checking that there are 13 reactions total, check instead that the particular reaction is not duplicated.

--- rmgpy/tools/testGenerateReactions.py
+++ rmgpy/tools/testGenerateReactions.py
@@ -33,20 +33,27 @@ class GenerateReactionsTest(unittest.TestCase):
         only appears once in the model.
 
         """
+
+        from rmgpy.reaction import Reaction
+        from rmgpy.molecule import Molecule
         folder = os.path.join(os.path.dirname(rmgpy.__file__),'tools/data/generate/duplicates')
         
         inputFile = os.path.join(folder,'input.py')
-        
+
         rmg = RMG()
         rmg = execute(rmg, inputFile, folder)
 
         self.assertIsNotNone(rmg)
+        
+        rxnFlagged = Reaction(reactants=[Molecule(SMILES='[CH]=O'),Molecule(SMILES='C=O')],
+                       products=[Molecule(SMILES='[CH2]OC=O')])
 
-        """
-        13 reactions will be created,12 from the seed mechanism,
-        1 additional one through the reaction family.
-        """
-        self.assertEquals(13, len(rmg.reactionModel.core.reactions))
+        count = 0
+        for reaction in rmg.reactionModel.core.reactions:
+            if reaction.isIsomorphic(rxnFlagged):
+                count += 1
+
+        self.assertEquals(count, 1)
 
         shutil.rmtree(os.path.join(folder,'pdep'))
 
