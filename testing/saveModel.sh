d5e0f8e69c761cccb53d5876f02c485081659ce3
Nick Vandewiele <nickvandewiele@gmail.com>
2016-01-24 15:31:08 -0500

    echo RMG-Py commit in saveModel.sh

---
+++ testing/saveModel.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo 'Travis Build Dir: '$TRAVIS_BUILD_DIR
+
+if $TRAVIS_PULL_REQUEST
+  then
+    GIT_BRANCH=$TRAVIS_PULL_REQUEST #"$(git rev-parse --abbrev-ref HEAD)"
+else
+  GIT_BRANCH=$TRAVIS_BRANCH
+fi
+
+echo 'Checked out RMG-Py branch: '$GIT_BRANCH
+
+echo 'Creating RMG-tests branch...'
+cd $TRAVIS_BUILD_DIR/../RMG-tests
+git checkout -b $GIT_BRANCH || true 
+git checkout $GIT_BRANCH
+
+echo 'Committing to RMG-tests branch with RMG-Py commit:  '$TRAVIS_COMMIT
+git commit -m $TRAVIS_COMMIT
+
+
