a134d3542c4f77db6441a63354360574b07d5e98
Nick Vandewiele <nickvandewiele@gmail.com>
2016-01-25 16:30:03 -0500

    update deploy.sh, travis based on SSH authetication
    rather than Personal Access Token
    
    private SSH deploy key for RMG-tests is encrypted in .enc file
    located in RMG-tests repo. It is used with encryption label
    stored as env var in current repo's travis yaml.

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,77 @@
 #!/bin/bash
+
 set -e # exit with nonzero exit code if anything fails
 
 echo 'Travis Build Dir: '$TRAVIS_BUILD_DIR
 
 if $TRAVIS_PULL_REQUEST
   then
-    GIT_BRANCH=$TRAVIS_PULL_REQUEST #"$(git rev-parse --abbrev-ref HEAD)"
+    DEPLOY_BRANCH=$TRAVIS_PULL_REQUEST
 else
-  GIT_BRANCH=$TRAVIS_BRANCH
+  DEPLOY_BRANCH=$TRAVIS_BRANCH
 fi
 
-echo 'Checked out RMG-Py branch: '$GIT_BRANCH
+# Deploy built site to this branch
+TARGET_BRANCH=GIT_BRANCH
 
-echo 'Creating RMG-tests branch...'
-cd $TRAVIS_BUILD_DIR/../RMG-tests
+
+REPO='https://github.com/ReactionMechanismGenerator/RMG-tests.git'
+
+REPO_NAME=$(basename $REPO)
+TARGET_DIR=$(mktemp -d /tmp/$REPO_NAME.XXXX)
+REV=$TRAVIS_COMMIT
+git clone ${REPO} ${TARGET_DIR}
+
+cd $TARGET_DIR
+
 git checkout -b $GIT_BRANCH || true 
 git checkout $GIT_BRANCH
 
+if [ -n "$TRAVIS_BUILD_ID" ]; then
+  # When running on Travis we need to use SSH to deploy to GitHub
+  #
+  # The following converts the repo URL to an SSH location,
+  # decrypts the SSH key and sets up the Git config with
+  # the correct user name and email (globally as this is a
+  # temporary travis environment)
+  #
+  # Set the following environment variables in the travis configuration (.travis.yml)
+  #
+  #   DEPLOY_BRANCH    - The only branch that Travis should deploy from
+  #   ENCRYPTION_LABEL - The label assigned when encrypting the SSH key using travis encrypt-file
+  #   GIT_NAME         - The Git user name
+  #   GIT_EMAIL        - The Git user email
+  #
+  echo DEPLOY_BRANCH: $DEPLOY_BRANCH
+  echo ENCRYPTION_LABEL: $ENCRYPTION_LABEL
+  echo GIT_NAME: $GIT_NAME
+  echo GIT_EMAIL: $GIT_EMAIL
+  if [ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
+    echo "Travis should only deploy from the DEPLOY_BRANCH ($DEPLOY_BRANCH) branch"
+    exit 0
+  else
+    if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+      echo "Travis should not deploy from pull requests"
+      exit 0
+    else
+      ENCRYPTED_KEY_VAR=encrypted_${ENCRYPTION_LABEL}_key
+      ENCRYPTED_IV_VAR=encrypted_${ENCRYPTION_LABEL}_iv
+      ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+      ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+      REPO=${REPO/git:\/\/github.com\//git@github.com:}
+      
+      # The `deploy_key.enc` file should have been added to the repo and should
+      # have been created from the deploy private key using `travis encrypt-file`
+      openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+      
+      chmod 600 deploy_key
+      eval `ssh-agent -s`
+      ssh-add deploy_key
+      git config --global user.name "$GIT_NAME"
+      git config --global user.email "$GIT_EMAIL"
+    fi
+  fi
+fi
 
-echo 'Committing to RMG-tests branch with RMG-Py commit:  '$TRAVIS_COMMIT
-git commit --allow-empty -m $TRAVIS_COMMIT
+git commit --allow-empty -m "Built from commit $REV"
+git push $REPO $TARGET_BRANCH
