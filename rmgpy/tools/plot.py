45f977e708e396e0c00892f43115a5fcd9c725b8
Connie Gao <connieg@mit.edu>
2016-02-22 15:47:13 -0500

    Create GenericData and GenericPlot classes, meant for primarily plotting RMG stuff
    GenericData contains attributes that are most commonly
    used for rmg-related data
    
    GenericPlot also contains smart functions for plotting
    against time and plotting barplots

--- rmgpy/tools/plot.py
+++ rmgpy/tools/plot.py
@@ -78,3 +78,117 @@ def findNearest(array, value):
     import numpy
     idx = (numpy.abs(array-value)).argmin()
     return idx
+
+class GenericData(object):
+    """
+    A generic data class for the purpose of plotting.
+    label is the original label for the data
+    data is the numpy array containing the data
+    The other parameters are additional common flags for data found in RMG
+    """
+    def __init__(self, label='', data=None, species=None, reaction=None, units=None, index=None):
+        self.label = label
+        self.data = data
+        self.species = species
+        self.reaction = reaction
+        self.units = units
+        self.index = index
+
+class GenericPlot(object):
+    """
+    A generic plotting class that can be extended to plot other things.
+    """
+    def __init__(self, xVar=None, yVar=None, title='', xlabel='', ylabel=''):
+        self.xVar = xVar
+        self.yVar = yVar
+        self.title = title
+        self.xlabel = xlabel
+        self.ylabel = ylabel
+        
+    def plot(self, filename=''):
+        """
+        Execute the actual plotting
+        """
+        import matplotlib as mpl
+        import matplotlib.pyplot as plt
+        
+        mpl.rc('font',family='monospace')
+        fig=plt.figure()
+        
+        ax = fig.add_subplot(111)
+        xVar = self.xVar
+        yVar = self.yVar
+        # Convert yVar to a list if it wasn't one already
+        if isinstance(yVar, GenericData):
+            yVar = [yVar]
+        
+            
+        if len(yVar) == 1:
+            y = yVar[0]
+            ax.plot(xVar.data, y.data)
+            
+            # Create a ylabel for the label of the y variable
+            if not self.ylabel and y.label:
+                ylabel = y.label
+                if y.units: ylabel += ' ({0})'.format(y.units)
+                plt.ylabel(ylabel)
+        else:
+            for y in yVar:
+                ax.plot(xVar.data, y.data, '-', label=y.label)
+        
+        if self.xlabel:
+            plt.xlabel(self.xlabel)
+        elif xVar.label:
+            xlabel = xVar.label
+            if xVar.units: xlabel += ' ({0})'.format(xVar.units)
+            plt.xlabel(xlabel)
+            
+        if self.ylabel:
+            plt.ylabel(self.ylabel)
+            
+        if self.title:
+            plt.title(self.title)
+            
+        ax.grid('on')
+        handles, labels = ax.get_legend_handles_labels()
+        if labels:
+            # Create a legend outside the plot and adjust width based off of longest legend label
+            maxStringLength = max([len(label) for label in labels])
+            width = 1.1 + .011*maxStringLength
+            legend = ax.legend(handles,labels,loc='upper center', numpoints=1, bbox_to_anchor=(width,1)) #bbox_to_anchor=(1.01,.9)
+            fig.savefig(filename, bbox_extra_artists=(legend,), bbox_inches='tight')
+        else:
+            fig.savefig(filename)
+            
+    def barplot(self, filename='', idx=None):
+        """
+        Plot a generic barplot using just the yVars.
+        idx is the index of the each y-variable to be plotted. if not given, the last value will be used
+        """
+        import matplotlib as mpl
+        import matplotlib.pyplot as plt
+        import numpy
+        
+        mpl.rc('font',family='monospace')
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        
+        position = numpy.arange(len(self.yVar),0,-1)+0.5
+        # Reverse in order to go front top to bottom
+        
+        if not idx:
+            idx = -1
+        ax.barh(position, numpy.array([y.data[idx] for y in self.yVar]), align='center', alpha=0.5)
+        plt.yticks(position, [y.label for y in self.yVar])
+        
+        # If any labels or titles are explicitly specified, write them
+        if self.xlabel:
+            plt.xlabel(self.xlabel)
+            
+        if self.ylabel:
+            plt.ylabel(self.ylabel)
+            
+        if self.title:
+            plt.title(self.title)
+            
+        fig.savefig(filename)
