69b262f502bbc48204db70815476aa256bd7db6e
Connie Gao <connieg@mit.edu>
2016-03-07 17:16:39 -0500

    Add unit test for CanteraCondition that tests that the repr() function works

--- rmgpy/tools/canteraTest.py
+++ rmgpy/tools/canteraTest.py
@@ -1,8 +1,8 @@
 import unittest
 import os
 import numpy
-from rmgpy.tools.canteraModel import *
-
+from rmgpy.tools.canteraModel import findIgnitionDelay, CanteraCondition, Cantera
+from rmgpy.quantity import Quantity
 class CanteraTest(unittest.TestCase):
 
     def testIgnitionDelay(self):
@@ -23,3 +23,24 @@ class CanteraTest(unittest.TestCase):
 
         t_ign = findIgnitionDelay(t,[OH,CO], 'maxSpeciesConcentrations')
         self.assertEqual(t_ign,3.5)
+
+    def testRepr(self):
+        """
+        Test that the repr function for a CanteraCondition object can reconstitute
+        the same object
+        """
+        reactorType='IdealGasReactor'
+        molFrac={'CC': 0.05, '[Ar]': 0.95}
+        P=(3,'atm')
+        T=(1500,'K')
+        terminationTime=(5e-5,'s')
+        condition = CanteraCondition(reactorType, 
+                        terminationTime,
+                        molFrac, 
+                        T0=T,
+                        P0=P)
+        reprCondition=eval(condition.__repr__())
+        self.assertEqual(reprCondition.T0.value_si,Quantity(T).value_si)
+        self.assertEqual(reprCondition.P0.value_si,Quantity(P).value_si)
+        self.assertEqual(reprCondition.V0,None)
+        self.assertEqual(reprCondition.molFrac,molFrac)
