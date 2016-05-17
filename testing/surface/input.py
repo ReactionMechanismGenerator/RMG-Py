dfa5428bb7b36b152d85b4aea52586f7b7602ce7
Franklin Goldsmith <franklin_goldsmith@brown.edu>
2016-05-17 13:50:40 -0400

    Updated input file for methane oxidation. I have added the reactionLibrary Surface

--- testing/surface/input.py
+++ testing/surface/input.py
@@ -1,7 +1,7 @@
 # Data sources
 database(
     thermoLibraries=['surfaceThermo', 'primaryThermoLibrary'],
-    reactionLibraries = [],
+    reactionLibraries = [('Surface', True)],
     seedMechanisms = [],
     kineticsDepositories = ['training'],
     kineticsFamilies = 'default',
@@ -9,11 +9,49 @@ database(
 )
 
 # List of species
+#species(
+#    label='methyl',
+#    reactive=True,
+#    structure=SMILES("[CH3]"),
+#)
+
+# List of species
+#species(
+#    label='methylene',
+#    reactive=True,
+#    structure=SMILES("[CH2]"),
+#)
+
 species(
-    label='methyl',
+    label='methane',
     reactive=True,
-    structure=SMILES("[CH3]"),
+    structure=SMILES("[CH4]"),
 )
+
+#species(
+#   label='c2h4',
+#   reactive=True,
+#   structure=adjacencyList(
+#       """
+#1 C u0 p0 c0 {2,D} {3,S} {4,S}
+#2 C u0 p0 c0 {1,D} {5,S} {6,S}
+#3 H u0 p0 c0 {1,S}
+#4 H u0 p0 c0 {1,S}
+#5 H u0 p0 c0 {2,S}
+#6 H u0 p0 c0 {2,S}
+#"""),
+#)
+
+species(
+   label='o2',
+   reactive=True,
+   structure=adjacencyList(
+       """
+1 O u1 p2 c0 {2,S}
+2 O u1 p2 c0 {1,S}
+"""),
+)
+
 
 species(
     label='site',
@@ -26,14 +64,16 @@ surfaceReactor(
     temperature=(1350,'K'),
     initialPressure=(1.0, 'bar'),
     initialGasMoleFractions={
-        "methyl": 1.0,
+#        "methyl": 1.0,
+        "methane": 1.0,
+	"o2": 1.0,
     },
     initialSurfaceCoverages={
-        "site": 1.0,
+        "site": 2.0,
     },
     surfaceVolumeRatio=(1.e5, 'm^-1'),
     surfaceSiteDensity=(2.9e-9, 'mol/cm^2'),
-    terminationTime=(1e-3, 's'),
+    terminationTime=(1e-1, 's'),
 )
 
 simulator(
