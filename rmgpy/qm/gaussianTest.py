11886755757d90226509344f836e24ea8fff096f
Richard West <r.west@northeastern.edu>
2016-06-04 00:00:29 -0400

    Replace tabs with spaces in gaussianTest.py
    RMG-Py "house style" is four spaces per indentation, not tabs.

--- rmgpy/qm/gaussianTest.py
+++ rmgpy/qm/gaussianTest.py
@@ -34,132 +34,132 @@ else:  # didn't break
             break
     else:  # didn't break
         executablePath = os.path.join(gaussEnv , '(Gaussian 2003 or 2009)')
-	
+    
 mol1 = Molecule().fromSMILES('C1=CC=C2C=CC=CC2=C1')
 
 class TestGaussianMolPM3(unittest.TestCase):
-	"""
-	Contains unit tests for the Geometry class.
-	"""
+    """
+    Contains unit tests for the Geometry class.
+    """
 
-	@unittest.skipIf(os.path.exists(executablePath)==False, "Gaussian not found. Try resetting your environment variables if you want to use it.")
-	def setUp(self):
-		"""
-		A function run before each unit test in this class.
-		"""
-		RMGpy_path = os.path.normpath(os.path.join(getPath(),'..'))
-		
-		qm = QMCalculator(software = 'gaussian',
-						  method = 'pm3',
-						  fileStore = os.path.join(RMGpy_path, 'testing', 'qm', 'QMfiles'),
-						  scratchDirectory = os.path.join(RMGpy_path, 'testing', 'qm', 'QMscratch'),
-						  )
+    @unittest.skipIf(os.path.exists(executablePath)==False, "Gaussian not found. Try resetting your environment variables if you want to use it.")
+    def setUp(self):
+        """
+        A function run before each unit test in this class.
+        """
+        RMGpy_path = os.path.normpath(os.path.join(getPath(),'..'))
+        
+        qm = QMCalculator(software = 'gaussian',
+                          method = 'pm3',
+                          fileStore = os.path.join(RMGpy_path, 'testing', 'qm', 'QMfiles'),
+                          scratchDirectory = os.path.join(RMGpy_path, 'testing', 'qm', 'QMscratch'),
+                          )
 
-		
-		if not os.path.exists(qm.settings.fileStore):
-			os.makedirs(qm.settings.fileStore)
+        
+        if not os.path.exists(qm.settings.fileStore):
+            os.makedirs(qm.settings.fileStore)
 
-		self.qmmol1 = GaussianMolPM3(mol1, qm.settings)
+        self.qmmol1 = GaussianMolPM3(mol1, qm.settings)
 
-	def testGenerateThermoData(self):
-		"""
-		Test that generateThermoData() works correctly.
-		"""
-		try:
-			fileList = os.listdir(self.qmmol1.settings.fileStore)
-			for fileName in fileList:
-				os.remove(os.path.join(self.qmmol1.settings.fileStore, fileName))
-		except OSError:
-			pass
+    def testGenerateThermoData(self):
+        """
+        Test that generateThermoData() works correctly.
+        """
+        try:
+            fileList = os.listdir(self.qmmol1.settings.fileStore)
+            for fileName in fileList:
+                os.remove(os.path.join(self.qmmol1.settings.fileStore, fileName))
+        except OSError:
+            pass
 
-		self.qmmol1.generateThermoData()
-		result = self.qmmol1.qmData
+        self.qmmol1.generateThermoData()
+        result = self.qmmol1.qmData
 
-		self.assertTrue(self.qmmol1.thermo.comment.startswith('QM GaussianMolPM3 calculation'))
-		self.assertEqual(result.numberOfAtoms, 18)
-		self.assertIsInstance(result.atomicNumbers, np.ndarray)
-		if result.molecularMass.units=='amu':
-			self.assertAlmostEqual(result.molecularMass.value, 128.0626, 3)
+        self.assertTrue(self.qmmol1.thermo.comment.startswith('QM GaussianMolPM3 calculation'))
+        self.assertEqual(result.numberOfAtoms, 18)
+        self.assertIsInstance(result.atomicNumbers, np.ndarray)
+        if result.molecularMass.units=='amu':
+            self.assertAlmostEqual(result.molecularMass.value, 128.0626, 3)
 
-	def testLoadThermoData(self):
-		"""
-		Test that generateThermoData() can load thermo from a previous run.
+    def testLoadThermoData(self):
+        """
+        Test that generateThermoData() can load thermo from a previous run.
 
-		Check that it loaded, and the values are the same as above.
-		"""
+        Check that it loaded, and the values are the same as above.
+        """
 
-		self.qmmol1.generateThermoData()
-		result = self.qmmol1.qmData
+        self.qmmol1.generateThermoData()
+        result = self.qmmol1.qmData
 
-		self.assertTrue(self.qmmol1.thermo.comment.startswith('QM GaussianMolPM3 calculation'))
-		self.assertEqual(result.numberOfAtoms, 18)
-		self.assertIsInstance(result.atomicNumbers, np.ndarray)
-		if result.molecularMass.units=='amu':
-			self.assertAlmostEqual(result.molecularMass.value, 128.0626, 3)
-		
+        self.assertTrue(self.qmmol1.thermo.comment.startswith('QM GaussianMolPM3 calculation'))
+        self.assertEqual(result.numberOfAtoms, 18)
+        self.assertIsInstance(result.atomicNumbers, np.ndarray)
+        if result.molecularMass.units=='amu':
+            self.assertAlmostEqual(result.molecularMass.value, 128.0626, 3)
+        
 class TestGaussianMolPM6(unittest.TestCase):
-	"""
-	Contains unit tests for the Geometry class.
-	"""
+    """
+    Contains unit tests for the Geometry class.
+    """
 
-	@unittest.skipIf(os.path.exists(executablePath)==False, "Gaussian not found. Try resetting your environment variables if you want to use it.")
-	def setUp(self):
-		"""
-		A function run before each unit test in this class.
-		"""
-		RMGpy_path = os.path.normpath(os.path.join(getPath(),'..'))
-		
-		qm = QMCalculator(software = 'gaussian',
-						  method = 'pm6',
-						  fileStore = os.path.join(RMGpy_path, 'testing', 'qm', 'QMfiles'),
-						  scratchDirectory = os.path.join(RMGpy_path, 'testing', 'qm', 'QMscratch'),
-						  )
+    @unittest.skipIf(os.path.exists(executablePath)==False, "Gaussian not found. Try resetting your environment variables if you want to use it.")
+    def setUp(self):
+        """
+        A function run before each unit test in this class.
+        """
+        RMGpy_path = os.path.normpath(os.path.join(getPath(),'..'))
+        
+        qm = QMCalculator(software = 'gaussian',
+                          method = 'pm6',
+                          fileStore = os.path.join(RMGpy_path, 'testing', 'qm', 'QMfiles'),
+                          scratchDirectory = os.path.join(RMGpy_path, 'testing', 'qm', 'QMscratch'),
+                          )
 
-		if not os.path.exists(qm.settings.fileStore):
-			os.makedirs(qm.settings.fileStore)
+        if not os.path.exists(qm.settings.fileStore):
+            os.makedirs(qm.settings.fileStore)
 
-		self.qmmol1 = GaussianMolPM6(mol1, qm.settings)
+        self.qmmol1 = GaussianMolPM6(mol1, qm.settings)
 
-	@unittest.skipIf('g03' in executablePath, "This test was shown not to work on g03.")
-	def testGenerateThermoData(self):
-		"""
-		Test that generateThermoData() works correctly.
-		"""
-		try:
-			fileList = os.listdir(self.qmmol1.settings.fileStore)
-			for fileName in fileList:
-				os.remove(os.path.join(self.qmmol1.settings.fileStore, fileName))
-		except OSError:
-			pass
+    @unittest.skipIf('g03' in executablePath, "This test was shown not to work on g03.")
+    def testGenerateThermoData(self):
+        """
+        Test that generateThermoData() works correctly.
+        """
+        try:
+            fileList = os.listdir(self.qmmol1.settings.fileStore)
+            for fileName in fileList:
+                os.remove(os.path.join(self.qmmol1.settings.fileStore, fileName))
+        except OSError:
+            pass
 
-		self.qmmol1.generateThermoData()
-		result = self.qmmol1.qmData
+        self.qmmol1.generateThermoData()
+        result = self.qmmol1.qmData
 
-		self.assertTrue(self.qmmol1.thermo.comment.startswith('QM GaussianMolPM6 calculation'))
-		self.assertEqual(result.numberOfAtoms, 18)
-		self.assertIsInstance(result.atomicNumbers, np.ndarray)
-		if result.molecularMass.units=='amu':
-			self.assertAlmostEqual(result.molecularMass.value, 128.0626, 3)
+        self.assertTrue(self.qmmol1.thermo.comment.startswith('QM GaussianMolPM6 calculation'))
+        self.assertEqual(result.numberOfAtoms, 18)
+        self.assertIsInstance(result.atomicNumbers, np.ndarray)
+        if result.molecularMass.units=='amu':
+            self.assertAlmostEqual(result.molecularMass.value, 128.0626, 3)
 
-	@unittest.skipIf('g03' in executablePath, "This test was shown not to work on g03.")
-	def testLoadThermoData(self):
-		"""
-		Test that generateThermoData() can load thermo from a previous run.
+    @unittest.skipIf('g03' in executablePath, "This test was shown not to work on g03.")
+    def testLoadThermoData(self):
+        """
+        Test that generateThermoData() can load thermo from a previous run.
 
-		Check that it loaded, and the values are the same as above.
-		"""
+        Check that it loaded, and the values are the same as above.
+        """
 
-		self.qmmol1.generateThermoData()
-		result = self.qmmol1.qmData
+        self.qmmol1.generateThermoData()
+        result = self.qmmol1.qmData
 
-		self.assertTrue(self.qmmol1.thermo.comment.startswith('QM GaussianMolPM6 calculation'))
-		self.assertEqual(result.numberOfAtoms, 18)
-		self.assertIsInstance(result.atomicNumbers, np.ndarray)
-		if result.molecularMass.units=='amu':
-			self.assertAlmostEqual(result.molecularMass.value, 128.0626, 3)
+        self.assertTrue(self.qmmol1.thermo.comment.startswith('QM GaussianMolPM6 calculation'))
+        self.assertEqual(result.numberOfAtoms, 18)
+        self.assertIsInstance(result.atomicNumbers, np.ndarray)
+        if result.molecularMass.units=='amu':
+            self.assertAlmostEqual(result.molecularMass.value, 128.0626, 3)
 
 
 ################################################################################
 
 if __name__ == '__main__':
-	unittest.main( testRunner = unittest.TextTestRunner(verbosity=2) )
+    unittest.main( testRunner = unittest.TextTestRunner(verbosity=2) )
