5011e9ac0cfb2197a56acbaaa5da925ddde214b2
Richard West <r.west@northeastern.edu>
2016-06-21 11:54:25 -0400

    Approximate Cp0 and CpInf for adsorbed species.
    See https://github.com/cfgoldsmith/RMG-Py/issues/5
    for more detailed version.
    
    Heat capacity for surface species is 0.01 not 0.
    Absolute 0 causes problem.

--- rmgpy/molecule/molecule.py
+++ rmgpy/molecule/molecule.py
@@ -1798,6 +1798,8 @@ class Molecule(Graph):
         """
         Return the value of the heat capacity at zero temperature in J/mol*K.
         """
+        if self.containsSurfaceSite():
+            return 0.01
         if len(self.atoms) == 1:
             return 2.5 * constants.R
         else:
@@ -1809,10 +1811,13 @@ class Molecule(Graph):
         """
         cython.declare(Natoms=cython.int, Nvib=cython.int, Nrotors=cython.int)
         
+        if self.containsSurfaceSite():
+            # ToDo: internal rotors could still act as rotors
+            return constants.R * 3 * len(self.vertices)
+
         if len(self.vertices) == 1:
             return self.calculateCp0()
         else:
-            
             Natoms = len(self.vertices)
             Nvib = 3 * Natoms - (5 if self.isLinear() else 6)
             Nrotors = self.countInternalRotors()
