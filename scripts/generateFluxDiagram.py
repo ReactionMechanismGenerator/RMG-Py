ec8f3aaf6b9b5d80df8d3066349cd2a23366cd86
Max Liu <mjliu@mit.edu>
2016-07-12 12:12:07 -0400

    Add speciesPath argument for generateFluxDiagram

--- scripts/generateFluxDiagram.py
+++ scripts/generateFluxDiagram.py
@@ -21,22 +21,24 @@ def parse_arguments():
 
     parser.add_argument('input', metavar='INPUT', type=str, nargs=1,
         help='the RMG input file to use')
-
+    parser.add_argument('species', metavar='SPECIES', type=str, nargs='?', default=None,
+        help='path to species images')
     parser.add_argument('--java', action='store_true', help='process RMG-Java model')
 
     args = parser.parse_args()
 
     inputFile = os.path.abspath(args.input[0])
+    speciesPath = os.path.abspath(args.species[0]) if args.species is not None else None
     useJava = args.java
     
-    return inputFile, useJava
+    return inputFile, speciesPath, useJava
 
 def main():
     # This might not work anymore because functions were modified for use with webserver
 
-    inputFile, useJava = parse_arguments()
+    inputFile, speciesPath, useJava = parse_arguments()
 
-    fluxdiagram.run(inputFile, useJava)
+    fluxdiagram.run(inputFile, speciesPath, useJava)
 
 if __name__ == '__main__':
     main()
