0c6c52847b77dd9762fbc3663d034db33630f392
Max Liu <mjliu@mit.edu>
2016-07-13 15:28:50 -0400

    Improve robustness of fluxtest
    Get rmgpy directory insted of cwd
    Remove slashes from path creation

--- rmgpy/tools/fluxtest.py
+++ rmgpy/tools/fluxtest.py
@@ -3,20 +3,21 @@ import os
 import os.path
 import shutil
 
+import rmgpy
 from rmgpy.tools.fluxdiagram import *
 
 class FluxDiagramTest(unittest.TestCase):
 
     def test_avi(self):
-        folder = os.path.join(os.getcwd(),'rmgpy/tools/data/flux/')
+        folder = os.path.join(os.path.dirname(rmgpy.__file__),'tools','data','flux')
         
         inputFile = os.path.join(folder,'input.py')
         run(inputFile)
 
-        outputdir = os.path.join(folder, 'flux/')
-        simfile = os.path.join(outputdir,'1/','flux_diagram.avi')
+        outputdir = os.path.join(folder,'flux')
+        simfile = os.path.join(outputdir,'1','flux_diagram.avi')
 
-        speciesdir = os.path.join(folder, 'species/')
+        speciesdir = os.path.join(folder,'species')
 
         self.assertTrue(os.path.isfile(simfile))
         
