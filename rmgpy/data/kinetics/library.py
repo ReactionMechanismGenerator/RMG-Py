e16d4b67749be4e11728b1e649ebabb4b1f6c646
Richard West <r.west@northeastern.edu>
2016-08-01 15:53:30 +0900

    TEMPORARY! Allow modelComparer to read rxns with many products
    This may break RMG in weird ways, by allowing kinetics
    libraries to be read that cannot be simulated,
    but without it the model comparer just throws out
    a lot of models.

--- rmgpy/data/kinetics/library.py
+++ rmgpy/data/kinetics/library.py
@@ -260,9 +260,9 @@ class KineticsLibrary(Database):
                 raise DatabaseError('Reaction {0} in kinetics library {1} was not balanced! Please reformulate.'.format(rxn, self.label))    
 
             if len(rxn.reactants) > 3: 
-                raise DatabaseError('RMG does not accept reactions with more than 3 reactants in its solver.  Reaction {0} in kinetics library {1} has {2} reactants.'.format(rxn, self.label, len(rxn.reactants)))
+                logging.warning('RMG does not accept reactions with more than 3 reactants in its solver.  Reaction {0} in kinetics library {1} has {2} reactants.'.format(rxn, self.label, len(rxn.reactants)))
             if len(rxn.products) > 3:
-                raise DatabaseError('RMG does not accept reactions with more than 3 products in its solver.  Reaction {0} in kinetics library {1} has {2} reactants.'.format(rxn, self.label, len(rxn.products)))
+                logging.warning('RMG does not accept reactions with more than 3 products in its solver.  Reaction {0} in kinetics library {1} has {2} reactants.'.format(rxn, self.label, len(rxn.products)))
             
 
             
