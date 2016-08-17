27a77fc6c922f9a012a5a2d750e558cb5a595248
Richard West <r.west@northeastern.edu>
2016-08-16 23:45:18 -0400

    Error if find an irreversible core rxn not in invalid network

--- rmgpy/rmg/model.py
+++ rmgpy/rmg/model.py
@@ -1460,12 +1460,14 @@ class CoreEdgeReactionModel:
         # direction from the list of core reactions
         # Note that well-skipping reactions may not have a reverse if the well
         # that they skip over is not itself in the core
-        for network in updatedNetworks:
+        for network in self.networkList:
             for reaction in network.netReactions:
                 try:
                     index = self.core.reactions.index(reaction)
                 except ValueError:
                     continue
+                if network not in updatedNetworks:
+                    assert reaction.reversible, "Core reactions in stale networks should be reversible!"
                 for index2, reaction2 in enumerate(self.core.reactions):
                     if isinstance(reaction2, PDepReaction) and reaction.reactants == reaction2.products and reaction.products == reaction2.reactants:
                         # We've found the PDepReaction for the reverse direction
