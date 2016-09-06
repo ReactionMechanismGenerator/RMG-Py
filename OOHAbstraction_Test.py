c8d9ab91927b3d8eed70d2907a7ec54c633d0f9f
Nate Harms <harms.n@husky.neu.edu>
2016-09-06 16:48:29 -0400

    Completed work on OOH_Abstraction up until the part that need Gaussian. Also, tried making an .idea for PyCharm (I'm not sure if I'll use it in the long run)

--- OOHAbstraction_Test.py
+++ OOHAbstraction_Test.py
@@ -52,6 +52,13 @@ def calculate(reaction):
 			os.remove(files)
 	return reaction
 
+qmCalc = QMCalculator(
+						software='gaussian',
+						method='m062x',
+						fileStore='/scratch/bhoorasingh.p/QMfiles',
+						scratchDirectory='/scratch/bhoorasingh.p/QMscratch',
+						)
+
 
 
 
@@ -84,13 +91,11 @@ for root, dirs, files in os.walk("../RMG-models", topdown=False):
 			print "Missing kinetics file"
 		"""
 
-
 ############
 
-for entry in importer_files:
-	smiles_file = entry[0]
-	kinetics_file = entry[1]
-	print smiles_file
+for item in importer_files:
+	smiles_file, kinetics_file = item
+
 
 	known_smiles = {}
 	known_names = []
@@ -122,7 +127,7 @@ for entry in importer_files:
 	        except Exception as e:
 	            logging.warning("Error reading line '{0}'".format(line))
 	            raise e
-	break
+	#break #do I need this break here? This might be why the script stops so abruptly...?
 	#####
 	allRxns = []
 
@@ -146,297 +151,184 @@ for entry in importer_files:
 		    if new_rxn:
 		        allRxns.append(chemkinRxn)
 
-
-	chemkinRxn = allRxns[i-1]
-	reaction = chemkinRxn['reaction']
-
-	chemkinRxn = allRxns[i-1]
-	reaction = chemkinRxn['reaction']
-	try:
-	    rcts, prdts = reaction.split('<=>')
-	except ValueError:
-	    rcts, prdts = reaction.split('=>')
-	react = rcts.split('+')
-	products = prdts.split('+')
-	reactants = []
-
-	### This is one of the specific things that I changed from Pierre's script. ###
-	### I need to move this part of the script to later... I'm unsure about
-	# where this needs to be moved, but I'll figure it out later. Just at little
-	# too tired at the moment. (9/2)
-	for r in react:
-		if "OOH" in r:
-			reactants.append(r) # Although, I think reactants is a dictionary? Or it's a list of lists? If it's a list of lists, I think this should work.
-	################################################################################
-
-	reactant_molecules = [Molecule().fromSMILES(known_smiles[key.strip()]) for key in reactants]
-	product_molecules = [Molecule().fromSMILES(known_smiles[key.strip()]) for key in products]
-
-	reactants_species = [Species(molecule=mol.generateResonanceIsomers()) for mol in reactant_molecules]
-	product_species = [Species(molecule=mol.generateResonanceIsomers()) for mol in product_molecules]
-
-	testReaction = Reaction(reactants = reactant_species, products = product_species, reversible = True)
-
-	checkRxn = rmgDatabase.kinetics.generateReactionsFromFamilies(reactant_molecules, product_molecules, only_families=chemkinRxn['possibleReactionFamilies'])
-	if checkRxn==[]:
-		reactIsoms = [mol.generateResonanceIsomers() for mol in reactant_molecules]
-		if len(reactIsoms)==2:
-			r1, r2 = reactIsoms
-			while checkRxn==[]:
-				for moleculeA in r1:
-					for moleculeB in r2:
-						reactant_molecules = [moleculeA, moleculeB]
-						checkRxn = rmgDatabase.kinetics.generateReactionsFromFamilies(reactant_molecules, product_molecules, only_families=chemkinRxn['possibleReactionFamilies'])
-
-	reaction = checkRxn[0]
-
-	assert testReaction.isIsomorphic(reaction)
-
-	atLblsR = dict([(lbl[0], False) for lbl in reaction.labeledAtoms])
-	atLblsP = dict([(lbl[0], False) for lbl in reaction.labeledAtoms])
-
-	gotOne = False
-	for reactant in reaction.reactants:
-	    reactant = reactant.molecule[0]
-	    reactant.clearLabeledAtoms()
-	    for atom in reactant.atoms:
-	        for atomLabel in reaction.labeledAtoms:
-	            if atom==atomLabel[1]:
-	                atom.label = atomLabel[0]
-	                atLblsR[atomLabel[0]] = True
-
-	for product in reaction.products:
-	    product = product.molecule[0]
-	    product.clearLabeledAtoms()
-	    for atom in product.atoms:
-	        for atomLabel in reaction.labeledAtoms:
-	            if atom==atomLabel[1]:
-	                atom.label = atomLabel[0]
-	                atLblsP[atomLabel[0]] = True
-
-	if all( atLblsR.values() ) and all( atLblsP.values() ):
-		gotOne=True
-	rxnFamily = reaction.family.label
-	assert gotOne
-	qmCalc = QMCalculator(
-	                        software='gaussian',
-	                        method='m062x',
-	                        fileStore='/scratch/bhoorasingh.p/QMfiles',
-	                        scratchDirectory='/scratch/bhoorasingh.p/QMscratch',
-	                        )
-	reaction = calculate(reaction)
-
-	if reaction.kinetics:
-		"""
-		REWRITE SARATHYKIN STUFF FOR VARYING IMPORTED MECHANISMS # Fixed?
-		"""
-		"""
-		Return the rate coefficient in the appropriate combination of cm^3,
-		mol, and s at temperature `T` in K and pressure `P` in Pa.
-		"""
-		importKin = chemkinRxn['rmgPyKinetics']
-		Temp = 1000 # Kelvin
-		idx = str(i)
-		row = [idx, rxnFamily]
-		row.extend([mol.label for mol in reaction.reactants])
-		row.extend([mol.label for mol in reaction.products])
-
-		rateCal = reaction.calculateTSTRateCoefficient(Temp)
-		row.extend(['AutoTST_fwd', str(rateCal)])
-
-		if rxnFamily == 'R_Addition_MultipleBond':
-			if reverseRxn:
-				revRate = reaction.generateReverseRateCoefficient()
-				rev_rateCal = revRate.getRateCoefficient(Temp)
-				row.extend(['AutoTST_rev', str(rev_rateCal)])
-			else:
-				row.extend(['AutoTST_rev', 'not reverse'])
-
-		if isinstance(importKin, PDepArrhenius) or isinstance(importKin, PDepKineticsModel):
-			row.extend([smiles_dict[entry], str(importKin.getRateCoefficient(Temp, 1000000))]) #1000000 Pa = 10 bar
-		else:
-			row.extend([smiles_dict[entry], str(importKin.getRateCoefficient(Temp))])
-
-		famDatabase = rmgDatabase.kinetics.families[rxnFamily]
-		famDatabase.addKineticsRulesFromTrainingSet(thermoDatabase=rmgDatabase.thermo)
-		famDatabase.fillKineticsRulesByAveragingUp()
-
-		# Using the `testReaction` will keep the reaction direction the same as the model
-		rxnTemplate = famDatabase.getReactionTemplate(testReaction)
-		kList = famDatabase.getKinetics(testReaction, rxnTemplate)
-
-		allRates = []
-		kComments = []
-		for rate in kList:
-			k = rate[0]
-			if k:
-				label = rate[1]
-				if isinstance(label, KineticsDepository):
-					label = label.label
-				elif isinstance(label, KineticsRules):
-					label = label.label
-				if label.lower()=='rate rules':
-					kComments = k.comment.split('\n')
-				allRates.append((label, k.getRateCoefficient(Temp)))
-
-		kDict = {'Rate Rules': []}
-		for kTuple in allRates:
-			if kTuple[0]=='AutoTST':
-				kDict['AutoTST'] = kDict['AutoTST'] + [kTuple[1]]
-			elif kTuple[0]=='rate rules':
-				kDict['Rate Rules'] = kDict['Rate Rules'] + [kTuple[1]]
-			elif kTuple[0].endswith('/NIST'):
-				kDict['NIST'] = kDict['NIST'] + [kTuple[1]]
-			elif kTuple[0].endswith('/rules'):
-				kDict['KineitcsRules'] = kDict['KineitcsRules'] + [kTuple[1]]
-
-		# Print the values in order
-		kineticsTypes = ['Rate Rules']#['AutoTST', 'Rate Rules', 'NIST', 'KineitcsRules']
-		for kinType in kineticsTypes:
-			row.append(kinType)
-			if kDict[kinType]==[]:
-				row.append('no Val')
-			else:
-				for val in kDict[kinType]:
-					row.append(str(val))
-
-		# Store line containing reaction from corresponding chemkin file
-		row.append(smiles_dict[entry])
-		row.append(chemkinRxn['chemkinKinetics'].strip())
-
-		# Store rmgpy kinetics group comments
-		row = row + kComments
-
-		folderPath = os.path.join('KinTxtFiles', idx)
-
-		if not os.path.exists(folderPath):
-			os.makedirs(folderPath)
-
-		input_string = ','.join(row)
-		with open(os.path.join(folderPath, smiles_dict[entry] + '_kinetics.txt'), 'w') as kinTxt:
-	            kinTxt.write(input_string)
+	# allRxns seems to be a dictionary... I'm a little drunk at the moment, so idk if I'll be able to decipher it rn
 
 
-"""
-if checkRxn == []:
-  reactIsoms = [mol.generateResonanceIsomers() for mol in reactant_molecules]
-  if len(reactIsoms) ==  2:
-    r1, r2 = reactIsoms
-    while checkRxn == []:
-      for moleculeA in r1:
-        for moleculeB in r2:
-          reactant_molecules = [moleculeA, moleculeB]
-          checkRxn = rmgDatabase.kinetics.generateReactionsFromFamilies(reactant_molecules, product_molecules, only_families = chemkinRxn)
+	# Now this portion is throwing errors ####!!!!
+	reactions = [d["reaction"] for d in allRxns]
 
-reaction = checkRxn[0]
+	reactant = []
+	product = []
 
-assert testRaction.isIsomorphic(reaction) # not really sure what this does, ask Pierre
+	for reaction in reactions:
+		try:
+		    rcts, prdts = reaction.split('<=>')
+		except ValueError:
+		    rcts, prdts = reaction.split('=>')
+		reactants = rcts.split('+') # A list of reactants for a given rxn
+		products = prdts.split('+') # A list of products for a given rxn
 
-# This adds labeled atoms to the molecule and checks that all the labels were added.
-atLblsR = dict([(lbl[0], False) for lbl in reaction.labeledAtoms])
-atLblsP = dict([(lbl[0], False) for lbl in reaction.labeledAtoms])
+		# Obtains a list of SMILES structures for a reaction.
+		reactant_molecules = [(known_smiles[key.strip()]) for key in reactants]
+		product_molecules = [(known_smiles[key.strip()]) for key in products]
 
-gotOne = False
-for reactant in reaction.reactants:
-  reactant = reactant.molecule[0]
-  reactant.clearLabeledAtoms()
-  for atoms in reactant.atoms:
-    for atomLabel in reaction.labeledAtoms:
-      if atom == atomLabel[1]
-      atom.label = atomLabel[0]
-      atLblsR[atomLabel[0]] = True
 
-for product in reaction.products:
-  product = product.molecule[0]
-  product.clearLabeledAtoms()
-  for atoms in product.atoms:
-    for atomLabel in reaction.labeledAtoms:
-      if atom == atomLabel[1]
-      atom.label = atomLabel[0]
-      atLblsP[atomLabel[0]] = True
 
-if all( atLblsR.values() ) and all( atLblsP.values() ):
-  gotOne = True # Still confused about what this thing is
+		### This section is designed to determine if OOH is in the reactants and if OO is in the products
+		# i.e. if this is OOH abstraction
 
-rxnFamily = reaction.family
-assert gotOne
+		reactant_mol = []
+		product_mol = []
+		if "[O]O" in reactant_molecules:
+			if "OO" in product_molecules:
+				reactant_mol = [Molecule().fromSMILES(key) for key in reactant_molecules]
+				product_mol = [Molecule().fromSMILES(key) for key in product_molecules]
 
-qmCalc = QMCalculator( software = 'gaussian', method = 'm062x', fileStore = '/scratch/harms.n/QMfiles', scratchDirectory = '/scratch/harms.n',)
+		if reactant_mol != [] and product_mol != []:
 
-reaction = calculate(reaction)
 
-if reaction.kinetics:
-  importerKin = chemkinRxn['rmgPyKinetics'] # May need to adjust this depending on what kinetics we load. Sarathy is from Pierre's last kinetic model thing, I need to pick a new variable here. Importer was selected.
-  Temp = 1000 # Kelvin
-  idx = str(i)
-  row = [idx, rxnFamily]
-  row.extend([mol.label for mol in reaction.reactants])
-  row.extend([mol.label for mol in reaction.products])
+			reactant_species = [Species(molecule=mol.generateResonanceIsomers()) for mol in reactant_mol]
+			product_species = [Species(molecule=mol.generateResonanceIsomers()) for mol in product_mol]
 
-  rateCal = reaction.calculateTSTRateCoefficient(Temp)
-  row.extend(['AutoTST_fwd',str(rateCal)])
+			testReaction = Reaction(reactants = reactant_species, products = product_species, reversible = True)
+			checkRxn = rmgDatabase.kinetics.generateReactionsFromFamilies(reactant_mol, product_mol, only_families=rxnFamilies)
+			#print [testReaction]
+			#print checkRxn, type(checkRxn), len(checkRxn)
 
-  if isinstance(importerKin, PDepArrhenius) or isinstance(importerKin, PDepKineticsModel):
-    row.extend(['Importer', str(importerKin.getRateCoefficient(Temp, 1000000))])
-  else:
-    row.extend(['Importer', str(importerKin.getRateCoefficient(Temp))])
 
-  famDatabase = rmgDatabase.kinetics.families[rxnFamily]
-  famDatabase.addKineticsRulesFromTrainingSet(thermoDatabase=rmgDatabase.thermo)
-	famDatabase.fillKineticsRulesByAveragingUp()
+			# This part was in Pierre's script, but I have no use for it (I think)
 
-  rxnTemplate = famDatabase.getReactionTemplate(testReaction)
-  kList = famDatabase.getKinetics(testReaction, rxnTemplate)
+			if checkRxn==[]:
+				reactIsoms = [mol.generateResonanceIsomers() for mol in reactant_mol]
+				if len(reactIsoms)==2:
+					r1, r2 = reactIsoms
+					while checkRxn==[]:
+						for moleculeA in r1:
+							for moleculeB in r2:
+								reactant_mol = [moleculeA, moleculeB]
+								checkRxn = rmgDatabase.kinetics.generateReactionsFromFamilies(reactant_mol, product_mol, only_families=chemkinRxn['possibleReactionFamilies'])
 
-  # Sort the calculated rates bases on how they were determined.
-  allRates = []
-  kComments = []
-  for rate in kList:
-    k = rate [0]
-    if k:
-      label = rate[1]
-      if isinstance(label, KineticsDepository):
-        label = label.label
-      elif is instance(label, KineticsRules):
-        label = label.label
-      if label.lower() == 'rate rules':
-        kComments = k.comment.split('\n')
-      allRates.append(label, k.getRateCoefficient(Temp))
+			reaction = checkRxn[0]
+			print reaction
 
-  kDict = {'Rate Rules': []}
-  for kTuple in allRates:
-    if kTuple[0] == 'AutoTST':
-      kDict['AutoTST'] = kDict['AutoTST'] + [kTuple[1]]
-    if kTuple[0] == 'rate rules':
-      kDict['Rate Rules'] = kDict['Rate Rules'] + [kTuple[1]]
-    if kTuple[0].endswith('/NIST'):
-      kDict['NIST'] = kDict['NIST'] + [kTuple[1]]
-    if kTuple[0].endswith('/rules'):
-      kDict['KineticsRules'] = kDict['KineticsRules'] + [kTuple[1]]
+			assert testReaction.isIsomorphic(reaction)
 
-  # Print the values in order
-  kineticsTypes = ['Rate Rules']#['AutoTST', 'Rate Rules', 'NIST', 'KineitcsRules']
-  for kinType in kineticsTypes:
-    row.append(kinType)
-    if kDict[kinType] == []:
-      row.append('no Val')
-    else:
-      for val in kDict[kinType]:
-        row.append(str(val))
+			atLblsR = dict([(lbl[0], False) for lbl in reaction.labeledAtoms])
+			atLblsP = dict([(lbl[0], False) for lbl in reaction.labeledAtoms])
 
-  # Store line containing reaction from source of intrest's chemkin file.
-  row.append('ImporterSource')
-  row.append(chemkinRxn['chemkinKinetics'].strip())
+			gotOne = False
+			for reactant in reaction.reactants:
+			    #reactant = reactant.molecule[0]
+			    reactant.clearLabeledAtoms()
+			    for atom in reactant.atoms:
+			        for atomLabel in reaction.labeledAtoms:
+			            if atom==atomLabel[1]:
+			                atom.label = atomLabel[0]
+			                atLblsR[atomLabel[0]] = True
 
-  # Store rmgpy kinetics group comments
-  row = row + kComments
+			for product in reaction.products:
+			    #product = product.molecule[0]
+			    product.clearLabeledAtoms()
+			    for atom in product.atoms:
+			        for atomLabel in reaction.labeledAtoms:
+			            if atom==atomLabel[1]:
+			                atom.label = atomLabel[0]
+			                atLblsP[atomLabel[0]] = True
 
-  folderPath = os.path.join('KinTxtFiles', idx)
+			if all( atLblsR.values() ) and all( atLblsP.values() ):
+				gotOne=True
+			rxnFamily = reaction.family
+			assert gotOne
+			
+			######### NO GUARENTEES IF ANYTHING AFTER THIS LINE WORKS
 
-  if not os.path.exists(folderPath):
-    os.makedirs(folderPath)
+			reaction = calculate(reaction)
 
-  input_string = ','.join(row)
-  with open(os.path.join(folderPath, 'kinetic.txt'), 'w') as kinTxt:
-    kinTxt.write(input_string)
-"""
+			if reaction.kinetics:
+				"""
+				Return the rate coefficient in the appropriate combination of cm^3,
+				mol, and s at temperature `T` in K and pressure `P` in Pa.
+				"""
+				importKin = chemkinRxn['rmgPyKinetics']
+				Temp = 1000 # Kelvin
+				idx = str(i)
+				row = [idx, rxnFamily]
+				row.extend([mol.label for mol in reaction.reactants])
+				row.extend([mol.label for mol in reaction.products])
+
+				rateCal = reaction.calculateTSTRateCoefficient(Temp)
+				row.extend(['AutoTST_fwd', str(rateCal)])
+
+				"""
+				# No reaction will be of this reaction family b/c H_Abstraction
+				if rxnFamily == 'R_Addition_MultipleBond':
+					if reverseRxn:
+						revRate = reaction.generateReverseRateCoefficient()
+						rev_rateCal = revRate.getRateCoefficient(Temp)
+						row.extend(['AutoTST_rev', str(rev_rateCal)])
+					else:
+						row.extend(['AutoTST_rev', 'not reverse'])
+				"""
+				if isinstance(importKin, PDepArrhenius) or isinstance(importKin, PDepKineticsModel):
+					row.extend([smiles_dict[entry], str(importKin.getRateCoefficient(Temp, 1000000))]) #1000000 Pa = 10 bar
+				else:
+					row.extend([smiles_dict[entry], str(importKin.getRateCoefficient(Temp))])
+
+				famDatabase = rmgDatabase.kinetics.families[rxnFamily]
+				famDatabase.addKineticsRulesFromTrainingSet(thermoDatabase=rmgDatabase.thermo)
+				famDatabase.fillKineticsRulesByAveragingUp()
+
+				# Using the `testReaction` will keep the reaction direction the same as the model
+				rxnTemplate = famDatabase.getReactionTemplate(testReaction)
+				kList = famDatabase.getKinetics(testReaction, rxnTemplate)
+
+				allRates = []
+				kComments = []
+				for rate in kList:
+					k = rate[0]
+					if k:
+						label = rate[1]
+						if isinstance(label, KineticsDepository):
+							label = label.label
+						elif isinstance(label, KineticsRules):
+							label = label.label
+						if label.lower()=='rate rules':
+							kComments = k.comment.split('\n')
+						allRates.append((label, k.getRateCoefficient(Temp)))
+
+				kDict = {'Rate Rules': []}
+				for kTuple in allRates:
+					if kTuple[0]=='AutoTST':
+						kDict['AutoTST'] = kDict['AutoTST'] + [kTuple[1]]
+					elif kTuple[0]=='rate rules':
+						kDict['Rate Rules'] = kDict['Rate Rules'] + [kTuple[1]]
+					elif kTuple[0].endswith('/NIST'):
+						kDict['NIST'] = kDict['NIST'] + [kTuple[1]]
+					elif kTuple[0].endswith('/rules'):
+						kDict['KineitcsRules'] = kDict['KineitcsRules'] + [kTuple[1]]
+
+				# Print the values in order
+				kineticsTypes = ['Rate Rules']#['AutoTST', 'Rate Rules', 'NIST', 'KineitcsRules']
+				for kinType in kineticsTypes:
+					row.append(kinType)
+					if kDict[kinType]==[]:
+						row.append('no Val')
+					else:
+						for val in kDict[kinType]:
+							row.append(str(val))
+
+				# Store line containing reaction from corresponding chemkin file
+				row.append(smiles_dict[entry])
+				row.append(chemkinRxn['chemkinKinetics'].strip())
+
+				# Store rmgpy kinetics group comments
+				row = row + kComments
+
+				folderPath = os.path.join('KinTxtFiles', idx)
+
+				if not os.path.exists(folderPath):
+					os.makedirs(folderPath)
+
+				input_string = ','.join(row)
+				with open(os.path.join(folderPath, smiles_dict[entry] + '_kinetics.txt'), 'w') as kinTxt:
+			            kinTxt.write(input_string)
