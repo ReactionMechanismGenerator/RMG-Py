c34cd5cdd9c2cfaebbae7f0d24c3dc7d209f40eb
Max Liu <mjliu@mit.edu>
2016-09-23 17:23:10 -0400

    Add method to analyze molecule for resonance purposes
    If we identify molecule features at the beginning,
    we can determine which methods need to be called.

--- rmgpy/molecule/resonance.py
+++ rmgpy/molecule/resonance.py
@@ -55,6 +55,42 @@ def populate_resonance_generation_algorithm():
 
     return algorithms
 
+def analyzeMolecule(mol):
+    """
+    Identify key features of molecule important for resonance structure generation.
+
+    Returns a dictionary of features.
+    """
+    cython.declare(features=dict)
+
+    features = {'isRadical': mol.isRadical(),
+                'isCyclic': mol.isCyclic(),
+                'isAromatic': False,
+                'isPolycyclicAromatic': False,
+                'isArylRadical': False,
+                'hasNitrogen': False,
+                'hasOxygen': False,
+                'hasLonePairs': False,
+                }
+
+    if features['isCyclic']:
+        ASSSR = mol.getAromaticSSSR()
+        if len(ASSSR) > 0:
+            features['isAromatic'] = True
+        if len(ASSSR) > 1:
+            features['isPolycyclicAromatic'] = True
+        if features['isRadical'] and features['isAromatic']:
+            features['isArylRadical'] = mol.isArylRadical(ASSSR)
+    for atom in mol.vertices:
+        if atom.isNitrogen():
+            features['hasNitrogen'] = True
+        if atom.isOxygen():
+            features['hasOxygen'] = True
+        if atom.lonePairs > 0:
+            features['hasLonePairs'] = True
+
+    return features
+
 def generateResonanceIsomers(mol):
     """
     Generate and return all of the resonance isomers of this molecule.
