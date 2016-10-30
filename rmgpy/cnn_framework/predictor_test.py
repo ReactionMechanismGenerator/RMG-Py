8ad6e6bbafcfba7271881651c8573990484310a5
KEHANG <kehanghan@gmail.com>
2016-10-30 18:16:23 -0400

    add build model from input feature in predictor class and corresponding unittest

--- rmgpy/cnn_framework/predictor_test.py
+++ rmgpy/cnn_framework/predictor_test.py
@@ -2,6 +2,8 @@ from .predictor import *
 from .layers import GraphFP
 from keras.layers.core import Dense
 import unittest
+import os
+import rmgpy
 
 class Test_Predictor(unittest.TestCase):
 
@@ -12,6 +14,24 @@ class Test_Predictor(unittest.TestCase):
 	def test_model(self):
 
 		self.predictor.build_model()
+		predictor_model = self.predictor.model
+		self.assertEqual(len(predictor_model.layers), 2)
+		self.assertTrue(isinstance(predictor_model.layers[0], GraphFP))
+		self.assertTrue(isinstance(predictor_model.layers[1], Dense))
+
+		self.assertEqual(predictor_model.layers[0].inner_dim, 8)
+		self.assertEqual(predictor_model.layers[0].output_dim, 512)
+
+	def test_load_input(self):
+
+		test_predictor_input = os.path.join(os.path.dirname(rmgpy.__file__),
+											'cnn_framework',
+											'test_data', 
+											'minimal_predictor', 
+											'predictor_input.py'
+											)
+		self.predictor.load_input(test_predictor_input)
+
 		predictor_model = self.predictor.model
 		self.assertEqual(len(predictor_model.layers), 2)
 		self.assertTrue(isinstance(predictor_model.layers[0], GraphFP))
