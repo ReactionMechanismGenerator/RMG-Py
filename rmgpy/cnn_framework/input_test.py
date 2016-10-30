02dbc0baccc9cbf609892a38fd7d961cf1a1e470
KEHANG <kehanghan@gmail.com>
2016-10-30 18:14:10 -0400

    add unittest for input module

---
+++ rmgpy/cnn_framework/input_test.py
@@ -0,0 +1,30 @@
+
+import unittest
+import rmgpy
+from .predictor import Predictor
+from .input import *
+from .layers import GraphFP
+from keras.layers.core import Dense
+
+class Test_Input(unittest.TestCase):
+
+	def test_read_input_file(self):
+
+		predictor_test = Predictor()
+
+		path = os.path.join(os.path.dirname(rmgpy.__file__),
+											'cnn_framework',
+											'test_data', 
+											'minimal_predictor', 
+											'predictor_input.py'
+											)
+		read_input_file(path, predictor_test)
+
+		predictor_model = predictor_test.model
+		self.assertEqual(len(predictor_model.layers), 2)
+		self.assertTrue(isinstance(predictor_model.layers[0], GraphFP))
+		self.assertTrue(isinstance(predictor_model.layers[1], Dense))
+
+		self.assertEqual(predictor_model.layers[0].inner_dim, 8)
+		self.assertEqual(predictor_model.layers[0].output_dim, 512)
+
