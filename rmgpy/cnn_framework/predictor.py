1e763992a48a19a9140199ebfa9cbcf96888c371
KEHANG <kehanghan@gmail.com>
2016-10-30 18:16:23 -0400

    add build model from input feature in predictor class and corresponding unittest

--- rmgpy/cnn_framework/predictor.py
+++ rmgpy/cnn_framework/predictor.py
@@ -1,15 +1,34 @@
 
 from cnn_model import build_model
+from .input import read_input_file
+import os
+import rmgpy
 
 class Predictor(object):
 
-	def __init__(self):
+	def __init__(self, input_file=None):
 
 		self.model = None
+		if input_file:
+			self.input_file = input_file
+		else:
+			self.input_file = os.path.join(os.path.dirname(rmgpy.__file__),
+										'cnn_framework',
+										'test_data', 
+										'minimal_predictor', 
+										'predictor_input.py'
+										)
 
 	def build_model(self):
 
 		self.model = build_model()
+
+	def load_input(self, path=None):
+		
+		if path is None: 
+			path = self.input_file
+			print path
+		read_input_file(path, self)
 
 	def train(self):
 
@@ -26,7 +45,4 @@ class Predictor(object):
 	def predict(self):
 
 		pass
-
-def readInputFile(path, predictor):
-
-	pass
+    
