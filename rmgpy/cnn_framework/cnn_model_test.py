fdc0c551874ac7175f75e1223b73bf04ef7d5d53
KEHANG <kehanghan@gmail.com>
2016-10-30 17:27:16 -0400

    add unittests for these new modules

---
+++ rmgpy/cnn_framework/cnn_model_test.py
@@ -0,0 +1,21 @@
+
+from .cnn_model import *
+from .layers import GraphFP
+from keras.layers.core import Dense
+import unittest
+
+class Test_CNN_Model(unittest.TestCase):
+
+	def test_build_model(self):
+
+		embedding_size = 300
+		attribute_vector_size = 10
+		test_model = build_model(embedding_size=embedding_size, 
+								attribute_vector_size=attribute_vector_size
+								)
+		self.assertEqual(len(test_model.layers), 2)
+		self.assertTrue(isinstance(test_model.layers[0], GraphFP))
+		self.assertTrue(isinstance(test_model.layers[1], Dense))
+
+		self.assertEqual(test_model.layers[0].inner_dim, attribute_vector_size-1)
+		self.assertEqual(test_model.layers[0].output_dim, embedding_size)
