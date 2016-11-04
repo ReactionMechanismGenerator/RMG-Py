5c438b049eec38a108e9979419cec9f4e14e1840
KEHANG <kehanghan@gmail.com>
2016-11-04 00:30:03 -0400

    added parameter loading method and its unittest

--- rmgpy/cnn_framework/predictor_test.py
+++ rmgpy/cnn_framework/predictor_test.py
@@ -63,3 +63,37 @@ class Test_Predictor(unittest.TestCase):
 		self.assertEqual(dense2.W.shape.eval()[0], 50)
 		self.assertEqual(dense2.W.shape.eval()[1], 1)
 		self.assertEqual(dense2.b.shape.eval()[0], 1)
+
+	def test_load_parameters(self):
+
+		test_predictor_input = os.path.join(os.path.dirname(rmgpy.__file__),
+											'cnn_framework',
+											'test_data', 
+											'minimal_predictor', 
+											'predictor_input.py'
+											)
+		self.predictor.load_input(test_predictor_input)
+
+		param_path = os.path.join(os.path.dirname(rmgpy.__file__),
+											'cnn_framework',
+											'test_data', 
+											'minimal_predictor', 
+											'weights.h5'
+											)
+		self.predictor.load_parameters(param_path)
+
+		gfp = self.predictor.model.layers[0]
+		dense1 = self.predictor.model.layers[1]
+		dense2 = self.predictor.model.layers[2]
+
+		self.assertAlmostEqual(gfp.W_inner.eval()[0][0][0], 1.050, 3)
+		self.assertAlmostEqual(gfp.b_inner.eval()[0][0][0], 0.036, 3)
+		self.assertAlmostEqual(gfp.W_output.eval()[0][0][0], 0.181, 3)
+		self.assertAlmostEqual(gfp.b_output.eval()[0][0][0], 0.071, 3)
+
+		self.assertAlmostEqual(dense1.W.eval()[0][0], 0.344, 3)
+		self.assertAlmostEqual(dense1.b.eval()[0], 0.486, 3)
+
+		self.assertAlmostEqual(dense2.W.eval()[0][0], 3.284, 3)
+		self.assertAlmostEqual(dense2.b.eval()[0], 3.074, 3)
+		
