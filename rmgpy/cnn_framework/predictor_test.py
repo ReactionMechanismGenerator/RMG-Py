29315fb641794ba7851a961fce4c9aa8cc1f0068
KEHANG <kehanghan@gmail.com>
2016-11-04 00:29:10 -0400

    add more detailed testings for load model input method

--- rmgpy/cnn_framework/predictor_test.py
+++ rmgpy/cnn_framework/predictor_test.py
@@ -36,6 +36,30 @@ class Test_Predictor(unittest.TestCase):
 		self.assertEqual(len(predictor_model.layers), 3)
 		self.assertTrue(isinstance(predictor_model.layers[0], GraphFP))
 		self.assertTrue(isinstance(predictor_model.layers[1], Dense))
+		self.assertTrue(isinstance(predictor_model.layers[2], Dense))
 
-		self.assertEqual(predictor_model.layers[0].inner_dim, 32)
-		self.assertEqual(predictor_model.layers[0].output_dim, 512)
+		gfp = self.predictor.model.layers[0]
+		dense1 = self.predictor.model.layers[1]
+		dense2 = self.predictor.model.layers[2]
+
+		self.assertEqual(gfp.W_inner.shape.eval()[0], 6)
+		self.assertEqual(gfp.W_inner.shape.eval()[1], 32)
+		self.assertEqual(gfp.W_inner.shape.eval()[2], 32)
+		self.assertEqual(gfp.b_inner.shape.eval()[0], 6)
+		self.assertEqual(gfp.b_inner.shape.eval()[1], 1)
+		self.assertEqual(gfp.b_inner.shape.eval()[2], 32)
+
+		self.assertEqual(gfp.W_output.shape.eval()[0], 6)
+		self.assertEqual(gfp.W_output.shape.eval()[1], 32)
+		self.assertEqual(gfp.W_output.shape.eval()[2], 512)
+		self.assertEqual(gfp.b_output.shape.eval()[0], 6)
+		self.assertEqual(gfp.b_output.shape.eval()[1], 1)
+		self.assertEqual(gfp.b_output.shape.eval()[2], 512)
+
+		self.assertEqual(dense1.W.shape.eval()[0], 512)
+		self.assertEqual(dense1.W.shape.eval()[1], 50)
+		self.assertEqual(dense1.b.shape.eval()[0], 50)
+
+		self.assertEqual(dense2.W.shape.eval()[0], 50)
+		self.assertEqual(dense2.W.shape.eval()[1], 1)
+		self.assertEqual(dense2.b.shape.eval()[0], 1)
