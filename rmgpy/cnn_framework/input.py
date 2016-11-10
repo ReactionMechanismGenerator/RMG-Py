2c602c45b59411caa3a6db099fe1cd0d8845c292
KEHANG <kehanghan@gmail.com>
2016-11-09 21:02:31 -0500

    turn the padding to False by default

--- rmgpy/cnn_framework/input.py
+++ rmgpy/cnn_framework/input.py
@@ -6,7 +6,7 @@ from .cnn_model import build_model
 predictor = None
 
 def predictor_model(embedding_size=512, attribute_vector_size=9, depth=2, 
-                scale_output=0.05, padding=True, 
+                scale_output=0.05, padding=False, 
                 hidden=0, hidden_activation='tanh',
                 output_activation='linear', output_size=1, 
                 lr=0.01, optimizer='adam', loss='mse'):
--- rmgpy/cnn_framework/test_data/minimal_predictor/predictor_input.py
+++ rmgpy/cnn_framework/test_data/minimal_predictor/predictor_input.py
@@ -1,4 +1,4 @@
-predictor_model(embedding_size=512, attribute_vector_size=33, depth=5, scale_output=0.05, padding=True, 
+predictor_model(embedding_size=512, attribute_vector_size=33, depth=5, scale_output=0.05, padding=False, 
 				hidden=50, hidden_activation='tanh',
 				output_activation='linear', output_size=1, 
 				lr=0.01, optimizer='adam', loss='mse')
