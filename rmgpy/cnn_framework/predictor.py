29d50e294ea10091ec8348c3115f2536a4932233
KEHANG <kehanghan@gmail.com>
2016-11-09 21:04:03 -0500

    use default parameters from data rather than from test_data

--- rmgpy/cnn_framework/predictor.py
+++ rmgpy/cnn_framework/predictor.py
@@ -15,8 +15,7 @@ class Predictor(object):
 		else:
 			self.input_file = os.path.join(os.path.dirname(rmgpy.__file__),
 										'cnn_framework',
-										'test_data', 
-										'minimal_predictor', 
+										'data',  
 										'predictor_input.py'
 										)
 
@@ -41,7 +40,15 @@ class Predictor(object):
 
 		pass
 
-	def load_parameters(self, param_path):
+	def load_parameters(self, param_path=None):
+
+		if not param_path:
+			param_path = os.path.join(os.path.dirname(rmgpy.__file__),
+									'cnn_framework',
+									'data', 
+									'weights', 
+									'polycyclic_enthalpy_weights.h5'
+									)
 
 		self.model.load_weights(param_path)
 
