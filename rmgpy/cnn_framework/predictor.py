15904e09f99b6b6219200b596924123f7de68949
KEHANG <kehanghan@gmail.com>
2016-11-11 00:18:32 -0500

    added kfcv_train method for Predictor

--- rmgpy/cnn_framework/predictor.py
+++ rmgpy/cnn_framework/predictor.py
@@ -1,10 +1,12 @@
 
-from cnn_model import build_model
+from cnn_model import build_model, train_model
 from .input import read_input_file
 from .molecule_tensor import get_molecule_tensor
 import os
 import rmgpy
 import numpy as np
+from .data import get_HC_polycyclics_data_from_db, prepare_folded_data, prepare_data_one_fold
+import logging
 
 class Predictor(object):
 
@@ -37,9 +39,33 @@ class Predictor(object):
 			print path
 		read_input_file(path, self)
 
-	def train(self):
+	def kfcv_train(self, folds):
 
-		pass
+		# prepare data for training
+		(X, y) = get_HC_polycyclics_data_from_db('sdata134k', 'sdata134k_table')
+		(folded_Xs, folded_ys) = prepare_folded_data(X, y, folds)
+
+		losses = []
+		val_losses = []
+		for fold in range(folds):
+			data = prepare_data_one_fold(folded_Xs, folded_ys, current_fold=fold)
+
+			# execute train_model
+			(model, loss, val_loss) = train_model(self.model, data, 
+												nb_epoch=150, lr_func='0.01', 
+												patience=10)
+
+			losses.append(loss)
+			val_losses.append(val_loss)
+			# save model
+			# self.save_model()
+
+		# mean loss and val_loss used for selecting parameters, 
+		# e.g., lr, epoch, attributes, etc
+		mean_loss = mean(losses)
+		mean_val_loss = mean(val_losses)
+
+		self.report_training_results(mean_loss, mean_val_loss)
 
 	def load_parameters(self, param_path=None):
 
