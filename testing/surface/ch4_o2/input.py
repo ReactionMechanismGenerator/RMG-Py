48a349672f824995427a9b73c72273db5b220cbf
Franklin Goldsmith <franklin_goldsmith@brown.edu>
2017-02-15 10:13:21 -0500

    Input file for catalytic combustion of methane.
    Edit CH4 + O2 surface input file.
    
    * Increased the O2 to make it stoichiometric,
    * Increased the termination conversion to 99% of CH4,
    * Tightened the tolerance /10 to 1e-5.
    
    Not sure the stoichiometric thing was a good idea
    (feel free to change it back)
    but I wanted to play with a tighter tolerance.
    
    It ran for 2 minutes.
    The final model core has 35 species and 89 reactions
    The final model edge has 119 species and 181 reactions
    
    Turn on surface reaction families in the testing example input file!
    
    The new syntax requires specifying different sets of reaction families.

---
+++ testing/surface/ch4_o2/input.py
@@ -0,0 +1,79 @@
+# Data sources
+database(
+    thermoLibraries=['surfaceThermo', 'primaryThermoLibrary', 'thermo_DFT_CCSDTF12_BAC','DFT_QCI_thermo'],
+    reactionLibraries = [('Deutschmann_Ni', False)],
+    seedMechanisms = [],
+    kineticsDepositories = ['training'],
+    kineticsFamilies = ['surface','default'],
+    kineticsEstimator = 'rate rules',
+)
+
+species(
+    label='CH4',
+    reactive=True,
+    structure=SMILES("[CH4]"),
+)
+
+
+species(
+   label='O2',
+   reactive=True,
+   structure=adjacencyList(
+       """
+1 O u1 p2 c0 {2,S}
+2 O u1 p2 c0 {1,S}
+"""),
+)
+
+species(
+    label='N2',
+    reactive=False,
+    structure=SMILES("N#N"),
+)
+
+
+#-------
+species(
+    label='vacantX',
+    reactive=True,
+    structure=adjacencyList("1 X u0"),
+)
+#----------
+# Reaction systems
+surfaceReactor(
+    temperature=(1300,'K'),
+    initialPressure=(1.0, 'bar'),
+    initialGasMoleFractions={
+        "CH4": 0.1,
+        "O2": 0.2,
+        "N2": 0.7,
+    },
+    initialSurfaceCoverages={
+        "vacantX": 1.0,
+    },
+    surfaceVolumeRatio=(1.e5, 'm^-1'),
+    surfaceSiteDensity=(2.9e-9, 'mol/cm^2'),
+    terminationConversion = { "CH4":0.99,},
+    terminationTime=(0.1, 's'),
+)
+
+simulator(
+    atol=1e-18,
+    rtol=1e-12,
+)
+
+model(
+    toleranceKeepInEdge=0.0,
+    toleranceMoveToCore=1e-5,
+    toleranceInterruptSimulation=0.1,
+    maximumEdgeSpecies=100000
+)
+
+options(
+    units='si',
+    saveRestartPeriod=None,
+    generateOutputHTML=True,
+    generatePlots=True,
+    saveEdgeSpecies=True,
+    saveSimulationProfiles=True,
+)
