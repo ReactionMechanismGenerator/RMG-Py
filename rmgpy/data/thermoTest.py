0d345a2b656900a6907eb87800646176318a494a
KEHANG <kehanghan@gmail.com>
2017-03-12 15:27:43 -0400

    adds two unittests for the isBicyclic method

--- rmgpy/data/thermoTest.py
+++ rmgpy/data/thermoTest.py
@@ -728,6 +728,26 @@ class TestMolecularManipulationInvolvedInThermoEstimation(unittest.TestCase):
         self.assertEqual(isAromaticRing(ring2mol), False)
         self.assertEqual(isAromaticRing(ring3mol), True)
 
+    def testIsBicyclic1(self):
+        """
+        Test isBicyclic identifies bicyclic correctly
+        """
+        smiles = 'C1=CCC2C1=C2'
+        mol = Molecule().fromSMILES(smiles)
+        polyring = mol.getDisparateRings()[1][0]
+
+        self.assertTrue(isBicyclic(polyring))
+
+    def testIsBicyclic2(self):
+        """
+        Test isBicyclic identifies bicyclic correctly
+        """
+        smiles = 'C1C=C2C=CC=C3C=CC4=CC=CC=1C4=C23'
+        mol = Molecule().fromSMILES(smiles)
+        polyring = mol.getDisparateRings()[1][0]
+
+        self.assertFalse(isBicyclic(polyring))
+
     def testFindAromaticBondsFromSubMolecule(self):
 
         smiles = "C1=CC=C2C=CC=CC2=C1"
