dfda4331aeff9ea7b5897b3c20ae2e56300f45ec
MarkGoldman <goldmanm@mit.edu>
2017-03-14 18:24:10 -0400

    give options for cythonizing code for coverage
    allows options to cythonize code in a way that enables
    coverage of the code to be used.

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ except ImportError:
     
 try:
     from Cython.Distutils import build_ext
+    from Cython.Build import cythonize
     import Cython.Compiler.Options
 except ImportError:
     print 'Cython (http://www.cython.org/) is required to build or install RMG Py.'
@@ -53,100 +54,102 @@ Cython.Compiler.Options.annotate = True
 
 # Turn on profiling capacity for all Cython modules
 #Cython.Compiler.Options.directive_defaults['profile'] = True
+# turn on or off cython coverage
+cython_coverage = 1
 
 ################################################################################
 
 def getMainExtensionModules():
     return [
         # Kinetics
-        Extension('rmgpy.kinetics.arrhenius', ['rmgpy/kinetics/arrhenius.pyx']),
-        Extension('rmgpy.kinetics.chebyshev', ['rmgpy/kinetics/chebyshev.pyx']),
-        Extension('rmgpy.kinetics.kineticsdata', ['rmgpy/kinetics/kineticsdata.pyx']),
-        Extension('rmgpy.kinetics.falloff', ['rmgpy/kinetics/falloff.pyx']),
-        Extension('rmgpy.kinetics.model', ['rmgpy/kinetics/model.pyx']),
-        Extension('rmgpy.kinetics.tunneling', ['rmgpy/kinetics/tunneling.pyx']),
+        Extension('rmgpy.kinetics.arrhenius', ['rmgpy/kinetics/arrhenius.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.kinetics.chebyshev', ['rmgpy/kinetics/chebyshev.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.kinetics.kineticsdata', ['rmgpy/kinetics/kineticsdata.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.kinetics.falloff', ['rmgpy/kinetics/falloff.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.kinetics.model', ['rmgpy/kinetics/model.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.kinetics.tunneling', ['rmgpy/kinetics/tunneling.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
         # Molecules and molecular representations
-        Extension('rmgpy.molecule.atomtype', ['rmgpy/molecule/atomtype.py'], include_dirs=['.']),
-        Extension('rmgpy.molecule.element', ['rmgpy/molecule/element.py'], include_dirs=['.']),
-        Extension('rmgpy.molecule.graph', ['rmgpy/molecule/graph.pyx'], include_dirs=['.']),
-        Extension('rmgpy.molecule.group', ['rmgpy/molecule/group.py'], include_dirs=['.']),
-        Extension('rmgpy.molecule.molecule', ['rmgpy/molecule/molecule.py'], include_dirs=['.']),
-        Extension('rmgpy.molecule.symmetry', ['rmgpy/molecule/symmetry.py'], include_dirs=['.']),
-        Extension('rmgpy.molecule.vf2', ['rmgpy/molecule/vf2.pyx'], include_dirs=['.']),
-        Extension('rmgpy.molecule.parser', ['rmgpy/molecule/parser.py'], include_dirs=['.']),
-        Extension('rmgpy.molecule.generator', ['rmgpy/molecule/generator.py'], include_dirs=['.']),
-        Extension('rmgpy.molecule.util', ['rmgpy/molecule/util.py'], include_dirs=['.']),
-        Extension('rmgpy.molecule.inchi', ['rmgpy/molecule/inchi.py'], include_dirs=['.']),
-        Extension('rmgpy.molecule.resonance', ['rmgpy/molecule/resonance.py'], include_dirs=['.']),
-        Extension('rmgpy.molecule.pathfinder', ['rmgpy/molecule/pathfinder.py'], include_dirs=['.']),
+        Extension('rmgpy.molecule.atomtype', ['rmgpy/molecule/atomtype.py'], include_dirs=['.'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.molecule.element', ['rmgpy/molecule/element.py'], include_dirs=['.'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.molecule.graph', ['rmgpy/molecule/graph.pyx'], include_dirs=['.'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.molecule.group', ['rmgpy/molecule/group.py'], include_dirs=['.'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.molecule.molecule', ['rmgpy/molecule/molecule.py'], include_dirs=['.'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.molecule.symmetry', ['rmgpy/molecule/symmetry.py'], include_dirs=['.'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.molecule.vf2', ['rmgpy/molecule/vf2.pyx'], include_dirs=['.'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.molecule.parser', ['rmgpy/molecule/parser.py'], include_dirs=['.'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.molecule.generator', ['rmgpy/molecule/generator.py'], include_dirs=['.'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.molecule.util', ['rmgpy/molecule/util.py'], include_dirs=['.'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.molecule.inchi', ['rmgpy/molecule/inchi.py'], include_dirs=['.'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.molecule.resonance', ['rmgpy/molecule/resonance.py'], include_dirs=['.'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.molecule.pathfinder', ['rmgpy/molecule/pathfinder.py'], include_dirs=['.'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
         # Pressure dependence
-        Extension('rmgpy.pdep.collision', ['rmgpy/pdep/collision.pyx']),
-        Extension('rmgpy.pdep.configuration', ['rmgpy/pdep/configuration.pyx']),
-        Extension('rmgpy.pdep.me', ['rmgpy/pdep/me.pyx']),
-        Extension('rmgpy.pdep.msc', ['rmgpy/pdep/msc.pyx']),
-        Extension('rmgpy.pdep.reaction', ['rmgpy/pdep/reaction.pyx']),
-        Extension('rmgpy.pdep.rs', ['rmgpy/pdep/rs.pyx']),
-        Extension('rmgpy.pdep.cse', ['rmgpy/pdep/cse.pyx']),
+        Extension('rmgpy.pdep.collision', ['rmgpy/pdep/collision.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.pdep.configuration', ['rmgpy/pdep/configuration.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.pdep.me', ['rmgpy/pdep/me.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.pdep.msc', ['rmgpy/pdep/msc.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.pdep.reaction', ['rmgpy/pdep/reaction.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.pdep.rs', ['rmgpy/pdep/rs.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.pdep.cse', ['rmgpy/pdep/cse.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
         # Statistical mechanics
-        Extension('rmgpy.statmech.conformer', ['rmgpy/statmech/conformer.pyx']),
-        Extension('rmgpy.statmech.mode', ['rmgpy/statmech/mode.pyx']),
-        Extension('rmgpy.statmech.rotation', ['rmgpy/statmech/rotation.pyx']),
-        Extension('rmgpy.statmech.schrodinger', ['rmgpy/statmech/schrodinger.pyx']),
-        Extension('rmgpy.statmech.torsion', ['rmgpy/statmech/torsion.pyx']),
-        Extension('rmgpy.statmech.translation', ['rmgpy/statmech/translation.pyx']),
-        Extension('rmgpy.statmech.vibration', ['rmgpy/statmech/vibration.pyx']),
+        Extension('rmgpy.statmech.conformer', ['rmgpy/statmech/conformer.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.statmech.mode', ['rmgpy/statmech/mode.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.statmech.rotation', ['rmgpy/statmech/rotation.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.statmech.schrodinger', ['rmgpy/statmech/schrodinger.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.statmech.torsion', ['rmgpy/statmech/torsion.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.statmech.translation', ['rmgpy/statmech/translation.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.statmech.vibration', ['rmgpy/statmech/vibration.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
         # Thermodynamics
-        Extension('rmgpy.thermo.thermodata', ['rmgpy/thermo/thermodata.pyx']),
-        Extension('rmgpy.thermo.model', ['rmgpy/thermo/model.pyx']),
-        Extension('rmgpy.thermo.nasa', ['rmgpy/thermo/nasa.pyx']),
-        Extension('rmgpy.thermo.wilhoit', ['rmgpy/thermo/wilhoit.pyx']),
+        Extension('rmgpy.thermo.thermodata', ['rmgpy/thermo/thermodata.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.thermo.model', ['rmgpy/thermo/model.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.thermo.nasa', ['rmgpy/thermo/nasa.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.thermo.wilhoit', ['rmgpy/thermo/wilhoit.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
         # Miscellaneous
-        Extension('rmgpy.constants', ['rmgpy/constants.py'], include_dirs=['.']),
-        Extension('rmgpy.quantity', ['rmgpy/quantity.py'], include_dirs=['.']),
-        Extension('rmgpy.reaction', ['rmgpy/reaction.py'], include_dirs=['.']),
-        Extension('rmgpy.species', ['rmgpy/species.py'], include_dirs=['.']),
+        Extension('rmgpy.constants', ['rmgpy/constants.py'], include_dirs=['.'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.quantity', ['rmgpy/quantity.py'], include_dirs=['.'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.reaction', ['rmgpy/reaction.py'], include_dirs=['.'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.species', ['rmgpy/species.py'], include_dirs=['.'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
     ]
     
 def getSolverExtensionModules():
     return [
-        Extension('rmgpy.solver.base', ['rmgpy/solver/base.pyx'], include_dirs=['.']),
-        Extension('rmgpy.solver.simple', ['rmgpy/solver/simple.pyx'], include_dirs=['.']),
-        Extension('rmgpy.solver.liquid', ['rmgpy/solver/liquid.pyx'], include_dirs=['.']),
+        Extension('rmgpy.solver.base', ['rmgpy/solver/base.pyx'], include_dirs=['.'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.solver.simple', ['rmgpy/solver/simple.pyx'], include_dirs=['.'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.solver.liquid', ['rmgpy/solver/liquid.pyx'], include_dirs=['.'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
     ]
 
 def getCanthermExtensionModules():
     return [
         # Kinetics
-        Extension('rmgpy.kinetics.arrhenius', ['rmgpy/kinetics/arrhenius.pyx']),
-        Extension('rmgpy.kinetics.chebyshev', ['rmgpy/kinetics/chebyshev.pyx']),
-        Extension('rmgpy.kinetics.kineticsdata', ['rmgpy/kinetics/kineticsdata.pyx']),
-        Extension('rmgpy.kinetics.falloff', ['rmgpy/kinetics/falloff.pyx']),
-        Extension('rmgpy.kinetics.model', ['rmgpy/kinetics/model.pyx']),
-        Extension('rmgpy.kinetics.tunneling', ['rmgpy/kinetics/tunneling.pyx']),
+        Extension('rmgpy.kinetics.arrhenius', ['rmgpy/kinetics/arrhenius.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.kinetics.chebyshev', ['rmgpy/kinetics/chebyshev.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.kinetics.kineticsdata', ['rmgpy/kinetics/kineticsdata.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.kinetics.falloff', ['rmgpy/kinetics/falloff.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.kinetics.model', ['rmgpy/kinetics/model.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.kinetics.tunneling', ['rmgpy/kinetics/tunneling.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
         # Pressure dependence
-        Extension('rmgpy.pdep.collision', ['rmgpy/pdep/collision.pyx']),
-        Extension('rmgpy.pdep.configuration', ['rmgpy/pdep/configuration.pyx']),
-        Extension('rmgpy.pdep.me', ['rmgpy/pdep/me.pyx']),
-        Extension('rmgpy.pdep.msc', ['rmgpy/pdep/msc.pyx']),
-        Extension('rmgpy.pdep.reaction', ['rmgpy/pdep/reaction.pyx']),
-        Extension('rmgpy.pdep.rs', ['rmgpy/pdep/rs.pyx']),
-        Extension('rmgpy.pdep.cse', ['rmgpy/pdep/cse.pyx']),
+        Extension('rmgpy.pdep.collision', ['rmgpy/pdep/collision.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.pdep.configuration', ['rmgpy/pdep/configuration.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.pdep.me', ['rmgpy/pdep/me.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.pdep.msc', ['rmgpy/pdep/msc.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.pdep.reaction', ['rmgpy/pdep/reaction.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.pdep.rs', ['rmgpy/pdep/rs.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.pdep.cse', ['rmgpy/pdep/cse.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
         # Statistical mechanics
-        Extension('rmgpy.statmech.conformer', ['rmgpy/statmech/conformer.pyx']),
-        Extension('rmgpy.statmech.mode', ['rmgpy/statmech/mode.pyx']),
-        Extension('rmgpy.statmech.rotation', ['rmgpy/statmech/rotation.pyx']),
-        Extension('rmgpy.statmech.schrodinger', ['rmgpy/statmech/schrodinger.pyx']),
-        Extension('rmgpy.statmech.torsion', ['rmgpy/statmech/torsion.pyx']),
-        Extension('rmgpy.statmech.translation', ['rmgpy/statmech/translation.pyx']),
-        Extension('rmgpy.statmech.vibration', ['rmgpy/statmech/vibration.pyx']),
+        Extension('rmgpy.statmech.conformer', ['rmgpy/statmech/conformer.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.statmech.mode', ['rmgpy/statmech/mode.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.statmech.rotation', ['rmgpy/statmech/rotation.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.statmech.schrodinger', ['rmgpy/statmech/schrodinger.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.statmech.torsion', ['rmgpy/statmech/torsion.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.statmech.translation', ['rmgpy/statmech/translation.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.statmech.vibration', ['rmgpy/statmech/vibration.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
         # Thermodynamics
-        Extension('rmgpy.thermo.thermodata', ['rmgpy/thermo/thermodata.pyx']),
-        Extension('rmgpy.thermo.model', ['rmgpy/thermo/model.pyx']),
-        Extension('rmgpy.thermo.nasa', ['rmgpy/thermo/nasa.pyx']),
-        Extension('rmgpy.thermo.wilhoit', ['rmgpy/thermo/wilhoit.pyx']),
+        Extension('rmgpy.thermo.thermodata', ['rmgpy/thermo/thermodata.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.thermo.model', ['rmgpy/thermo/model.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.thermo.nasa', ['rmgpy/thermo/nasa.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.thermo.wilhoit', ['rmgpy/thermo/wilhoit.pyx'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
         # Miscellaneous
-        Extension('rmgpy.constants', ['rmgpy/constants.py'], include_dirs=['.']),
-        Extension('rmgpy.quantity', ['rmgpy/quantity.py'], include_dirs=['.']),
+        Extension('rmgpy.constants', ['rmgpy/constants.py'], include_dirs=['.'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
+        Extension('rmgpy.quantity', ['rmgpy/quantity.py'], include_dirs=['.'], define_macros=[('CYTHON_TRACE',cython_coverage)]),
     ]
 
 ################################################################################
@@ -211,6 +214,6 @@ setup(name='RMG-Py',
     py_modules = modules,
     scripts=scripts,
     cmdclass = {'build_ext': build_ext},
-    ext_modules = ext_modules,
+    ext_modules = cythonize(ext_modules, compiler_directives={'linetrace': bool(cython_coverage)}),
     include_dirs=['.', numpy.get_include()],
 )
