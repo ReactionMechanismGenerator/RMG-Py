9d24940bafc5e96d35259086695f8e0c13460fca
KEHANG <kehanghan@gmail.com>
2017-03-14 21:48:29 -0400

    update minimal_predictor weights and predictor_input

--- rmgpy/cnn_framework/test_data/minimal_predictor/predictor_input.py
+++ rmgpy/cnn_framework/test_data/minimal_predictor/predictor_input.py
@@ -1,4 +1,6 @@
-predictor_model(embedding_size=512, attribute_vector_size=33, depth=5, scale_output=0.05, padding=False, 
+predictor_model(embedding_size=512, depth=2, 
+				add_extra_atom_attribute=True, add_extra_bond_attribute=False,
+				scale_output=0.05, padding=False, 
 				hidden=50, hidden_activation='tanh',
 				output_activation='linear', output_size=1, 
 				lr=0.01, optimizer='adam', loss='mse')
