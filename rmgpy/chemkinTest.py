1713bfbfc722a9ad2ff0f947c8ba1db6c08fca75
Max Liu <mjliu@mit.edu>
2017-03-17 14:42:51 -0400

    Reformat chemkinTest.py with pycharm
    Automatic formatting adjustment to meet PEP guidelines

--- rmgpy/chemkinTest.py
+++ rmgpy/chemkinTest.py
@@ -2,121 +2,121 @@ import unittest
 import os
 from chemkin import *
 import rmgpy
+
+
 ###################################################
 
 class ChemkinTest(unittest.TestCase):
+    def testReadTemplateReactionFamilyForMinimalExample(self):
+        """
+        This example is mainly to test if family info can be correctly
+        parsed from comments like '!Template reaction: R_Recombination'.
+        """
+        folder = os.path.join(os.path.dirname(rmgpy.__file__), 'test_data/chemkin/chemkin_py')
 
-	def testReadTemplateReactionFamilyForMinimalExample(self):
-		"""
-		This example is mainly to test if family info can be correctly 
-		parsed from comments like '!Template reaction: R_Recombination'.
-		"""
-		folder = os.path.join(os.path.dirname(rmgpy.__file__),'test_data/chemkin/chemkin_py')
-		
-		chemkinPath = os.path.join(folder, 'minimal', 'chem.inp')
-		dictionaryPath = os.path.join(folder,'minimal', 'species_dictionary.txt')
+        chemkinPath = os.path.join(folder, 'minimal', 'chem.inp')
+        dictionaryPath = os.path.join(folder, 'minimal', 'species_dictionary.txt')
 
-		# loadChemkinFile
-		species, reactions = loadChemkinFile(chemkinPath, dictionaryPath) 
+        # loadChemkinFile
+        species, reactions = loadChemkinFile(chemkinPath, dictionaryPath)
 
-		reaction1 = reactions[0]
-		self.assertEqual(reaction1.family, "R_Recombination")
+        reaction1 = reactions[0]
+        self.assertEqual(reaction1.family, "R_Recombination")
 
-		reaction2 = reactions[1]
-		self.assertEqual(reaction2.family, "H_Abstraction")
-	
-	def testReadTemplateReactionFamilyForPDDExample(self):
-		"""
-		This example is mainly to ensure comments like 
-		'! Kinetics were estimated in this direction instead 
-		of the reverse because:' or '! This direction matched 
-		an entry in H_Abstraction, the other was just an estimate.'
-		won't interfere reaction family info retrival.
-		"""
-		folder = os.path.join(os.path.dirname(rmgpy.__file__),'test_data/chemkin/chemkin_py')
-		
-		chemkinPath = os.path.join(folder, 'pdd', 'chem.inp')
-		dictionaryPath = os.path.join(folder,'pdd', 'species_dictionary.txt')
+        reaction2 = reactions[1]
+        self.assertEqual(reaction2.family, "H_Abstraction")
 
-		# loadChemkinFile
-		species, reactions = loadChemkinFile(chemkinPath, dictionaryPath) 
+    def testReadTemplateReactionFamilyForPDDExample(self):
+        """
+        This example is mainly to ensure comments like
+        '! Kinetics were estimated in this direction instead
+        of the reverse because:' or '! This direction matched
+        an entry in H_Abstraction, the other was just an estimate.'
+        won't interfere reaction family info retrival.
+        """
+        folder = os.path.join(os.path.dirname(rmgpy.__file__), 'test_data/chemkin/chemkin_py')
 
-		reaction1 = reactions[0]
-		self.assertEqual(reaction1.family, "H_Abstraction")
+        chemkinPath = os.path.join(folder, 'pdd', 'chem.inp')
+        dictionaryPath = os.path.join(folder, 'pdd', 'species_dictionary.txt')
 
-		reaction2 = reactions[1]
-		self.assertEqual(reaction2.family, "H_Abstraction")
-		
-	def testTransportDataReadAndWrite(self):
-		"""
-		Test that we can write to chemkin and recreate the same transport object
-		"""
-		from rmgpy.species import Species
-		from rmgpy.molecule import Molecule
-		from rmgpy.transport import TransportData
-		
-		Ar = Species(label="Ar", transportData=TransportData(shapeIndex=0, epsilon=(1134.93,'J/mol'), sigma=(3.33,'angstrom'), dipoleMoment=(0,'De'), polarizability=(0,'angstrom^3'), rotrelaxcollnum=0.0, comment="""GRI-Mech"""))
-		
-		Ar_write = Species(label="Ar")
-		folder = os.path.join(os.path.dirname(rmgpy.__file__),'test_data')
-		
-		tempTransportPath = os.path.join(folder, 'tran_temp.dat')
-		
-		saveTransportFile(tempTransportPath, [Ar])
-		speciesDict = {'Ar': Ar_write}
-		loadTransportFile(tempTransportPath, speciesDict)
-		self.assertEqual(repr(Ar),repr(Ar_write))
-		
-		os.remove(tempTransportPath)
-		
+        # loadChemkinFile
+        species, reactions = loadChemkinFile(chemkinPath, dictionaryPath)
 
-	def testUseChemkinNames(self):
-		"""
-		Test that the official chemkin names are used as labels for the created Species objects.
-		"""
+        reaction1 = reactions[0]
+        self.assertEqual(reaction1.family, "H_Abstraction")
 
-		folder = os.path.join(os.path.dirname(rmgpy.__file__),'test_data/chemkin/chemkin_py')
-		
-		chemkinPath = os.path.join(folder, 'minimal', 'chem.inp')
-		dictionaryPath = os.path.join(folder,'minimal', 'species_dictionary.txt')
+        reaction2 = reactions[1]
+        self.assertEqual(reaction2.family, "H_Abstraction")
 
-		# loadChemkinFile
-		species, reactions = loadChemkinFile(chemkinPath, dictionaryPath, useChemkinNames=True) 
+    def testTransportDataReadAndWrite(self):
+        """
+        Test that we can write to chemkin and recreate the same transport object
+        """
+        from rmgpy.species import Species
+        from rmgpy.molecule import Molecule
+        from rmgpy.transport import TransportData
 
-		expected = [
-			'Ar',
-			'He',
-			'Ne',
-			'N2',
-			'ethane',
-			'CH3',
-			'C2H5',
-			'C'
-		]
+        Ar = Species(label="Ar",
+                     transportData=TransportData(shapeIndex=0, epsilon=(1134.93, 'J/mol'), sigma=(3.33, 'angstrom'),
+                                                 dipoleMoment=(0, 'De'), polarizability=(0, 'angstrom^3'),
+                                                 rotrelaxcollnum=0.0, comment="""GRI-Mech"""))
 
-		for spc, label in zip(species, expected):
-			self.assertEqual(spc.label, label)
+        Ar_write = Species(label="Ar")
+        folder = os.path.join(os.path.dirname(rmgpy.__file__), 'test_data')
 
-	def testReactantN2IsReactiveAndGetsRightSpeciesIdentifier(self):
-		"""
-		Test that after loading chemkin files, species such as N2, which is in the default 
-		inert list of RMG, should be treated as reactive species and given right species
-		Identifier when it's reacting in reactions.
-		"""
-		folder = os.path.join(os.path.dirname(rmgpy.__file__),'test_data/chemkin/chemkin_py')
-		
-		chemkinPath = os.path.join(folder, 'NC', 'chem.inp')
-		dictionaryPath = os.path.join(folder,'NC', 'species_dictionary.txt')
+        tempTransportPath = os.path.join(folder, 'tran_temp.dat')
 
-		# loadChemkinFile
-		species, reactions = loadChemkinFile(chemkinPath, dictionaryPath, useChemkinNames=True) 
+        saveTransportFile(tempTransportPath, [Ar])
+        speciesDict = {'Ar': Ar_write}
+        loadTransportFile(tempTransportPath, speciesDict)
+        self.assertEqual(repr(Ar), repr(Ar_write))
 
-		for n2 in species:
-		    if n2.label == 'N2':
-		        break
-		self.assertTrue(n2.reactive)
+        os.remove(tempTransportPath)
 
-		self.assertEqual(getSpeciesIdentifier(n2), 'N2(35)') 
+    def testUseChemkinNames(self):
+        """
+        Test that the official chemkin names are used as labels for the created Species objects.
+        """
 
-	
+        folder = os.path.join(os.path.dirname(rmgpy.__file__), 'test_data/chemkin/chemkin_py')
 
+        chemkinPath = os.path.join(folder, 'minimal', 'chem.inp')
+        dictionaryPath = os.path.join(folder, 'minimal', 'species_dictionary.txt')
+
+        # loadChemkinFile
+        species, reactions = loadChemkinFile(chemkinPath, dictionaryPath, useChemkinNames=True)
+
+        expected = [
+            'Ar',
+            'He',
+            'Ne',
+            'N2',
+            'ethane',
+            'CH3',
+            'C2H5',
+            'C'
+        ]
+
+        for spc, label in zip(species, expected):
+            self.assertEqual(spc.label, label)
+
+    def testReactantN2IsReactiveAndGetsRightSpeciesIdentifier(self):
+        """
+        Test that after loading chemkin files, species such as N2, which is in the default
+        inert list of RMG, should be treated as reactive species and given right species
+        Identifier when it's reacting in reactions.
+        """
+        folder = os.path.join(os.path.dirname(rmgpy.__file__), 'test_data/chemkin/chemkin_py')
+
+        chemkinPath = os.path.join(folder, 'NC', 'chem.inp')
+        dictionaryPath = os.path.join(folder, 'NC', 'species_dictionary.txt')
+
+        # loadChemkinFile
+        species, reactions = loadChemkinFile(chemkinPath, dictionaryPath, useChemkinNames=True)
+
+        for n2 in species:
+            if n2.label == 'N2':
+                break
+        self.assertTrue(n2.reactive)
+
+        self.assertEqual(getSpeciesIdentifier(n2), 'N2(35)')
