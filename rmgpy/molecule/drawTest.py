a38b3efcc8cf7d3201f56559f92c0a8e2788fe3e
MarkGoldman <goldmanm@mit.edu>
2017-03-27 00:58:13 -0400

    allow for intermediate bond orders to be drawn properly
    added functionality to __renderBond and __drawLine to make dotted
    lines for intermediate bond orders
    
    reduced bond orders to 1 before sending to RDKit for coordinate placement
    and then rejuvenated the bond orders afterwards, without deepcoypying
    the molecule. This required two new methods `__make_single_bond` and
    `__replace_bonds` to accomplish this.

--- rmgpy/molecule/drawTest.py
+++ rmgpy/molecule/drawTest.py
@@ -38,7 +38,7 @@ import os.path
 
 from rmgpy.molecule import  Molecule
 from rmgpy.molecule.draw import MoleculeDrawer
-
+from rmgpy.species import Species
 ################################################################################
 
 class TestMoleculeDrawer(unittest.TestCase):
@@ -113,6 +113,17 @@ class TestMoleculeDrawer(unittest.TestCase):
         surface, cr, (xoff, yoff, width, height) = self.drawer.draw(self.molecule, format='pdf')
         self.assertIsInstance(surface, PDFSurface)
         self.assertGreater(width, height)
+
+    def testDrawNonStandardBonds(self):
+        
+        spec = Species().fromSMILES('[CH2]C=C[CH2]')
+        hybrid = spec.getResonanceHybrid()
+        try:
+            from cairocffi import PDFSurface
+        except ImportError:
+            from cairo import PDFSurface
+        surface, cr, (xoff, yoff, width, height) = self.drawer.draw(hybrid, format='pdf')
+        self.assertIsInstance(surface, PDFSurface)
 
 ################################################################################
 
