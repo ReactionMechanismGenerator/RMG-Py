fbf49572f67138a8064ebb7f28c69eead5c1a1bf
Matt Johnson <mjohnson541@gmail.com>
2017-03-29 16:40:11 -0400

    added tests for surface initialization and the surface layering constraint application process

--- rmgpy/solver/baseTest.py
+++ rmgpy/solver/baseTest.py
@@ -1,7 +1,7 @@
 import unittest
 import pickle
 import os.path
-
+import numpy
 from rmgpy.tools.loader import loadRMGPyJob
 import rmgpy
 
@@ -19,7 +19,7 @@ class ReactionSystemTest(unittest.TestCase):
 
     def setUp(self):
         self.listener = ConcentrationPrinter()
-
+        
         folder = os.path.join(os.path.dirname(rmgpy.__file__),'solver/files/listener/')
         inputFile = os.path.join(folder, 'input.py')
         chemkinFile = os.path.join(folder, 'chemkin/chem.inp')
@@ -27,7 +27,59 @@ class ReactionSystemTest(unittest.TestCase):
 
         self.rmg = loadRMGPyJob(inputFile, chemkinFile, spc_dict, generateImages=False)
 
+    def testSurfaceInitialization(self):
+        """
+        test that initialize_surface is correctly removing species and reactions when
+        they are no longer consistent with the surface (due to other species/reactions moving to the 
+        bulk core)
+        """
+        reactionSystem = self.rmg.reactionSystems[0]
+        reactionSystem.attach(self.listener)
+        reactionModel = self.rmg.reactionModel
+        
+        coreSpecies = reactionModel.core.species
+        coreReactions = reactionModel.core.reactions
+        surfaceSpecies = [coreSpecies[7],coreSpecies[6]] 
+        surfaceReactions = [coreReactions[0],coreReactions[2],coreReactions[3]]
+        
+        reactionSystem.initializeModel(coreSpecies,coreReactions,
+                                       reactionModel.edge.species,reactionModel.edge.reactions,surfaceSpecies,surfaceReactions)
+        
+        self.assertEquals(len(surfaceSpecies),1) #only H should be left
+        self.assertEquals(len(surfaceReactions),2) #all the reactions with H should stay
+        
+    
+    def testSurfaceLayeringConstraint(self):
+        """
+        test that the correct maximum under the surface layering constraint is being
+        found
+        """
+        reactionSystem = self.rmg.reactionSystems[0]
+        reactionSystem.attach(self.listener)
+        reactionModel = self.rmg.reactionModel
+        coreSpecies = reactionModel.core.species
+        coreReactions = reactionModel.core.reactions
+        
+        edgeSpecies = [coreSpecies[6],coreSpecies[7]]
+        edgeReactions = coreReactions[1:]
+        surfaceSpecies = [coreSpecies[5]] 
+        surfaceReactions = [coreReactions[0]]
+        coreSpecies = coreSpecies[0:6]+[coreSpecies[8]]
+        coreReactions = surfaceReactions[:]
+        reactionSystem.numCoreReactions = 1
+        reactionSystem.numCoreSpecies = 7
+        
+        reactionSystem.initializeModel(coreSpecies,coreReactions,
+                                       edgeSpecies,edgeReactions,surfaceSpecies,surfaceReactions)
+        
+        self.assertEquals(len(reactionSystem.surfaceSpeciesIndices),1) #surfaceSpeciesIndices calculated correctly
+        self.assertEquals(reactionSystem.surfaceSpeciesIndices[0],5) #surfaceSpeciesIndices calculated correctly
 
+        arr = numpy.array([5.,1.,2.])
+        ind = reactionSystem.maxIndUnderSurfaceLayeringConstraint(arr,reactionSystem.surfaceSpeciesIndices)
+        
+        self.assertEquals(ind,2) #maxIndUnderSurfaceLayeringConstraint worked correctly
+        
     def testAttachDetach(self):
         """
         Test that a ReactionSystem listener can be attached/detached.
@@ -52,7 +104,7 @@ class ReactionSystemTest(unittest.TestCase):
         reactionModel = self.rmg.reactionModel
 
         self.assertEqual(self.listener.data, [])
-
+        
         # run simulation:
         terminated, obj,sspcs,srxns = reactionSystem.simulate(
             coreSpecies = reactionModel.core.species,
@@ -67,7 +119,8 @@ class ReactionSystemTest(unittest.TestCase):
         ) 
 
         self.assertNotEqual(self.listener.data, [])
-
+    
+    
     def testPickle(self):
         """
         Test that a ReactionSystem object can be un/pickled.
