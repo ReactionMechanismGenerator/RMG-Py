524605ec51f35fddc418528b588118aec5d5e084
mark goldman <goldmanm@mit.edu>
2017-05-25 00:37:45 -0400

    added changeBaseEntropy method for NASA thermo class
    added a method to modify the base entropy of nasa polynomials.
    This allows for modifications of entropy without converting to ThermoData objects,
    which often results in slight rounding errors.

--- rmgpy/thermo/nasa.pyx
+++ rmgpy/thermo/nasa.pyx
@@ -136,6 +136,12 @@ cdef class NASAPolynomial(HeatCapacityModel):
         """
         self.c5 += deltaH / constants.R
 
+    cpdef changeBaseEntropy(self, double deltaS):
+        """
+        Add deltaS in J/molK to the base entropy of formation S298.
+        """
+        self.c6 += deltaS / constants.R
+
     cdef double integral2_T0(self, double T):
         """
         Return the value of the dimensionless integral
@@ -350,7 +356,16 @@ cdef class NASA(HeatCapacityModel):
         for poly in self.polynomials:
             poly.changeBaseEnthalpy(deltaH)
         return self
-    
+
+    cpdef NASA changeBaseEntropy(self, double deltaS):
+        """
+        Add deltaS in J/molK to the base entropy of formation S298 and return the
+        modified NASA object
+        """
+        for poly in self.polynomials:
+            poly.changeBaseEntropy(deltaS)
+        return self
+
     def toCantera(self):
         """
         Return the cantera equivalent NasaPoly2 object from this NASA object.
