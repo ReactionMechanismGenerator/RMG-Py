40789ff6ae3e25e299e5771b219605aadc1f4a35
Mark Payne <ampayne@mit.edu>
2017-05-30 20:46:25 -0400

    Update Copywrite Dates

--- rmgpy/qm/main.py
+++ rmgpy/qm/main.py
@@ -4,9 +4,8 @@
 #
 #   RMG - Reaction Mechanism Generator
 #
-#   Copyright (c) 2002-2012 Prof. Richard H. West (r.west@neu.edu),
-#                           Prof. William H. Green (whgreen@mit.edu)
-#                           and the RMG Team (rmg_dev@mit.edu)
+#   Copyright (c) 2002-2017 Prof. William H. Green (whgreen@mit.edu), 
+#   Prof. Richard H. West (r.west@neu.edu) and the RMG Team (rmg_dev@mit.edu)
 #
 #   Permission is hereby granted, free of charge, to any person obtaining a
 #   copy of this software and associated documentation files (the 'Software'),
