e3ed455c0b393b57fa080f91d2d2aabb71c5ccf1
Max Liu <mjliu@mit.edu>
2017-08-03 15:30:14 -0400

    Add unit tests for ring perception
    Test getRelevantCycles
    Test that output from getSSSR and getRelevant cycles has properly ordered vertices

--- rmgpy/molecule/graphTest.py
+++ rmgpy/molecule/graphTest.py
@@ -616,6 +616,68 @@ class TestGraph(unittest.TestCase):
         self.assertEqual(len(cycleList), 1)
         self.assertEqual(len(cycleList[0]), 4)
 
+    def test_getRelevantCycles(self):
+        """
+        Test the Graph.getRelevantCycles() method.
+        """
+        cycleList = self.graph.getRelevantCycles()
+        self.assertEqual(len(cycleList), 0)
+        # Create a cycle of length 4
+        edge = Edge(self.graph.vertices[0], self.graph.vertices[3])
+        self.graph.addEdge(edge)
+        # Create a second cycle of length 4
+        edge = Edge(self.graph.vertices[0], self.graph.vertices[5])
+        self.graph.addEdge(edge)
+        # Create a bridge forming multiple cycles of length 4
+        edge = Edge(self.graph.vertices[1], self.graph.vertices[4])
+        self.graph.addEdge(edge)
+
+        # SSSR should be 3 cycles of length 4
+        cycleList = self.graph.getSmallestSetOfSmallestRings()
+        self.assertEqual(len(cycleList), 3)
+        sizeList = sorted([len(cycle) for cycle in cycleList])
+        self.assertEqual(sizeList, [4, 4, 4])
+
+        # RC should be 5 cycles of length 4
+        cycleList = self.graph.getRelevantCycles()
+        self.assertEqual(len(cycleList), 5)
+        sizeList = sorted([len(cycle) for cycle in cycleList])
+        self.assertEqual(sizeList, [4, 4, 4, 4, 4])
+
+    def test_cycleListOrderSSSR(self):
+        """
+        Test that getSmallestSetOfSmallestRings return vertices in the proper order.
+
+        There are methods such as symmetry and molecule drawing which rely
+        on the fact that subsequent list entries are connected.
+        """
+        # Create a cycle of length 5
+        edge = Edge(self.graph.vertices[0], self.graph.vertices[4])
+        self.graph.addEdge(edge)
+        # Test SSSR
+        sssr = self.graph.getSmallestSetOfSmallestRings()
+        self.assertEqual(len(sssr), 1)
+        self.assertEqual(len(sssr[0]), 5)
+        for i in range(5):
+            self.assertTrue(self.graph.hasEdge(sssr[0][i], sssr[0][i - 1]))
+
+    def test_cycleListOrderRC(self):
+        """
+        Test that getRelevantCycles return vertices in the proper order.
+
+        There are methods such as symmetry and molecule drawing which rely
+        on the fact that subsequent list entries are connected.
+        """
+        # Create a cycle of length 5
+        edge = Edge(self.graph.vertices[0], self.graph.vertices[4])
+        self.graph.addEdge(edge)
+        # Test RC
+        rc = self.graph.getRelevantCycles()
+        self.assertEqual(len(rc), 1)
+        self.assertEqual(len(rc[0]), 5)
+        for i in range(5):
+            self.assertTrue(self.graph.hasEdge(rc[0][i], rc[0][i - 1]))
+
     def test_getPolycyclicRings(self):
         """
         Test that the Graph.getPolycyclicRings() method returns only polycyclic rings.
