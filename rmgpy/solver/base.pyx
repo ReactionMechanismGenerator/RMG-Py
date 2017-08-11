a551a96616aa806c20963bde38b0a6417697148d
Matt Johnson <mjohnson541@gmail.com>
2017-08-11 11:06:35 -0400

    pruning fix and naming mistake fix

--- rmgpy/solver/base.pyx
+++ rmgpy/solver/base.pyx
@@ -545,7 +545,7 @@ cdef class ReactionSystem(DASx):
         toleranceKeepInEdge = modelSettings.fluxToleranceKeepInEdge if prune else 0
         toleranceMoveToCore = modelSettings.fluxToleranceMoveToCore
         toleranceMoveEdgeReactionToCore = modelSettings.toleranceMoveEdgeReactionToCore
-        toleranceInterruptSimulation =  modelSettings.toleranceInterruptSimulation if prune else modelSettings.fluxToleranceMoveToCore
+        toleranceInterruptSimulation =  modelSettings.fluxToleranceInterrupt if prune else modelSettings.fluxToleranceMoveToCore
         toleranceMoveEdgeReactionToCoreInterrupt=  modelSettings.toleranceMoveEdgeReactionToCore if prune else modelSettings.toleranceMoveEdgeReactionToCore
         toleranceMoveEdgeReactionToSurface = modelSettings.toleranceMoveEdgeReactionToSurface
         toleranceMoveSurfaceSpeciesToCore = modelSettings.toleranceMoveSurfaceSpeciesToCore
