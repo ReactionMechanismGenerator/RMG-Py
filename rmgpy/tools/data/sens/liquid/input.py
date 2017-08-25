2591fb6b5fe432a5d6dbc4f9945f753cbb83e9a3
cgrambow <cgrambow@mit.edu>
2017-08-25 18:20:46 -0400

    Add unit test for liquid phase sensitivity
    Also move data for minimal sensitivity test

---
+++ rmgpy/tools/data/sens/liquid/input.py
@@ -0,0 +1,65 @@
+# Data sources
+database(
+    thermoLibraries = ['primaryThermoLibrary'],
+    reactionLibraries = [],
+    seedMechanisms = [],
+    kineticsDepositories = ['training'],
+    kineticsFamilies = 'default',
+    kineticsEstimator = 'rate rules',
+)
+
+# Constraints on generated species
+generatedSpeciesConstraints(
+    maximumRadicalElectrons = 3,
+)
+
+# List of species
+species(
+    label='octane',
+    reactive=True,
+    structure=SMILES("C(CCCCC)CC"),
+)
+
+species(
+    label='oxygen',
+    reactive=True,
+    structure=SMILES("[O][O]"),
+)
+
+# Reaction systems
+liquidReactor(
+    temperature=(500,'K'),
+    initialConcentrations={
+        "octane": (6.154e-3,'mol/cm^3'),
+        "oxygen": (4.953e-6,'mol/cm^3')
+    },
+    terminationTime=(50,'s'),
+    sensitivity=['octane'],
+    sensitivityThreshold=1e-8,
+)
+
+solvation(
+	solvent='octane'
+)
+
+simulator(
+    atol=1e-16,
+    rtol=1e-8,
+    sens_atol=1e-6,
+    sens_rtol=1e-4,
+)
+
+model(
+    toleranceKeepInEdge=1E-9,
+    toleranceMoveToCore=0.001,
+    toleranceInterruptSimulation=0.1,
+    maximumEdgeSpecies=100000
+)
+
+options(
+    units='si',
+    saveRestartPeriod=None,
+    generateOutputHTML=False,
+    generatePlots=False,
+    saveSimulationProfiles=True,
+)
