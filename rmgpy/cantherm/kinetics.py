58ee91f14c1b192c1d02718434dd654d15601503
Mark Goldman <goldmanm@mit.edu>
2017-11-23 23:57:29 -0500

    prevent printing kinetics if not generated with cantherm
    When calling the kinetics() method in cantherm, it will automatically
    print out kinetic TST statistics, even if TST is not used, which
    throws an error in cantherm. This commit
    prevents printing out these statistics when TST is not used.

--- rmgpy/cantherm/kinetics.py
+++ rmgpy/cantherm/kinetics.py
@@ -54,6 +54,9 @@ class KineticsJob(object):
     """
     A representation of a CanTherm kinetics job. This job is used to compute 
     and save the high-pressure-limit kinetics information for a single reaction.
+
+    `usedTST` - a boolean representing if TST was used to calculate the kinetics
+                if kinetics is already given in the input, then it is False.
     """
     
     def __init__(self, reaction,  
@@ -62,7 +65,7 @@ class KineticsJob(object):
                  Tlist=None,
                  Tcount=0,
                  sensitivity_conditions=None):
-        
+        self.usedTST = False
         if Tmin is not None:
             self.Tmin = quantity.Quantity(Tmin)
         else:
@@ -150,7 +153,7 @@ class KineticsJob(object):
 
         if isinstance(self.reaction.kinetics, Arrhenius):
             return None
-
+        self.usedTST=True
         kineticsClass = 'Arrhenius'
         
         tunneling = self.reaction.transitionState.tunneling
@@ -200,69 +203,71 @@ class KineticsJob(object):
         factor = 1e6 ** (order-1)
         
         f = open(outputFile, 'a')
-    
-        f.write('#   ======= =========== =========== =========== ===============\n')
-        f.write('#   Temp.   k (TST)     Tunneling   k (TST+T)   Units\n')
-        f.write('#   ======= =========== =========== =========== ===============\n')
-        
-        if self.Tlist is None:
-            Tlist = numpy.array([300,400,500,600,800,1000,1500,2000])
-        else:
-            Tlist =self.Tlist.value_si
 
-        for T in Tlist:  
-            tunneling = reaction.transitionState.tunneling
-            reaction.transitionState.tunneling = None
-            try:
-                k0 = reaction.calculateTSTRateCoefficient(T) * factor
-            except SpeciesError:
-                k0 = 0
-            reaction.transitionState.tunneling = tunneling
-            try:
-                k = reaction.calculateTSTRateCoefficient(T) * factor
-                kappa = k / k0
-            except SpeciesError:
-                k = reaction.getRateCoefficient(T)
-                kappa = 0
-                logging.info("The species in reaction {} do not have adequate information for TST, using default kinetics values.".format(reaction))
-            tunneling = reaction.transitionState.tunneling
-            ks.append(k)
-            k0s.append(k0)
-
-            f.write('#    {0:4g} K {1:11.3e} {2:11g} {3:11.3e} {4}\n'.format(T, k0, kappa, k, self.kunits))
-        f.write('#   ======= =========== =========== =========== ===============\n')
-        f.write('\n\n')
-        
-        f.write('#   ======= ============ =========== ============ ============= =========\n')
-        f.write('#   Temp.    Kc (eq)        Units     krev (TST)   krev (TST+T)   Units\n')
-        f.write('#   ======= ============ =========== ============ ============= =========\n')
-
-        # Initialize Object for Converting Units
-        if self.Kequnits != '       ':
-            keq_unit_converter = quantity.Units(self.Kequnits).getConversionFactorFromSI()
-        else:
-            keq_unit_converter = 1
-
-        for n,T in enumerate(Tlist):
-            k = ks[n]
-            k0 = k0s[n]
-            Keq = keq_unit_converter * reaction.getEquilibriumConstant(T)  # getEquilibriumConstant returns SI units
-            k0rev = k0/Keq
-            krev =  k/Keq
-            k0revs.append(k0rev)
-            krevs.append(krev)
-            f.write('#    {0:4g} K {1:11.3e}   {2}  {3:11.3e}   {4:11.3e}      {5}\n'.format(T, Keq, self.Kequnits, k0rev, krev, self.krunits))
-
+        if self.usedTST:
+            #If TST is not used, eg. it was given in 'reaction', then this will
+            #throw an error.
+            f.write('#   ======= =========== =========== =========== ===============\n')
+            f.write('#   Temp.   k (TST)     Tunneling   k (TST+T)   Units\n')
+            f.write('#   ======= =========== =========== =========== ===============\n')
             
-        f.write('#   ======= ============ =========== ============ ============= =========\n')
-        f.write('\n\n')
-        
-        kinetics0rev = Arrhenius().fitToData(Tlist, numpy.array(k0revs), kunits=self.krunits)
-        kineticsrev = Arrhenius().fitToData(Tlist, numpy.array(krevs), kunits=self.krunits)
-        
-        f.write('# krev (TST) = {0} \n'.format(kinetics0rev))
-        f.write('# krev (TST+T) = {0} \n\n'.format(kineticsrev))
-                
+            if self.Tlist is None:
+                Tlist = numpy.array([300,400,500,600,800,1000,1500,2000])
+            else:
+                Tlist =self.Tlist.value_si
+
+            for T in Tlist:  
+                tunneling = reaction.transitionState.tunneling
+                reaction.transitionState.tunneling = None
+                try:
+                    k0 = reaction.calculateTSTRateCoefficient(T) * factor
+                except SpeciesError:
+                    k0 = 0
+                reaction.transitionState.tunneling = tunneling
+                try:
+                    k = reaction.calculateTSTRateCoefficient(T) * factor
+                    kappa = k / k0
+                except (SpeciesError,ZeroDivisionError):
+                    k = reaction.getRateCoefficient(T)
+                    kappa = 0
+                    logging.info("The species in reaction {} do not have adequate information for TST, using default kinetics values.".format(reaction))
+                tunneling = reaction.transitionState.tunneling
+                ks.append(k)
+                k0s.append(k0)
+
+                f.write('#    {0:4g} K {1:11.3e} {2:11g} {3:11.3e} {4}\n'.format(T, k0, kappa, k, self.kunits))
+            f.write('#   ======= =========== =========== =========== ===============\n')
+            f.write('\n\n')
+            
+            f.write('#   ======= ============ =========== ============ ============= =========\n')
+            f.write('#   Temp.    Kc (eq)        Units     krev (TST)   krev (TST+T)   Units\n')
+            f.write('#   ======= ============ =========== ============ ============= =========\n')
+
+            # Initialize Object for Converting Units
+            if self.Kequnits != '       ':
+                keq_unit_converter = quantity.Units(self.Kequnits).getConversionFactorFromSI()
+            else:
+                keq_unit_converter = 1
+
+            for n,T in enumerate(Tlist):
+                k = ks[n]
+                k0 = k0s[n]
+                Keq = keq_unit_converter * reaction.getEquilibriumConstant(T)  # getEquilibriumConstant returns SI units
+                k0rev = k0/Keq
+                krev =  k/Keq
+                k0revs.append(k0rev)
+                krevs.append(krev)
+                f.write('#    {0:4g} K {1:11.3e}   {2}  {3:11.3e}   {4:11.3e}      {5}\n'.format(T, Keq, self.Kequnits, k0rev, krev, self.krunits))
+
+            f.write('#   ======= ============ =========== ============ ============= =========\n')
+            f.write('\n\n')
+
+            kinetics0rev = Arrhenius().fitToData(Tlist, numpy.array(k0revs), kunits=self.krunits)
+            kineticsrev = Arrhenius().fitToData(Tlist, numpy.array(krevs), kunits=self.krunits)
+            
+            f.write('# krev (TST) = {0} \n'.format(kinetics0rev))
+            f.write('# krev (TST+T) = {0} \n\n'.format(kineticsrev))
+
         # Reaction path degeneracy is INCLUDED in the kinetics itself!
         string = 'kinetics(label={0!r}, kinetics={1!r})'.format(reaction.label, reaction.kinetics)
         f.write('{0}\n\n'.format(prettify(string)))
