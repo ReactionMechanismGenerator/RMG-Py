98b8d6f39c2e2067a6462a1d54fa73d3b4eceeef
KEHANG <kehanghan@gmail.com>
2017-12-12 10:41:59 -0500

    changes needed to enable padding for molecule tensor

--- rmgpy/cnn_framework/test_data/minimal_predictor/predictor_input.py
+++ rmgpy/cnn_framework/test_data/minimal_predictor/predictor_input.py
@@ -1,6 +1,6 @@
 predictor_model(embedding_size=512, depth=2, 
 				add_extra_atom_attribute=True, add_extra_bond_attribute=False,
-				scale_output=0.05, padding=False, 
+				scale_output=0.05, padding=True, padding_final_size=20,
 				hidden=50, hidden_activation='tanh',
 				output_activation='linear', output_size=1, 
 				lr=0.01, optimizer='adam', loss='mse')
