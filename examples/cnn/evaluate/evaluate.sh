41b97f1c933198e785976cd52645cda21ff52503
KEHANG <kehanghan@gmail.com>
2017-12-13 11:14:16 -0500

    set keras backend to be theano

--- examples/cnn/evaluate/evaluate.sh
+++ examples/cnn/evaluate/evaluate.sh
@@ -11,5 +11,6 @@
 CNN_MODEL='test_model'
 DATA_FILE='test_datasets.txt'
 source activate rmg_env
+export KERAS_BACKEND=theano
 python $RMGPy/evaluate_cnn.py -d ${DATA_FILE} -m ${CNN_MODEL}
 source deactivate
