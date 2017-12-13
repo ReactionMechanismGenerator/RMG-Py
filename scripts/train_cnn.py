ffbcc1c2f380faeea56968e1ae93a333dbcb047a
KEHANG <kehanghan@gmail.com>
2017-12-13 10:49:44 -0500

    standardize train_cnn script

--- scripts/train_cnn.py
+++ scripts/train_cnn.py
@@ -3,21 +3,85 @@ from rmgpy.cnn_framework.predictor import Predictor
 import os
 import rmgpy
 import logging
-from rmgpy.rmg.main import initializeLog
+import argparse
 
-level = logging.INFO
-initializeLog(level, os.path.join('train_cnn_results', 'train0.log'))
+def parseCommandLineArguments():
+    """
+    Parse the command-line arguments being passed to RMG Py. This uses the
+    :mod:`argparse` module, which ensures that the command-line arguments are
+    sensible, parses them, and returns them.
+    """
 
-h298_predictor = Predictor()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', metavar='FILE', type=str, nargs=1,
+        help='a predictor training input file')
 
-predictor_input = os.path.join(os.path.dirname(rmgpy.__file__),
-											'cnn_framework',
-											'test_data', 
-											'minimal_predictor', 
-											'predictor_input.py'
-											)
+################################################################################
 
-h298_predictor.load_input(predictor_input)
+def initializeLog(verbose, log_file_name):
+    """
+    Set up a logger to print output to stdout. The
+    `verbose` parameter is an integer specifying the amount of log text seen
+    at the console; the levels correspond to those of the :data:`logging` module.
+    """
+    # Create logger
+    logger = logging.getLogger()
+    logger.setLevel(verbose)
 
-lr_func = "float(0.0007 * np.exp(- epoch / 30.0))"
-h298_predictor.kfcv_train(folds=5, lr_func=lr_func)
+    # Create console handler and set level to debug; send everything to stdout
+    # rather than stderr
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(verbose)
+
+    logging.addLevelName(logging.CRITICAL, 'Critical: ')
+    logging.addLevelName(logging.ERROR, 'Error: ')
+    logging.addLevelName(logging.WARNING, 'Warning: ')
+    logging.addLevelName(logging.INFO, '')
+    logging.addLevelName(logging.DEBUG, '')
+    logging.addLevelName(1, '')
+
+    # Create formatter and add to console handler
+    #formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', '%Y-%m-%d %H:%M:%S')
+    #formatter = Formatter('%(message)s', '%Y-%m-%d %H:%M:%S')
+    formatter = logging.Formatter('%(levelname)s%(message)s')
+    ch.setFormatter(formatter)
+
+    # create file handler
+    if os.path.exists(log_file_name):
+        backup = os.path.join(log_file_name[:-9], 'train_backup.log')
+        if os.path.exists(backup):
+            print "Removing old "+backup
+            os.remove(backup)
+        print 'Moving {0} to {1}\n'.format(log_file_name, backup)
+        shutil.move(log_file_name, backup)
+    fh = logging.FileHandler(filename=log_file_name) #, backupCount=3)
+    fh.setLevel(min(logging.DEBUG,verbose)) # always at least VERBOSE in the file
+    fh.setFormatter(formatter)
+    # notice that STDERR does not get saved to the log file
+    # so errors from underlying libraries (eg. openbabel) etc. that report
+    # on stderr will not be logged to disk.
+
+    # remove old handlers!
+    while logger.handlers:
+        logger.removeHandler(logger.handlers[0])
+
+    # Add console and file handlers to logger
+    logger.addHandler(ch)
+    logger.addHandler(fh)
+
+################################################################################
+
+if __name__ == '__main__':
+
+	input_file = args.file[0]
+	input_directory = os.path.abspath(os.path.dirname(input_file))
+	
+	level = logging.INFO
+	initializeLog(level, os.path.join(input_directory, 'train.log'))
+
+	h298_predictor = Predictor()
+
+	h298_predictor.load_input(input_file)
+
+	lr_func = "float(0.0007 * np.exp(- epoch / 30.0))"
+	h298_predictor.kfcv_train(folds=5, lr_func=lr_func)
