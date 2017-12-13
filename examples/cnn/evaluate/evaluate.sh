d59fdaa38de21d83c84fdcfdf1de1d9fc3d34c05
KEHANG <kehanghan@gmail.com>
2017-12-13 11:25:32 -0500

    move evaluate_cnn script to scripts folder

--- examples/cnn/evaluate/evaluate.sh
+++ examples/cnn/evaluate/evaluate.sh
@@ -12,5 +12,5 @@ CNN_MODEL='test_model'
 DATA_FILE='test_datasets.txt'
 source activate rmg_env
 export KERAS_BACKEND=theano
-python $RMGPy/evaluate_cnn.py -d ${DATA_FILE} -m ${CNN_MODEL}
+python $RMGPy/scripts/evaluate_cnn.py -d ${DATA_FILE} -m ${CNN_MODEL}
 source deactivate
