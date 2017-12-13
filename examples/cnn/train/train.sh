312d2c051de49cdbd1c1be171d4ae8c996ef1ad5
KEHANG <kehanghan@gmail.com>
2017-12-13 11:09:10 -0500

    move train_cnn script to scripts folder

--- examples/cnn/train/train.sh
+++ examples/cnn/train/train.sh
@@ -13,5 +13,5 @@ TRAIN_MODE='full_train'
 BATCH_SIZE=1
 source activate rmg_env
 export KERAS_BACKEND=theano
-python $RMGPy/train_cnn.py -i $INPUT -d ${DATA_FILE} -t ${TRAIN_MODE} -bs ${BATCH_SIZE}
+python $RMGPy/scripts/train_cnn.py -i $INPUT -d ${DATA_FILE} -t ${TRAIN_MODE} -bs ${BATCH_SIZE}
 source deactivate
