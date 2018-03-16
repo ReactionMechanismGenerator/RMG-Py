ee46ff976fe326c951db6880724c30e136144637
rgillis8 <rgillis@mit.edu>
2018-03-16 10:31:00 -0400

    Adds an annotation explaining the source of Iodine's spin orbital correction

--- rmgpy/cantherm/statmech.py
+++ rmgpy/cantherm/statmech.py
@@ -570,6 +570,8 @@ def applyEnergyCorrections(E0, modelChemistry, atoms, bonds,
         # Spin orbit correction (SOC) in Hartrees
         # Values taken from ref 22 of http://dx.doi.org/10.1063/1.477794 and converted to hartrees
         # Values in millihartree are also available (with fewer significant figures) from table VII of http://dx.doi.org/10.1063/1.473182
+        # Iodine SOC calculated as a weighted average of the electronic spin splittings of the lowest energy state. The splittings are
+        # obtained from Huber, K.P.; Herzberg, G., Molecular Spectra and Molecular Structure. IV. Constants of Diatomic Molecules, Van Nostrand Reinhold Co., 1979
         SOC = {'H':0.0, 'N':0.0, 'O': -0.000355, 'C': -0.000135, 'S':  -0.000893, 'P': 0.0, 'I':-0.011547226,}
 
         # Step 1: Reference all energies to a model chemistry-independent basis
