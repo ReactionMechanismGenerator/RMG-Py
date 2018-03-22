85d24e47c4087117a0f71b41f880eed31aa3fe6a
Mark Goldman <goldmanm@mit.edu>
2018-03-22 11:42:30 -0400

    Improve chemkin docstring to describe all optional parameters

--- rmgpy/chemkin.pyx
+++ rmgpy/chemkin.pyx
@@ -1549,7 +1549,9 @@ def writeReactionString(reaction, javaLibrary = False):
 def writeKineticsEntry(reaction, speciesList, verbose = True, javaLibrary = False, commented=False):
     """
     Return a string representation of the reaction as used in a Chemkin
-    file. Use verbose = True to turn on comments.  Use javaLibrary = True in order to 
+    file. Use `verbose = True` to turn on kinetics comments.
+    Use `commented = True` to comments out the entire reaction.
+    Use javaLibrary = True in order to
     generate a kinetics entry suitable for an RMG-Java kinetics library.  
     """
     string = ""
