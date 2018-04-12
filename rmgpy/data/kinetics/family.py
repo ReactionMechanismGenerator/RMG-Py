186c08723e7078755331379772077957c789333d
Colin Grambow <cgrambow@mit.edu>
2018-04-12 19:28:04 -0400

    Clean up applyRecipe
    Instead of checking if the recipe is applied in the forward direction
    for each family separately, check `forward` only once. Do the same for
    the block of code applying the reverse recipe.
    
    Also remove a duplicate assignment to the variable `label`.

--- rmgpy/data/kinetics/family.py
+++ rmgpy/data/kinetics/family.py
@@ -1192,82 +1192,83 @@ class KineticsFamily(Database):
         for s in reactantStructures:
             reactantStructure = reactantStructure.merge(s.copy(deep=True))
 
-        # Hardcoding of reaction family for radical recombination (colligation)
-        # because the two reactants are identical, they have the same tags
-        # In this case, we must change the labels from '*' and '*' to '*1' and
-        # '*2'
-        if label == 'r_recombination' and forward:
-            identicalCenterCounter = 0
-            for atom in reactantStructure.atoms:
-                if atom.label == '*':
-                    identicalCenterCounter += 1
-                    atom.label = '*' + str(identicalCenterCounter)
-            if identicalCenterCounter != 2:
-                raise KineticsError(
-                    'Trying to apply recipe for reaction family {}: Only one occurrence of "*" found.'.format(label)
-                )
-        # Hardcoding of reaction family for peroxyl disproportionation
-        # '*1' and '*2' have to be changed to '*3' and '*4' for the second reactant
-        elif label == 'peroxyl_disproportionation' and forward:
-            identicalCenterCounter1 = identicalCenterCounter2 = 0
-            for atom in reactantStructure.atoms:
-                if atom.label == '*1':
-                    identicalCenterCounter1 += 1
-                    if identicalCenterCounter1 > 1:
-                        atom.label = '*3'
-                elif atom.label == '*2':
-                    identicalCenterCounter2 += 1
-                    if identicalCenterCounter2 > 1:
-                        atom.label = '*4'
-            msg = 'Trying to apply recipe for reaction family {}:'.format(label)
-            error = False
-            if identicalCenterCounter1 != 2:
-                msg += ' Only one occurrence of "*1" found.'
-                error = True
-            if identicalCenterCounter2 != 2:
-                msg += ' Only one occurrence of "*2" found.'
-                error = True
-            if error:
-                raise KineticsError(msg)
-        # Hardcoding of reaction family for bimolecular hydroperoxide decomposition
-        # '*2' has to be changed to '*4' for the second reactant and '*1' has to be
-        # changed to '*6'. '*3' has to be changed to '*5' for the first reactant.
-        # '*5' and '*6' do no participate in the reaction but are required for
-        # relabeling in the reverse direction.
-        elif label == 'bimolec_hydroperoxide_decomposition' and forward:
-            identicalCenterCounter1 = identicalCenterCounter2 = identicalCenterCounter3 = 0
-            for atom in reactantStructure.atoms:
-                if atom.label == '*1':
-                    identicalCenterCounter1 += 1
-                    if identicalCenterCounter1 > 1:
-                        atom.label = '*6'
-                elif atom.label == '*2':
-                    identicalCenterCounter2 += 1
-                    if identicalCenterCounter2 > 1:
-                        atom.label = '*4'
-                elif atom.label == '*3':
-                    identicalCenterCounter3 += 1
-                    if identicalCenterCounter3 == 1:
-                        atom.label = '*5'
-            msg = 'Trying to apply recipe for reaction family {}:'.format(label)
-            error = False
-            if identicalCenterCounter1 != 2:
-                msg += ' Only one occurrence of "*1" found.'
-                error = True
-            if identicalCenterCounter2 != 2:
-                msg += ' Only one occurrence of "*2" found.'
-                error = True
-            if identicalCenterCounter3 != 2:
-                msg += ' Only one occurrence of "*3" found.'
-                error = True
-            if error:
-                raise KineticsError(msg)
-
-        # Generate the product structure by applying the recipe
         if forward:
+            # Hardcoding of reaction family for radical recombination (colligation)
+            # because the two reactants are identical, they have the same tags
+            # In this case, we must change the labels from '*' and '*' to '*1' and
+            # '*2'
+            if label == 'r_recombination':
+                identicalCenterCounter = 0
+                for atom in reactantStructure.atoms:
+                    if atom.label == '*':
+                        identicalCenterCounter += 1
+                        atom.label = '*' + str(identicalCenterCounter)
+                if identicalCenterCounter != 2:
+                    raise KineticsError(
+                        'Trying to apply recipe for reaction family {}: Only one occurrence of "*" found.'.format(label)
+                    )
+            # Hardcoding of reaction family for peroxyl disproportionation
+            # '*1' and '*2' have to be changed to '*3' and '*4' for the second reactant
+            elif label == 'peroxyl_disproportionation':
+                identicalCenterCounter1 = identicalCenterCounter2 = 0
+                for atom in reactantStructure.atoms:
+                    if atom.label == '*1':
+                        identicalCenterCounter1 += 1
+                        if identicalCenterCounter1 > 1:
+                            atom.label = '*3'
+                    elif atom.label == '*2':
+                        identicalCenterCounter2 += 1
+                        if identicalCenterCounter2 > 1:
+                            atom.label = '*4'
+                msg = 'Trying to apply recipe for reaction family {}:'.format(label)
+                error = False
+                if identicalCenterCounter1 != 2:
+                    msg += ' Only one occurrence of "*1" found.'
+                    error = True
+                if identicalCenterCounter2 != 2:
+                    msg += ' Only one occurrence of "*2" found.'
+                    error = True
+                if error:
+                    raise KineticsError(msg)
+            # Hardcoding of reaction family for bimolecular hydroperoxide decomposition
+            # '*2' has to be changed to '*4' for the second reactant and '*1' has to be
+            # changed to '*6'. '*3' has to be changed to '*5' for the first reactant.
+            # '*5' and '*6' do no participate in the reaction but are required for
+            # relabeling in the reverse direction.
+            elif label == 'bimolec_hydroperoxide_decomposition':
+                identicalCenterCounter1 = identicalCenterCounter2 = identicalCenterCounter3 = 0
+                for atom in reactantStructure.atoms:
+                    if atom.label == '*1':
+                        identicalCenterCounter1 += 1
+                        if identicalCenterCounter1 > 1:
+                            atom.label = '*6'
+                    elif atom.label == '*2':
+                        identicalCenterCounter2 += 1
+                        if identicalCenterCounter2 > 1:
+                            atom.label = '*4'
+                    elif atom.label == '*3':
+                        identicalCenterCounter3 += 1
+                        if identicalCenterCounter3 == 1:
+                            atom.label = '*5'
+                msg = 'Trying to apply recipe for reaction family {}:'.format(label)
+                error = False
+                if identicalCenterCounter1 != 2:
+                    msg += ' Only one occurrence of "*1" found.'
+                    error = True
+                if identicalCenterCounter2 != 2:
+                    msg += ' Only one occurrence of "*2" found.'
+                    error = True
+                if identicalCenterCounter3 != 2:
+                    msg += ' Only one occurrence of "*3" found.'
+                    error = True
+                if error:
+                    raise KineticsError(msg)
+
+            # Generate the product structure by applying the recipe
             self.forwardRecipe.applyForward(reactantStructure, unique)
         else:
             self.reverseRecipe.applyForward(reactantStructure, unique)
+
         if not reactantStructure.props['validAromatic']:
             if isinstance(reactantStructure, Molecule):
                 # For molecules, kekulize the product to redistribute bonds appropriately
@@ -1279,34 +1280,35 @@ class KineticsFamily(Database):
                 return []
         productStructure = reactantStructure
 
-        # Hardcoding of reaction family for reverse of radical recombination
-        # (Unimolecular homolysis)
-        # Because the two products are identical, they should the same tags
-        # In this case, we must change the labels from '*1' and '*2' to '*' and
-        # '*'
-        if label == 'r_recombination' and not forward:
-            for atom in productStructure.atoms:
-                if atom.label == '*1' or atom.label == '*2':
-                    atom.label = '*'
-        # Hardcoding of reaction family for reverse of peroxyl disproportionation
-        # Labels '*3' and '*4' have to be changed back to '*1' and '*2'
-        elif label == 'peroxyl_disproportionation' and not forward:
-            for atom in productStructure.atoms:
-                if atom.label == '*3':
-                    atom.label = '*1'
-                elif atom.label == '*4':
-                    atom.label = '*2'
-        # Hardcoding of reaction family for bimolecular hydroperoxide decomposition
-        # '*5' has to be changed back to '*3', '*6' has to be changed to '*1', and
-        # '*4' has to be changed to '*2'
-        elif label == 'bimolec_hydroperoxide_decomposition' and not forward:
-            for atom in productStructure.atoms:
-                if atom.label == '*5':
-                    atom.label = '*3'
-                elif atom.label == '*6':
-                    atom.label = '*1'
-                elif atom.label == '*4':
-                    atom.label = '*2'
+        if not forward:
+            # Hardcoding of reaction family for reverse of radical recombination
+            # (Unimolecular homolysis)
+            # Because the two products are identical, they should the same tags
+            # In this case, we must change the labels from '*1' and '*2' to '*' and
+            # '*'
+            if label == 'r_recombination':
+                for atom in productStructure.atoms:
+                    if atom.label == '*1' or atom.label == '*2':
+                        atom.label = '*'
+            # Hardcoding of reaction family for reverse of peroxyl disproportionation
+            # Labels '*3' and '*4' have to be changed back to '*1' and '*2'
+            elif label == 'peroxyl_disproportionation':
+                for atom in productStructure.atoms:
+                    if atom.label == '*3':
+                        atom.label = '*1'
+                    elif atom.label == '*4':
+                        atom.label = '*2'
+            # Hardcoding of reaction family for bimolecular hydroperoxide decomposition
+            # '*5' has to be changed back to '*3', '*6' has to be changed to '*1', and
+            # '*4' has to be changed to '*2'
+            elif label == 'bimolec_hydroperoxide_decomposition':
+                for atom in productStructure.atoms:
+                    if atom.label == '*5':
+                        atom.label = '*3'
+                    elif atom.label == '*6':
+                        atom.label = '*1'
+                    elif atom.label == '*4':
+                        atom.label = '*2'
 
         # If reaction family is its own reverse, relabel atoms
         # This allows comparison of the product species to forbidden
@@ -1320,7 +1322,6 @@ class KineticsFamily(Database):
                 if atom.label != '':
                     atomLabels[atom.label] = atom
 
-            label = self.label.lower()
             if label == 'h_abstraction':
                 # '*2' is the H that migrates
                 # it moves from '*1' to '*3'
@@ -1372,8 +1373,10 @@ class KineticsFamily(Database):
                 atomLabels['*2'].label = '*3'
                 atomLabels['*3'].label = '*2'
 
-        if not forward: template = self.reverseTemplate
-        else:           template = self.forwardTemplate
+        if not forward:
+            template = self.reverseTemplate
+        else:
+            template = self.forwardTemplate
 
         # Split product structure into multiple species if necessary
         productStructures = productStructure.split()
