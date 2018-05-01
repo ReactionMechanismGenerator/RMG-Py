1125d5ff95df476c0cc2fa221c2d7ce5b5f33e47
KEHANG <kehanghan@gmail.com>
2018-05-01 15:23:12 -0400

    update with latest train and evaluate examples

--- examples/cnn/train/predictor_input.py
+++ examples/cnn/train/predictor_input.py
@@ -1,9 +1,9 @@
 predictor_model(prediction_task="Cp(cal/mol/K)", 
 				embedding_size=512, depth=3, scale_output=0.05, 
-				padding=True, padding_final_size=20,
+				padding=True, padding_final_size=25,
 				add_extra_atom_attribute=True, add_extra_bond_attribute=True,
 				mol_conv_inner_activation='tanh',
 				mol_conv_outer_activation='softmax',
 				hidden=50, hidden_activation='tanh',
-				output_activation='linear', output_size=1, 
+				output_activation='linear', output_size=7, 
 				lr=0.01, optimizer='adam', loss='mse')
