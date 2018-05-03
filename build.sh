8d15bc4fe5b62f926f1155beb8435fba83028900
KEHANG <kehanghan@gmail.com>
2018-05-02 20:51:42 -0400

    utilize conda's compiler

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
-export CC=${PREFIX}/bin/gcc
+export CC=${GCC}
 export CXX=${PREFIX}/bin/g++
 export F77=${PREFIX}/bin/gfortran
 export F90=${PREFIX}/bin/gfortran
 make -j${CPU_COUNT}
 make check
-$PYTHON setup.py install
 
+$PYTHON ${SRC_DIR}/setup.py install
 # lazy "install" of everything in our 'external' folder.
 # most of which should probably be elsewhere
 cp -R ${SRC_DIR}/external ${SP_DIR}
