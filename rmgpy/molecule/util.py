8c70ccf226a72520d36abbbc913df0bf52ab1d35
KEHANG <kehanghan@gmail.com>
2018-05-06 13:16:40 -0400

    allow Fragment in mol.util.retrieveElementCount()

--- rmgpy/molecule/util.py
+++ rmgpy/molecule/util.py
@@ -35,6 +35,7 @@ from .molecule import Molecule
 
 def retrieveElementCount(obj):
     """Converts an (augmented) inchi or Molecule into a dictionary element -> count"""
+    from afm.fragment import Fragment
     element_count = {}
 
     if isinstance(obj, str):
@@ -60,7 +61,7 @@ def retrieveElementCount(obj):
 
         return element_count
     
-    elif isinstance(obj, Molecule):
+    elif isinstance(obj, Molecule) or isinstance(obj, Fragment):
         return obj.get_element_count()
 
     else:
