a374c48bb2b0c1224c6c8de0b2353c18808630d0
Xiaorui <xiaorui@mit.edu>
2018-05-14 17:26:08 -0400

    add new unittest to familyTest about famililes with recipes that modify charges

---
+++ rmgpy/test_data/testing_database/kinetics/families/R_Addition_COm/training/reactions.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+name = "R_Addition_COm/training"
+shortDesc = u"Kinetics used to train group additivity values"
+longDesc = u"""
+Put kinetic parameters for reactions to use as a training set for fitting
+group additivity values in this file.
+"""
