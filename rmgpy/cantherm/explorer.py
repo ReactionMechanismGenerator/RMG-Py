8467a412393e7202e7def1eeaf2cb6bcc17d15a3
Matt Johnson <mjohnson541@gmail.com>
2018-06-06 14:32:01 -0400

    Run reduction in an explorer job
    This cycles through the list of temperatures and pressure and finds the intersection of all reactions
    that can be removed by both reduction methods at each T and P
    
    these reactions are removed to make the reduced model
    
    then warnings are given for input file reactions that are not included in the reduced model and the pdepjob network is replaced and the pdepjob
    is executed again.

--- rmgpy/cantherm/explorer.py
+++ rmgpy/cantherm/explorer.py
@@ -188,7 +188,46 @@ class ExplorerJob(object):
             if rxn not in network.pathReactions:
                 warns.append('Reaction {0} in the input file was not explored during network expansion and was not included in the full network.  This is likely because your explore_tol value is too high.'.format(rxn))
         
+        #reduction process
         
+        if self.energy_tol != np.inf or self.flux_tol != 0.0:
+            
+            rxnSet = None
+            
+            for T in Tlist:
+                if self.energy_tol != np.inf:
+                    rxns = network.get_energy_filtered_reactions(T,self.energy_tol)
+                    if rxnSet is not None:
+                        rxnSet &= set(rxns)
+                    else:
+                        rxnSet = set(rxns)
+                    
+                for P in Plist:
+                    if self.flux_tol != 0.0:
+                        rxns = network.get_rate_filtered_reactions(T,P,self.flux_tol)
+                        if rxnSet is not None:
+                            rxnSet &= set(rxns)
+                        else:
+                            rxnSet = set(rxns) 
+                            
+            logging.info('removing reactions during reduction:')
+            for rxn in rxnSet:
+                logging.info(rxn)
+            
+            network.remove_reactions(reaction_model,list(rxnSet))
         
+            for rxn in jobRxns:
+                if rxn not in network.pathReactions:
+                    warns.append('Reaction {0} in the input file was not included in the reduced model.'.format(rxn))
+    
+        self.network = network
         
+        self.pdepjob.network = network
         
+        self.pdepjob.execute(outputFile, plot, format='pdf', print_summary=True)
+        
+        if warns != []:
+            logging.info('\nOUTPUT WARNINGS:\n')
+            for w in warns:
+                logging.warning(w)
+                
