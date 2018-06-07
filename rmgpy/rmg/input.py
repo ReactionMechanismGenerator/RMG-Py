28b2effb47ad6d1b7e11a2c6c3f55d19f9b1778c
Matt Johnson <mjohnson541@gmail.com>
2018-06-07 17:16:11 -0400

    allow setting of edgeCheckFrequency in model block

--- rmgpy/rmg/input.py
+++ rmgpy/rmg/input.py
@@ -546,8 +546,7 @@ def model(toleranceMoveToCore=None, toleranceMoveEdgeReactionToCore=np.inf, tole
           ignoreOverallFluxCriterion=False,
           maxNumSpecies=None, maxNumObjsPerIter=1, terminateAtMaxObjects=False,
           toleranceThermoKeepSpeciesInEdge=np.inf, dynamicsTimeScale=(0.0, 'sec'),
-          toleranceBranchReactionToCore=0.0, branchingIndex=0.5, branchingRatioMax=1.0):
-    """
+          toleranceBranchReactionToCore=0.0, branchingIndex=0.5, branchingRatioMax=1.0    """
     How to generate the model. `toleranceMoveToCore` must be specified. 
     toleranceMoveReactionToCore and toleranceReactionInterruptSimulation refers to an additional criterion for forcing an edge reaction to be included in the core
     by default this criterion is turned off
@@ -587,6 +586,7 @@ def model(toleranceMoveToCore=None, toleranceMoveEdgeReactionToCore=np.inf, tole
             tol_branch_rxn_to_core=toleranceBranchReactionToCore,
             branching_index=branchingIndex,
             branching_ratio_max=branchingRatioMax,
+            edgeCheckFrequency=edgeCheckFrequency,
         )
     )
 
