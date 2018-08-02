7139170d18d5d9661a293f93a987af3404c0b1a1
Matt Johnson <mjohnson541@gmail.com>
2018-08-02 15:38:11 -0400

    don't clear rules when saving tree now that we are creating rules during tree generation

--- rmgpy/data/kinetics/family.py
+++ rmgpy/data/kinetics/family.py
@@ -2979,8 +2979,6 @@ class KineticsFamily(Database):
             path = settings['database.directory']
             path = os.path.join(path,'kinetics','families',self.label)
         
-        self.rules.entries = OrderedDict() #have to clear the new rules made
-        
         self.save(path)
     
     def getTrainingSet(self, thermoDatabase=None, removeDegeneracy=False):
