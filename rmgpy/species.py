9b998c897ee8bfd224d15d6035dae50aba469905
yentingw <yentingw@mit.edu>
2018-09-10 11:46:37 -0400

    modify for loading Chemkin file and simulation in Cantera.

--- rmgpy/species.py
+++ rmgpy/species.py
@@ -341,12 +341,16 @@ class Species(object):
         instead. Be sure that species' labels are unique when setting it False.
         """
         import cantera as ct
+        from afm.fragment import CuttingLabel
         
         # Determine the number of each type of element in the molecule
         elementDict = {} # elementCounts = [0,0,0,0]
-        for atom in self.molecule[0].atoms:
+        for vertex in self.molecule[0].vertices:
             # The atom itself
-            symbol = atom.element.symbol
+            if not isinstance(vertex, CuttingLabel):
+                symbol = vertex.element.symbol
+            else: # that means this vertex is CuttingLabel
+                symbol = 'Cl'
             if symbol not in elementDict:
                 elementDict[symbol] = 1
             else:
