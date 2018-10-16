789478133ffb7d62eb41b521ea056ff1b7ddac9b
Mark Payne <ampayne@mit.edu>
2018-10-16 18:54:04 -0400

    Save sensitivity files for all species by specifying 'all'

--- rmgpy/tools/loader.py
+++ rmgpy/tools/loader.py
@@ -108,7 +108,8 @@ def loadRMGPyJob(inputFile, chemkinFile=None, speciesDict=None, generateImages=T
         for t in reactionSystem.termination:
             if isinstance(t, TerminationConversion):
                 t.species = speciesDict[t.species]
-        reactionSystem.sensitiveSpecies = [speciesDict[spec] for spec in reactionSystem.sensitiveSpecies]
+        if reactionSystem.sensitiveSpecies != ['all']:
+            reactionSystem.sensitiveSpecies = [speciesDict[spec] for spec in reactionSystem.sensitiveSpecies]
     
     # Set reaction model to match model loaded from Chemkin file
     rmg.reactionModel.core.species = speciesList
