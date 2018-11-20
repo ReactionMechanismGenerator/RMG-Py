dd547cdea4ddc471bc947555302ab669338d1a4b
alongd <alongd@mit.edu>
2018-11-20 12:23:36 -0500

    Added an Arkane test that runs all its examples
    Also renamed some input files for automating this

--- examples/arkane/species/Toulene_Free_Rotor/toluene_FreeRotor.py
+++ examples/arkane/species/Toulene_Free_Rotor/toluene_FreeRotor.py
@@ -23,6 +23,4 @@ geometry = Log('TolueneFreq.log')
 
 frequencies = Log('TolueneFreq.log')
 
-rotors = [
-FreeRotor(pivots=[3,12],top=[12,13,14,15],symmetry=6)
-]
+rotors = [FreeRotor(pivots=[3,12], top=[12,13,14,15], symmetry=6)]
--- examples/arkane/species/Toulene_Hindered_Rotor/toluene_HinderedRotor.py
+++ examples/arkane/species/Toulene_Hindered_Rotor/toluene_HinderedRotor.py
@@ -23,6 +23,4 @@ geometry = Log('TolueneFreq.log')
 
 frequencies = Log('TolueneFreq.log')
 
-rotors = [
-HinderedRotor(scanLog=Log('TolueneRot1.log'),pivots=[3,12],top=[12,13,14,15],symmetry=6, fit='best'),
-]
+rotors = [HinderedRotor(scanLog=Log('TolueneRot1.log'), pivots=[3,12], top=[12,13,14,15], symmetry=6, fit='best')]
