a7ca8b85e3b6d92816292770a3201d620ef28397
Mark Payne <ampayne@mit.edu>
2019-01-25 22:14:51 -0500

    Add reaction and species classes for error canceling reaction schemes

--- arkane/isodesmic.py
+++ arkane/isodesmic.py
@@ -42,6 +42,104 @@ corannulene and C60 by means of inexpensive theoretical procedures. Journal of P
 https://doi.org/10.1021/jp404158v
 """
 
+from __future__ import division
+
+from rmgpy.molecule import Molecule
+from rmgpy.quantity import ScalarQuantity
+
+
+class ErrorCancelingSpecies(object):
+    """Class for target and known (benchmark) species participating in an error canceling reaction"""
+
+    def __init__(self, molecule, low_level_hf298, model_chemistry, high_level_hf298=None, source=None):
+        """
+
+        Args:
+            molecule (rmgpy.molecule.Molecule): molecule object to represent the species
+            low_level_hf298 (tuple): (Hf298, unit) evaluated using a lower level of theory (e.g. DFT)
+            model_chemistry (str): Level of theory used to calculate the low level thermo
+            high_level_hf298 (tuple, optional): (Hf298 , unit) evaluated using a high level of theory
+                (e.g. expt. data) that is serving as the "reference" for the isodesmic calculation
+            source (str): Literature source from which the high level data was taken
+        """
+        if isinstance(molecule, Molecule):
+            self.molecule = molecule
+        else:
+            raise ValueError('ErrorCancelingSpecies molecule attribute must be an rmgpy Molecule object. Instead a '
+                             '{0} object was given'.format(type(molecule)))
+
+        if isinstance(model_chemistry, str):
+            self.model_chemistry = model_chemistry
+        else:
+            raise ValueError('The model chemistry string used to calculate the low level Hf298 must be provided '
+                             'consistency checks. Instead, a {0} object was given'.format(type(model_chemistry)))
+
+        self.low_level_hf298 = ScalarQuantity(*low_level_hf298)
+
+        # If the species is a reference species, then the high level data is already known
+        self.high_level_hf298 = ScalarQuantity(*high_level_hf298) if high_level_hf298 else None
+        self.source = source
+
+    def __repr__(self):
+        return '<ErrorCancelingSpecies {0}>'.format(self.molecule.toSMILES())
+
+
+class ErrorCancelingReaction(object):
+    """Class for representing an error canceling reaction, with the target species being an implicit reactant"""
+
+    def __init__(self, target, species):
+        """
+        Initialize an error canceling reaction from ErrorCancelingSpecies objects
+
+        The reaction must be written with the target species participating as a reactant with stoichiometric coefficient
+        v=-1
+
+        The species dictionary should be given as species/stochiometric coefficient pairs in the following format:
+        {ErrorCancelingSpecies_1: v_1, ErrorCancelingSpecies_2: v_2, ... }
+
+        Args:
+            target (ErrorCancelingSpecies): high level H_f(298 K) will be estimated for this species
+            species (dict): species taking place in the reaction (excluding the target)
+        """
+
+        self.target = target
+        self.model_chemistry = self.target.model_chemistry
+
+        # Perform a consistency check that all species are using the same model chemistry
+        for spcs in species.keys():
+            if spcs.model_chemistry != self.model_chemistry:
+                raise ValueError('Species {0} has model chemistry {1}, which does not match the model chemistry of the '
+                                 'reaction of {2}'.format(spcs, spcs.model_chemistry, self.model_chemistry))
+
+        # Does not include the target, which is handled separately.
+        self.species = species
+
+    def __repr__(self):
+        reactant_string = '1.0*{0} + '.format(self.target.molecule.toSMILES())
+        product_string = ''
+        for spcs, coeff in self.species.items():
+            if coeff > 0:
+                product_string += '{0}*{1} + '.format(coeff, spcs.molecule.toSMILES())
+            else:
+                reactant_string += '{0}*{1} + '.format(-1*coeff, spcs.molecule.toSMILES())
+
+        return '<ErrorCancelingReaction {0}== {1}>'.format(reactant_string[:-2], product_string[:-2])
+
+    def calculate_target_thermo(self):
+        """
+        Estimate the high level thermochemistry for the target species using the error canceling scheme
+
+        Returns:
+            rmgpy.quantity.ScalarQuantity: Hf298 in 'J/mol' estimated for the target species
+
+        """
+        low_level_h_rxn = sum(map(lambda spec: spec[0].low_level_hf298.value_si*spec[1], self.species.items())) - \
+            self.target.low_level_hf298.value_si
+
+        target_thermo = sum(map(lambda spec: spec[0].high_level_hf298.value_si*spec[1], self.species.items())) - \
+            low_level_h_rxn
+        return ScalarQuantity(target_thermo, 'J/mol')
+
 
 if __name__ == '__main__':
     pass
