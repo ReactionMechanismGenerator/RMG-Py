60f0e8ce2da36becc6a8b0f47e34be79987699c6
Matt Johnson <mjohnson541@gmail.com>
2019-02-06 17:08:12 -0500

    fix import in arrhenius now that uncertainties is properly spelled

--- rmgpy/kinetics/arrhenius.pyx
+++ rmgpy/kinetics/arrhenius.pyx
@@ -35,7 +35,7 @@ from scipy.optimize import curve_fit
 cimport rmgpy.constants as constants
 import rmgpy.quantity as quantity
 from rmgpy.exceptions import KineticsError
-from rmgpy.kinetics.uncertainies import rank_accuracy_map
+from rmgpy.kinetics.uncertainties import rank_accuracy_map
 from rmgpy.molecule.molecule import Bond
 # Prior to numpy 1.14, `numpy.linalg.lstsq` does not accept None as a value
 RCOND = -1 if int(numpy.__version__.split('.')[1]) < 14 else None
