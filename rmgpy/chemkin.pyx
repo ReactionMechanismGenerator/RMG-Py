a4e57c89770921fcd26a627cee4c7c6b0bb63a89
MarkGoldman <goldmanm@mit.edu>
2019-04-23 14:43:28 -0400

    Allow 24 character species names in thermo entry
    The chem.inp files produced by Arkane can contain 24 character
    species names, but our current code truncates them at 16 characters.
    
    This commit allows them to be their full length.
    
    Since RMG already has a check on file length by replacing them with
    'S(XXXX)', this should not cause an issue in running RMG.

--- rmgpy/chemkin.pyx
+++ rmgpy/chemkin.pyx
@@ -73,10 +73,10 @@ def read_thermo_entry(entry, Tmin=0, Tint=0, Tmax=0):
     the label of the species and the thermodynamics model as a :class:`NASA`
     object.
     
-    Format specification at http://www2.galcit.caltech.edu/EDL/public/formats/chemkin.html
+    For format specification, see the Chemkin Input Manual
     """
     lines = entry.splitlines()
-    species = str(lines[0][0:18].split()[0].strip())
+    species = str(lines[0][0:24].split()[0].strip())
 
     comment = lines[0][len(species):24].strip()
     formula = {}
@@ -1510,7 +1510,8 @@ def write_thermo_entry(species, element_counts=None, verbose=True):
                     string += "! {0}\n".format(line)
 
     # Line 1
-    string += '{0:<16}        '.format(get_species_identifier(species))
+
+    string += '{0:<24}'.format(get_species_identifier(species))
     if len(element_counts) <= 4:
         # Use the original Chemkin syntax for the element counts
         for key, count in element_counts.items():
@@ -2034,9 +2035,9 @@ def save_chemkin_file(path, species, reactions, verbose=True, check_for_duplicat
     for spec in sorted_species:
         label = get_species_identifier(spec)
         if verbose:
-            f.write('    {0!s:<16}    ! {1}\n'.format(label, str(spec)))
+            f.write('    {0!s:<24}  ! {1}\n'.format(label, str(spec)))
         else:
-            f.write('    {0!s:<16}\n'.format(label))
+            f.write('    {0!s:<24}\n'.format(label))
     f.write('END\n\n\n\n')
 
     # Thermodynamics section
