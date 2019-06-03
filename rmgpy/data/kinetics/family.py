99a7cd54e7c2a609d3105a51b18efe679f25537d
Matt Johnson <mjohnson541@gmail.com>
2019-06-03 11:14:58 -0400

    adapt tree cleaning to handle duplicate labels

--- rmgpy/data/kinetics/family.py
+++ rmgpy/data/kinetics/family.py
@@ -3548,8 +3548,12 @@ class KineticsFamily(Database):
             if grp is None:
                 grp = ent.item
             else:
+                if any([isinstance(x,list) for x in ent.item.getLabeledAtoms().values()]):
+                    grp = grp.mergeGroups(ent.item, keepIdenticalLabels=True)
+            else:
                 grp = grp.mergeGroups(ent.item)
-        
+
+
         #clear everything
         self.groups.entries = {x.label:x for x in self.groups.entries.itervalues() if x.index == -1}
         
