ac65f3cb9600a3a5487dceb936e1e88899054ec2
Richard West <r.west@northeastern.edu>
2019-07-18 13:58:23 -0400

    Test that we can make a sample molecule for every atom type.
    This unit test was inspired by efforts to debug something in some
    changes to the database, which caused a segfault when running
    the database Tests, but was in fact caused by an undetected bug in RMG. This should detect similar bugs.
    The idea is that for every atomType, such as 'Cds', the
    method Group().make_sample_molecule() can create a molecule
    from the group definition, eg.
      1  Cd  ux
    should make the molecule
      1 C u0 p0 c0 {2,D} {3,S} {4,S}
      2 C u0 p0 c0 {1,D} {5,S} {6,S}
      3 H u0 p0 c0 {1,S}
      4 H u0 p0 c0 {1,S}
      5 H u0 p0 c0 {2,S}
      6 H u0 p0 c0 {2,S}
    
    Many work, but quite a few fail.
    Updated to RMG3/Python3.

--- rmgpy/molecule/atomtypeTest.py
+++ rmgpy/molecule/atomtypeTest.py
@@ -32,7 +32,7 @@ This module contains unit tests of the rmgpy.molecule.atomtype module.
 """
 
 import unittest
-
+import logging
 import rmgpy.molecule
 from rmgpy.molecule import Molecule
 from rmgpy.molecule.atomtype import get_atomtype
@@ -128,6 +128,22 @@ class TestAtomType(unittest.TestCase):
         self.assertEqual(self.atomtype.increment_radical, other.increment_radical)
         self.assertEqual(self.atomtype.decrement_radical, other.decrement_radical)
 
+
+    def test_make_sample_molecule(self):
+        """
+        Test we can make a sample molecule for every atom type.
+        """
+        failed = []
+        for name, atom_type in rmgpy.molecule.atomtype.ATOMTYPES.items():
+            adjlist = f"1 {name} ux"
+            group = rmgpy.molecule.Group().from_adjacency_list(adjlist)
+            try:
+                result = group.make_sample_molecule()
+                # logging.info(f"For {name} made\n{result.to_adjacency_list()}")
+            except:
+                logging.exception(f"Couldn't make sample molecule for atomType {name}")
+                failed.append(name)
+        self.assertFalse(failed, f"Couldn't make sample molecules for types {', '.join(failed)}")
 
 ################################################################################
 
