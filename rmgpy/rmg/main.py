ea80df783cdfe98a411296c8047bc5d2c7683c70
Mark Payne <ampayne@mit.edu>
2019-08-13 08:23:48 -0400

    Prevent file locking for HDF5 files

--- rmgpy/rmg/main.py
+++ rmgpy/rmg/main.py
@@ -84,6 +84,11 @@ from rmgpy.tools.uncertainty import Uncertainty, process_local_results
 
 ################################################################################
 
+# This module uses the HDF5 data format, which can cause problems on files systems that use NFS (common for network
+# mounted file systems. The following sets an environment variable that prevents file locking that would otherwise
+# cause a problem for NFS.
+os.environ['HDF5_USE_FILE_LOCKING'] = 'FALSE'
+
 solvent = None
 
 # Maximum number of user defined processors
