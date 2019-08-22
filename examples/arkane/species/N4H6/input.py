efe8bd55d0ce540a446b05d7c785c9310063fe74
Max Liu <mjliu@mit.edu>
2019-08-22 16:49:04 -0400

    Adjust directory structure for N4H6 in arkane examples

--- examples/arkane/species/N4H6/input.py
+++ examples/arkane/species/N4H6/input.py
@@ -8,6 +8,6 @@ modelChemistry = "CCSD(T)-F12/cc-pVTZ-f12//B3LYP/6-311+G(3df,2p)"
 useHinderedRotors = True
 useBondCorrections = True
 
-species('N4H6','N4H6/N4H6.py')
+species('N4H6', 'N4H6.py')
 
 thermo('N4H6', 'NASA')
