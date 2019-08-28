38071bc1b242049558c2f44116e9bcb90e68cc92
Yen-Ting Wang <yentingw@mit.edu>
2019-08-28 12:40:38 -0400

    Species as Fragment even without cuttinglabel

--- rmgpy/data/kinetics/family.py
+++ rmgpy/data/kinetics/family.py
@@ -1464,11 +1464,11 @@ class KineticsFamily(Database):
         # Split product structure into multiple species if necessary
         productStructures = productStructure.split()
         # check if a Fragment is a Molecule, then change it to Molecule
-        if isinstance(productStructures[0], Fragment):
-            for index, product in enumerate(productStructures):
-                if not any(isinstance(vertex, CuttingLabel) for vertex in product.vertices):
-                    mol = Molecule(atoms=product.vertices)
-                    productStructures[index] = mol
+        # if isinstance(productStructures[0], Fragment):
+        #     for index, product in enumerate(productStructures):
+        #         if not any(isinstance(vertex, CuttingLabel) for vertex in product.vertices):
+        #             mol = Molecule(atoms=product.vertices)
+        #             productStructures[index] = mol
 
         # Make sure we've made the expected number of products
         if productNum != len(productStructures):
