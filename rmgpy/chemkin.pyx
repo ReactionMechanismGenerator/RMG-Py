b8f964b748d819d260c9292884632024c342e882
davidfarinajr <farina.d@husky.neu.edu>
2019-10-21 11:43:42 -0400

    added F and Br to chemkin elements

--- rmgpy/chemkin.pyx
+++ rmgpy/chemkin.pyx
@@ -2221,7 +2221,7 @@ def writeElementsSection(f):
     s = 'ELEMENTS\n'
 
     # map of isotope elements with chemkin-compatible element representation:
-    elements = ('H', ('H', 2), ('H',3), 'C', ('C', 13), 'O', ('O',18), 'N', 'Ne', 'Ar', 'He', 'Si', 'S', 'Cl')
+    elements = ('H', ('H', 2), ('H',3), 'C', ('C', 13), 'O', ('O',18), 'N', 'Ne', 'Ar', 'He', 'Si', 'S', 'F', 'Cl', 'Br')
     for el in elements:
         if isinstance(el, tuple):
             symbol, isotope = el
