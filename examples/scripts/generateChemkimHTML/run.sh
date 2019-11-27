fbf0807c55c4a0c8412c71ee1f6d1750ac47c810
yunsiechung <yunsie@mit.edu>
2019-11-27 14:30:32 -0500

    added example for generateChemkimHTML.py
    An example for scripts/generateChemkimHTML.py is added

---
+++ examples/scripts/generateChemkimHTML/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run generateChemkinHTML.py on a Chemkin input file and a RMG species dictionary file
+python ../../../scripts/generateChemkinHTML.py chem_annotated.inp species_dictionary.txt
