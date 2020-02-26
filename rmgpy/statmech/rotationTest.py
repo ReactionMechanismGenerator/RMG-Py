89b6bc2bb2c0c787716aba04fa5737b012811391
Alon Grinberg Dana <alongd@mit.edu>
2020-02-25 20:02:43 -0500

    Tests: SymmetricTorRotor

--- rmgpy/statmech/rotationTest.py
+++ rmgpy/statmech/rotationTest.py
@@ -36,7 +36,7 @@ import unittest
 import numpy as np
 
 import rmgpy.constants as constants
-from rmgpy.statmech.rotation import KRotor, LinearRotor, NonlinearRotor, SphericalTopRotor
+from rmgpy.statmech.rotation import KRotor, LinearRotor, NonlinearRotor, SphericalTopRotor, SymmetricTopRotor
 
 ################################################################################
 
@@ -862,5 +862,240 @@ class TestSphericalTopRotor(unittest.TestCase):
         mode = pickle.loads(pickle.dumps(self.mode, -1))
         self.assertAlmostEqual(self.mode.inertia.value, mode.inertia.value, 6)
         self.assertEqual(self.mode.inertia.units, mode.inertia.units)
+        self.assertEqual(self.mode.symmetry, mode.symmetry)
+        self.assertEqual(self.mode.quantum, mode.quantum)
+
+################################################################################
+
+
+class TestSymmetricTopRotor(unittest.TestCase):
+    """
+    Contains unit tests of the SymmetricTopRotor class.
+    """
+
+    def setUp(self):
+        """
+        A function run before each unit test in this class.
+        """
+        self.inertia = np.array([3.196, 3.197, 20.07])
+        self.symmetry = 2
+        self.quantum = False
+        self.mode = SymmetricTopRotor(
+            inertia=(self.inertia, "amu*angstrom^2"),
+            symmetry=self.symmetry,
+            quantum=self.quantum,
+        )
+
+    def test_get_rotationalConstant(self):
+        """
+        Test getting the SymmetricTopRotor.rotationalConstant property.
+        """
+        b_exp = np.array([4.93635, 1.0125, 0.839942])
+        b_act = self.mode.rotationalConstant.value_si
+        for b0, b in zip(b_exp, b_act):
+            self.assertAlmostEqual(b0, b, 4)
+
+    def test_set_rotationalConstant(self):
+        """
+        Test setting the SymmetricTopRotor.rotationalConstant property.
+        """
+        B = self.mode.rotationalConstant
+        B.value_si *= 2
+        self.mode.rotationalConstant = B
+        Iexp = 0.5 * self.inertia
+        Iact = self.mode.inertia.value_si * constants.Na * 1e23
+        for I0, I in zip(Iexp, Iact):
+            self.assertAlmostEqual(I0, I, 4)
+
+    def test_get_level_energy(self):
+        """
+        Test the SymmetricTopRotor.get_level_energy() method.
+        """
+        b = self.mode.rotationalConstant.value_si * constants.h * constants.c * 100. * constants.Na
+        for j in range(0, 100):
+            e_exp = b * j * (j + 1)
+            e_act = self.mode.get_level_energy(j)
+            if j == 0:
+                self.assertEqual(e_act, 0)
+            else:
+                for e0, e in zip(e_exp, e_act):
+                    self.assertAlmostEqual(e0, e, delta=1e-4 * e_exp)
+
+    def test_get_level_degeneracy(self):
+        """
+        Test the SymmetricTopRotor.get_level_degeneracy() method.
+        """
+        for j in range(0, 100):
+            g_exp = (2 * j + 1) ** 2
+            g_act = self.mode.get_level_degeneracy(j)
+            self.assertEqual(g_exp, g_act)
+
+    def test_get_partition_function_classical(self):
+        """
+        Test the SymmetricTopRotor.get_partition_function() method for a classical rotor.
+        """
+        self.mode.quantum = False
+        t_list = np.array([300, 500, 1000, 1500, 2000])
+        q_exp_list = np.array([1552.74, 3340.97, 9449.69, 17360.2, 26727.8])
+        for t, q_exp in zip(t_list, q_exp_list):
+            q_act = self.mode.get_partition_function(t)
+            self.assertAlmostEqual(q_exp, q_act, delta=1e-4 * q_exp)
+
+    def test_get_partition_function_quantum(self):
+        """
+        Test the SymmetricTopRotor.get_partition_function() method for a quantum rotor.
+        """
+        self.mode.quantum = True
+        t_list = np.array([300, 500, 1000, 1500, 2000])
+        q_exp_list = np.array([1555.42, 3344.42, 9454.57, 17366.2, 26734.7])
+        for t, q_exp in zip(t_list, q_exp_list):
+            q_act = self.mode.get_partition_function(t)
+            self.assertAlmostEqual(q_exp, q_act, delta=1e-4 * q_exp)
+
+    def test_get_heat_capacity_classical(self):
+        """
+        Test the SymmetricTopRotor.get_heat_capacity() method using a classical rotor.
+        """
+        self.mode.quantum = False
+        t_list = np.array([300, 500, 1000, 1500, 2000])
+        cv_exp_list = np.array([1.5, 1.5, 1.5, 1.5, 1.5]) * constants.R
+        for t, cv_exp in zip(t_list, cv_exp_list):
+            cv_act = self.mode.get_heat_capacity(t)
+            self.assertAlmostEqual(cv_exp, cv_act, delta=1e-4 * cv_exp)
+
+    def test_get_heat_capacity_quantum(self):
+        """
+        Test the SymmetricTopRotor.get_heat_capacity() method using a quantum rotor.
+        """
+        self.mode.quantum = True
+        t_list = np.array([300, 500, 1000, 1500, 2000])
+        cv_exp_list = np.array([1.5, 1.5, 1.5, 1.5, 1.5]) * constants.R
+        for t, cv_exp in zip(t_list, cv_exp_list):
+            cv_act = self.mode.get_heat_capacity(t)
+            self.assertAlmostEqual(cv_exp, cv_act, delta=1e-4 * cv_exp)
+
+    def test_get_enthalpy_classical(self):
+        """
+        Test the SymmetricTopRotor.get_enthalpy() method using a classical rotor.
+        """
+        self.mode.quantum = False
+        t_list = np.array([300, 500, 1000, 1500, 2000])
+        h_exp_list = np.array([1.5, 1.5, 1.5, 1.5, 1.5]) * constants.R * t_list
+        for t, h_exp in zip(t_list, h_exp_list):
+            h_act = self.mode.get_enthalpy(t)
+            self.assertAlmostEqual(h_exp, h_act, delta=1e-4 * h_exp)
+
+    def test_get_enthalpy_quantum(self):
+        """
+        Test the SymmetricTopRotor.get_enthalpy() method using a quantum rotor.
+        """
+        self.mode.quantum = True
+        t_list = np.array([300, 500, 1000, 1500, 2000])
+        Hexplist = np.array([1.49828, 1.49897, 1.49948, 1.49966, 1.49974]) * constants.R * t_list
+        for t, h_exp in zip(t_list, Hexplist):
+            h_act = self.mode.get_enthalpy(t)
+            self.assertAlmostEqual(h_exp, h_act, delta=1e-4 * h_exp)
+
+    def test_get_entropy_classical(self):
+        """
+        Test the SymmetricTopRotor.get_entropy() method using a classical rotor.
+        """
+        self.mode.quantum = False
+        t_list = np.array([300, 500, 1000, 1500, 2000])
+        s_exp_list = np.array([8.84778, 9.61402, 10.6537, 11.2619, 11.6935]) * constants.R
+        for t, s_exp in zip(t_list, s_exp_list):
+            s_act = self.mode.get_entropy(t)
+            self.assertAlmostEqual(s_exp, s_act, delta=1e-4 * s_exp)
+
+    def test_get_entropy_quantum(self):
+        """
+        Test the SymmetricTopRotor.get_entropy() method using a quantum rotor.
+        """
+        self.mode.quantum = True
+        t_list = np.array([300, 500, 1000, 1500, 2000])
+        s_exp_list = np.array([8.84778, 9.61402, 10.6537, 11.2619, 11.6935]) * constants.R
+        for t, s_exp in zip(t_list, s_exp_list):
+            s_act = self.mode.get_entropy(t)
+            self.assertAlmostEqual(s_exp, s_act, delta=1e-4 * s_exp)
+
+    def test_get_sum_of_states_classical(self):
+        """
+        Test the SymmetricTopRotor.get_sum_of_states() method using a classical rotor.
+        """
+        self.mode.quantum = False
+        e_list = np.arange(0, 2000 * 11.96, 1.0 * 11.96)
+        dens_states = self.mode.get_density_of_states(e_list)
+        sum_states = self.mode.get_sum_of_states(e_list)
+        for n in range(20, len(e_list)):
+            self.assertAlmostEqual(np.sum(dens_states[0:n+1]) / sum_states[n], 1.0, 1)
+
+    def test_get_sum_of_states_quantum(self):
+        """
+        Test the SymmetricTopRotor.get_sum_of_states() method using a quantum rotor.
+        """
+        self.mode.quantum = True
+        e_list = np.arange(0, 2000 * 11.96, 1.0 * 11.96)
+        dens_states = self.mode.get_density_of_states(e_list)
+        sum_states = self.mode.get_sum_of_states(e_list)
+        for n in range(1, len(e_list)):
+            self.assertAlmostEqual(np.sum(dens_states[0: n + 1]) / sum_states[n], 1.0, 3)
+
+    def test_get_density_of_states_classical(self):
+        """
+        Test the SymmetricTopRotor.get_density_of_states() method using a classical rotor.
+        """
+        self.mode.quantum = False
+        t_list = np.array([300, 400, 500])
+        e_list = np.arange(0, 2000 * 11.96, 1.0 * 11.96)
+        for t in t_list:
+            dens_states = self.mode.get_density_of_states(e_list)
+            q_act = np.sum(dens_states * np.exp(-e_list / constants.R / t))
+            q_exp = self.mode.get_partition_function(t)
+            self.assertAlmostEqual(q_exp, q_act, delta=1e-2 * q_exp)
+
+        # self.mode.quantum = False
+        # Elist = np.arange(0, 1000 * 11.96, 1 * 11.96)
+        # densStates = self.mode.getDensityOfStates(Elist)
+        # T = 100
+        # Qact = np.sum(densStates * np.exp(-Elist / constants.R / T))
+        # Qexp = self.mode.getPartitionFunction(T)
+        # self.assertAlmostEqual(Qexp, Qact, delta=1e-2 * Qexp)
+
+    def test_get_density_of_states_quantum(self):
+        """
+        Test the SymmetricTopRotor.get_density_of_states() method using a quantum rotor.
+        """
+        self.mode.quantum = True
+        t_list = np.array([300, 400, 500])
+        e_list = np.arange(0, 4000*11.96, 2.0 * 11.96)
+        for t in t_list:
+            dens_states = self.mode.get_density_of_states(e_list)
+            q_act = np.sum(dens_states * np.exp(-e_list / constants.R / t))
+            q_exp = self.mode.get_partition_function(t)
+            self.assertAlmostEqual(q_exp, q_act, delta=1e-2 * q_exp)
+
+    def test_repr(self):
+        """
+        Test that a SymmetricTopRotor object can be reconstructed from its
+        repr() output with no loss of information.
+        """
+        mode = None
+        exec(f'mode = {self.mode!r}')
+        for self_mode, mode in zip(self.mode.inertia, mode.inertia):
+            self.assertAlmostEqual(self_mode.value, mode.value, 6)
+            self.assertEqual(self_mode.units, mode.units)
+        self.assertEqual(self.mode.symmetry, mode.symmetry)
+        self.assertEqual(self.mode.quantum, mode.quantum)
+
+    def test_pickle(self):
+        """
+        Test that a SymmetricTopRotor object can be pickled and unpickled
+        with no loss of information.
+        """
+        import cPickle
+        mode = cPickle.loads(cPickle.dumps(self.mode, -1))
+        for self_mode, mode in zip(self.mode.inertia, mode.inertia):
+            self.assertAlmostEqual(self_mode.value, mode.value, 6)
+            self.assertEqual(self_mode.units, mode.units)
         self.assertEqual(self.mode.symmetry, mode.symmetry)
         self.assertEqual(self.mode.quantum, mode.quantum)
