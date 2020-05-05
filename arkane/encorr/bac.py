a184efce107fd844dfdda303db699b0f20c9702c
Colin Grambow <cgrambow@mit.edu>
2020-05-05 16:01:35 -0400

    Add BAC fitting functionality
    Add methods to fit Petersson- and Melius-type BACs. Also add methods to
    calculate fitting statistics and save fitted BACs to data.py

--- arkane/encorr/bac.py
+++ arkane/encorr/bac.py
@@ -38,15 +38,18 @@ The second type, Melius-type BACs, are described in:
 Anantharaman and Melius, J. Phys. Chem. A 2005, 109, 1734-1747
 """
 
+import importlib
+import json
 import logging
 import re
-from typing import Dict, Iterable, Union
+from typing import Dict, Iterable, List, Tuple, Union
 
 import numpy as np
+import scipy.optimize as optimize
 
 import arkane.encorr.data as data
-from arkane.encorr.data import BACDatapoint, geo_to_mol
-from arkane.encorr.reference import ReferenceSpecies
+from arkane.encorr.data import BACDatapoint, extract_dataset, geo_to_mol
+from arkane.encorr.reference import ReferenceSpecies, ReferenceDatabase
 from arkane.exceptions import BondAdditivityCorrectionError
 
 
@@ -55,6 +58,7 @@ class BAC:
     A class for deriving and applying bond additivity corrections.
     """
 
+    ref_databases = {}
     atom_spins = {
         'H': 0.5, 'C': 1.0, 'N': 1.5, 'O': 1.0, 'F': 0.5,
         'Si': 1.0, 'P': 1.5, 'S': 1.0, 'Cl': 0.5, 'Br': 0.5, 'I': 0.5
@@ -76,6 +80,10 @@ class BAC:
         self._model_chemistry = self._bac_type = None  # Set these first to avoid errors in setters
         self.model_chemistry = model_chemistry
         self.bac_type = bac_type
+
+        # Attributes related to fitting BACs for a given model chemistry
+        self.database_key = None  # Dictionary key to access reference database
+        self.dataset = None  # Collection of BACDatapoints in BACDataset
 
     @property
     def bac_type(self) -> str:
@@ -108,6 +116,37 @@ class BAC:
                 self.bacs = data.pbac[self.model_chemistry]
         except KeyError:
             pass
+
+    @classmethod
+    def load_database(cls,
+                      paths: Union[str, List[str]] = None,
+                      names: Union[str, List[str]] = None,
+                      reload: bool = False) -> str:
+        """
+        Load a reference database.
+
+        Args:
+            paths: Paths to database folders.
+            names: Names of database folders in RMG database.
+            reload: Force reload of database.
+
+        Returns:
+            Key to access just loaded database.
+        """
+        paths = ReferenceDatabase.get_database_paths(paths=paths, names=names)
+        key = cls.get_database_key(paths)
+        if key not in cls.ref_databases or reload:
+            logging.info(f'Loading reference database from {paths}')
+            cls.ref_databases[key] = ReferenceDatabase()
+            cls.ref_databases[key].load(paths=paths)
+        return key
+
+    @staticmethod
+    def get_database_key(paths: Union[str, List[str]]) -> Union[str, Tuple[str, ...]]:
+        """Get a key to access a stored reference database based on the database paths."""
+        if not (isinstance(paths, str) or (isinstance(paths, list) and all(isinstance(p, str) for p in paths))):
+            raise ValueError(f'{paths} paths is invalid')
+        return tuple(sorted(paths)) if isinstance(paths, list) else paths
 
     def get_correction(self,
                        bonds: Dict[str, int] = None,
@@ -274,3 +313,226 @@ class BAC:
 
         # Note the minus sign
         return -(bac_mol + bac_atom + bac_bond) * 4184.0  # Convert kcal/mol to J/mol
+
+    def fit(self, db_names: Union[str, List[str]] = 'main', **kwargs):
+        """
+        Fits bond additivity corrections using calculated and reference
+        data available in the RMG database. The resulting BACs stored
+        in self.bacs will be based on kcal/mol.
+
+        Args:
+            db_names: Optionally specify database names to train on (defaults to main).
+            kwargs: Keyword arguments for fitting Melius-type BACs (see self._fit_melius).
+        """
+        self.database_key = self.load_database(names=db_names)
+
+        self.dataset = extract_dataset(self.ref_databases[self.database_key], self.model_chemistry)
+        if len(self.dataset) == 0:
+            raise BondAdditivityCorrectionError(f'No species available for {self.model_chemistry} model chemistry')
+
+        if self.bac_type == 'm':
+            logging.info(f'Fitting Melius-type BACs for {self.model_chemistry}...')
+            self._fit_melius(**kwargs)
+        elif self.bac_type == 'p':
+            logging.info(f'Fitting Petersson-type BACs for {self.model_chemistry}...')
+            self._fit_petersson()
+
+        stats_before = self.dataset.calculate_stats()
+        stats_after = self.dataset.calculate_stats(for_bac_data=True)
+        logging.info(f'RMSE/MAE before fitting: {stats_before.rmse:.2f}/{stats_before.mae:.2f} kcal/mol')
+        logging.info(f'RMSE/MAE after fitting: {stats_after.rmse:.2f}/{stats_after.mae:.2f} kcal/mol')
+
+    def _fit_petersson(self):
+        """
+        Fit Petersson-type BACs.
+        """
+        features = self.dataset.bonds
+        feature_keys = list({k for f in features for k in f})
+        feature_keys.sort()
+
+        def make_feature_mat(_features: List[Dict[str, int]]) -> np.ndarray:
+            _x = np.zeros((len(_features), len(feature_keys)))
+            for idx, f in enumerate(_features):
+                flist = []
+                for k in feature_keys:
+                    try:
+                        flist.append(f[k])
+                    except KeyError:
+                        flist.append(0.0)
+                _x[idx] = np.array(flist)
+            return _x
+
+        x = make_feature_mat(features)
+        y = self.dataset.ref_data - self.dataset.calc_data
+        w = np.linalg.solve(x.T @ x, x.T @ y)
+        ypred = x @ w
+
+        self.dataset.bac_data = self.dataset.calc_data + ypred
+        self.bacs = {fk: wi for fk, wi in zip(feature_keys, w)}
+
+    def _fit_melius(self,
+                    fit_mol_corr: bool = True,
+                    global_opt: bool = True,
+                    global_opt_iter: int = 10,
+                    lsq_max_nfev: int = 500):
+        """
+        Fit Melius-type BACs.
+
+        Args:
+            fit_mol_corr: Also fit molecular correction term.
+            global_opt: Perform a global optimization.
+            global_opt_iter: Number of global opt iterations.
+            lsq_max_nfev: Maximum function evaluations in least squares optimizer.
+        """
+        mols = self.dataset.get_mols(from_geo=True)
+
+        all_atom_symbols = list({atom.element.symbol for mol in mols for atom in mol.atoms})
+        all_atom_symbols.sort()
+        nelements = len(all_atom_symbols)
+
+        # The order of parameters is
+        #     atom_corr (alpha)
+        #     bond_corr_length (beta)
+        #     bond_corr_neighbor (gamma)
+        #     optional: mol_corr (k)
+        # where atom_corr are the atomic corrections, bond_corr_length are the bondwise corrections
+        # due to bond lengths (bounded by 0 below), bond_corr_neighbor are the bondwise corrections
+        # due to neighboring atoms, and mol_corr (optional) is a molecular correction.
+
+        # Choose reasonable bounds depending on the parameter
+        lim_alpha = (-5.0, 5.0)
+        lim_beta = (0.0, 1e4)
+        lim_gamma = (-1.0, 1.0)
+        lim_k = (-5.0, 5.0)
+
+        wmin = [lim_alpha[0]] * nelements + [lim_beta[0]] * nelements + [lim_gamma[0]] * nelements
+        wmax = [lim_alpha[1]] * nelements + [lim_beta[1]] * nelements + [lim_gamma[1]] * nelements
+        if fit_mol_corr:
+            wmin.append(lim_k[0])
+            wmax.append(lim_k[1])
+
+        def get_params(_w: np.ndarray) -> Dict[str, Union[float, Dict[str, float]]]:
+            _atom_corr = dict(zip(all_atom_symbols, _w[:nelements]))
+            _bond_corr_length = dict(zip(all_atom_symbols, _w[nelements:2*nelements]))
+            _bond_corr_neighbor = dict(zip(all_atom_symbols, _w[2*nelements:3*nelements]))
+            _mol_corr = _w[3*nelements] if fit_mol_corr else 0.0
+            return dict(
+                atom_corr=_atom_corr,
+                bond_corr_length=_bond_corr_length,
+                bond_corr_neighbor=_bond_corr_neighbor,
+                mol_corr=_mol_corr
+            )
+
+        def get_bac_data(_w: np.ndarray) -> np.ndarray:
+            corr = np.array(
+                [self._get_melius_correction(datapoint=d, params=get_params(_w)) / 4184 for d in self.dataset]
+            )
+            return self.dataset.calc_data + corr
+
+        def residuals(_w: np.ndarray) -> Union[float, np.ndarray]:
+            """Calculate residuals"""
+            bac_data = get_bac_data(_w)
+            return self.dataset.ref_data - bac_data
+
+        global_opt_iter = global_opt_iter if global_opt else 1
+        results = []
+
+        for it in range(global_opt_iter):
+            if global_opt:
+                logging.info(f'Global opt iteration {it}')
+
+            # Get random initial guess
+            w_alpha = np.random.uniform(*lim_alpha, nelements)
+            w_beta = np.exp(np.random.uniform(-5, np.log(lim_beta[1]), nelements))
+            w_gamma = np.random.uniform(*lim_gamma, nelements)
+            w = np.concatenate((w_alpha, w_beta, w_gamma))
+            if fit_mol_corr:
+                w_k = np.random.uniform(*lim_k, 1)
+                w = np.concatenate((w, w_k))
+
+            res = optimize.least_squares(residuals, w, jac='3-point', bounds=(wmin, wmax),
+                                         max_nfev=lsq_max_nfev, verbose=1)
+            results.append(res)
+
+        res = min(results, key=lambda r: r.cost)
+        w = res.x
+
+        self.dataset.bac_data = get_bac_data(w)
+        self.bacs = get_params(w)
+
+    def write_to_database(self, overwrite: bool = False, alternate_path: str = None):
+        """
+        Write BACs to database.
+
+        Args:
+            overwrite: Overwrite existing BACs.
+            alternate_path: Write BACs to this path instead.
+        """
+        if self.bacs is None:
+            raise BondAdditivityCorrectionError('No BACs available for writing')
+
+        data_path = data.quantum_corrections_path
+        with open(data_path) as f:
+            lines = f.readlines()
+
+        bacs_formatted = self.format_bacs(indent=True)
+
+        bac_dict = data.mbac if self.bac_type == 'm' else data.pbac
+        keyword = 'mbac' if self.bac_type == 'm' else 'pbac'
+        has_entries = bool(data.mbac) if self.bac_type == 'm' else bool(data.pbac)
+
+        # Add new BACs to file without changing existing formatting
+        for i, line in enumerate(lines):
+            if keyword in line:
+                if has_entries:
+                    if self.model_chemistry in bac_dict:
+                        if overwrite:
+                            # Does not overwrite comments
+                            del_idx_start = del_idx_end = None
+                            for j, line2 in enumerate(lines[i:]):
+                                if self.model_chemistry in line2:
+                                    del_idx_start = i + j
+                                    del_idx_end = None
+                                elif line2.rstrip() == '    },':  # Can't have comment after final brace
+                                    del_idx_end = i + j + 1
+                                if del_idx_start is not None and del_idx_end is not None:
+                                    if (lines[del_idx_start - 1].lstrip().startswith('#')
+                                            or lines[del_idx_end + 1].lstrip().startswith('#')):
+                                        logging.warning('There may be left over comments from previous BACs')
+                                    lines[del_idx_start:del_idx_end] = bacs_formatted
+                                    break
+                        else:
+                            raise IOError(
+                                f'{self.model_chemistry} model chemistry already exists. Set `overwrite` to True.'
+                            )
+                    else:
+                        lines[(i+1):(i+1)] = ['\n'] + bacs_formatted
+                else:
+                    lines[i] = f'{keyword} = {{\n'
+                    lines[(i+1):(i+1)] = ['\n'] + bacs_formatted + ['\n}\n']
+                break
+
+        with open(data_path if alternate_path is None else alternate_path, 'w') as f:
+            f.writelines(lines)
+
+        # Reload data to update BAC dictionaries
+        if alternate_path is None:
+            importlib.reload(data)
+
+    def format_bacs(self, indent=False):
+        """
+        Obtain a list of nicely formatted BACs suitable for writelines.
+
+        Args:
+            indent: Indent each line for printing in database.
+
+        Returns:
+            Formatted list of BACs.
+        """
+        bacs_formatted = json.dumps(self.bacs, indent=4).replace('"', "'").split('\n')
+        bacs_formatted[0] = f"'{self.model_chemistry}': " + bacs_formatted[0]
+        bacs_formatted[-1] += ','
+        bacs_formatted = [e + '\n' for e in bacs_formatted]
+        if indent:
+            bacs_formatted = ['    ' + e for e in bacs_formatted]
+        return bacs_formatted
