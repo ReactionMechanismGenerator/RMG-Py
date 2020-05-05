498993e0d5d6b69985e55e1f4ae39bbc5cea002c
Colin Grambow <cgrambow@mit.edu>
2020-05-05 16:01:35 -0400

    Add BACDatapoint and BACDataset classes
    Fit BACs using specialized classes that cache all important data needed
    during fitting.

--- arkane/encorr/data.py
+++ arkane/encorr/data.py
@@ -30,12 +30,28 @@
 """
 This file loads in the atomic energies, frequency scale factors, and BAC parameters for several model chemistries. The
 data is stored in the RMG-database in input/quantum_corrections/data.py
+
+This module also provides classes to store data for BAC fitting and evaluating.
 """
+
+import functools
 import importlib.util
 import os
+from collections import Counter
+from dataclasses import dataclass
+from typing import Callable, Iterable, List, Union
+
+import numpy as np
+import pybel
 
 from rmgpy import settings
+from rmgpy.molecule import Atom, Bond, get_element
+from rmgpy.molecule import Molecule as RMGMolecule
 
+from arkane.encorr.reference import ReferenceSpecies, ReferenceDatabase
+from arkane.exceptions import BondAdditivityCorrectionError
+
+# ######## Database loading ##########
 quantum_corrections_path = os.path.join(settings['database.directory'], 'quantum_corrections', 'data.py')
 spec = importlib.util.spec_from_file_location("quantum_calculations", quantum_corrections_path)
 data = importlib.util.module_from_spec(spec)
@@ -45,3 +61,272 @@ spec.loader.exec_module(data)
 for k, v in vars(data).items():
     if not k.startswith('__'):
         vars()[k] = v
+######################################
+
+
+# ########## Data classes ############
+BOND_SYMBOLS = {1: '-', 2: '=', 3: '#'}
+
+
+class Molecule(RMGMolecule):
+    """Wrapper for RMG Molecule to add ID attribute"""
+    def __init__(self, *args, mol_id=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.id = mol_id
+
+
+@dataclass
+class Stats:
+    """Small class to store BAC fitting statistics"""
+    rmse: Union[float, np.ndarray]
+    mae: Union[float, np.ndarray]
+
+
+class BACDatapoint:
+    """A BACDatapoint contains a single species"""
+
+    class _Decorators:
+        @staticmethod
+        def assert_model_chemistry(func):
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs):
+                if args[0].model_chemistry is None:  # args[0] is the instance
+                    raise BondAdditivityCorrectionError('Model chemistry is not defined')
+                return func(*args, **kwargs)
+            return wrapper
+
+    def __init__(self, spc: ReferenceSpecies, model_chemistry: str = None):
+        self.spc = spc
+        self.model_chemistry = model_chemistry
+
+        self._mol = None
+        self._mol_type = None
+        self._bonds = None
+        self._ref_data = None
+        self._calc_data = None
+        self._bac_data = None
+        self.weight = 1
+
+    @property
+    def mol(self) -> Molecule:
+        if self._mol is None:
+            raise ValueError('Call `BACDatapoint.to_mol` first')
+        return self._mol
+
+    def to_mol(self, from_geo: bool = False) -> Molecule:
+        """
+        Convert to RMG molecule. If `from_geo` is True, a single-bonded
+        molecule is perceived from 3D coordinates with Open Babel.
+        """
+        if self._mol is None:
+            if from_geo:
+                self._mol_from_geo()
+            else:
+                self._mol_from_adjlist()
+        else:  # Use cached molecule if possible
+            if from_geo and self._mol_type != 'geo':
+                self._mol_from_geo()
+            elif not from_geo and self._mol_type != 'adj':
+                self._mol_from_adjlist()
+
+        return self._mol
+
+    @_Decorators.assert_model_chemistry
+    def _mol_from_geo(self):
+        xyz = self.spc.calculated_data[self.model_chemistry].xyz_dict
+        self._mol = geo_to_mol(xyz['coords'], symbols=xyz['symbols'])
+        self._mol_type = 'geo'
+
+    def _mol_from_adjlist(self):
+        self._mol = Molecule().from_adjacency_list(self.spc.adjacency_list,
+                                                   raise_atomtype_exception=False,
+                                                   raise_charge_exception=False)
+        self._mol_type = 'adj'
+
+    @property
+    def bonds(self) -> Counter:
+        """Get bond counts"""
+        if self._bonds is None:
+            mol = self.to_mol(from_geo=False)
+            bonds = Counter()
+            for bond in mol.get_all_edges():
+                symbols = [bond.atom1.element.symbol, bond.atom2.element.symbol]
+                symbols.sort()
+                symbol = symbols[0] + BOND_SYMBOLS[bond.order] + symbols[1]
+                bonds[symbol] += 1
+            self._bonds = bonds
+        return self._bonds
+
+    @property
+    def ref_data(self) -> float:
+        """Get reference enthalpy in kcal/mol"""
+        if self._ref_data is None:
+            self._ref_data = self.spc.get_reference_enthalpy().h298.value_si / 4184
+        return self._ref_data
+
+    @property
+    @_Decorators.assert_model_chemistry
+    def calc_data(self) -> float:
+        """Get calculated enthalpy in kcal/mol"""
+        if self._calc_data is None:
+            self._calc_data = self.spc.calculated_data[self.model_chemistry].thermo_data.H298.value_si / 4184
+        return self._calc_data
+
+    @property
+    def bac_data(self) -> float:
+        if self._bac_data is None:
+            raise ValueError('No BAC data available')
+        return self._bac_data
+
+    @bac_data.setter
+    def bac_data(self, val: float):
+        self._bac_data = val
+
+
+class DatasetProperty:
+    """
+    Descriptor to simplify BACDataset properties.
+
+    This descriptor is essentially a specialized version of Python
+    properties. An instance of this descriptor can be defined as a
+    class attribute in a class containing a `data` attribute, where
+    each item in the `data` sequence contains an attribute with its
+    name corresponding to the first argument passed to `__init__`.
+
+    The descriptor is used by accessing the attribute with the <attr>
+    name, which implicitly retrieves the value cached in the <_attr>
+    attribute. If no cached value exists, the <attr> attributes from
+    all items in `data` are obtained as a list or as a Numpy array (if
+    `asarray` is `True`), cached in <_attr> (only if <attr> in each
+    `data` item cannot be changed, i.e., `settable` should be `False`),
+    and returned. If all <attr>'s in `data` are `None`, `None` is
+    returned (instead of a sequence of `None`s).
+
+    The descriptor can also be used to set the <attr> attributes in the
+    `data` items. This requires that `settable` is `True`, otherwise an
+    `AttributeError` is raised. It also requires that the length of the
+    sequence used for setting the <attr> attributes is the same length
+    as `data`.
+    """
+
+    def __init__(self, attr, asarray=False, settable=False):
+        self.pub_attr = attr  # Name of attribute defined in BACDataset and available in BACDatapoint
+        self.priv_attr = '_' + attr  # Private attribute that is set in BACDataset and used to retrieve cached values
+        self.asarray = asarray  # Whether the data should be get/set as Numpy arrays
+        self.settable = settable  # Whether the BACDatapoint attributes can be set
+
+    def __get__(self, obj, objtype=None):
+        if hasattr(obj, self.priv_attr):  # Return cached value if available
+            return getattr(obj, self.priv_attr)
+        val = [getattr(d, self.pub_attr) for d in obj.data]  # Retrieve the attributes from the items in data
+        if all(v is None for v in val):  # Instead of returning list of None's, just return None
+            val = None
+        elif self.asarray:
+            val = np.array(val)
+        if not self.settable:  # Only cache sequence if it cannot be changed in BACDatapoint
+            setattr(obj, self.priv_attr, val)
+        return val
+
+    def __set__(self, obj, val):
+        if not self.settable:
+            raise AttributeError(f'Cannot set {self.pub_attr}')
+        if len(val) != len(obj):  # Requires that __len__ is defined in obj
+            raise ValueError('Number of data do not match number of datapoints')
+        for d, v in zip(obj.data, val):
+            setattr(d, self.pub_attr, v)
+
+
+class BACDataset:
+    """A BACDataset contains a list of BACDatapoints"""
+    def __init__(self, data: List[BACDatapoint]):
+        self.data = data
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __getitem__(self, item) -> Union[BACDatapoint, List[BACDatapoint]]:
+        return self.data[item]
+
+    def append(self, item: BACDatapoint):
+        self.data.append(item)
+
+    def sort(self, key: Callable, reverse: bool = False):
+        self.data.sort(key=key, reverse=reverse)
+
+    bonds = DatasetProperty('bonds')
+    ref_data = DatasetProperty('ref_data', asarray=True)
+    calc_data = DatasetProperty('calc_data', asarray=True)
+    bac_data = DatasetProperty('bac_data', asarray=True, settable=True)
+    weight = DatasetProperty('weight', asarray=True, settable=True)
+    weights = weight  # Alias for weight
+
+    def get_mols(self, from_geo: bool = False) -> List[Molecule]:
+        return [d.to_mol(from_geo=from_geo) for d in self.data]
+
+    def calculate_stats(self, for_bac_data: bool = False) -> Stats:
+        """
+        Calculate RMSE and MAE with respect to `calc_data` or `bac_data`.
+
+        Args:
+            for_bac_data: Use the fitted BAC data to calculate RMSE/MAE if set to True.
+                          Otherwise, calculate RMSE/MAE with respect to the calculated (quantum) data.
+
+        Returns:
+            Stats dataclass containing RMSE and MAE.
+        """
+        other_data = self.bac_data if for_bac_data else self.calc_data
+        diff = self.ref_data - other_data
+        rmse = np.sqrt(np.dot(diff, diff) / len(self.ref_data))
+        mae = np.sum(np.abs(diff)) / len(self.ref_data)
+        return Stats(rmse, mae)
+
+
+def extract_dataset(ref_database: ReferenceDatabase, model_chemistry: str) -> BACDataset:
+    """
+    Extract species for a given model chemistry from a reference
+    database and convert to a BACDataset.
+
+    Args:
+         ref_database: Reference database
+         model_chemistry: Model chemistry.
+
+    Returns:
+        BACDataset containing species with data available at given model chemistry.
+    """
+    species = ref_database.extract_model_chemistry(model_chemistry, as_error_canceling_species=False)
+    return BACDataset([BACDatapoint(spc, model_chemistry=model_chemistry) for spc in species])
+
+
+def geo_to_mol(coords: np.ndarray, symbols: Iterable[str] = None, nums: Iterable[int] = None) -> Molecule:
+    """
+    Convert molecular geometry specified by atomic coordinates and
+    atomic symbols/numbers to RMG molecule.
+
+    Use Open Babel because it's better at recognizing long bonds.
+    """
+    if nums is None and symbols is None:
+        raise ValueError('Must specify nums or symbols')
+
+    symbols = [get_element(int(n)).symbol for n in nums] if symbols is None else list(symbols)
+    xyz = f'{len(symbols)}\n\n'
+    xyz += '\n'.join(f'{s}  {c[0]: .10f}  {c[1]: .10f}  {c[2]: .10f}' for s, c in zip(symbols, coords))
+    mol = pybel.readstring('xyz', xyz)
+    return _pybel_to_rmg(mol)
+
+
+def _pybel_to_rmg(pybel_mol: pybel.Molecule) -> Molecule:
+    """
+    Convert Pybel molecule to RMG molecule but ignore charge,
+    multiplicity, and bond orders.
+    """
+    mol = Molecule()
+    for pybel_atom in pybel_mol:
+        element = get_element(pybel_atom.atomicnum)
+        atom = Atom(element=element, coords=np.array(pybel_atom.coords))
+        mol.vertices.append(atom)
+    for obbond in pybel.ob.OBMolBondIter(pybel_mol.OBMol):
+        begin_idx = obbond.GetBeginAtomIdx() - 1  # Open Babel indexes atoms starting at 1
+        end_idx = obbond.GetEndAtomIdx() - 1
+        bond = Bond(mol.vertices[begin_idx], mol.vertices[end_idx])
+        mol.add_bond(bond)
+    return mol
