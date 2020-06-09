fa87f72ebdebdfe3b613c6ebaf4ff65389e47838
hwpang <hwpang@mit.edu>
2020-06-09 11:07:19 -0400

    Change rxnlists[i] to rxnlists[i][0]
    rxnlists[i] is a tuple ([list of training reactions], entry.label), so number of training reactions should be rxnlists[i][0].

--- rmgpy/data/kinetics/family.py
+++ rmgpy/data/kinetics/family.py
@@ -3400,7 +3400,7 @@ class KineticsFamily(Database):
             if kinetics is not None:
                 entry = entries[i]
                 std = kinetics.uncertainty.get_expected_log_uncertainty() / 0.398  # expected uncertainty is std * 0.398
-                st = "BM rule fitted to {0} training reactions at node {1}".format(len(rxnlists[i]), entry.label)
+                st = "BM rule fitted to {0} training reactions at node {1}".format(len(rxnlists[i][0]), entry.label)
                 st += "\nTotal Standard Deviation in ln(k): {0}".format(std)
                 new_entry = Entry(
                     index=index,
