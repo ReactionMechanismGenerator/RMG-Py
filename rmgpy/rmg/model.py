65ab78454812dedbf6bac4747faccd4ec37215f5
Matt Johnson <mjohnson541@gmail.com>
2020-06-19 20:50:44 -0400

    bugfix for too large for pdep reactions getting lost

--- rmgpy/rmg/model.py
+++ rmgpy/rmg/model.py
@@ -1654,7 +1654,7 @@ class CoreEdgeReactionModel:
             # the core later in the mechanism generation
             if not (self.pressure_dependence and rxn.elementary_high_p and rxn.is_unimolecular()
                     and isinstance(rxn, LibraryReaction) and isinstance(rxn.kinetics, Arrhenius) and (self.pressure_dependence.maximum_atoms is None  \
-                            or self.pressure_dependence.maximum_atoms >= sum([len(spec.molecule[0].atoms) for spec in r.reactants]))):
+                            or self.pressure_dependence.maximum_atoms >= sum([len(spec.molecule[0].atoms) for spec in rxn.reactants]))):
                 # Don't add to the edge library reactions that were already processed
                 self.add_reaction_to_edge(rxn)
 
