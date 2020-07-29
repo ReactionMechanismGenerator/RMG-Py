219590135056ceb98571a6bd04a47b6955456eeb
hwpang <hwpang@mit.edu>
2020-07-29 16:59:01 -0400

    Modify Pmin/Pmax in main.py to first check reaction_system has Prange attribute.

--- rmgpy/rmg/main.py
+++ rmgpy/rmg/main.py
@@ -674,12 +674,8 @@ class RMG(util.Subject):
         # determine min and max values for T and P (don't determine P values for liquid reactors)
         self.Tmin = min([x.Trange[0].value_si if x.Trange else x.T.value_si for x in self.reaction_systems])
         self.Tmax = max([x.Trange[1].value_si if x.Trange else x.T.value_si for x in self.reaction_systems])
-        try:
-            self.Pmin = min([x.Prange[0].value_si if x.Prange else x.P.value_si for x in self.reaction_systems])
-            self.Pmax = max([x.Prange[1].value_si if x.Prange else x.P.value_si for x in self.reaction_systems])
-        except AttributeError:
-            # For LiquidReactor, Pmin and Pmax remain with the default value of `None`
-            pass
+        self.Pmin = min([x.Prange[0].value_si if hasattr(x, 'Prange') and x.Prange else x.P.value_si for x in self.reaction_systems])
+        self.Pmax = max([x.Prange[1].value_si if hasattr(x, 'Prange') and x.Prange else x.P.value_si for x in self.reaction_systems])
 
         self.rmg_memories = []
 
