ac905187128dcbb524e5c1341ea41ffd435a4b10
davidfarinajr <farina.d@northeastern.edu>
2020-08-04 19:10:32 -0400

    added `is_electron` method for Species class

--- rmgpy/species.py
+++ rmgpy/species.py
@@ -451,6 +451,10 @@ class Species(object):
         """Return ``True`` if the species is a vacant surface site."""
         return self.molecule[0].is_surface_site()
 
+    def is_electron(self):
+        """Return ``True`` if the species is an electron"""
+        return self.molecule[0].is_electron()
+
     def get_partition_function(self, T):
         """
         Return the partition function for the species at the specified
