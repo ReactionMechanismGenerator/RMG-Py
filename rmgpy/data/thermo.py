4d239a54467f7e1191a7440f41545cc7dde72ea2
Richard West <r.west@northeastern.edu>
2020-10-22 11:56:26 -0400

    Catch more errors when reporting problem adsorbate structures.
    Helps address #2030 which shows that sometimes you hit a DatabaseError bug
    and don't know where it came from.

--- rmgpy/data/thermo.py
+++ rmgpy/data/thermo.py
@@ -1569,7 +1569,7 @@ class ThermoDatabase(object):
         )
         try:
             self._add_group_thermo_data(adsorption_thermo, self.groups['adsorptionPt111'], molecule, {})
-        except KeyError:
+        except (KeyError, DatabaseError):
             logging.error("Couldn't find in adsorption thermo database:")
             logging.error(molecule)
             logging.error(molecule.to_adjacency_list())
