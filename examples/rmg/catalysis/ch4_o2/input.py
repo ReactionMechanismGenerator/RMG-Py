db12098d17bc847dbedb1667405dc0efdb9a556e
Richard West <r.west@northeastern.edu>
2020-11-16 22:29:13 -0500

    Revert "Making ch4_o2 example NOT save the TS structures, for debugging"
    This reverts commit ca768413e024b67e778fc4ef810b0a0f9f28eb2d.
    
    We want to turn it back on, in this branch

--- examples/rmg/catalysis/ch4_o2/input.py
+++ examples/rmg/catalysis/ch4_o2/input.py
@@ -80,7 +80,7 @@ options(
     units='si',
     generateOutputHTML=True,
     generatePlots=False, # Enable to make plots of core and edge size etc. But takes a lot of the total runtime!
-    generateLabeledReactions=False,
+    generateLabeledReactions=True,
     saveEdgeSpecies=True,
     saveSimulationProfiles=True,
 )
