f2f3f2610faccee28cda2b44a15e34cefec8e854
davidfarinajr <farina.d@northeastern.edu>
2021-01-18 11:24:13 -0500

    mark species as reactive in add_atom_labels_for_reaction

--- rmgpy/data/kinetics/family.py
+++ rmgpy/data/kinetics/family.py
@@ -2828,6 +2828,7 @@ class KineticsFamily(Database):
             for labeled_molecule in labeled_products:
                 if species.is_isomorphic(labeled_molecule, save_order=self.save_order):
                     species.molecule = [labeled_molecule]
+                    labeled_molecule.reactive = True
                     reaction.products[index] = species
                     labeled_products.remove(labeled_molecule)
                     break
@@ -2838,6 +2839,7 @@ class KineticsFamily(Database):
             for labeled_molecule in labeled_reactants:
                 if species.is_isomorphic(labeled_molecule, save_order=self.save_order):
                     species.molecule = [labeled_molecule]
+                    labeled_molecule.reactive = True
                     reaction.reactants[index] = species
                     labeled_reactants.remove(labeled_molecule)
                     break
