e2620d92df457fad1d391040a7c14aa43fa0581f
davidfarinajr <farina.d@northeastern.edu>
2021-01-20 17:58:12 -0500

    added to_surface_charge_transfer method

--- rmgpy/kinetics/surface.pyx
+++ rmgpy/kinetics/surface.pyx
@@ -428,6 +428,24 @@ cdef class SurfaceArrhenius(Arrhenius):
         return (SurfaceArrhenius, (self.A, self.n, self.Ea, self.T0, self.Tmin, self.Tmax, self.Pmin, self.Pmax,
                                    self.uncertainty, self.comment))
 
+    cpdef SurfaceChargeTransfer to_surface_charge_transfer(self, double V0, double ne=-1):
+        """
+        Return an :class:`SurfaceChargeTransfer` instance of the kinetics model with reversible
+        potential `V0` in Volts and electron stochiometric coeff `ne`
+        """
+        return SurfaceChargeTransfer(
+            A=self.A,
+            n=self.n,
+            ne=ne,
+            Ea=self.Ea,
+            V0=(V0,'V'),
+            T0=(1, "K"),
+            Tmin=self.Tmin,
+            Tmax=self.Tmax,
+            uncertainty = self.uncertainty,
+            comment=self.comment,
+        )
+
 
 ################################################################################
 
