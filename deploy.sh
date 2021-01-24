b6889b5a04d521e478f91b7e4ce4f0463e082cc0
davidfarinajr <farina.d@northeastern.edu>
2021-01-24 14:37:24 -0500

    use `vdw_actions` db branch for tests

--- deploy.sh
+++ deploy.sh
@@ -34,14 +34,15 @@ cd $TARGET_DIR
 
 # create a new branch in RMG-tests with the name equal to
 # the branch name of the tested RMG-Py branch:
-RMGTESTSBRANCH=rmgpy-$DEPLOY_BRANCH
+RMGTESTSBRANCH=rmgpydb-$DEPLOY_BRANCH
 
 git checkout -b $RMGTESTSBRANCH || true 
 git checkout $RMGTESTSBRANCH
 
 # create an empty commit with the SHA-ID of the 
 # tested commit of the RMG-Py branch:
-git commit --allow-empty -m rmgpy-$REV
+DB_DEPLOY_BRANCH="vdw_actions"
+git commit --allow-empty -m rmgpydb-$REV-${DB_DEPLOY_BRANCH}
 
 # push to the branch to the RMG/RMG-tests repo:
 git push -f $REPO $RMGTESTSBRANCH > /dev/null
