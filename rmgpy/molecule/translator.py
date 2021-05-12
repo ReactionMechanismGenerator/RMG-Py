96f89e392244c3972acc74173614b439c1cd87c6
davidfarinajr <farina.d@northeastern.edu>
2021-05-12 16:40:36 -0400

    update_atomtypes -> update in translator.py

--- rmgpy/molecule/translator.py
+++ rmgpy/molecule/translator.py
@@ -519,7 +519,7 @@ def _read(mol, identifier, identifier_type, backend, raise_atomtype_exception=Tr
 
     if _lookup(mol, identifier, identifier_type) is not None:
         if _check_output(mol, identifier):
-            mol.update_atomtypes(log_species=True, raise_exception=raise_atomtype_exception)
+            mol.update(log_species=True, raise_atomtype_exception=raise_atomtype_exception, sort_atoms=False)
             return mol
 
     for option in _get_backend_list(backend):
@@ -531,7 +531,7 @@ def _read(mol, identifier, identifier_type, backend, raise_atomtype_exception=Tr
             raise NotImplementedError("Unrecognized backend {0}".format(option))
 
         if _check_output(mol, identifier):
-            mol.update_atomtypes(log_species=True, raise_exception=raise_atomtype_exception)
+            mol.update(log_species=True, raise_atomtype_exception=raise_atomtype_exception, sort_atoms=False)
             return mol
         else:
             logging.debug('Backend {0} is not able to parse identifier {1}'.format(option, identifier))
