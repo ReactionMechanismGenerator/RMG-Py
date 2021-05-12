82fb9b11b0a8d79a831d81e1ce9c1e4962c22bc1
davidfarinajr <farina.d@northeastern.edu>
2021-05-12 13:30:12 -0400

    try to generate_resonance_structures, and return a deep copy of the molecule if it fails

--- rmgpy/molecule/molecule.py
+++ rmgpy/molecule/molecule.py
@@ -2319,10 +2319,15 @@ class Molecule(Graph):
 
     def generate_resonance_structures(self, keep_isomorphic=False, filter_structures=True, save_order=False):
         """Returns a list of resonance structures of the molecule."""
-        return resonance.generate_resonance_structures(self, keep_isomorphic=keep_isomorphic,
+
+        try:
+            return resonance.generate_resonance_structures(self, keep_isomorphic=keep_isomorphic,
                                                        filter_structures=filter_structures,
                                                        save_order=save_order,
                                                        )
+        except:
+            logging.warning("Resonance structure generation failed for {}".format(self))
+            return [self.copy(deep=True)]
 
     def get_url(self):
         """
