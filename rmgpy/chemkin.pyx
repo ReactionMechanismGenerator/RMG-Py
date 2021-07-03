fcd741c8fdbc4a5537cb39474772e85177f80103
Richard West <r.west@northeastern.edu>
2021-07-02 22:33:10 -0400

    Chemkin COV writing: correct units for the E parameter
    The E should be in the same units as Ea, which is kcal/mol.
    That's 4184 times smaller.

--- rmgpy/chemkin.pyx
+++ rmgpy/chemkin.pyx
@@ -1831,7 +1831,7 @@ def write_kinetics_entry(reaction, species_list, verbose=True, java_library=Fals
                 for species, cov_params in kinetics.coverage_dependence.items():
                     label = get_species_identifier(species)
                     string += f'\n    COV / {label:<41}'
-                    string += f"{cov_params['a'].value:<9.3e} {cov_params['m'].value:<6.3f} {cov_params['E'].value_si:<6.3f} /"
+                    string += f"{cov_params['a'].value:<9.3g} {cov_params['m'].value:<6.3f} {cov_params['E'].value_si/4184.:<6.3f} /"
     elif isinstance(kinetics, (_kinetics.Lindemann, _kinetics.Troe)):
         arrhenius = kinetics.arrheniusHigh
         conversion_factor = arrhenius.A.get_conversion_factor_from_si_to_cm_mol_s()
