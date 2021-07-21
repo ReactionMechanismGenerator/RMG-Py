fa6e4be44de0bbdfc6c2bdc2c1f6637e8a3c2d15
Matt Johnson <mjohnson541@gmail.com>
2021-07-21 16:02:54 -0400

    manage removing a species from the edge in the phase_system

--- rmgpy/rmg/model.py
+++ rmgpy/rmg/model.py
@@ -1351,6 +1351,7 @@ class CoreEdgeReactionModel:
         # remove the species
         self.edge.species.remove(spec)
         self.index_species_dict.pop(spec.index)
+        self.edge.phase_system.remove_species(spec)
 
         # clean up species references in reaction_systems
         for reaction_system in reaction_systems:
