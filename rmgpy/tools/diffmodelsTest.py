aa877280d4c2460a79043a18a8a8ee61db253c1b
Sevy Harris <harris.se@northeastern.edu>
2021-10-01 15:52:42 -0400

    added asserts so the test actually tests that the diff worked

--- rmgpy/tools/diffmodelsTest.py
+++ rmgpy/tools/diffmodelsTest.py
@@ -49,8 +49,66 @@ class DiffModelsTest(unittest.TestCase):
             'wd': folder,
         }
 
-        execute(chemkin1, species_dict1, None, chemkin2, species_dict2, None, **kwargs)
+        results = execute(chemkin1, species_dict1, None, chemkin2, species_dict2, None, **kwargs)
+        assert len(results) == 6, 'Wrong number of return items'
 
+        common_species = results[0]
+        unique_species1 = results[1]
+        unique_species2 = results[2]
+        common_reactions = results[3]
+        unique_reactions1 = results[4]
+        unique_reactions2 = results[5]
+
+        assert len(common_species) == 13, 'different number of expected common species'
+        assert len(unique_species1) == 0, 'model 1 has unique species'
+        assert len(unique_species2) == 0, 'model 2 has unique species'
+
+        assert len(common_reactions) == 20, 'different number of expected common reactions'
+        assert len(unique_reactions1) == 0, 'model 1 has unique reactions'
+        assert len(unique_reactions2) == 0, 'model 2 has unique reactions'
+
+
+        # check that it made the diff.html
+        assert os.path.exists(os.path.join(folder, 'diff.html'))
+
+        shutil.rmtree(os.path.join(folder, 'species1'))
+        shutil.rmtree(os.path.join(folder, 'species2'))
+        os.remove(os.path.join(folder, 'diff.html'))
+
+    
+    def test_different_models(self):
+        folder = os.path.join(os.getcwd(), 'rmgpy/tools/data/diffmodels')
+
+        chemkin1 = os.path.join(folder, 'chem1.inp')
+        species_dict1 = os.path.join(folder, 'species_dictionary1.txt')
+
+        chemkin3 = os.path.join(folder, 'chem3.inp')
+        species_dict3 = os.path.join(folder, 'species_dictionary3.txt')
+
+        kwargs = {
+            'wd': folder,
+        }
+
+        results = execute(chemkin1, species_dict1, None, chemkin3, species_dict3, None, **kwargs)
+        assert len(results) == 6, 'Wrong number of return items'
+
+        common_species = results[0]
+        unique_species1 = results[1]
+        unique_species2 = results[2]
+        common_reactions = results[3]
+        unique_reactions1 = results[4]
+        unique_reactions2 = results[5]
+
+        assert len(common_species) != 13, 'different number of expected common species'
+        assert len(unique_species1) > 0, 'model 1 has no unique species'
+        assert len(unique_species2) > 0, 'model 2 has no unique species'
+
+        assert len(common_reactions) != 20, 'different number of expected common reactions'
+        assert len(unique_reactions1) > 0, 'model 1 has no unique reactions'
+        # assert len(unique_reactions2) > 0, 'model 2 has no unique reactions'
+
+
+        # Assert that this gives different results
         shutil.rmtree(os.path.join(folder, 'species1'))
         shutil.rmtree(os.path.join(folder, 'species2'))
         os.remove(os.path.join(folder, 'diff.html'))
