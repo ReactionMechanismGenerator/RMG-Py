9feee82403c75122601a1181901d6e4444e868e1
Sevy Harris <harris.se@northeastern.edu>
2021-10-03 09:16:41 -0400

    started surface diff identical test

--- rmgpy/tools/diffmodelsTest.py
+++ rmgpy/tools/diffmodelsTest.py
@@ -36,6 +36,7 @@ from rmgpy.tools.diffmodels import execute
 
 class DiffModelsTest(unittest.TestCase):
 
+
     def test_identical_models(self):
         folder = os.path.join(os.getcwd(), 'rmgpy/tools/data/diffmodels')
 
@@ -109,6 +110,52 @@ class DiffModelsTest(unittest.TestCase):
 
 
         # Assert that this gives different results
+        shutil.rmtree(os.path.join(folder, 'species1'))
+        shutil.rmtree(os.path.join(folder, 'species2'))
+        os.remove(os.path.join(folder, 'diff.html'))
+
+    def test_identical_surface_models(self):
+        folder = os.path.join(os.getcwd(), 'rmgpy/tools/data/diffmodels')
+
+        chemkin1 = os.path.join(folder, 'chem_annotated-surface1.inp')
+        species_dict1 = os.path.join(folder, 'surf_species_dictionary1.txt')
+
+        chemkin2 = os.path.join(folder, 'chem_annotated-surface1.inp')
+        species_dict2 = os.path.join(folder, 'surf_species_dictionary1.txt')
+
+        from rmgpy.chemkin import load_chemkin_file
+
+        species_list, reaction_list = load_chemkin_file(chemkin1,
+                                                        dictionary_path=species_dict1)
+                                                        
+
+
+        kwargs = {
+            'wd': folder,
+        }
+
+        results = execute(chemkin1, species_dict1, None, chemkin2, species_dict2, None, **kwargs)
+        assert len(results) == 6, 'Wrong number of return items'
+
+        common_species = results[0]
+        unique_species1 = results[1]
+        unique_species2 = results[2]
+        common_reactions = results[3]
+        unique_reactions1 = results[4]
+        unique_reactions2 = results[5]
+
+        assert len(common_species) == 13, 'different number of expected common species'
+        assert len(unique_species1) == 0, 'model 1 has unique species'
+        assert len(unique_species2) == 0, 'model 2 has unique species'
+
+        assert len(common_reactions) == 20, 'different number of expected common reactions'
+        assert len(unique_reactions1) == 0, 'model 1 has unique reactions'
+        assert len(unique_reactions2) == 0, 'model 2 has unique reactions'
+
+
+        # check that it made the diff.html
+        assert os.path.exists(os.path.join(folder, 'diff.html'))
+
         shutil.rmtree(os.path.join(folder, 'species1'))
         shutil.rmtree(os.path.join(folder, 'species2'))
         os.remove(os.path.join(folder, 'diff.html'))
