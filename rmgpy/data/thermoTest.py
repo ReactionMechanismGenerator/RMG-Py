a9974c41d85a67b94e412ff5ec3216d8e32a7a86
Hao-Wei Pang <hwpang@mit.edu>
2022-01-06 17:50:14 -0500

    Update missing group test now that the data was added for the original missing group

--- rmgpy/data/thermoTest.py
+++ rmgpy/data/thermoTest.py
@@ -613,10 +613,10 @@ multiplicity 2
     def test_identifying_missing_group(self):
         """Test identifying a missing GAV group"""
         # this test should be updated once data is added to the missing group
-        spc = Species(smiles='C[N+]#[C-]')
+        spc = Species(smiles='S[N+]#[C-]')
         spc.generate_resonance_structures()
         thermo_gav = self.database.get_thermo_data_from_groups(spc)
-        self.assertIn('missing(N5tc-C2tcCs)', thermo_gav.comment)
+        self.assertIn('missing(N5tc-C2tcS2s)', thermo_gav.comment)
 
     def test_adsorbate_thermo_generation_gav(self):
         """Test thermo generation for adsorbate from Group Additivity value.
