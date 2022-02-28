815d1110f9def13c7bb566ccdc7576d9ce01e4c6
Matt Johnson <mjohnson541@gmail.com>
2022-02-28 14:20:31 -0500

    add method for getting uncertainties for SLS predictions
    this approach approximately associates independent
    uncertainty factors with each rate coefficient

--- rmgpy/pdep/sls.py
+++ rmgpy/pdep/sls.py
@@ -306,6 +306,19 @@ end""")
 
     return kmat,u
 
+def get_uncertainties(kmat, xssource, dxdtssource):
+    """
+    approximate the independent factor uncertainty in individual
+    rate coefficients
+    """
+    u = np.zeros(kmat.shape)
+    for i,x in enumerate(xssource):
+        flux = calcfluxes(kmat, x)
+        dxdt = dxdtssource[i]
+        for j in range(len(xssource)):
+            u[j,i] = np.exp(np.abs(np.log(dxdt[j]/flux[j])))
+    return u
+
 def ravel_kmat(kmat, n_isomreac):
     """
     reduce rate coefficient matrix to a vector
