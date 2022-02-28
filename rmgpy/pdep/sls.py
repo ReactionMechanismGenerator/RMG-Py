1fd48aa240fa418577c3844c4eb77fedaac51abb
Matt Johnson <mjohnson541@gmail.com>
2022-02-28 12:45:19 -0500

    add file and imports for SLS methods

---
+++ rmgpy/pdep/sls.py
@@ -0,0 +1,44 @@
+###############################################################################
+#                                                                             #
+# RMG - Reaction Mechanism Generator                                          #
+#                                                                             #
+# Copyright (c) 2002-2021 Prof. William H. Green (whgreen@mit.edu),           #
+# Prof. Richard H. West (r.west@neu.edu) and the RMG Team (rmg_dev@mit.edu)   #
+#                                                                             #
+# Permission is hereby granted, free of charge, to any person obtaining a     #
+# copy of this software and associated documentation files (the 'Software'),  #
+# to deal in the Software without restriction, including without limitation   #
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,    #
+# and/or sell copies of the Software, and to permit persons to whom the       #
+# Software is furnished to do so, subject to the following conditions:        #
+#                                                                             #
+# The above copyright notice and this permission notice shall be included in  #
+# all copies or substantial portions of the Software.                         #
+#                                                                             #
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,    #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER      #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING     #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER         #
+# DEALINGS IN THE SOFTWARE.                                                   #
+#                                                                             #
+###############################################################################
+
+"""
+Contains functionality for directly simulating the master equation
+and implementing the SLS master equation reduction method
+"""
+
+from diffeqpy import de
+from julia import Main
+import scipy.sparse as sparse
+import numpy as np
+import scipy.linalg
+import mpmath
+import scipy.optimize as opt
+
+import rmgpy.constants as constants
+from rmgpy.pdep.reaction import calculate_microcanonical_rate_coefficient
+from rmgpy.pdep.me import generate_full_me_matrix
+from rmgpy.statmech.translation import IdealGasTranslation
