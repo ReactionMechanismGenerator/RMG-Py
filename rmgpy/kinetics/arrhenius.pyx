f261d91e1b269ab16d2644bd46992f17772ac2fa
Sevy Harris <harris.se@northeastern.edu>
2022-07-19 10:50:58 -0400

    allow user to pass in initial guess of BM params

--- rmgpy/kinetics/arrhenius.pyx
+++ rmgpy/kinetics/arrhenius.pyx
@@ -602,13 +602,15 @@ cdef class ArrheniusBM(KineticsModel):
             comment=self.comment,
         )
 
-    def fit_to_reactions(self, rxns, w0=None, recipe=None, Ts=None):
+    def fit_to_reactions(self, rxns, w0=None, recipe=None, Ts=None, param_guess=None):
         """
         Fit an ArrheniusBM model to a list of reactions at the given temperatures,
         w0 must be either given or estimated using the family object
 
         WARNING: there's a lot of code duplication with ArrheniusChargeTransferBM.fit_to_reactions
                  so anything you change here you should probably change there too and vice versa!
+        If param_guess, a list with the initial guesses for [lnA, n, E0] is specified,
+        pass that in as the starting point of the curve_fit
         """
         assert w0 is not None or recipe is not None, 'either w0 or recipe must be specified'
 
@@ -670,7 +672,9 @@ cdef class ArrheniusBM(KineticsModel):
             while keep_trying:
                 keep_trying = False
                 try:
-                    params = curve_fit(kfcn, xdata, ydata, sigma=sigmas, p0=[1.0, 1.0, w0 / 10.0], xtol=xtol, ftol=ftol)
+                    if param_guess is None:
+                        param_guess = [1.0, 1.0, w0 / 10.0]
+                    params = curve_fit(kfcn, xdata, ydata, sigma=sigmas, p0=param_guess, xtol=xtol, ftol=ftol)
                 except RuntimeError:
                     if xtol < 1.0:
                         keep_trying = True
