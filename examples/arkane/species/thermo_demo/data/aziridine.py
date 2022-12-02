96ebe0c4e8e1b4c7d6077dc3fb03a56c3bad45a8
Alon Grinberg Dana <alon@technion.ac.il>
2022-12-02 10:59:14 +0200

    Added a thermo_demo example for Arkane

---
+++ examples/arkane/species/thermo_demo/data/aziridine.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+bonds = {'C-C': 1, 'C-N': 2, 'C-H': 4, 'H-N': 1}
+
+externalSymmetry = 1
+
+spinMultiplicity = 1
+
+opticalIsomers = 1
+
+energy = Log('aziridine/sp.out')
+
+geometry = Log('aziridine/freq.out')
+
+frequencies = Log('aziridine/freq.out')
+
