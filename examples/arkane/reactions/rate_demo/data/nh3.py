d6749f22b961709ddb5557f3029521c08e20fce5
Alon Grinberg Dana <alon@technion.ac.il>
2022-12-02 23:32:26 +0200

    Added a rate_demo example for Arkane

---
+++ examples/arkane/reactions/rate_demo/data/nh3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+externalSymmetry = 3
+
+spinMultiplicity = 1
+
+opticalIsomers = 1
+
+energy = Log('nh3/sp.out')
+
+geometry = Log('nh3/freq.out')
+
+frequencies = Log('nh3/freq.out')
+
