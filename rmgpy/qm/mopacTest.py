853a9cbbad7c0ed304486bc34ba07368b062ab78
JacksonBurns <33505528+JacksonBurns@users.noreply.github.com>
2023-05-08 22:22:50 -0400

    fix typo (delta not selta), decrease required close enough, format with black

--- rmgpy/qm/mopacTest.py
+++ rmgpy/qm/mopacTest.py
@@ -39,8 +39,8 @@ from rmgpy.molecule.molecule import Molecule
 from rmgpy.qm.main import QMCalculator
 from rmgpy.qm.mopac import Mopac, MopacMolPM3, MopacMolPM6, MopacMolPM7
 
-mol1 = Molecule().from_smiles('C1=CC=C2C=CC=CC2=C1')
-MOPAC_CLOSE_ENOUGH_PERCENT = 0.0001  # 0.01%
+mol1 = Molecule().from_smiles("C1=CC=C2C=CC=CC2=C1")
+MOPAC_CLOSE_ENOUGH_PERCENT = 0.001  # 0.1%
 
 
 class TestMopacMolPM3(unittest.TestCase):
@@ -52,13 +52,14 @@ class TestMopacMolPM3(unittest.TestCase):
         """
         A function run before each unit test in this class.
         """
-        rmgpy_path = os.path.normpath(os.path.join(get_path(), '..'))
+        rmgpy_path = os.path.normpath(os.path.join(get_path(), ".."))
 
-        qm = QMCalculator(software='mopac',
-                          method='pm3',
-                          fileStore=os.path.join(rmgpy_path, 'testing', 'qm', 'QMfiles'),
-                          scratchDirectory=os.path.join(rmgpy_path, 'testing', 'qm', 'QMscratch'),
-                          )
+        qm = QMCalculator(
+            software="mopac",
+            method="pm3",
+            fileStore=os.path.join(rmgpy_path, "testing", "qm", "QMfiles"),
+            scratchDirectory=os.path.join(rmgpy_path, "testing", "qm", "QMscratch"),
+        )
 
         if not os.path.exists(qm.settings.fileStore):
             os.makedirs(qm.settings.fileStore)
@@ -70,39 +71,62 @@ class TestMopacMolPM3(unittest.TestCase):
         Test that generate_thermo_data() works correctly for MOPAC PM3
         """
         # First ensure any old data are removed, or else they'll be reused!
-        for directory in (self.qmmol1.settings.fileStore, self.qmmol1.settings.scratchDirectory):
+        for directory in (
+            self.qmmol1.settings.fileStore,
+            self.qmmol1.settings.scratchDirectory,
+        ):
             shutil.rmtree(directory, ignore_errors=True)
 
         self.qmmol1.generate_thermo_data()
         result = self.qmmol1.qm_data
         self.assertTrue(self.qmmol1.verify_output_file())
-        self.assertTrue(self.qmmol1.thermo.comment.startswith('QM MopacMolPM3 calculation'))
+        self.assertTrue(
+            self.qmmol1.thermo.comment.startswith("QM MopacMolPM3 calculation")
+        )
         self.assertEqual(result.numberOfAtoms, 18)
         self.assertIsInstance(result.atomicNumbers, np.ndarray)
-        if result.molecularMass.units == 'amu':
+        if result.molecularMass.units == "amu":
             self.assertAlmostEqual(result.molecularMass.value, 128.173, 2)
 
-        self.assertAlmostEqual(self.qmmol1.thermo.H298.value_si, 169708, delta=169708*MOPAC_CLOSE_ENOUGH_PERCENT)
-        self.assertAlmostEqual(self.qmmol1.thermo.S298.value_si, 334.500, delta=334.500*MOPAC_CLOSE_ENOUGH_PERCENT)
+        self.assertAlmostEqual(
+            self.qmmol1.thermo.H298.value_si,
+            169708,
+            delta=169708 * MOPAC_CLOSE_ENOUGH_PERCENT,
+        )
+        self.assertAlmostEqual(
+            self.qmmol1.thermo.S298.value_si,
+            334.500,
+            delta=334.500 * MOPAC_CLOSE_ENOUGH_PERCENT,
+        )
 
     def test_load_thermo_data(self):
         """
         Test that generate_thermo_data() can load thermo from the previous MOPAC PM3 run.
-        
+
         Check that it loaded, and the values are the same as above.
         """
 
         self.qmmol1.generate_thermo_data()
         result = self.qmmol1.qm_data
 
-        self.assertTrue(self.qmmol1.thermo.comment.startswith('QM MopacMolPM3 calculation'))
+        self.assertTrue(
+            self.qmmol1.thermo.comment.startswith("QM MopacMolPM3 calculation")
+        )
         self.assertEqual(result.numberOfAtoms, 18)
         self.assertIsInstance(result.atomicNumbers, np.ndarray)
-        if result.molecularMass.units == 'amu':
+        if result.molecularMass.units == "amu":
             self.assertAlmostEqual(result.molecularMass.value, 128.173, 2)
 
-        self.assertAlmostEqual(self.qmmol1.thermo.H298.value_si, 169708, delta=169708*MOPAC_CLOSE_ENOUGH_PERCENT)
-        self.assertAlmostEqual(self.qmmol1.thermo.S298.value_si, 334.500, delta=334.500*MOPAC_CLOSE_ENOUGH_PERCENT)
+        self.assertAlmostEqual(
+            self.qmmol1.thermo.H298.value_si,
+            169708,
+            delta=169708 * MOPAC_CLOSE_ENOUGH_PERCENT,
+        )
+        self.assertAlmostEqual(
+            self.qmmol1.thermo.S298.value_si,
+            334.500,
+            delta=334.500 * MOPAC_CLOSE_ENOUGH_PERCENT,
+        )
 
 
 class TestMopacMolPM6(unittest.TestCase):
@@ -114,13 +138,14 @@ class TestMopacMolPM6(unittest.TestCase):
         """
         A function run before each unit test in this class.
         """
-        rmgpy_path = os.path.normpath(os.path.join(get_path(), '..'))
+        rmgpy_path = os.path.normpath(os.path.join(get_path(), ".."))
 
-        qm = QMCalculator(software='mopac',
-                          method='pm6',
-                          fileStore=os.path.join(rmgpy_path, 'testing', 'qm', 'QMfiles'),
-                          scratchDirectory=os.path.join(rmgpy_path, 'testing', 'qm', 'QMscratch'),
-                          )
+        qm = QMCalculator(
+            software="mopac",
+            method="pm6",
+            fileStore=os.path.join(rmgpy_path, "testing", "qm", "QMfiles"),
+            scratchDirectory=os.path.join(rmgpy_path, "testing", "qm", "QMscratch"),
+        )
 
         if not os.path.exists(qm.settings.fileStore):
             os.makedirs(qm.settings.fileStore)
@@ -132,40 +157,63 @@ class TestMopacMolPM6(unittest.TestCase):
         Test that generate_thermo_data() works correctly for MOPAC PM6
         """
         # First ensure any old data are removed, or else they'll be reused!
-        for directory in (self.qmmol1.settings.fileStore, self.qmmol1.settings.scratchDirectory):
+        for directory in (
+            self.qmmol1.settings.fileStore,
+            self.qmmol1.settings.scratchDirectory,
+        ):
             shutil.rmtree(directory, ignore_errors=True)
 
         self.qmmol1.generate_thermo_data()
         result = self.qmmol1.qm_data
         self.assertTrue(self.qmmol1.verify_output_file())
 
-        self.assertTrue(self.qmmol1.thermo.comment.startswith('QM MopacMolPM6 calculation'))
+        self.assertTrue(
+            self.qmmol1.thermo.comment.startswith("QM MopacMolPM6 calculation")
+        )
         self.assertEqual(result.numberOfAtoms, 18)
         self.assertIsInstance(result.atomicNumbers, np.ndarray)
-        if result.molecularMass.units == 'amu':
+        if result.molecularMass.units == "amu":
             self.assertAlmostEqual(result.molecularMass.value, 128.173, 2)
 
-        self.assertAlmostEqual(self.qmmol1.thermo.H298.value_si, 167704, delta=167704*MOPAC_CLOSE_ENOUGH_PERCENT)
-        self.assertAlmostEqual(self.qmmol1.thermo.S298.value_si, 338.099, delta=338.099*MOPAC_CLOSE_ENOUGH_PERCENT)
+        self.assertAlmostEqual(
+            self.qmmol1.thermo.H298.value_si,
+            167704,
+            delta=167704 * MOPAC_CLOSE_ENOUGH_PERCENT,
+        )
+        self.assertAlmostEqual(
+            self.qmmol1.thermo.S298.value_si,
+            338.099,
+            delta=338.099 * MOPAC_CLOSE_ENOUGH_PERCENT,
+        )
 
     def test_load_thermo_data(self):
         """
         Test that generate_thermo_data() can load thermo from the previous MOPAC PM6 run.
-        
+
         Check that it loaded, and the values are the same as above.
         """
 
         self.qmmol1.generate_thermo_data()
         result = self.qmmol1.qm_data
 
-        self.assertTrue(self.qmmol1.thermo.comment.startswith('QM MopacMolPM6 calculation'))
+        self.assertTrue(
+            self.qmmol1.thermo.comment.startswith("QM MopacMolPM6 calculation")
+        )
         self.assertEqual(result.numberOfAtoms, 18)
         self.assertIsInstance(result.atomicNumbers, np.ndarray)
-        if result.molecularMass.units == 'amu':
+        if result.molecularMass.units == "amu":
             self.assertEqual(result.molecularMass.value, 128.173)
 
-        self.assertAlmostEqual(self.qmmol1.thermo.H298.value_si, 167704, delta=167704*MOPAC_CLOSE_ENOUGH_PERCENT)  # to 0 decimal place
-        self.assertAlmostEqual(self.qmmol1.thermo.S298.value_si, 338.099, selta=338.099*MOPAC_CLOSE_ENOUGH_PERCENT)
+        self.assertAlmostEqual(
+            self.qmmol1.thermo.H298.value_si,
+            167704,
+            delta=167704 * MOPAC_CLOSE_ENOUGH_PERCENT,
+        )  # to 0 decimal place
+        self.assertAlmostEqual(
+            self.qmmol1.thermo.S298.value_si,
+            338.099,
+            delta=338.099 * MOPAC_CLOSE_ENOUGH_PERCENT,
+        )
 
 
 class TestMopacMolPM7(unittest.TestCase):
@@ -177,18 +225,19 @@ class TestMopacMolPM7(unittest.TestCase):
         """
         A function run before each unit test in this class.
         """
-        rmgpy_path = os.path.normpath(os.path.join(get_path(), '..'))
+        rmgpy_path = os.path.normpath(os.path.join(get_path(), ".."))
 
-        qm = QMCalculator(software='mopac',
-                          method='pm7',
-                          fileStore=os.path.join(rmgpy_path, 'testing', 'qm', 'QMfiles'),
-                          scratchDirectory=os.path.join(rmgpy_path, 'testing', 'qm', 'QMscratch'),
-                          )
+        qm = QMCalculator(
+            software="mopac",
+            method="pm7",
+            fileStore=os.path.join(rmgpy_path, "testing", "qm", "QMfiles"),
+            scratchDirectory=os.path.join(rmgpy_path, "testing", "qm", "QMscratch"),
+        )
 
         if not os.path.exists(qm.settings.fileStore):
             os.makedirs(qm.settings.fileStore)
 
-        mol1 = Molecule().from_smiles('C1=CC=C2C=CC=CC2=C1')
+        mol1 = Molecule().from_smiles("C1=CC=C2C=CC=CC2=C1")
         self.qmmol1 = MopacMolPM7(mol1, qm.settings)
 
     def test_generate_thermo_data(self):
@@ -196,43 +245,66 @@ class TestMopacMolPM7(unittest.TestCase):
         Test that generate_thermo_data() works correctly for MOPAC PM7
         """
         # First ensure any old data are removed, or else they'll be reused!
-        for directory in (self.qmmol1.settings.fileStore, self.qmmol1.settings.scratchDirectory):
+        for directory in (
+            self.qmmol1.settings.fileStore,
+            self.qmmol1.settings.scratchDirectory,
+        ):
             shutil.rmtree(directory, ignore_errors=True)
 
         self.qmmol1.generate_thermo_data()
         result = self.qmmol1.qm_data
         self.assertTrue(self.qmmol1.verify_output_file())
 
-        self.assertTrue(self.qmmol1.thermo.comment.startswith('QM MopacMolPM7 calculation'))
+        self.assertTrue(
+            self.qmmol1.thermo.comment.startswith("QM MopacMolPM7 calculation")
+        )
         self.assertEqual(result.numberOfAtoms, 18)
         self.assertIsInstance(result.atomicNumbers, np.ndarray)
-        if result.molecularMass.units == 'amu':
+        if result.molecularMass.units == "amu":
             self.assertAlmostEqual(result.molecularMass.value, 128.173, 2)
 
-        self.assertAlmostEqual(self.qmmol1.thermo.H298.value_si, 166169, delta=166169*MOPAC_CLOSE_ENOUGH_PERCENT)
-        self.assertAlmostEqual(self.qmmol1.thermo.S298.value_si, 336.333, delta=336.333*MOPAC_CLOSE_ENOUGH_PERCENT)
+        self.assertAlmostEqual(
+            self.qmmol1.thermo.H298.value_si,
+            166169,
+            delta=166169 * MOPAC_CLOSE_ENOUGH_PERCENT,
+        )
+        self.assertAlmostEqual(
+            self.qmmol1.thermo.S298.value_si,
+            336.333,
+            delta=336.333 * MOPAC_CLOSE_ENOUGH_PERCENT,
+        )
 
     def test_load_thermo_data(self):
         """
         Test that generate_thermo_data() can load thermo from the previous MOPAC PM7 run.
-        
+
         Check that it loaded, and the values are the same as above.
         """
 
         self.qmmol1.generate_thermo_data()
         result = self.qmmol1.qm_data
 
-        self.assertTrue(self.qmmol1.thermo.comment.startswith('QM MopacMolPM7 calculation'))
+        self.assertTrue(
+            self.qmmol1.thermo.comment.startswith("QM MopacMolPM7 calculation")
+        )
         self.assertEqual(result.numberOfAtoms, 18)
         self.assertIsInstance(result.atomicNumbers, np.ndarray)
-        if result.molecularMass.units == 'amu':
+        if result.molecularMass.units == "amu":
             self.assertAlmostEqual(result.molecularMass.value, 128.173, 2)
 
-        self.assertAlmostEqual(self.qmmol1.thermo.H298.value_si, 166169, delta=166169*MOPAC_CLOSE_ENOUGH_PERCENT)
-        self.assertAlmostEqual(self.qmmol1.thermo.S298.value_si, 336.333, delta=336.333*MOPAC_CLOSE_ENOUGH_PERCENT)
+        self.assertAlmostEqual(
+            self.qmmol1.thermo.H298.value_si,
+            166169,
+            delta=166169 * MOPAC_CLOSE_ENOUGH_PERCENT,
+        )
+        self.assertAlmostEqual(
+            self.qmmol1.thermo.S298.value_si,
+            336.333,
+            delta=336.333 * MOPAC_CLOSE_ENOUGH_PERCENT,
+        )
 
 
 ################################################################################
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main(testRunner=unittest.TextTestRunner(verbosity=2))
