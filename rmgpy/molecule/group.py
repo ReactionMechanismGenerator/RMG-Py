316f840bae92c4e4a52a7ff0338ff8b977701a7e
Richard West <r.west@northeastern.edu>
2023-05-12 23:00:02 -0400

    Replace hard-coded list of charged atomtypes with check for charge.
    The atomtypes each have a charge list - let's just see if it matches.

--- rmgpy/molecule/group.py
+++ rmgpy/molecule/group.py
@@ -2727,28 +2727,16 @@ class Group(Graph):
 
         # Check that the charge of atoms is expected
         for atom in new_molecule.atoms:
-            if abs(atom.charge) > 0:
+            if atom.charge != 0:
                 if atom in mol_to_group:
                     group_atom = mol_to_group[atom]
                 else:
                     raise UnexpectedChargeError(graph=new_molecule)
-                # check hardcoded atomtypes
-                positive_charged = ['Csc', 'Cdc', 'Ctc',
-                                    'N3sc', 'N5sc', 'N5dc', 'N5ddc', 'N5tc', 'N5b',
-                                    'O2sc', 'O4sc', 'O4dc', 'O4tc',
-                                    'P5sc', 'P5dc', 'P5ddc', 'P5tc', 'P5b',
-                                    'S2sc', 'S4sc', 'S4dc', 'S4tdc', 'S6sc', 'S6dc', 'S6tdc']
-                negative_charged = ['C2sc', 'C2dc', 'C2tc',
-                                    'N0sc', 'N1sc', 'N1dc', 'N5dddc',
-                                    'O0sc',
-                                    'P0sc', 'P1sc', 'P1dc', 'P5sc',
-                                    'S0sc', 'S2sc', 'S2dc', 'S2tc', 'S4sc', 'S4dc', 'S4tdc', 'S6sc', 'S6dc', 'S6tdc']
-                if group_atom.atomtype[0] in [ATOMTYPES[x] for x in positive_charged] and atom.charge > 0:
+                if atom.charge in group_atom.atomtype[0].charge:
+                    # declared charge in atomtype is same as new charge
                     pass
-                elif group_atom.atomtype[0] in [ATOMTYPES[x] for x in negative_charged] and atom.charge < 0:
-                    pass
-                # declared charge in original group is not same as new charge
                 elif atom.charge in group_atom.charge:
+                    # declared charge in original group is same as new charge
                     pass
                 else:
                     raise UnexpectedChargeError(graph=new_molecule)
