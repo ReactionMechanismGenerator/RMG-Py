304bad301f6d00689669dcaab95c09700459397c
Jackson Burns <jwburns@mit.edu>
2023-07-22 14:25:43 -0400

    update the default filepath for `test_data` to its new location

--- rmgpy/__init__.py
+++ rmgpy/__init__.py
@@ -40,6 +40,7 @@ from rmgpy.exceptions import SettingsError
 
 ################################################################################
 
+
 class Settings(dict):
     """
     A dictionary-like object containing global settings for RMG jobs. These
@@ -49,7 +50,7 @@ class Settings(dict):
     and adds methods for loading and resetting the settings, as well as a
     custom :meth:`__setitem__` method for processing the setting values before
     adding them to the dictionary.
-    
+
     In general you should be working with the module-level variable
     ``settings`` in this module, which is an instance of this class.
     """
@@ -61,36 +62,36 @@ class Settings(dict):
         self.load(path)
 
     def __setitem__(self, key, value):
-        if key == 'database.directory':
+        if key == "database.directory":
             value = os.path.abspath(os.path.expandvars(value))
-        elif key == 'test_data.directory':
+        elif key == "test_data.directory":
             value = os.path.abspath(os.path.expandvars(value))
         else:
             raise SettingsError('Unexpecting setting "{0}" encountered.'.format(key))
-        self.sources[key] = '-'
+        self.sources[key] = "-"
         super(Settings, self).__setitem__(key, value)
 
     def report(self):
         """
         Returns a string saying what is set and where things came from, suitable for logging
         """
-        lines = ['Global RMG Settings:']
+        lines = ["Global RMG Settings:"]
         for key in self.keys():
             lines.append("   {0:20s} = {1:20s} ({2})".format(key, self[key], self.sources[key]))
-        return '\n'.join(lines)
+        return "\n".join(lines)
 
     def load(self, path=None):
         """
         Load settings from a file on disk. If an explicit file is not specified,
         the following locations will be searched for a settings file, and the
         first one found will be loaded:
-        
+
         * An rmgrc file in the current working directory
-        
+
         * An rmgrc file in the user's $HOME/.rmg directory
-        
+
         * An rmgrc file in the same directory as this package
-        
+
         If none of these can be found, a SettingsError is raised.
         """
         # First set all settings to their default values
@@ -107,36 +108,36 @@ class Settings(dict):
             # The user did not specify an explicit file to use for the settings
             # Load one of the default settings files instead
             working_dir = os.path.abspath(os.path.dirname(__file__))
-            if os.path.exists('rmgrc'):
-                self.filename = 'rmgrc'
-            elif os.path.exists(os.path.expanduser('~/.rmg/rmgrc')):
-                self.filename = os.path.expanduser('~/.rmg/rmgrc')
-            elif os.path.exists(os.path.join(working_dir, 'rmgrc')):
-                self.filename = os.path.join(working_dir, 'rmgrc')
+            if os.path.exists("rmgrc"):
+                self.filename = "rmgrc"
+            elif os.path.exists(os.path.expanduser("~/.rmg/rmgrc")):
+                self.filename = os.path.expanduser("~/.rmg/rmgrc")
+            elif os.path.exists(os.path.join(working_dir, "rmgrc")):
+                self.filename = os.path.join(working_dir, "rmgrc")
             else:
                 return  # fail silently, instead of raising an error
 
         # From here on we assume that we have identified the appropriate
         # settings file to load
 
-        with open(self.filename, 'r') as f:
+        with open(self.filename, "r") as f:
             for line in f:
                 # Remove any comments from the line
-                index = line.find('#')
+                index = line.find("#")
                 if index != -1:
                     line = line[:index]
                 # Is there a key-value pair remaining?
-                if line.find('database.directory') != -1:
+                if line.find("database.directory") != -1:
                     value = line.split()[-1]  # Get the last token from this line
                     value = value.strip()
-                    self['database.directory'] = value
-                    self.sources['database.directory'] = "from {0}".format(self.filename)
+                    self["database.directory"] = value
+                    self.sources["database.directory"] = "from {0}".format(self.filename)
 
-                elif line.find('test_data.directory') != -1:
+                elif line.find("test_data.directory") != -1:
                     value = line.split()[-1]  # Get the last token from this line
                     value = value.strip()
-                    self['test_data.directory'] = value
-                    self.sources['test_data.directory'] = "from {0}".format(self.filename)
+                    self["test_data.directory"] = value
+                    self.sources["test_data.directory"] = "from {0}".format(self.filename)
 
     def reset(self):
         """
@@ -144,11 +145,10 @@ class Settings(dict):
         """
         self.filename = None
         rmgpy_module_dir = os.path.abspath(os.path.dirname(__file__))
-        self['database.directory'] = os.path.realpath(
-            os.path.join(rmgpy_module_dir, '..', '..', 'RMG-database', 'input'))
-        self.sources['database.directory'] = 'Default, relative to RMG-Py source code'
-        self['test_data.directory'] = os.path.realpath(os.path.join(rmgpy_module_dir, 'test_data'))
-        self.sources['test_data.directory'] = 'Default, relative to RMG-Py source code'
+        self["database.directory"] = os.path.realpath(os.path.join(rmgpy_module_dir, "..", "..", "RMG-database", "input"))
+        self.sources["database.directory"] = "Default, relative to RMG-Py source code"
+        self["test_data.directory"] = os.path.realpath(os.path.join(rmgpy_module_dir, "..", "test", "rmgpy", "test_data"))
+        self.sources["test_data.directory"] = "Default, relative to RMG-Py source code"
 
 
 # The global settings object
@@ -156,6 +156,7 @@ settings = Settings(path=None)
 
 
 ################################################################################
+
 
 def get_path():
     """
