10a919e5cfef7ebcd762ed7306ccac51fa974708
Jackson Burns <jwburns@mit.edu>
2023-07-24 21:10:36 -0400

    remove windows batch files we can't use anymore

--- .conda/bld.bat
+++
@@ -1,7 +0,0 @@
-:: Install RMG
-mingw32-make install
-
-:: lazy "install" of everything in our 'external' folder.
-:: most of which should probably be elsewhere
-mkdir %SP_DIR%\external
-xcopy %SRC_DIR%\external %SP_DIR%\external /E /Y
