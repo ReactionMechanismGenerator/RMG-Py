84eb7c8e3755dc055980d93119924f41a561fb81
Richard West <r.west@northeastern.edu>
2023-07-27 20:55:59 -0400

    Convert setup_class to setup_method in arrheniusTest
    Jackson wrote:
    Upon closer inspection, all of the setup_class methods in this file could be
    changed to setup_method, I just missed the others. None of the test_* methods
    change self (they just read it) so we don't need to execute the setup method
    over and over again

--- test/rmgpy/kinetics/arrheniusTest.py
+++ test/rmgpy/kinetics/arrheniusTest.py
@@ -286,7 +286,7 @@ class TestArrheniusEP:
     Contains unit tests of the :class:`ArrheniusEP` class.
     """
 
-    def setup_class(self):
+    def setup_method(self):
         self.A = 1.0e12
         self.n = 0.5
         self.alpha = 0.5
@@ -439,7 +439,7 @@ class TestArrheniusBM:
     Contains unit tests of the :class:`ArrheniusBM` class.
     """
 
-    def setup_class(self):
+    def setup_method(self):
         self.A = 8.00037e12
         self.n = 0.391734
         self.w0 = 798000
@@ -624,7 +624,7 @@ class TestPDepArrhenius:
     Contains unit tests of the :class:`PDepArrhenius` class.
     """
 
-    def setup_class(self):
+    def setup_method(self):
         self.arrhenius0 = Arrhenius(
             A=(1.0e6, "s^-1"),
             n=1.0,
@@ -885,7 +885,7 @@ class TestMultiArrhenius:
     Contains unit tests of the :class:`MultiArrhenius` class.
     """
 
-    def setup_class(self):
+    def setup_method(self):
         self.Tmin = 350.0
         self.Tmax = 1500.0
         self.comment = "Comment"
@@ -1077,7 +1077,7 @@ class TestMultiPDepArrhenius:
     Contains unit tests of the :class:`MultiPDepArrhenius` class.
     """
 
-    def setup_class(self):
+    def setup_method(self):
         self.Tmin = 350.0
         self.Tmax = 1500.0
         self.Pmin = 1e-1
