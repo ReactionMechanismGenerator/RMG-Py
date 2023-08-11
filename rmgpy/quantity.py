350495b3035dddbc03c7c5286099de4905e37229
Richard West <r.west@northeastern.edu>
2023-08-11 14:11:11 -0400

    Quantity: just adding comments. No substantive changes.

--- rmgpy/quantity.py
+++ rmgpy/quantity.py
@@ -840,24 +840,23 @@ def RateCoefficient(*args, **kwargs):
 # SurfaceRateCoefficient is handled as a special case since it can take various
 # units depending on the reaction order
 SURFACERATECOEFFICIENT_CONVERSION_FACTORS = {
-    (1.0 / pq.s).dimensionality: 1.0,
-    (pq.m ** 3 / pq.s).dimensionality: 1.0,
+    (1.0 / pq.s).dimensionality: 1.0,  # unimolecular
+    (pq.m ** 3 / pq.s).dimensionality: 1.0,  # single site adsorption
     (pq.m ** 6 / pq.s).dimensionality: 1.0,
     (pq.m ** 9 / pq.s).dimensionality: 1.0,
-    (pq.m ** 3 / (pq.mol * pq.s)).dimensionality: 1.0,
+    (pq.m ** 3 / (pq.mol * pq.s)).dimensionality: 1.0,  # single site adsorption
     (pq.m ** 6 / (pq.mol ** 2 * pq.s)).dimensionality: 1.0,
     (pq.m ** 9 / (pq.mol ** 3 * pq.s)).dimensionality: 1.0,
-    (pq.m ** 2 / pq.s).dimensionality: 1.0,
-    (pq.m ** 5 / pq.s).dimensionality: 1.0,
-    (pq.m ** 2 / (pq.mol * pq.s)).dimensionality: 1.0,
-    (pq.m ** 5 / (pq.mol ** 2 * pq.s)).dimensionality: 1.0,
+    (pq.m ** 2 / pq.s).dimensionality: 1.0,  # bimolecular surface (Langmuir-Hinshelwood)
+    (pq.m ** 5 / pq.s).dimensionality: 1.0,  # dissociative adsorption
+    (pq.m ** 2 / (pq.mol * pq.s)).dimensionality: 1.0,  # bimolecular surface (Langmuir-Hinshelwood)
+    (pq.m ** 5 / (pq.mol ** 2 * pq.s)).dimensionality: 1.0,  # dissociative adsorption
     (pq.m ** 4 / (pq.mol ** 2 * pq.s)).dimensionality: 1.0,
 }
 SURFACERATECOEFFICIENT_COMMON_UNITS = [
     's^-1',  # unimolecular
     'm^3/(mol*s)', 'cm^3/(mol*s)', 'm^3/(molecule*s)', 'cm^3/(molecule*s)',  # single site adsorption
-    'm^2/(mol*s)', 'cm^2/(mol*s)', 'm^2/(molecule*s)', 'cm^2/(molecule*s)',
-    # bimolecular surface (Langmuir-Hinshelwood)
+    'm^2/(mol*s)', 'cm^2/(mol*s)', 'm^2/(molecule*s)', 'cm^2/(molecule*s)',  # bimolecular surface (Langmuir-Hinshelwood)
     'm^5/(mol^2*s)', 'cm^5/(mol^2*s)', 'm^5/(molecule^2*s)', 'cm^5/(molecule^2*s)',  # dissociative adsorption
     'm^4/(mol^2*s)', 'cm^4/(mol^2*s)', 'm^4/(molecule^2*s)', 'cm^4/(molecule^2*s)',  # Surface_Bidentate_Dissociation
 ]
