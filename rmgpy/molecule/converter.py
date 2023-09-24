e5998d7f59b4fe7bf9438402cd71f1ba972f4426
Xiaorui Dong <xiaorui@mit.edu>
2023-09-24 12:16:44 -0400

    Correct set no implicit Hs with more conventional for loop
    This is to improve the readability of commit da3d4b and make it follow a more Pythonic style.

--- rmgpy/molecule/converter.py
+++ rmgpy/molecule/converter.py
@@ -116,8 +116,9 @@ def to_rdkit_mol(mol, remove_h=True, return_mapping=False, sanitize=True, save_o
         for label, ind_list in label_dict.items():
             for ind in ind_list:
                 Chem.SetSupplementalSmilesLabel(rdkitmol.GetAtomWithIdx(ind), label)
-    [atom.SetNoImplicit(True) for atom in rdkitmol.GetAtoms()
-     if atom.GetAtomicNum() > 1]
+    for atom in rdkitmol.GetAtoms():
+        if atom.GetAtomicNum() > 1:
+            atom.SetNoImplicit(True)
     if sanitize:
         Chem.SanitizeMol(rdkitmol)
     if remove_h:
