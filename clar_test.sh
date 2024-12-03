6b02e09ca54175e5b961bcfc5746ff8cec236128
Jackson Burns <jwburns@mit.edu>
2024-12-03 13:33:38 -0500

    save demo notebook and test-running script

---
+++ clar_test.sh
@@ -0,0 +1,17 @@
+pytest \
+    test/rmgpy/data/thermoTest.py::TestCyclicThermo::test_add_poly_ring_correction_thermo_data_from_heuristic_using_pyrene \
+    test/rmgpy/data/thermoTest.py::TestMolecularManipulationInvolvedInThermoEstimation::test_bicyclic_decomposition_for_polyring_using_pyrene \
+    test/rmgpy/molecule/moleculeTest.py::TestMolecule::test_count_aromatic_rings \
+    test/rmgpy/molecule/resonanceTest.py::ResonanceTest::test_naphthyl \
+    test/rmgpy/molecule/resonanceTest.py::ResonanceTest::test_methyl_napthalene \
+    test/rmgpy/molecule/resonanceTest.py::ResonanceTest::test_methyl_phenanthrene \
+    test/rmgpy/molecule/resonanceTest.py::ResonanceTest::test_methyl_phenanthrene_radical \
+    test/rmgpy/molecule/resonanceTest.py::ResonanceTest::test_c13h11_rad \
+    test/rmgpy/molecule/resonanceTest.py::ResonanceTest::test_aryne_3_rings \
+    test/rmgpy/molecule/resonanceTest.py::ResonanceTest::test_polycyclic_aromatic_with_non_aromatic_ring2 \
+    test/rmgpy/molecule/resonanceTest.py::ResonanceTest::test_false_negative_polycyclic_aromaticity_perception \
+    test/rmgpy/molecule/resonanceTest.py::ResonanceTest::test_false_negative_polycylic_aromaticity_perception2 \
+    test/rmgpy/molecule/resonanceTest.py::ClarTest::test_clar_optimization \
+    test/rmgpy/molecule/resonanceTest.py::ClarTest::test_phenanthrene \
+    test/rmgpy/molecule/resonanceTest.py::ClarTest::test_phenalene \
+    test/rmgpy/molecule/resonanceTest.py::ClarTest::test_corannulene
