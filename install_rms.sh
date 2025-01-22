5268688e19558c9a1b5c1ebeed626e1c62fabec2
JacksonBurns <jwburns@mit.edu>
2025-01-22 15:52:28 -0500

    try and make sure juliacall uses the correct julia

--- install_rms.sh
+++ install_rms.sh
@@ -4,9 +4,10 @@
 # # restart shell
 # juliaup add 1.9
 # juliaup default 1.9
+# juliaup remove release
 
 # actual steps
-conda install -y conda-forge::pyjuliacall
+conda install -y conda-forge::pyjuliacall  # conda-forge::pyside2
 # export JULIA_CONDAPKG_BACKEND=Current
 # export JULIA_CONDAPKG_EXE=$CONDA_EXE
 # export JULIA_PYTHONCALL_EXE=$CONDA_PREFIX/bin/python
@@ -14,5 +15,9 @@ conda install -y conda-forge::pyjuliacall
 # https://juliapy.github.io/PythonCall.jl/stable/pythoncall/#If-you-already-have-Python-and-required-Python-packages-installed
 export JULIA_CONDAPKG_BACKEND=Null
 export JULIA_PYTHONCALL_EXE=$CONDA_PREFIX/bin/python
+# export MPLBACKEND=qt5agg  # supported backend for PythonPlot.jl, needed by RMS
 
-julia -t 1 -e 'using Pkg; Pkg.add(Pkg.PackageSpec(name="ReactionMechanismSimulator", url="https://github.com/hwpang/ReactionMechanismSimulator.jl.git", rev="fix_installation")); using ReactionMechanismSimulator'
+julia -e 'using Pkg; Pkg.add(Pkg.PackageSpec(name="ReactionMechanismSimulator", url="https://github.com/hwpang/ReactionMechanismSimulator.jl.git", rev="fix_installation")); using ReactionMechanismSimulator; Pkg.instantiate()'
+
+# ensure that juliacall in Python uses the correct julia executable: https://github.com/JuliaPy/PyJuliaPkg?tab=readme-ov-file#which-julia-gets-used
+export PYTHON_JULIAPKG_EXE=$(which julia)
