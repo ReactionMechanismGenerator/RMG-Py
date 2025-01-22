ab2f40893955f304f544d3167b343909d5316157
JacksonBurns <jwburns@mit.edu>
2025-01-22 10:39:02 -0500

    don't allow rms to install anything in the conda environment; we have

--- install_rms.sh
+++ install_rms.sh
@@ -7,7 +7,12 @@
 
 # actual steps
 conda install -y conda-forge::pyjuliacall
-export JULIA_CONDAPKG_BACKEND=Current
-export JULIA_CONDAPKG_EXE=$CONDA_EXE
+# export JULIA_CONDAPKG_BACKEND=Current
+# export JULIA_CONDAPKG_EXE=$CONDA_EXE
+# export JULIA_PYTHONCALL_EXE=$CONDA_PREFIX/bin/python
+
+# https://juliapy.github.io/PythonCall.jl/stable/pythoncall/#If-you-already-have-Python-and-required-Python-packages-installed
+export JULIA_CONDAPKG_BACKEND=Null
 export JULIA_PYTHONCALL_EXE=$CONDA_PREFIX/bin/python
+
 julia -t 1 -e 'using Pkg; Pkg.add(Pkg.PackageSpec(name="ReactionMechanismSimulator", url="https://github.com/hwpang/ReactionMechanismSimulator.jl.git", rev="fix_installation")); using ReactionMechanismSimulator'
