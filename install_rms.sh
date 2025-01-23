cc2c9ba2ae361f1dcec4756b1108011e54343612
JacksonBurns <jwburns@mit.edu>
2025-01-22 22:40:30 -0500

    make juliapackage use our globally installed RMS package

--- install_rms.sh
+++ install_rms.sh
@@ -19,5 +19,6 @@ export JULIA_PYTHONCALL_EXE=$CONDA_PREFIX/bin/python
 
 julia -e 'using Pkg; Pkg.add(Pkg.PackageSpec(name="ReactionMechanismSimulator", url="https://github.com/hwpang/ReactionMechanismSimulator.jl.git", rev="fix_installation")); using ReactionMechanismSimulator; Pkg.instantiate()' | echo "RMS install error - continuing anyway ¯\_(ツ)_/¯"
 
-# ensure that juliacall in Python uses the correct julia executable: https://github.com/JuliaPy/PyJuliaPkg?tab=readme-ov-file#which-julia-gets-used
+# ensure that juliacall in Python uses the correct julia executable and packages: https://github.com/JuliaPy/PyJuliaPkg?tab=readme-ov-file#which-julia-gets-used
 export PYTHON_JULIAPKG_EXE=$(which julia)
+export PYTHON_JULIAPKG_PROJECT=$HOME/.julia/packages
