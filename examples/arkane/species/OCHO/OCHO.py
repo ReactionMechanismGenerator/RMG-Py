70903586e1ffae8bd7e5c23132beeca0dcae1716
Alon Grinberg Dana <alon@technion.ac.il>
2025-02-03 11:09:42 +0200

    Added an example for inputting E0 directly

---
+++ examples/arkane/species/OCHO/OCHO.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+externalSymmetry = 2
+
+spinMultiplicity = 2
+
+opticalIsomers = 1
+
+energy =  (-342.84,'kJ/mol')
+
+geometry = Log('freq.out')
+
+frequencies = Log('freq.out')
