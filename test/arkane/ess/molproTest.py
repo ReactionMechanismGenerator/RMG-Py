5e759e55ca2fb68d9e238af6dc8b413b9fdae57e
Alon Grinberg Dana <alon@technion.ac.il>
2025-03-04 10:06:32 +0200

    Tests: Read Molpro's CCSD(T)-F12a if "a" isn't specified

--- test/arkane/ess/molproTest.py
+++ test/arkane/ess/molproTest.py
@@ -106,8 +106,11 @@ class MolproLogTest:
         """
         log = MolproLog(os.path.join(self.data_path, "OH_f12.out"))
         e0 = log.load_energy()
-
         assert round(abs(e0 / constants.Na / constants.E_h - -75.663696424380), 5) == 0
+
+        log = MolproLog(os.path.join(self.data_path, "C5OH5_CCSD(T)_F12.out"))
+        e0 = log.load_energy()
+        assert round(abs(e0 / constants.Na / constants.E_h - -268.317057640597), 5) == 0
 
     def test_load_hosi_from_molpro_log(self):
         """
