b21e4bbf8d1ae66022c4e03cb27b793995adba3f
JacksonBurns <jwburns@mit.edu>
2025-04-15 08:39:19 -0400

    [DEBUG] set julia t0 1.10

--- install_rms.sh
+++ install_rms.sh
@@ -5,8 +5,8 @@
 # Note that you will have to manually install juliaup before running this script:
 # curl -fsSL https://install.julialang.org | sh
 # # restart shell
-# juliaup add 1.11
-# juliaup default 1.11
+# juliaup add 1.10
+# juliaup default 1.10
 # juliaup remove release
 
 
