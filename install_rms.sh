b80ebe6c493e0b364674e186ad7238338171fb13
Richard West <r.west@northeastern.edu>
2025-06-06 19:45:37 -0400

    Check environment variables.

--- install_rms.sh
+++ install_rms.sh
@@ -53,6 +53,8 @@ export PYTHON_JULIAPKG_EXE="$(which julia)"
 
 conda install -y conda-forge::pyjuliacall
 
+env | grep JULIA
+
 julia -e 'using Pkg; Pkg.add(Pkg.PackageSpec(name="ReactionMechanismSimulator", url="https://github.com/ReactionMechanismGenerator/ReactionMechanismSimulator.jl.git", rev="for_rmg")); using ReactionMechanismSimulator; Pkg.instantiate()' || echo "RMS install error - continuing anyway ¯\_(ツ)_/¯"
 
 python -c "from juliacall import Main; import sys; sys.exit(0 if Main.seval('Base.identify_package(\"ReactionMechanismSimulator\") !== nothing') and print('ReactionMechanismSimulator is installed in $current_env') is None else 1)"
