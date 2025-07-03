16f163ff9acf01829b8722b6c38a400d5b74bf86
Richard West <r.west@northeastern.edu>
2025-07-02 22:59:46 -0400

    Minor refactor of kinetics _make_rule and rate averaging comments.

--- rmgpy/data/kinetics/family.py
+++ rmgpy/data/kinetics/family.py
@@ -4590,15 +4590,16 @@ def _make_rule(rr):
     for i, rxn in enumerate(rxns):
         rxn.rank = ranks[i]
     rxns = np.array(rxns)
-    rs = np.array([r for r in rxns if type(r.kinetics) != KineticsModel]) # KineticsModel is the base class with no data.
+    rs = np.array([r for r in rxns if type(r.kinetics) is not KineticsModel]) # KineticsModel is the base class with no data.
     n = len(rs)
-    if n > 0 and isinstance(rs[0].kinetics, Marcus):
-        kin = average_kinetics([r.kinetics for r in rs])
-        return kin
-    data_mean = np.mean(np.log([r.kinetics.get_rate_coefficient(Tref) for r in rs]))
-
     if n == 0:
         return None
+
+    if isinstance(rs[0].kinetics, Marcus):
+        kin = average_kinetics([r.kinetics for r in rs])
+        return kin
+
+    data_mean = np.mean(np.log([r.kinetics.get_rate_coefficient(Tref) for r in rs]))
 
     if isinstance(rs[0].kinetics, Arrhenius):
         arr = ArrheniusBM
@@ -4765,7 +4766,7 @@ def average_kinetics(kinetics_list):
             beta=(beta,"1/m"),
             wr=(wr * 0.001, "kJ/mol"),
             wp=(wp * 0.001, "kJ/mol"),
-            comment="Averaged from {} reactions.".format(len(kinetics_list)),
+            comment=f"Averaged from {len(kinetics_list)} rate expressions.",
             )
     elif isinstance(kinetics, SurfaceChargeTransfer):
         averaged_kinetics = SurfaceChargeTransfer(
@@ -4775,6 +4776,7 @@ def average_kinetics(kinetics_list):
             alpha=alpha,
             V0=(V0,'V'),
             Ea=(Ea * 0.001, "kJ/mol"),
+            comment=f"Averaged from {len(kinetics_list)} rate expressions.",
             )
     elif isinstance(kinetics, ArrheniusChargeTransfer):
         averaged_kinetics = ArrheniusChargeTransfer(
@@ -4784,6 +4786,7 @@ def average_kinetics(kinetics_list):
             alpha=alpha,
             V0=(V0,'V'),
             Ea=(Ea * 0.001, "kJ/mol"),
+            comment=f"Averaged from {len(kinetics_list)} rate expressions.",
             )
     else:
         averaged_kinetics = Arrhenius(
