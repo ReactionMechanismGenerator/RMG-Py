0bfbb762c88beee7d9695fe95455ef539e9250f2
jonwzheng <jonzheng@mit.edu>
2025-07-24 15:50:55 -0400

    update setup.py to also exclude python 3.12

--- setup.py
+++ setup.py
@@ -146,7 +146,8 @@ setup(
     author='William H. Green and the RMG Team',
     author_email='rmg_dev@mit.edu',
     url='http://reactionmechanismgenerator.github.io',
-    python_requires='>=3.9,<3.13',
+    python_requires='>=3.9,<3.12',
+
     packages=find_packages(where='.', include=["rmgpy*"]) + find_packages(where='.', include=["arkane*"]),
     scripts=scripts,
     entry_points={
