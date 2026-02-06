7d528f1fd682046d30930d4185742b151f5be2a3
Richard West <r.west@northeastern.edu>
2026-02-06 16:39:25 -0500

    Tweak test_yaml

--- test/rmgpy/yaml_writer/test_yaml.py
+++ test/rmgpy/yaml_writer/test_yaml.py
@@ -2,11 +2,12 @@ from compare_yaml_outputs import *
 import os
 import pytest
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def compare_manager():
     '''Create instance of a Compare Yaml before each test'''
-    yaml_path_1 = os.path.join('test/rmgpy/test_data/yaml_writer_data/chemkin/', 'chem0047-gas.yaml')
-    yaml_path_2 = os.path.join('test/rmgpy/test_data/yaml_writer_data/cantera/', 'chem47.yaml')
+    test_data_folder='test/rmgpy/test_data/yaml_writer_data/chemkin/'
+    yaml_path_1 = os.path.join(test_data_folder, 'chem0047-gas.yaml')
+    yaml_path_2 = os.path.join(test_data_folder, 'chem47.yaml')
     return CompareYaml(yaml_path_1, yaml_path_2)
 
 def test_compare_number_of_species(compare_manager):
