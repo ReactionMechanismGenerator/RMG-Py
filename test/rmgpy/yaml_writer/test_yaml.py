7f71a8be1931fb0aafe1948d4193c3461b474112
Richard West <r.west@northeastern.edu>
2026-02-06 17:01:55 -0500

    Update test_yaml.py to use new YAML test files for comparison
    - Modified the paths for the YAML files used in the CompareYaml fixture.
    - Updated to use 'chem37.yaml' files generated in recent functional tests instead of the previous 'chem0047-gas.yaml' and 'chem47.yaml'.
    
    These don't yet work

--- test/rmgpy/yaml_writer/test_yaml.py
+++ test/rmgpy/yaml_writer/test_yaml.py
@@ -5,9 +5,14 @@ import pytest
 @pytest.fixture(scope="module")
 def compare_manager():
     '''Create instance of a Compare Yaml before each test'''
-    test_data_folder='test/rmgpy/test_data/yaml_writer_data/chemkin/'
-    yaml_path_1 = os.path.join(test_data_folder, 'chem0047-gas.yaml')
-    yaml_path_2 = os.path.join(test_data_folder, 'chem47.yaml')
+    test_data_folder='test/rmgpy/test_data/yaml_writer_data/'
+    # saved by Prosper in earlier commit
+    yaml_path_1 = os.path.join(test_data_folder, 'chemkin/chem0047-gas.yaml')
+    yaml_path_2 = os.path.join(test_data_folder, 'cantera/chem47.yaml')
+
+    # generated on the fly in recent functional test
+    yaml_path_1 = os.path.join(test_data_folder, 'chemkin/chem37.yaml')
+    yaml_path_2 = os.path.join(test_data_folder, 'cantera/chem37.yaml')
     return CompareYaml(yaml_path_1, yaml_path_2)
 
 def test_compare_number_of_species(compare_manager):
