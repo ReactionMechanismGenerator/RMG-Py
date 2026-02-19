a1307b5b2973cb8ba21df163430d2dc17dc9a969
Alon Grinberg Dana <alon@technion.ac.il>
2026-02-19 09:48:15 +0200

    Fix attempt for 'E' in atom dictionary

--- rmgpy/yaml_cantera.py
+++ rmgpy/yaml_cantera.py
@@ -48,6 +48,20 @@ from rmgpy.util import make_output_subdirectory
 from datetime import datetime
 from rmgpy.chemkin import get_species_identifier
 
+SYMBOL_BY_NUMBER = {0: 'e', 1: 'H', 2: 'He', 3: 'Li', 4: 'Be', 5: 'B', 6: 'C', 7: 'N', 8: 'O', 9: 'F', 10: 'Ne',
+                    11: 'Na', 12: 'Mg', 13: 'Al', 14: 'Si', 15: 'P', 16: 'S', 17: 'Cl', 18: 'Ar', 19: 'K', 20: 'Ca',
+                    21: 'Sc', 22: 'Ti', 23: 'V', 24: 'Cr', 25: 'Mn', 26: 'Fe', 27: 'Co', 28: 'Ni', 29: 'Cu', 30: 'Zn',
+                    31: 'Ga', 32: 'Ge', 33: 'As', 34: 'Se', 35: 'Br', 36: 'Kr', 37: 'Rb', 38: 'Sr', 39: 'Y', 40: 'Zr',
+                    41: 'Nb', 42: 'Mo', 43: 'Tc', 44: 'Ru', 45: 'Rh', 46: 'Pd', 47: 'Ag', 48: 'Cd', 49: 'In', 50: 'Sn',
+                    51: 'Sb', 52: 'Te', 53: 'I', 54: 'Xe', 55: 'Cs', 56: 'Ba', 57: 'La', 58: 'Ce', 59: 'Pr', 60: 'Nd',
+                    61: 'Pm', 62: 'Sm', 63: 'Eu', 64: 'Gd', 65: 'Tb', 66: 'Dy', 67: 'Ho', 68: 'Er', 69: 'Tm', 70: 'Yb',
+                    71: 'Lu', 72: 'Hf', 73: 'Ta', 74: 'W', 75: 'Re', 76: 'Os', 77: 'Ir', 78: 'Pt', 79: 'Au', 80: 'Hg',
+                    81: 'Tl', 82: 'Pb', 83: 'Bi', 84: 'Po', 85: 'At', 86: 'Rn', 87: 'Fr', 88: 'Ra', 89: 'Ac', 90: 'Th',
+                    91: 'Pa', 92: 'U', 93: 'Np', 94: 'Pu', 95: 'Am', 96: 'Cm', 97: 'Bk', 98: 'Cf', 99: 'Es', 100: 'Fm',
+                    101: 'Md', 102: 'No', 103: 'Lr', 104: 'Rf', 105: 'Db', 106: 'Sg', 107: 'Bh', 108: 'Hs', 109: 'Mt',
+                    110: 'Ds', 111: 'Rg', 112: 'Cn', 113: 'Nh', 114: 'Fl', 115: 'Mc', 116: 'Lv', 117: 'Ts', 118: 'Og'}
+NUMBER_BY_SYMBOL = {value: key for key, value in SYMBOL_BY_NUMBER.items()}
+
 
 def _convert_anymap_to_dict(obj):
     """
@@ -430,6 +444,22 @@ def species_to_dict(species):
 
     cantera_species = species.to_cantera(use_chemkin_identifier=True)
     species_data = cantera_species.input_data
+
+    mol = species.molecule[0]
+    atom_dict = dict(mol.get_element_count())
+
+    Z_mol = sum(NUMBER_BY_SYMBOL.get(atom, 0) * count for atom, count in atom_dict.items())
+    charge = mol.get_net_charge()
+
+    # Only add 'E' if the species is not neutral (or is a pure electron)
+    # E = protons - charge
+    new_E = Z_mol - charge
+    if new_E != 0:
+        atom_dict['E'] = new_E
+    elif 'E' in atom_dict:
+        del atom_dict['E']
+
+    species_data['composition'] = atom_dict
 
     try:
         transport_comment = species.transport_data.comment
